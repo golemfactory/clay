8fd074c6fcab04bd6c413a46d6d6f493ccf607c0
devel <devel@BuildSlaveWin7.ibs.org.pl>
2014-03-17 18:23:24 +0100

    -chuj

--- poc/golemPy/src/golem.py
+++ poc/golemPy/src/golem.py
@@ -1,38 +1,92 @@
 from twisted.web import server, resource
 from twisted.internet import reactor
-from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol, TCP4ServerEndpoint
 from twisted.internet.protocol import Factory, Protocol
 from twisted.protocols.amp import AMP
+import json
+
+PingMessage = "\0x02"
+PongMessage = "\0x03"
+
+class Message:
+    def __init__(self, type):
+        self.type = type
+
+    def serialize(self):
+        mess = self.serializeTyped()
+
+        l = len(mess) 
+        s = bytearray()
+        s.append((l >> 24) & 0xff)
+        s.append((l >> 16) & 0xff)
+        s.append((l >> 8) & 0xff)
+        s.append(l & 0xff)
+
+        return s + mess;
+        
+    def serializeTyped(self):
+        pass
+
+class HelloMessage(Message):
+    Type = 0
+    def __init__(self):
+        super(HelloMessage, self).__init__(HelloMessage.Type)
+
+    def serializeTyped(self):
+        return json.dumps([HelloMessage.Type, "Hello World !!!"])
+
+class PingMessage(Message):
+    Type = 1
+    def __init__(self):
+        super(PingMessage, self).__init__(PingMessage.Type)
+
+    def serializeTyped(self):
+        return json.dumps([PingMessage.Type, "Ping"])
+
+class PongMessage(Message):
+    Type = 2
+    def __init__(self):
+        super(PongMessage, self).__init__(PongMessage.Type)
+
+    def serializeTyped(self):
+        return json.dumps([PongMessage.Type ,"Pong"])
+
+
 
 class GolemProtocol(Protocol):
-    def seal(self, messSize):
-        s = []
-        s.append(0x22)
-        s.append(0x40)
-        s.append(0x08)
-        s.append(0x91)
-        len = messSize;
-        s.append((len >> 24) & 0xff)
-        s.append((len >> 16) & 0xff)
-        s.append((len >> 8) & 0xff)
-        s.append(len & 0xff)
-        return s
-
     def sendMessage(self, msg):
         buf = self.seal(len(msg))
-        self.transport.write(buf + msg)
+        out = buf + msg
+        print self.transport.__repr__()
+        print self.transport.__str__()
+        self.transport.write(str(out))
 
-def gotProtocol(p):
-    print "gotProtocol"
-    p.sendMessage("test Message")
+    def connectionMade(self):
+        print self.transport.__str__()
+        print "Connection made"
+
+    def dataReceived(self, data):
+        print data
+
 
 class Client:
-    seeds = []
+
+    def __init__(self, port):
+        self.listenPort = port
+
+    def start(self):
+        print "Start listening ..."
+        endpoint = TCP4ServerEndpoint(reactor, self.listenPort)
+        endpoint.listen(Factory.forProtocol(GolemProtocol))
+
+    def gotProtocol(self, p):
+        p.sendMessage("Nie lubie ruskich")
 
     def connect(self, address, port):
+        print "Connecting to host {} : {}".format(address ,port)
         endpoint = TCP4ClientEndpoint(reactor, address, port)
         endpoint.connect(Factory.forProtocol(AMP))
         d = connectProtocol(endpoint, GolemProtocol())
-        d.addCallback(gotProtocol)
+        d.addCallback(self.gotProtocol)
        
 
--- poc/golemPy/test/test_golem.py
+++ poc/golemPy/test/test_golem.py
@@ -1,13 +1,23 @@
+import sys
+sys.path.append('../src/')
+
 from golem import *
+import sys
 
 
-c = Client()
+args = sys.argv
 
-c.connect("127.0.0.1", 30302)
+c = Client(int(args[1]))
+
+c.start()
+
+if len(args) > 2:
+    c.connect("127.0.0.1", int(args[2]))
 
 #hr1 = HelloResource()
 #hr2 = HelloResource()
 #reactor.callInThread(reactor.listenTCP, 8080, server.Site(hr1))
 #reactor.callInThread(reactor.listenTCP, 8081, server.Site(hr2))
 #reactor.listenTCP(8080, server.Site(hr1))
+
 reactor.run()
