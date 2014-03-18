b3e9ca34695e50c8da8e741282b59a77652b4f3d
viggith <devnull@localhost>
2014-03-18 18:10:00 +0100

    serialization, buffering and messages machinery

--- poc/golemPy/src/message.py
+++ poc/golemPy/src/message.py
@@ -1,3 +1,4 @@
+import abc
 import databuffer
 import json
 import time
@@ -11,81 +12,81 @@ class Message:
         self.type = type
         self.serializer = databuffer.DataBuffer()
 
+    def getType( self ):
+        return self.type
+
+    def serialize( self ):
+        strJSONRepr = json.dumps( [ self.type, self.dictRepr() ] )
+        self.seralizer.appendLenPrefixedString( strJSONRepr )
+
+        return self.serializer.readAll()
+
+    @classmethod
+    def deserialize( cls, db ):
+        assert isinstance( db, databuffer.DataBuffer )
+        messages = []
+        msg = db.readLenPrefixedString()
+
+        while msg:
+            messages.append( msg )
+            msg = db.readLenPrefixedString()
+
+        return messages
+  
+    @classmethod
+    def deserializeMessage( cls, msg ):
+        msgRepr  = json.loads( msg )
+
+        msgType  = msgRepr[ 0 ]
+        dictRepr = msgRepr[ 1 ]
+
+        if messType == HelloMessage.Type:
+            return HelloMessage( dictRepr )
+        elif messType == PingMessage.Type:
+            return PingMessage( dictRepr )
+        elif messType == PongMessage.Type:
+            return PongMessage( dictRepr )
+
+        return None
+     
+    @abc.abstractmethod
+    def dictRepr(self):
+        """
+        Returns dictionary/list representation of 
+        any subclassed message
+        """
+        return
+
     def __str__( self ):
         return "{}".format( self.__class__ )
 
     def __repr__( self ):
         return "{}".format( self.__class__ )
 
-    def getType( self ):
-        return self.type
-
-    def serialize( self ):
-        strRepr =  json.dumps( self.dictRepr() )
-
-        self.serializer.appendUInt( len( strRepr ) )
-        self.serializer.appendString( strRepr )
-
-        return self.serializer.readAll()
-
-    @classmethod
-    def deserialize( cls, message ):
-        curIdx = 0
-        
-        messages = []
-        
-        while curIdx < len(message):
-            msg, l = cls.deserializeSingle( message[curIdx:] )
-            
-            if msg is None:
-                print "Failed to deserialize multiple at: {} {}".format( curIdx, message[curIdx:] )
-
-            curIdx += l
-            messages.append( msg )
-            
-        return messages
-  
-    @classmethod
-    def deserializeSingle(cls, message):
-
-        if(len(message) < 4):
-            print "Message shorter than 4 bytes"
-            return None, 0
-
-        (l,) = struct.unpack( "!L", message[0:4])
-
-        m = message[4:l + 4]
-
-        if l > len(m):
-            print "Wrong message length: {} > {}".format(l, len(m))
-            return None, 0
-
-        dMessage = json.loads(str(m))
-        messType = dMessage[0]
-
-        if messType == HelloMessage.Type:
-            return HelloMessage(), l + 4
-        elif messType == PingMessage.Type:
-            return PingMessage(), l + 4
-        elif messType == PongMessage.Type:
-            return PongMessage(), l + 4
-        
-        return None, 0
-     
-    def serializeTyped(self):
-        pass
-
 
 class HelloMessage(Message):
-    Type = 0
-    def __init__(self):
-        Message.__init__(self, HelloMessage.Type)
 
+    Type = 0
+
+    def __init__( self, protoId, cliVer ):
+        Message.__init__( self, HelloMessage.Type )
+        self.protoId    = protoId
+        self.clientVer  = cliVer
+
+    def __init__( self, dictRepr ):
+        self.protoId    = dictRepr[ "protoId" ]
+        self.clientVer  = dictRepr[ "clientVer" ]
+
+    def dictRepr(self):
+        
+        return super(HelloMessage, self).dictRepr()( sel
     def serializeTyped(self):
         return json.dumps([HelloMessage.Type, "Hello World !!!"])
 
 class PingMessage(Message):
+
     Type = 1
+
     def __init__(self):
         Message.__init__(self, PingMessage.Type)
 
@@ -93,7 +94,9 @@ class PingMessage(Message):
         return json.dumps([PingMessage.Type, "Ping"])
 
 class PongMessage(Message):
+
     Type = 2
+
     def __init__(self):
         Message.__init__(self, PongMessage.Type)
 
