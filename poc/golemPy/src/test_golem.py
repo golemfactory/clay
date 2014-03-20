4f532b1b4acf3efa47d319b575174a6daa17c0f4
devel <devel@BuildSlaveWin7.ibs.org.pl>
2014-03-20 13:06:53 +0100

    -przeniesienie testu do src.

---
+++ poc/golemPy/src/test_golem.py
@@ -0,0 +1,25 @@
+import sys
+#sys.path.append('../src/')
+
+from client import Client
+from twisted.internet import reactor
+
+args = sys.argv
+
+c = Client(int(args[1]))
+
+c.startNetwork()
+
+if len(args) > 2:
+    #c.connect("10.30.10.69", int(args[2]))
+    c.connect("127.0.0.1", int(args[2]))
+
+#hr1 = HelloResource()
+#hr2 = HelloResource()
+#reactor.callInThread(reactor.listenTCP, 8080, server.Site(hr1))
+#reactor.callInThread(reactor.listenTCP, 8081, server.Site(hr2))
+#reactor.listenTCP(8080, server.Site(hr1))
+
+
+
+reactor.run()
