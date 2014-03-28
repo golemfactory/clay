9499c6632df682b7d7ff78b603c1676af8c3eb11
viggith <devnull@localhost>
2014-03-28 02:23:57 +0100

    basic node state snapshot

--- poc/golemPy/src/ui/uicustomizer.py
+++ poc/golemPy/src/ui/uicustomizer.py
@@ -7,6 +7,14 @@ except AttributeError:
     def _fromUtf8(s):
         return s
 
+class TableDataEntry:
+
+    def __init__( self, uidItem, timestampItem, remoteProgressBar, localProgressBar ):
+        self.uid = uidItem
+        self.timestamp = timestampItem
+        self.remoteProgress = remoteProgressBar
+        self.localProgress = localProgressBar
+
 class ManagerUiCustomizer:
 
     def __init__( self, widget ):
@@ -14,7 +22,6 @@ class ManagerUiCustomizer:
         self.table = widget.nodeTableWidget
         
         self.tableData = {}
-
 
     def __createWrappedProgressBar( self ):
 
@@ -25,23 +32,29 @@ class ManagerUiCustomizer:
         progressBar.setGeometry(7, 2, 159, 16)
         progressBar.setProperty("value", 24)
 
-        return widget
+        return widget, progressBar
 
     def addProgressBar( self, row, col ):
-        self.table.setCellWidget( row, col, self.__createWrappedProgressBar() )
+        w, p = self.__createWrappedProgressBar()
+        self.table.setCellWidget( row, col, w )
+        return p
 
     def appendRow( self, nodeUid, nodeTime ):
         nextRow = self.table.rowCount()
         
         self.table.insertRow( nextRow )
 
-        item = QtGui.QTableWidgetItem()
-        item.setText( nodeUid )
-        self.table.setItem( nextRow, 0, item )
+        item0 = QtGui.QTableWidgetItem()
+        item0.setText( nodeUid )
+        self.table.setItem( nextRow, 0, item0 )
 
-        item = QtGui.QTableWidgetItem()
-        item.setText( nodeTime )
-        self.table.setItem( nextRow, 1, item )
+        item1 = QtGui.QTableWidgetItem()
+        item1.setText( nodeTime )
+        self.table.setItem( nextRow, 1, item1 )
 
-        self.addProgressBar( nextRow, 2 )
-        self.addProgressBar( nextRow, 3 )
+        pRem = self.addProgressBar( nextRow, 2 )
+        pLoc = self.addProgressBar( nextRow, 3 )
+
+        assert nodeUid not in self.tableData
+
+        self.tableData[ nodeUid ] = TableDataEntry( item0, item1, pRem, pLoc )
