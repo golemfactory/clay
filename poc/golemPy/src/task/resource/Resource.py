00a0ed780c529ff29f3fc624cb498b85ccc39205
viggith <devnull@localhost>
2014-04-04 15:54:57 +0200

    progressing with da risors

--- poc/golemPy/src/task/resource/Resource.py
+++ poc/golemPy/src/task/resource/Resource.py
@@ -71,6 +71,103 @@ class TaskResourceHeader:
     def __str__( self ):
         return self.toString()
 
+class TaskResource:
+
+    ####################
+    @classmethod
+    def readFile( cls, fileName ):
+        try:
+            f = open( filename, "rb" )
+            data = f.read()
+        except Exception as ex:
+            print "ex"
+            return None
+
+        return data
+
+    ####################
+    @classmethod
+    def validateHeader( cls, header ):
+        assert isinstance(header, TaskResourceHeader)
+
+        for f in header.filesData:
+            fname = os.path.join( header.absolutePath, f[ 0 ] ) 
+
+            if not os.path.exists( fname ):
+                return False, "File {} does not exist".format( fname )
+
+            if os.path.isfile( fname ):
+                return False, "Entry {} is not a file".format( fname )
+
+        for dh in header.subDirHeaders:
+            validated, msg = cls.validateHeader( dh )
+
+            if not validated:
+                return validated, msg
+
+        return True, None
+
+    ####################
+    @classmethod
+    def buildFromHeader( cls, header ):
+        assert isinstance(header, TaskResourceHeader)
+
+        curTr = TaskResource( header.relativePath, header.absolutePath )
+
+        filesData = []
+        for f in header.filesData:
+            fname = os.path.join( header.absolutePath, f[ 0 ] ) 
+            fdata = cls.readFile( fname )
+            
+            if fdata is None:
+                return None
+
+            filesData.append( ( f[ 0 ], f[ 1 ], fdata ) )
+
+        curTr.filesData = filesData
+
+        subDirResources = []
+        for sdh in header.subDirHeaders:
+            subDirRes = cls.buildFromHeader( sdh )
+
+            if subDirRes is None:
+                return None
+            
+            subDirResources.append( subDirRes )
+
+        curTr.subDirResources = subDirResources
+
+        return curTr        
+
+    ####################
+    def __init__( self, relativePath, absolutePath ):
+        self.filesData          = []
+        self.subDirResources    = []
+        self.relativePath       = relativePath
+        self.absolutePath       = absolutePath
+
+    ####################
+    def toString( self ):
+        out = "\nROOT '{}' \n".format( self.absolutePath )
+
+        if len( self.subDirHeaders ) > 0:
+            out += "DIRS \n"
+            for d in self.subDirHeaders:
+                out += "    {}\n".format( d.relativePath )
+
+        if len( self.filesData ) > 0:
+            out += "FILES \n"
+            for f in self.filesData:
+                out += "    {} {}".format( f[ 0 ], f[ 1 ] )
+
+        for d in self.subDirHeaders:
+            out += d.toString()
+
+        return out
+
+    ####################
+    def __str__( self ):
+        return self.toString()
 
 if __name__ == "__main__":
 
@@ -91,3 +188,6 @@ if __name__ == "__main__":
     print th
     #walk_test( "." )
     #main()
+
+    tc = TaskResource()
+    tc.a()
