e9a119c5de5a8b1612e86fdf186277a3e7154a38
Aleksandra Skrzypczak <devnull@localhost>
2014-08-14 13:03:37 +0200

    zmiana parametru sampler i podparametrów w pliku pbrt dla example

--- poc/golemPy/examples/gnr/task/SceneFileEditor.py
+++ poc/golemPy/examples/gnr/task/SceneFileEditor.py
@@ -3,10 +3,19 @@ import re
 def regenerateFile( sceneFileSrc, xres, yres, pixelFilter, sampler, samplesPerPixel ):
     out = ""
 
+    pixelSamplesSamplers = ['bestcandidate', 'lowdiscrepancy', 'halton', 'random']
+    minMaxSamplesSamplers = ['adaptive']
+    jitterSamplers = ['stratified']
+
     for l in sceneFileSrc.splitlines():
         line = re.sub( r'("integer\s+xresolution"\s*)(\[\s*\d*\s*\])', r'\1[{}]'.format( xres ), l )
         line = re.sub( r'("integer\s+yresolution"\s*)(\[\s*\d*\s*\])', r'\1[{}]'.format( yres ), line )
-        line = re.sub( r'(Sampler\s+)("\w*")(\s+"integer\s+pixelsamples"\s+)(\[\s*\d*\s*\])', r'\1"{}"\3[{}]'.format( sampler, samplesPerPixel ), line )
+        if sampler in pixelSamplesSamplers:
+            line = re.sub( r'(Sampler\s+)([\s*\d*\w*\"*\[*\]*]*)', r'\1"{}" "integer pixelsamples" [{}]'.format( sampler, samplesPerPixel ), line )
+        if sampler in minMaxSamplesSamplers:
+            line = re.sub( r'(Sampler\s+)([\s*\d*\w*\"*\[*\]*]*)', r'\1"{}" "integer minsamples" [{}] "integer maxsamples" [{}]'.format( sampler, samplesPerPixel, samplesPerPixel ), line )
+        if sampler in jitterSamplers:
+            line = re.sub( r'(Sampler\s+)([\s*\d*\w*\"*\[*\]*]*)', r'\1"{}" "integer xsamples" [{}] "integer ysamples" [{}]'.format( sampler, samplesPerPixel, samplesPerPixel ), line )
         line = re.sub( r'(PixelFilter\s+)("\w*")', r'\1"{}"'.format( pixelFilter ), line )
         out += line + "\n"
 
