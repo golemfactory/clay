cafc32a662073eaf230182dd6e23dcbabedf3900
Aleksandra Skrzypczak <devnull@localhost>
2014-09-01 17:09:24 +0200

    Potwierdzenie zamknięcia

--- poc/golemPy/examples/gnr/ui/MainWindow.py
+++ poc/golemPy/examples/gnr/ui/MainWindow.py
@@ -1,11 +1,23 @@
-from PyQt4.QtGui import QMainWindow, QPixmap
+from PyQt4.QtGui import QMainWindow, QPixmap, QMessageBox
 
 from gen.ui_MainWindow import Ui_MainWindow
+
+class MainWindow( QMainWindow ):
+
+    def closeEvent( self, event ):
+        print("event")
+        reply = QMessageBox.question(self, 'Golem Message',
+            "Are you sure you want to quit?", QMessageBox.Yes, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 class GNRMainWindow:
     ##########################
     def __init__( self ):
-        self.window     = QMainWindow()
+        self.window     = MainWindow()
         self.ui         = Ui_MainWindow()
 
         self.ui.setupUi( self.window )
