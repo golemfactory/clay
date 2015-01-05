0c3607167aa5891474b20e63291d8966eb948b15
Aleksandra Skrzypczak <devnull@localhost>
2015-01-05 15:31:13 +0100

    Brakująca klasa clickablelabel

---
+++ poc/golemPy/examples/gnr/clickableqlabel.py
@@ -0,0 +1,13 @@
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+class   ClickableQLabel(QLabel):
+
+    def __init(self, parent):
+        QLabel.__init__(self, parent)
+
+    def mouseReleaseEvent(self, ev):
+        self.emit(SIGNAL('mouseReleaseEvent( int, int, QMouseEvent)'), ev.pos().x(), ev.pos().y(), ev )
+
+    def mouseMoveEvent(self, ev):
+        self.emit(SIGNAL('mouseMoveEvent( int, int, QMouseEvent)'), ev.pos().x(), ev.pos().y(), ev )
