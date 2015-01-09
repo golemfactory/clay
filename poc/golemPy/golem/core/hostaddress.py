0c76a6df748c27dbfb104fd2a951a777c1c11be1
Aleksandra Skrzypczak <devnull@localhost>
2015-01-09 13:17:59 +0100

    fixme dla hostaddress

--- poc/golemPy/golem/core/hostaddress.py
+++ poc/golemPy/golem/core/hostaddress.py
@@ -10,18 +10,10 @@ def ip4_addresses():
 def getHostAddress( seedAddr = None):
     ips = ip4_addresses()
     try:
+        #FIXME
         if seedAddr is not None:
             lenPref = [ len( os.path.commonprefix( [addr, seedAddr] ) ) for addr in ips ]
             return ips[ lenPref.index( max( lenPref ) ) ]
     except Exception, err:
         logger.error( "getHostAddress error {}".format( str( err ) ) )
         return socket.gethostbyname( socket.gethostname() )
-        #for ip in ips:
-        #    if ip.startswith('10.30.'):
-        #        return ip
-
-        #for ip in ips:
-        #    if not ip.startswith("127."):
-        #         return ip
-        #return ips[0]
-    #return socket.gethostbyname( socket.gethostname() )
