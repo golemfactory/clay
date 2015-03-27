1a452ac52f5c89e2a627e8ef074a0f2030273a89
Aleksandra Skrzypczak <devnull@localhost>
2015-03-27 18:12:53 +0100

    Usuniecie zbednego adresu w testach komunikacji z Eth

--- poc/golemPy/golem/transactions/test/testEthereumConnector.py
+++ poc/golemPy/golem/transactions/test/testEthereumConnector.py
@@ -9,7 +9,7 @@ from golem.transactions.EthereumConnector import EthereumConnector, EthJSON
 
 sys.path.append( os.environ.get( 'GOLEM' ) )
 
-address = "http://10.30.10.75:8080"
+address = "http://localhost:8080"
 
 class TestEthereumConnector( unittest.TestCase ):
     def testSha3(self):
@@ -20,6 +20,24 @@ class TestEthereumConnector( unittest.TestCase ):
         data = dataDesc.getData()
         ec = EthereumConnector(address)
         self.assertEqual( ec.sendJsonRpc(data), {"id":64, "jsonrpc":"2.0", "result":"0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad" })
+
+    def testBlock(self):
+        dataDesc = EthJSON()
+        dataDesc.setMethod("eth_blockNumber")
+        dataDesc.setId(83)
+        data = dataDesc.getData()
+        ec = EthereumConnector(address)
+        self.assertGreater( int(ec.sendJsonRpc(data)["result"], 16), 30000)
+
+    def testGetLogs(self):
+        dataDesc = EthJSON()
+        dataDesc.setMethod("eth_getLogs")
+        dataDesc.setId(74)
+       # dataDesc.addParam({"topics": ["0x12341234"]})b6d97503bff4edd93591c57fc91925b41a19bd9c
+        data = dataDesc.getData()
+        ec = EthereumConnector(address)
+        print ec.sendJsonRpc(data)
+
 
 if __name__ == '__main__':
     unittest.main()
