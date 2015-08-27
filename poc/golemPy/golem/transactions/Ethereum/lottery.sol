6b61a3023c6565a03ffc997e831452f7303718ed
Aleksandra Skrzypczak <devnull@localhost>
2015-08-27 16:14:13 +0200

    loteria

---
+++ poc/golemPy/golem/transactions/Ethereum/lottery.sol
@@ -0,0 +1,71 @@
+contract LotteryAgent {
+    
+    struct LotteryData {
+        uint value;
+        uint timeout;
+        uint deposit;
+        address winner;
+    }
+    
+    mapping (bytes32 => LotteryData) lotteries;
+	
+	function initLottery(bytes32 descriptionHash, uint maturity, uint deposit) {
+		LotteryData lottery = lotteries[descriptionHash];
+		if (lottery.value != 0) 
+		    return;
+		lotteries[descriptionHash] = LotteryData(msg.value, maturity, deposit, 0);
+	}
+	
+	function winnerLottery(bytes32 descriptionHash) {
+	    LotteryData lottery = lotteries[descriptionHash];
+	    if (lottery.value == 0 || lottery.winner != 0 || block.number <= lottery.timeout || msg.value < lottery.deposit)
+	        return;
+	    lotteries[descriptionHash].deposit = msg.value;
+	    lotteries[descriptionHash].winner = msg.sender;
+	    lotteries[descriptionHash].timeout = block.number + 7200;
+	}
+	
+	function payoutLottery(bytes32 descriptionHash) {
+	    LotteryData lottery = lotteries[descriptionHash];
+	    if (lottery.value == 0 || lottery.winner == 0 || block.number <= lottery.timeout)
+	        return;
+	    lottery.winner.send(lottery.value);
+	    delete lotteries[descriptionHash];
+	}
+
+	function checkLottery(uint maturity, uint lotteryId, address[] participants, uint[] probabilities) external {
+	    bytes32 descriptionHash = sha3(maturity, lotteryId, participants, probabilities);
+	    LotteryData lottery = lotteries[descriptionHash];
+	    if (lottery.value == 0 || (lottery.winner == 0 && block.number <= lottery.timeout))
+	        return;
+	    
+		uint r = random(maturity);
+		address winner;
+		uint target = 0;
+		for (uint i = 0; i < probabilities.length; ++i) {
+			target += probabilities[i];
+			if (r <= target) {
+				winner = participants[i];
+				break;
+			}
+		}
+		
+		uint value = lotteries[descriptionHash].value;
+		if (lotteries[descriptionHash].winner == 0) {
+		    winner.send(value);
+		} else {
+		    if (winner != msg.sender) {
+		        winner.send(value);
+		        msg.sender.send(lotteries[descriptionHash].deposit);
+		    } else {
+		        winner.send(value + lotteries[descriptionHash].deposit);
+		    }
+		}
+		delete lotteries[descriptionHash];
+		
+	}
+	
+	function random(uint maturity) internal constant returns(uint) {
+		return uint(block.blockhash(maturity));
+	}
+}
