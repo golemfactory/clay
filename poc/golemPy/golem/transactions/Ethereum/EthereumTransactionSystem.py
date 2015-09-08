34e40967315ff71ba450e1a9b5b11dc318b91fef
Aleksandra Skrzypczak <devnull@localhost>
2015-09-08 15:15:08 +0200

    zmiana nazwy -> transaction system

--- poc/golemPy/golem/transactions/Ethereum/EthereumTransactionSystem.py
+++ poc/golemPy/golem/transactions/Ethereum/EthereumTransactionSystem.py
@@ -1,4 +1,4 @@
-from golem.transactions.TransactionSystem import TransactionSystem
+from golem.transactions.transaction_system import TransactionSystem
 from EthereumConnector import EthereumConnector
 
 from golem.core.variables import ETH_CONN_ADDR
