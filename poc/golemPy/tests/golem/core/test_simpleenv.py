271daa7fcc9c8c24bbdfa5df04b7b3a35627e655
badb <aleksandra.skrzypczak@imapp.pl>
2015-11-23 19:22:58 +0100

    simpleenv test quickfix

--- poc/golemPy/tests/golem/core/test_simpleenv.py
+++ poc/golemPy/tests/golem/core/test_simpleenv.py
@@ -1,25 +1,28 @@
 import unittest
 import os
 import shutil
-from golem.core.simpleenv import SimpleEnv, DATA_DIRECTORY
+from golem.core.simpleenv import SimpleEnv
+
+SimpleEnv.DATA_DIRECTORY = os.path.abspath("tmpdir")
 
 
 class TestSimpleEnv(unittest.TestCase):
     def testEnvFileName(self):
-        shutil.rmtree(DATA_DIRECTORY)
-        self.assertFalse(os.path.isdir(DATA_DIRECTORY))
+        if os.path.isdir(SimpleEnv.DATA_DIRECTORY):
+            shutil.rmtree(SimpleEnv.DATA_DIRECTORY)
+        self.assertFalse(os.path.isdir(SimpleEnv.DATA_DIRECTORY))
         fname = SimpleEnv.env_file_name('testFile.txt')
-        self.assertTrue(os.path.isdir(DATA_DIRECTORY))
-        self.assertTrue(DATA_DIRECTORY in fname)
-        fname2 = SimpleEnv.env_file_name(os.path.join(DATA_DIRECTORY, 'testFile.txt'))
-        self.assertTrue(os.path.isdir(DATA_DIRECTORY))
-        self.assertTrue(DATA_DIRECTORY in fname2)
-        shutil.rmtree(DATA_DIRECTORY)
+        self.assertTrue(os.path.isdir(SimpleEnv.DATA_DIRECTORY))
+        self.assertTrue(SimpleEnv.DATA_DIRECTORY in fname)
+        fname2 = SimpleEnv.env_file_name(os.path.join(SimpleEnv.DATA_DIRECTORY, 'testFile.txt'))
+        self.assertTrue(os.path.isdir(SimpleEnv.DATA_DIRECTORY))
+        self.assertTrue(SimpleEnv.DATA_DIRECTORY in fname2)
+        shutil.rmtree(SimpleEnv.DATA_DIRECTORY)
 
     def testOpenEnvFile(self):
         f = SimpleEnv.open_env_file('testFile.txt')
-        self.assertTrue(os.path.isdir(DATA_DIRECTORY))
+        self.assertTrue(os.path.isdir(SimpleEnv.DATA_DIRECTORY))
         self.assertFalse(f.closed)
-        self.assertTrue(os.path.isfile(os.path.join(DATA_DIRECTORY, 'testFile.txt')))
+        self.assertTrue(os.path.isfile(os.path.join(SimpleEnv.DATA_DIRECTORY, 'testFile.txt')))
         f.close()
         self.assertTrue(f.closed)
