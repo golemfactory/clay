f6c4ccaf1bdeea45eb7e25f0bfbbc21ab5ab9ac7
Paweł Bylica <chfast@gmail.com>
2015-11-23 18:56:28 +0100

    Skip some network tests.

--- poc/golemPy/tests/golem/core/test_hostaddress.py
+++ poc/golemPy/tests/golem/core/test_hostaddress.py
@@ -4,6 +4,9 @@ from golem.core.hostaddress import get_host_address
 
 class TestHostAddress(unittest.TestCase):
     def testGetHostAddress(self):
+        self.assertGreater(len(get_host_address('127.0.0.1')), 0)
+
+    @unittest.skip("Find network testing framework")
+    def testGetHostAddress2(self):
         self.assertEqual(get_host_address('10.30.100.100'), '10.30.10.216')
         self.assertEqual(get_host_address('10.30.10.217'), '10.30.10.216')
-        self.assertGreater(len(get_host_address('127.0.0.1')), 0)
