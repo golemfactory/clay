ff963d8f792c87c0becac8b82d808d1c204a0d4d
Artur Zawłocki <artur.zawlocki@imapp.pl>
2015-11-24 18:56:26 +0100

    Removed duplicate code in Message.init_messages()

--- poc/golemPy/golem/network/transport/message.py
+++ poc/golemPy/golem/network/transport/message.py
@@ -1827,6 +1827,13 @@ class MessageNewNodes(Message):
     def dict_repr(self):
         return {MessageNewNodes.NUM_STR: self.num}
 
+def init_manager_messages():
+    """ Add manager messages to registered messages list"""
+    MessagePeerStatus()
+    MessageKillNode()
+    MessageKillAllNodes()
+    MessageNewTask()
+    MessageNewNodes()
 
 def init_messages():
     """ Add supported messages to register messages list """
@@ -1892,20 +1899,7 @@ def init_messages():
     MessageSendResource()
 
     # Manager messages
-    MessagePeerStatus()
-    MessageNewTask()
-    MessageKillNode()
-    MessageKillAllNodes()
-    MessageNewNodes()
-
-
-def init_manager_messages():
-    """ Add manager messages to registered messages list"""
-    MessagePeerStatus()
-    MessageKillNode()
-    MessageKillAllNodes()
-    MessageNewTask()
-    MessageNewNodes()
+    init_manager_messages()
 
 
 if __name__ == "__main__":
