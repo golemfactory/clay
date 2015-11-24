23036daf6d6ab050ab96434009ebebb95ea03b3b
Paweł Bylica <chfast@gmail.com>
2015-11-24 11:36:16 +0100

    Tests moved and cleaned up.

--- poc/golemPy/tests/golem/ranking/test_simplerank.py
+++ poc/golemPy/tests/golem/ranking/test_simplerank.py
@@ -1,13 +1,10 @@
 import unittest
-import sys
-import os
+from golem.ranking.simplerank import SimpleRank
 
-sys.path.append(os.environ.get('GOLEM'))
-
-from golem.ranking.simpleRank import SimpleRank
 
 class TestSimpleRank(unittest.TestCase):
-    def testRank (self):
+
+    def testRank(self):
         sr = SimpleRank()
         self.assertIsNone(sr.get_node_rank('abc'))
         sr.set_node_rank('abc', 1.0)
@@ -19,7 +16,7 @@ class TestSimpleRank(unittest.TestCase):
         sr.set_node_rank('ghi', 3.0)
         self.assertEquals(sr.get_node_rank('ghi'), 1.0)
 
-    def testNetworkRank(self) :
+    def testNetworkRank(self):
         node1 = SimpleRank()
         node2 = SimpleRank()
         # 2 dolacza do sieci
@@ -31,14 +28,14 @@ class TestSimpleRank(unittest.TestCase):
         node3 = SimpleRank()
         node3.set_seed_rank('node1')
         node1.set_basic_node_rank('node3')
-        node2.set_node_rank('node3', { 'node1': node1.get_node_rank('node3') })
-        node3.set_node_rank('node2', { 'node2': node1.get_node_rank('node2') })
+        node2.set_node_rank('node3', {'node1': node1.get_node_rank('node3')})
+        node3.set_node_rank('node2', {'node2': node1.get_node_rank('node2')})
         print "Node1: {}".format(node1.ranking)
         print "Node2: {}".format(node2.ranking)
         print "Node3: {}".format(node3.ranking)
         # 2 chce cos policzyc
-        node1.set_node_rank('node2', { 'node3': node1.get_node_rank('node2') })
-        node3.set_node_rank('node2', { 'node1': node3.get_node_rank('node2') })
+        node1.set_node_rank('node2', {'node3': node1.get_node_rank('node2')})
+        node3.set_node_rank('node2', {'node1': node3.get_node_rank('node2')})
         print "Node1: {}".format(node1.ranking)
         print "Node2: {}".format(node2.ranking)
         print "Node3: {}".format(node3.ranking)
@@ -47,8 +44,8 @@ class TestSimpleRank(unittest.TestCase):
         # 1 policzyla poprawnie
         node2.set_node_rank('node1', node2.get_node_rank('node1') + 0.1)
         # 2 zaplacila
-        node3.set_node_rank('node2', node3.get_node_rank('node2')  + 0.15)
-        node1.set_node_rank('node2', node1.get_node_rank('node2')  + 0.15)
+        node3.set_node_rank('node2', node3.get_node_rank('node2') + 0.15)
+        node1.set_node_rank('node2', node1.get_node_rank('node2') + 0.15)
         print "Node1: {}".format(node1.ranking)
         print "Node2: {}".format(node2.ranking)
         print "Node3: {}".format(node3.ranking)
@@ -57,19 +54,17 @@ class TestSimpleRank(unittest.TestCase):
         node4.set_seed_rank('node2 ')
         node2.set_basic_node_rank('node4')
         # 4 chce liczyc
-        node1.set_node_rank('node4', {'node2': node2.get_node_rank('node4'), 'node3': node3.get_node_rank('node4') })
-        node2.set_node_rank('node4', {'node1': node1.get_node_rank('node4'), 'node3': node3.get_node_rank('node4') })
-        node3.set_node_rank('node4', {'node2': node2.get_node_rank('node4'), 'node1': node1.get_node_rank('node4') })
-        node4.set_node_rank('node1', {'node2': node2.get_node_rank('node1') })
-        node4.set_node_rank('node2', {'node1': node1.get_node_rank('node2') })
-        node4.set_node_rank('node3', {'node2': node2.get_node_rank('node3'), 'node1': node1.get_node_rank('node3') })
+        node1.set_node_rank(
+            'node4', {'node2': node2.get_node_rank('node4'), 'node3': node3.get_node_rank('node4')})
+        node2.set_node_rank(
+            'node4', {'node1': node1.get_node_rank('node4'), 'node3': node3.get_node_rank('node4')})
+        node3.set_node_rank(
+            'node4', {'node2': node2.get_node_rank('node4'), 'node1': node1.get_node_rank('node4')})
+        node4.set_node_rank('node1', {'node2': node2.get_node_rank('node1')})
+        node4.set_node_rank('node2', {'node1': node1.get_node_rank('node2')})
+        node4.set_node_rank(
+            'node3', {'node2': node2.get_node_rank('node3'), 'node1': node1.get_node_rank('node3')})
         print "Node1: {}".format(node1.ranking)
         print "Node2: {}".format(node2.ranking)
         print "Node3: {}".format(node3.ranking)
         print "Node4: {}".format(node4.ranking)
-
-
-
-
-
-
