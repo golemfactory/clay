276d194d7bc1fca9512c4e01e06e665151914d11
Magda Stasiewicz <magdalena.stasiewicz@imapp.pl>
2015-12-04 11:37:46 +0100

    Added checking if test tasks aren't corrupted

--- testtasks/blender/blender_test.py
+++ testtasks/blender/blender_test.py
@@ -1,7 +1,18 @@
 from testtasks.execution_time_measure import measure
+from testtasks.dir_hash_builder import get_hash_of_dir, get_current_directory
+import os
+
+EXPECTED_CHECKSUM = "4f348c63485f76efcc26a8264cea003936ffbf46"
 
 def blender_performance():
     '''
     returns time (in seconds) needed to render the example scene
     '''
+    current_dir = get_current_directory(__file__)
+    directory = os.path.join(current_dir, r'blender_task')
+    checksum = get_hash_of_dir(directory)
+    if checksum == "-1":
+        return -1.
+    if str(checksum) != EXPECTED_CHECKSUM:
+        return -1.
     return measure(["blender","-b","blender_task/scene-Helicopter-27.blend","-F","JPEG","-x","1","-f","1"])
--- testtasks/luxrender/lux_test.py
+++ testtasks/luxrender/lux_test.py
@@ -1,7 +1,18 @@
 from testtasks.execution_time_measure import measure
+from testtasks.dir_hash_builder import get_hash_of_dir, get_current_directory
+import os
+
+EXPECTED_CHECKSUM = "725ad6c8c3391c730708bf3619bfd236690fef29"
 
 def lux_performance():
     '''
     returns time (in seconds) needed to render the example scene
     '''
+    current_dir = get_current_directory(__file__)
+    directory = os.path.join(current_dir, r'lux_task')
+    checksum = get_hash_of_dir(directory)
+    if checksum == "-1":
+        return -1.
+    if str(checksum) != EXPECTED_CHECKSUM:
+        return -1.
     return measure(["luxconsole", "lux_task/scene-Helicopter-27.Scene.00001.lxs"])
