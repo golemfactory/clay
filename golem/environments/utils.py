b692bc1ee5c2a3efc4f4656849a645979d06e879
Paweł Bylica <chfast@gmail.com>
2015-12-07 18:15:16 +0100

    Use extensions from $PATHEXT for finding programs on Windows.

--- golem/environments/utils.py
+++ golem/environments/utils.py
@@ -12,14 +12,22 @@ def find_program(program):
     def isexe(fpath):
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
+    # Handle full paths first
     fpath, fname = os.path.split(program)
     if fpath:
-        if isexe(program):
-            return program
-    else:
-        for path in os.environ.get("PATH").split(os.pathsep):
-            path = path.strip('"')
-            exe_file = os.path.join(path, program)
-            if isexe(exe_file):
-                return exe_file
+        return program if isexe(program) else None
+
+    for path in os.environ.get("PATH").split(os.pathsep):
+        path = path.strip('"')
+        exe_file = os.path.join(path, program)
+        if isexe(exe_file):
+            return exe_file
+        else:
+            exts = os.environ.get("PATHEXT")
+            if exts:
+                exts = exts.split(os.pathsep)
+                for ext in exts:
+                    exe_file_ext = exe_file + ext
+                    if isexe(exe_file_ext):
+                        return exe_file_ext
     return None
--- tests/golem/environments/test_utils.py
+++ tests/golem/environments/test_utils.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from golem.environments.utils import find_program
 
 
@@ -6,3 +7,7 @@ class TestCheckCmd(unittest.TestCase):
     def test_find_program(self):
         self.assertTrue(find_program('python'))
         self.assertFalse(find_program('afjaljl'))
+
+        if os.name == 'nt':
+            cmd = find_program('cmd')
+            assert cmd.lower().endswith('cmd.exe')
