173cc59c616b912703bd3ad4e8f5cbb5823c0215
badb <aleksandra.skrzypczak@imapp.pl>
2015-12-16 20:08:43 +0100

    lux task support

--- examples/gnr/nodelogic.py
+++ examples/gnr/nodelogic.py
@@ -1,5 +1,6 @@
-from examples.gnr.renderingenvironment import BlenderEnvironment
+from examples.gnr.renderingenvironment import BlenderEnvironment, LuxRenderEnvironment
 from examples.gnr.task.blenderrendertask import BlenderRenderTaskBuilder
+from examples.gnr.task.luxrendertask import LuxRenderTaskBuilder
 from golem.task.taskbase import Task
 
 
@@ -10,7 +11,10 @@ class Logic(object):
     def load_environments(self):
         blender_env = BlenderEnvironment()
         blender_env.accept_tasks = True
+        lux_env = LuxRenderEnvironment()
+        lux_env.accept_tasks = True
         self.client.environments_manager.add_environment(blender_env)
+        self.client.environments_manager.add_environment(lux_env)
 
     def connect_with_peers(self, peers):
         for peer in peers:
@@ -18,7 +22,14 @@ class Logic(object):
 
     def add_tasks(self, tasks):
         for task_def in tasks:
-            golem_task = Task.build_task(BlenderRenderTaskBuilder(self.client.get_node_name(),
+            #FIXME: temporary solution
+            if task_def.main_scene_file.endswith('.blend'):
+                golem_task = Task.build_task(BlenderRenderTaskBuilder(self.client.get_node_name(),
+                                                                      task_def,
+                                                                      self.client.get_root_path()))
+            else:
+                golem_task = Task.build_task(LuxRenderTaskBuilder(self.client.get_node_name(),
                                                                   task_def,
                                                                   self.client.get_root_path()))
+
             self.client.enqueue_new_task(golem_task)
