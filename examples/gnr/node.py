4326315ec458e6fec77c2e2064cba9d1d042b1cf
badb <aleksandra.skrzypczak@imapp.pl>
2015-12-17 17:19:29 +0100

    changing Logic class to Node class

--- examples/gnr/node.py
+++ examples/gnr/node.py
@@ -6,15 +6,64 @@ import click
 import uuid
 import logging.config
 
+from twisted.internet import reactor
+
 from golem.client import create_client
 from golem.network.transport.tcpnetwork import TCPAddress
-from examples.gnr.nodelogic import Logic
-from twisted.internet import reactor
+from golem.task.taskbase import Task
+
+from examples.gnr.task.blenderrendertask import BlenderRenderTaskBuilder
+from examples.gnr.task.luxrendertask import LuxRenderTaskBuilder
+from examples.gnr.renderingenvironment import BlenderEnvironment, LuxRenderEnvironment
 
 config_file = os.path.join(os.path.dirname(__file__), "logging.ini")
 logging.config.fileConfig(config_file, disable_existing_loggers=False)
 
 logger = logging.getLogger(__name__)
+
+
+class Node(object):
+    """ Simple Golem Node connecting console user interface with Client
+    """
+    default_environments = []
+
+    def __init__(self):
+        self.client = create_client()
+
+    def initialize(self):
+        self.client.start_network()
+        self.load_environments(self.default_environments)
+
+    def load_environments(self, environments):
+        for env in environments:
+            env.accept_task = True
+            self.client.environments_manager.add_environment(env)
+
+    def connect_with_peers(self, peers):
+        for peer in peers:
+            self.client.connect(peer)
+
+    def add_tasks(self, tasks):
+        for task_def in tasks:
+            task_builder = self._get_task_builder(task_def)
+            golem_task = Task.build_task(task_builder(self.client.get_node_name(), task_def,
+                                                      self.client.get_root_path()))
+            self.client.enqueue_new_task(golem_task)
+
+    def run(self):
+        reactor.run()
+
+
+class GNRNode(Node):
+    default_environments = [BlenderEnvironment(), LuxRenderEnvironment()]
+
+    @staticmethod
+    def _get_task_builder(task_def):
+        #FIXME: temporary solution
+        if task_def.main_scene_file.endswith('.blend'):
+            return BlenderRenderTaskBuilder
+        else:
+            return LuxRenderTaskBuilder
 
 
 def parse_peer(ctx, param, value):
@@ -47,19 +96,14 @@ def parse_task_file(ctx, param, value):
 @click.option('--task', '-t', multiple=True, type=click.File(lazy=True), callback=parse_task_file,
               help="Request task from file")
 def start(peer, task):
-    client = create_client()
-    client.start_network()
-    logic = Logic(client)
 
-    logic.load_environments()
-    logic.connect_with_peers(peer)
-    logic.add_tasks(task)
+    node = GNRNode()
+    node.initialize()
 
-    run_node()
+    node.connect_with_peers(peer)
+    node.add_tasks(task)
 
-
-def run_node():
-    reactor.run()
+    node.run()
 
 
 def __parse_ipv6(addr_arg):
--- tests/gnr/test_node.py
+++ tests/gnr/test_node.py
@@ -70,49 +70,43 @@ class TestNode(unittest.TestCase):
         mock_reactor.run.assert_called_with()
         mock_reactor.stop()
 
-    @patch('examples.gnr.node.create_client')
-    @patch('examples.gnr.node.Logic')
-    @patch('examples.gnr.node.reactor')
-    def test_wrong_peer_good_peer(self, mock_reactor, mock_logic, mock_client):
+    @patch('examples.gnr.node.GNRNode')
+    def test_wrong_peer_good_peer(self, mock_node):
         runner = CliRunner()
         return_value = runner.invoke(start, ['--peer', '10.30.10.216:40111', '--peer', 'bla'])
-        time.sleep(1)
         self.assertEqual(return_value.exit_code, 0)
-        mock_reactor.run.assert_called_with()
-        peer_arg = mock_logic.mock_calls[2][1][0]
+        mock_node.assert_has_calls([call().run(), call().add_tasks([])], any_order=True)
+        call_names = [name for name, arg, kwarg in mock_node.mock_calls]
+        self.assertTrue('().connect_with_peers' in call_names)
+        peer_num = call_names.index('().connect_with_peers')
+        peer_arg = mock_node.mock_calls[peer_num][1][0]
         self.assertEqual(len(peer_arg), 1)
         self.assertEqual(peer_arg[0], TCPAddress('10.30.10.216', 40111))
-        mock_logic.assert_has_calls([call().add_tasks([])])
 
-    @patch('examples.gnr.node.create_client')
-    @patch('examples.gnr.node.Logic')
-    @patch('examples.gnr.node.reactor')
-    def test_peers(self, mock_reactor, mock_logic, mock_client):
+    @patch('examples.gnr.node.GNRNode')
+    def test_peers(self, mock_node):
         runner = CliRunner()
         return_value = runner.invoke(start, ['--peer', '10.30.10.216:40111', '--peer',
                                                   '[2001:db8:85a3:8d3:1319:8a2e:370:7348]:443'])
         self.assertEqual(return_value.exit_code, 0)
-        mock_reactor.run.assert_called_with()
-        self.assertEqual(mock_logic.mock_calls[2][0], '().connect_with_peers')
-        peer_arg = mock_logic.mock_calls[2][1][0]
+        mock_node.assert_has_calls([call().run(), call().add_tasks([])], any_order=True)
+        call_names = [name for name, arg, kwarg in mock_node.mock_calls]
+        self.assertTrue('().connect_with_peers' in call_names)
+        peer_num = call_names.index('().connect_with_peers')
+        peer_arg = mock_node.mock_calls[peer_num][1][0]
         self.assertEqual(len(peer_arg), 2)
         self.assertEqual(peer_arg[0], TCPAddress('10.30.10.216', 40111))
         self.assertEqual(peer_arg[1], TCPAddress('2001:db8:85a3:8d3:1319:8a2e:370:7348', 443))
-        mock_logic.assert_has_calls([call().add_tasks([])])
-        mock_reactor.stop()
 
-    @patch('examples.gnr.node.Logic')
-    @patch('examples.gnr.node.reactor')
-    def test_wrong_task(self, mock_reactor, mock_logic):
+    @patch('examples.gnr.node.GNRNode')
+    def test_wrong_task(self, mock_node):
         runner = CliRunner()
         return_value = runner.invoke(start, ['--task', 'testtask.gt'])
         self.assertEqual(return_value.exit_code, 2)
         self.assertTrue('Error' in return_value.output and 'Usage' in return_value.output)
 
-    @patch('examples.gnr.node.create_client')
-    @patch('examples.gnr.node.Logic')
-    @patch('examples.gnr.node.reactor')
-    def test_task(self, mock_reactor, mock_logic, mock_client):
+    @patch('examples.gnr.node.GNRNode')
+    def test_task(self, mock_node):
         runner = CliRunner()
 
         a = A()
@@ -120,8 +114,11 @@ class TestNode(unittest.TestCase):
             cPickle.dump(a, f)
         return_value = runner.invoke(start, ['--task', 'testclassdump', '--task', 'testclassdump'])
         self.assertEqual(return_value.exit_code, 0)
-        self.assertEqual(mock_logic.mock_calls[3][0], '().add_tasks')
-        task_arg = mock_logic.mock_calls[3][1][0]
+        mock_node.assert_has_calls([call().run()])
+        call_names = [name for name, arg, kwarg in mock_node.mock_calls]
+        self.assertTrue('().add_tasks' in call_names)
+        task_num = call_names.index('().add_tasks')
+        task_arg = mock_node.mock_calls[task_num][1][0]
         self.assertEqual(len(task_arg), 2)
         self.assertIsInstance(task_arg[0], A)
         if os.path.exists('testclassdump'):
