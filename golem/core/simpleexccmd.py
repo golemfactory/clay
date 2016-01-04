5ae24c880a21069df318bf984154945ea3647e66
Magda Stasiewicz <magdalena.stasiewicz@imapp.pl>
2016-01-04 14:50:47 +0100

    Removed setting niceness of subprocess (was causing error)

--- golem/core/simpleexccmd.py
+++ golem/core/simpleexccmd.py
@@ -1,10 +1,10 @@
 import subprocess
 import psutil
-
 from common import is_windows
 
 
 def exec_cmd(cmd, nice=20, wait=True):
+    print("Still alive0")
     """ Execute a child process from command in a new process
     :param list|str cmd: sequence of program arguments or a single string. On Unix single string is interpreted
     as the path of the program to execute, but it's only working if not passing arguments to the program.
@@ -12,7 +12,7 @@ def exec_cmd(cmd, nice=20, wait=True):
     :param bool wait: *Default: True* if True, program will wait for child process to terminate
     :return:
     """
-    pc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    pc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     stdout, stderr = pc.communicate()
     if is_windows():
         import win32process
@@ -20,9 +20,7 @@ def exec_cmd(cmd, nice=20, wait=True):
         import win32con
         handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pc.pid)
         win32process.SetPriorityClass(handle, win32process.IDLE_PRIORITY_CLASS)
-    else:
-        p = psutil.Process(pc.pid)
-        p.nice(nice)
+
 
     if wait:
         pc.wait()
