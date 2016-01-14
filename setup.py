f6c83c9b1453c8fcf473464234a3cfc83718fadc
badb <aleksandra.skrzypczak@imapp.pl>
2016-01-14 16:34:54 +0100

    manifest for setup

--- setup.py
+++ setup.py
@@ -75,9 +75,8 @@ setup(
     author_email='contact@golemproject.net',
     url='http://golemproject.net',
     packages=find_packages(include=['golem*', 'gnr*']),
-    package_data={'gnr': ['logging.ini', 'ui/nopreview.png', 'tools/taskcollector/Release/taskcollector']},
-    include_package_data=True,
     install_requires=requirements,
+    include_package_data=True,
     dependency_links=dependency_links,
     # TODO: No license yet
     # license="ISCL",
