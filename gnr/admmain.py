07c7ad2d0f59e79f1054d191e6777beb79313423
badb <aleksandra.skrzypczak@imapp.pl>
2016-01-14 21:43:07 +0100

    killing and exiting golem kills computations

--- gnr/admmain.py
+++ gnr/admmain.py
@@ -1,3 +1,6 @@
+import sys
+from multiprocessing import freeze_support
+
 from gnrstartapp import start_app, config_logging
 from renderingadmapplicationlogic import RenderingAdmApplicationLogic
 from gnr.ui.administrationmainwindow import AdministrationMainWindow
@@ -12,10 +15,17 @@ def main():
     app = GNRGui(logic, AdministrationMainWindow)
     gui = RenderingAdmMainWindowCustomizer
 
-    start_app(logic, app, gui, rendering=True, start_add_task_client=False, start_add_task_server=False)
+    try:
+        start_app(logic, app, gui, rendering=True, start_add_task_client=False, start_add_task_server=False)
+    finally:
+        try:
+            logic.client.task_server.task_computer.end_task()
+        except Exception as ex:
+            import logging
+            logger = logging.getLogger(__name__)
+            logger.warning("Exception when closing Golem {}".format(ex))
+        sys.exit(0)
 
-
-from multiprocessing import freeze_support
 
 if __name__ == "__main__":
     freeze_support()
