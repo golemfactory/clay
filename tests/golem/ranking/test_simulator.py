47c2be2cabf88b7a02026c2ae029bab0e32c6c5f
Artur Zawłocki <artur.zawlocki@imapp.pl>
2016-01-28 14:23:37 +0100

    cleanup in tests/

--- tests/golem/ranking/test_simulator.py
+++
@@ -1,4 +0,0 @@
-import golem.ranking.simulator.differentialgossiptrustsimulator
-import golem.ranking.simulator.diffgossiptrustrank
-import golem.ranking.simulator.gossiptrustsimulator
-import golem.ranking.simulator.networksimulator
