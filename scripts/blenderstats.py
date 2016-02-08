440adafa552abc7cdcc72d6cbbfdffab0c5315ef
badb <aleksandra.skrzypczak@imapp.pl>
2016-02-08 17:13:23 +0100

    small bug fix and better output representation in blenderstats

--- scripts/blenderstats.py
+++ scripts/blenderstats.py
@@ -3,6 +3,7 @@ import statistics
 import math
 import random
 
+
 @click.command()
 @click.argument("results")
 @click.option("--probs", default=0)
@@ -27,11 +28,20 @@ def run_stats(results, probs):
 
 def __str_to_time(result):
     time_ = result.split(":")
-    return float(time_[0]*60) + float(time_[1])
+    if len(time_) == 2:
+        return float(time_[0])*60 + float(time_[1])
+    else:
+        return float(time_[0])*60*60 + float(time_[1])*60*60 + float(time_[2])
 
 
 def __time_to_str(time_):
-    return "{0}:{1:08.5f}".format(int(math.floor(time_ / 60)), time_ % 60)
+    hours = int(math.floor(time_ / 3600))
+    minutes = int(math.floor((time_ - 3600 * hours) / 60))
+    seconds = time_ % 60
+    if hours > 0:
+        return "{0:02.0f}:{1:02.0f}:{2:08.5f}".format(hours, minutes, seconds)
+    else:
+        return "{0:02.0f}:{1:08.5f}".format(minutes, seconds)
 
 
 if __name__ == "__main__":
