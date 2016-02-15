b0ffec2a8160a6608e22c4ccdef0d38c71dfd97f
badb <aleksandra.skrzypczak@imapp.pl>
2016-02-15 11:54:09 +0100

    Some additional statistics for blenderstats

--- scripts/blenderstats.py
+++ scripts/blenderstats.py
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 @click.argument("results")
 @click.option("--probs", default=0)
 @click.option("--name", default="Rendering time")
-def run_stats(results, probs, name):
+@click.option("--plot/--no-plot", default=True)
+@click.option("--repeat_prob/--no-repeat_prob", default=False)
+def run_stats(results, probs, name, plot, repeat_prob):
     times = []
     with open(results) as f:
         for line in f:
@@ -19,20 +21,38 @@ def run_stats(results, probs, name):
     n = len(times)
     if probs == 0:
         probs = len(times)
-    times = random.sample(times, probs)
-    mean = statistics.mean(times)
-    print "PROBS: {}".format(len(times))
+    times_p = random.sample(times, probs)
+    mean = statistics.mean(times_p)
+    print "PROBS: {}".format(len(times_p))
     print "ESTM_TIME: {} ({})".format(__time_to_str(n*mean), n*mean)
     print "MEAN: {} ({})".format(__time_to_str(mean), mean)
-    print "DEVIATION: {} ({})".format(__time_to_str(statistics.stdev(times)), statistics.stdev(times))
-    print "VARIANCE: {} ({})".format(__time_to_str(statistics.variance(times)), statistics.variance(times))
-    print "MAX_VAL: {} ({})".format(__time_to_str(max(times)), max(times))
-    print "MIN_VAL: {} ({})".format(__time_to_str(min(times)), min(times))
-    plt.hist(times)
-    plt.title(name)
-    plt.xlabel("time in sec")
-    plt.ylabel("frequency")
-    plt.show()
+    print "DEVIATION: {} ({})".format(__time_to_str(statistics.stdev(times_p)), statistics.stdev(times_p))
+    print "VARIANCE: {} ({})".format(__time_to_str(statistics.variance(times_p)), statistics.variance(times_p))
+    print "MAX_VAL: {} ({})".format(__time_to_str(max(times_p)), max(times_p))
+    print "MIN_VAL: {} ({})".format(__time_to_str(min(times_p)), min(times_p))
+
+    if repeat_prob:
+        estm_times = []
+        for i in range(100):
+            times_p = random.sample(times, probs)
+            estm_times.append(statistics.mean(times_p))
+        print "#######################"
+        mean = statistics.mean(estm_times)
+        std_dev = statistics.stdev(estm_times)
+        min_ = min(estm_times)
+        max_ = max(estm_times)
+        print "100 PROBS ESTM_TIME: {} ({})".format(__time_to_str(n*mean), n*mean)
+        print "100 PROBS MEAN: {} ({})".format(__time_to_str(mean), mean)
+        print "100 PROBS DEVIATION: {} ({})".format(__time_to_str(std_dev), std_dev)
+        print "100 PROBS MAX: {} ({})".format(__time_to_str(max_*n), max_*n)
+        print "100 PROBS MIN: {} ({})".format(__time_to_str(min_*n), min_*n)
+
+    if plot:
+        plt.hist(times)
+        plt.title(name)
+        plt.xlabel("time in sec")
+        plt.ylabel("frequency")
+        plt.show()
 
 
 def __str_to_time(result):
