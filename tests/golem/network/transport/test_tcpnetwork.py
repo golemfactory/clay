ac80e69be1a6e83c1dba0b660b8c25311d8f792a
badb <aleksandra.skrzypczak@imapp.pl>
2016-02-23 19:47:56 +0100

    tests

--- tests/golem/network/transport/test_tcpnetwork.py
+++ tests/golem/network/transport/test_tcpnetwork.py
@@ -1,13 +1,15 @@
 import unittest
 import math
 import os
-import shutil
 
-from golem.network.transport.tcpnetwork import DataProducer, DataConsumer, FileProducer, FileConsumer
+from mock import MagicMock
+
+from golem.network.transport.tcpnetwork import (DataProducer, DataConsumer, FileProducer, FileConsumer,
+                                                EncryptFileProducer, DecryptFileConsumer)
 from golem.core.variables import BUFF_SIZE
+from golem.core.keysauth import EllipticalKeysAuth
 from golem.tools.captureoutput import captured_output
 from golem.tools.testdirfixture import TestDirFixture
-from mock import MagicMock
 
 
 class TestDataProducerAndConsumer(unittest.TestCase):
@@ -62,26 +64,47 @@ class TestFileProducerAndConsumer(TestDirFixture):
         long_text = "abcdefghij\nklmn opqrstuvwxy\tz"
         with open(self.tmp_file1, 'w') as f:
             f.write(long_text)
-        with open(self.tmp_file2, 'w') as f:
+        with open(self.tmp_file3, 'w') as f:
             f.write(long_text * 10000)
-        with open(self.tmp_file3, 'w'):
+        with open(self.tmp_file2, 'w'):
             pass
 
     def test_progress(self):
-        self.__producer_consumer_test([])
-        self.__producer_consumer_test([self.tmp_file1])
-        self.__producer_consumer_test([self.tmp_file3])
-        self.__producer_consumer_test([self.tmp_file1, self.tmp_file2])
-        self.__producer_consumer_test([self.tmp_file1, self.tmp_file2, self.tmp_file3], 32)
+        self.__producer_consumer_test([], session=MagicMock())
+        self.__producer_consumer_test([self.tmp_file1], session=MagicMock())
+        self.__producer_consumer_test([self.tmp_file2], session=MagicMock())
+        self.__producer_consumer_test([self.tmp_file1, self.tmp_file3], session=MagicMock())
+        self.__producer_consumer_test([self.tmp_file1, self.tmp_file2, self.tmp_file3], 32, session=MagicMock())
+        self.ek = EllipticalKeysAuth()
+        self.__producer_consumer_test([], file_producer_cls=EncryptFileProducer, file_consumer_cls=DecryptFileConsumer,
+                                      session=self.__make_encrypted_session_mock())
+        self.__producer_consumer_test([self.tmp_file1], file_producer_cls=EncryptFileProducer,
+                                      file_consumer_cls=DecryptFileConsumer,
+                                      session=self.__make_encrypted_session_mock())
+        self.__producer_consumer_test([self.tmp_file2], file_producer_cls=EncryptFileProducer,
+                                      file_consumer_cls=DecryptFileConsumer,
+                                      session=self.__make_encrypted_session_mock())
+        self.__producer_consumer_test([self.tmp_file1, self.tmp_file3], file_producer_cls=EncryptFileProducer,
+                                      file_consumer_cls=DecryptFileConsumer,
+                                      session=self.__make_encrypted_session_mock())
+        self.__producer_consumer_test([self.tmp_file1, self.tmp_file2, self.tmp_file3], 32,
+                                      file_producer_cls=EncryptFileProducer, file_consumer_cls=DecryptFileConsumer,
+                                      session=self.__make_encrypted_session_mock())
 
-    def __producer_consumer_test(self, file_list, buff_size=None):
+    def __make_encrypted_session_mock(self):
+        session = MagicMock()
+        session.encrypt.side_effect = self.ek.encrypt
+        session.decrypt.side_effect = self.ek.decrypt
+        return session
+
+    def __producer_consumer_test(self, file_list, buff_size=None, file_producer_cls=FileProducer,
+                                 file_consumer_cls=FileConsumer, session=MagicMock()):
         producer_progress_value = "Sending progress 100 %"
         consumer_progress_value = "File data receiving 100 %"
-        session = MagicMock()
         if buff_size:
-            p = FileProducer(file_list, session, buff_size)
+            p = file_producer_cls(file_list, session, buff_size)
         else:
-            p = FileProducer(file_list, session)
+            p = file_producer_cls(file_list, session)
             buff_size = BUFF_SIZE
         with captured_output() as (out, err):
             while session.conn.transport.unregisterProducer.call_count == 0:
@@ -98,7 +121,7 @@ class TestFileProducerAndConsumer(TestDirFixture):
         self.assertEqual(err.getvalue().strip(), "")
 
         consumer_list = ["consumer{}".format(i + 1) for i in range(len(file_list))]
-        c = FileConsumer(consumer_list, self.path, MagicMock())
+        c = file_consumer_cls(consumer_list, self.path, session)
 
         with captured_output() as (out, err):
             for chunk in session.conn.transport.write.call_args_list:
