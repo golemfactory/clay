ba120ae629c35ea0e8e1d1dc08d14f16445fe266
Artur Zawłocki <artur.zawlocki@imapp.pl>
2016-02-25 23:59:54 +0100

    Test cases for dummy task updated

--- tests/golem/task/dummy/test_dummy_task.py
+++ tests/golem/task/dummy/test_dummy_task.py
@@ -1,22 +1,24 @@
-import unittest
 import mock
 
 import runner
 from golem.tools.testwithappconfig import TestWithAppConfig
 
 
+#  @mock.patch('golem.transactions.incomeskeeper.IncomesDatabase')
 class TestDummyTask(TestWithAppConfig):
 
-    def test_dummy_task_computation(self):
-        runner.run_nodes(num_computing_nodes = 2, num_subtasks = 4, timeout= 60)
-        self.assertIsNone(runner.computation_error)
+    def test_dummy_task_computation(self, *mocks):
+        error_msg = runner.run_simulation(
+            num_computing_nodes=2, num_subtasks=3, timeout=120)
+        self.assertIsNone(error_msg)
 
-    def test_dummy_task_computation_timeout(self):
-        runner.run_nodes(timeout = 1)
-        self.assertEqual(runner.computation_error, "Task computation timed out")
+    def test_dummy_task_computation_timeout(self, *mocks):
+        error_msg = runner.run_simulation(timeout=1)
+        self.assertEqual(error_msg, "Computation timed out")
 
     @mock.patch('subprocess.Popen')
-    def test_dummy_task_computation_subprocess_error(self, mock_popen):
-        runner.run_nodes(num_computing_nodes = 2, num_subtasks = 4, timeout= 60)
-        self.assertTrue(
-            runner.computation_error.startswith("Computing process exited"))
+    @mock.patch('re.compile')
+    def test_dummy_task_computation_subprocess_error(self, *mocks):
+        error_msg = runner.run_simulation(
+            num_computing_nodes=2, num_subtasks=4, timeout=60)
+        self.assertTrue(error_msg.startswith("Node exited with return code"))
