6f2a29c13825a2dcc80fa670f6fdf694efb5f831
Artur Zawłocki <artur.zawlocki@imapp.pl>
2016-03-15 19:06:52 +0100

    started implementation of luxrender tasks in docker

--- gnr/docker_environments.py
+++ gnr/docker_environments.py
@@ -5,12 +5,24 @@ from golem.task.docker.image import DockerImage
 class BlenderDockerEnvironment(DockerEnvironment):
     @classmethod
     def get_id(cls):
-        return "BLENDER@DOCKER"
+        return "BLENDER"
 
-    def __init__(self, tag = "latest", id = None):
-        image = DockerImage(id = id) if id \
-            else DockerImage("golem/blender", tag = tag)
+    def __init__(self, tag="latest", id=None):
+        image = DockerImage(id=id) if id \
+            else DockerImage("golem/blender", tag=tag)
         DockerEnvironment.__init__(self, [image])
 
-        self.short_description = "Blender (http://www.blender.org/) " \
-                                 "running inside a Docker container"
+        self.short_description = "Blender (www.blender.org)"
+
+
+class LuxRenderDockerEnvironment(DockerEnvironment):
+    @classmethod
+    def get_id(cls):
+        return "LUXRENDER"
+
+    def __init__(self, tag="latest", id=None):
+        image = DockerImage(id=id) if id \
+            else DockerImage("golem/blender", tag=tag)
+        DockerEnvironment.__init__(self, [image])
+
+        self.short_description = "LuxRender (www.luxrender.net)"
---
+++ tests/golem/docker/test_docker_luxrender_task.py
@@ -0,0 +1,112 @@
+import jsonpickle
+import logging
+from os import makedirs, path
+import shutil
+
+from golem.core.common import get_golem_path
+from golem.task.taskbase import result_types
+from golem.task.taskcomputer import DockerTaskThread
+from golem.task.taskserver import TaskServer
+from golem.task.docker.image import DockerImage
+import gnr.node
+from gnr.task.luxrendertask import LuxRenderTaskBuilder
+from golem.tools.testwithappconfig import TestWithAppConfig
+
+# Make peewee logging less verbose
+logging.getLogger("peewee").setLevel("INFO")
+
+
+class TestDockerLuxrenderTask(TestWithAppConfig):
+
+    TASK_FILE = "docker-luxrender-test-task.json"
+
+    def setUp(self):
+        TestWithAppConfig.setUp(self)
+        self.error_msg = None
+        self.dirs_to_remove = []
+        self.task_computer_send_task_failed = TaskServer.send_task_failed
+
+    def tearDown(self):
+        for dir in self.dirs_to_remove:
+            shutil.rmtree(dir)
+        TaskServer.send_task_failed = self.task_computer_send_task_failed
+        TestWithAppConfig.tearDown(self)
+
+    def _test_task_definition(self):
+        task_file = path.join(path.dirname(__file__), self.TASK_FILE)
+        with open(task_file, "r") as f:
+            task_def = jsonpickle.decode(f.read())
+
+        # Replace $GOLEM_DIR in paths in task definition by get_golem_path()
+        golem_dir = get_golem_path()
+
+        def set_root_dir(p): return p.replace("$GOLEM_DIR", golem_dir)
+
+        task_def.resources = set(set_root_dir(p) for p in task_def.resources)
+        task_def.main_scene_file = set_root_dir(task_def.main_scene_file)
+        task_def.main_program_file = set_root_dir(task_def.main_program_file)
+        task_def.output_file = set_root_dir(task_def.output_file)
+        return task_def
+
+    def _test_task(self):
+        task_def = self._test_task_definition()
+        node_name = "0123456789abcdef"
+        root_path = get_golem_path()
+        task_builder = LuxRenderTaskBuilder(node_name, task_def, root_path)
+        render_task = task_builder.build()
+        render_task.__class__._update_task_preview = lambda self_: ()
+        return render_task
+
+    def _run_docker_task(self, render_task, timeout=0):
+        task_id = render_task.header.task_id
+        ctd = render_task.query_extra_data(1.0)
+
+        # Create the computing node
+        node = gnr.node.GNRNode()
+        node.initialize()
+
+        task_computer = node.client.task_server.task_computer
+        resource_dir = task_computer.resource_manager.get_resource_dir(task_id)
+        temp_dir = task_computer.resource_manager.get_temporary_dir(task_id)
+        self.dirs_to_remove.append(resource_dir)
+        self.dirs_to_remove.append(temp_dir)
+
+        # Copy the task resources
+        common_prefix = path.commonprefix(render_task.task_resources)
+        common_prefix = path.dirname(common_prefix)
+
+        for res_file in render_task.task_resources:
+            dest_file = path.join(resource_dir,
+                                  res_file[len(common_prefix) + 1:])
+            dest_dirname = path.dirname(dest_file)
+            if not path.exists(dest_dirname):
+                makedirs(dest_dirname)
+            shutil.copyfile(res_file, dest_file)
+
+        def send_task_failed(self_, subtask_id, task_id, error_msg, *args):
+            self.error_msg = error_msg
+
+        TaskServer.send_task_failed = send_task_failed
+
+        # Start task computation
+        task_computer.task_given(ctd, timeout)
+        result = task_computer.resource_given(ctd.task_id)
+        self.assertTrue(result)
+
+        # Thread for task computation should be created by now
+        task_thread = None
+        with task_computer.lock:
+            if task_computer.current_computations:
+                task_thread = task_computer.current_computations[0]
+
+        if task_thread:
+            task_thread.join(60.0)
+
+        return task_thread, self.error_msg, temp_dir
+
+    def test_luxrender_subtask(self):
+        task = self._test_task()
+        task_thread, error_msg, out_dir = self._run_docker_task(task)
+        #self.assertIsInstance(task_thread, DockerTaskThread)
+        #self.assertIsNone(error_msg)
+
