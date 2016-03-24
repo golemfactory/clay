24bd5799b6e8984454b3b1f244a26294fa4f19b7
Magdalena Stasiewicz <ms345742@students.mimuw.edu.pl>
2016-03-24 16:10:28 +0100

    test for _put_img_together from blender task

--- tests/gnr/task/test_blenderrendertask.py
+++ tests/gnr/task/test_blenderrendertask.py
@@ -3,6 +3,7 @@ import os
 from random import randrange, shuffle
 
 from PIL import Image
+import OpenEXR, array
 
 from gnr.task.blenderrendertask import BlenderDefaults, BlenderRenderTask, PreviewUpdater
 
@@ -25,7 +26,7 @@ class TestBlenderTaskDivision(unittest.TestCase):
                  res_x = 200,
                  res_y = 300,
                  outfilebasename = "example_out",
-                 output_file = "",
+                 output_file = os.path.join(os.getcwd(), "example_out"),
                  output_format = "PNG",
                  full_task_timeout = 1,
                  subtask_timeout = 1,
@@ -57,7 +58,27 @@ class TestBlenderTaskDivision(unittest.TestCase):
                     self.assertTrue(max_y == cur_max_y)
                     cur_max_y = min_y
                 self.assertTrue(cur_max_y == 0)
-        
+
+    def test_put_img_together(self):
+        for chunks in [1, 5, 7, 11, 13, 31, 57, 100]:
+            res_y = 0
+            chunks_sizes = {}
+            self.bt.collected_file_names = {}
+            for i in range (1, chunks + 1): #subtask numbers start from 1
+                y = randrange(1, 100)
+                res_y += y
+                file1 = os.path.join(os.getcwd(), 'chunk{}.exr'.format(i))
+                exr = OpenEXR.OutputFile(file1, OpenEXR.Header(self.bt.res_x, y))
+                data = array.array('f', [ 1.0 ] * (self.bt.res_x * y)).tostring()
+                exr.writePixels({'R': data, 'G': data, 'B': data, 'F': data, 'A': data})
+                exr.close()
+                self.bt.collected_file_names[i] = file1
+            self.bt.res_y = res_y
+            self.bt._put_image_together(os.getcwd())
+            self.assertTrue(os.path.isfile(self.bt.output_file))
+            img = Image.open(self.bt.output_file)
+            img_x, img_y = img.size
+            self.assertTrue(self.bt.res_x == img_x and res_y == img_y)
         
 class TestPreviewUpdater(unittest.TestCase):
     def test_update_preview(self):
