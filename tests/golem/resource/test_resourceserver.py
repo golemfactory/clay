1e34255e15954845f359df2de3129c2b259b6eab
Paweł Bylica <chfast@gmail.com>
2016-04-06 20:25:38 +0200

    Remove file leak in ResourceServer test.

--- tests/golem/resource/test_resourceserver.py
+++ tests/golem/resource/test_resourceserver.py
@@ -1,12 +1,12 @@
-import unittest
 from mock import Mock
+from golem.testutils import TempDirFixture
 from golem.resource.resourceserver import ResourceServer
 
 
-class TestResourceServer(unittest.TestCase):
+class TestResourceServer(TempDirFixture):
 
     def test_resource_server_datadir(self):
         client = Mock()
-        client.datadir = "great/datadir"
+        client.datadir = self.tempdir
         rs = ResourceServer(Mock(), Mock(), client)
         assert rs.dir_manager.root_path == client.datadir
