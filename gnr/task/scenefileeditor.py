97f1760b2474fb9981c2d274eff336fff7e4e3e6
badb <aleksandra.skrzypczak@imapp.pl>
2016-04-22 18:44:57 +0200

    scenefileeditor add write lux format + test

--- gnr/task/scenefileeditor.py
+++ gnr/task/scenefileeditor.py
@@ -60,26 +60,34 @@ def regenerate_lux_file(scene_file_src, xres, yres, halttime, haltspp, writeinte
         add_crop_window = False
     else:
         add_crop_window = True
-    
+
     if '"bool write_resume_flm" ["true"]' in scene_file_src:
         add_write_resume_flm = False
     else:
         add_write_resume_flm = True
-    
+
     exr = "false"
     png = "false"
     tga = "false"
+    add_exr, add_png, add_tga = False, False, False
     if output_format.lower() == "exr":
         exr = "true"
+        add_exr = '"bool write_exr"' not in scene_file_src
     elif output_format.lower() == "png":
         png = "true"
+        add_png = '"bool write_png"' not in scene_file_src
     elif output_format.lower() == "tga":
         tga = "true"
+        add_tga = '"bool write_tga"' not in scene_file_src
 
     next_line_add_halt = False
     next_line_add_crop = False
     next_line_add_haltspp = False
     next_line_add_write_resume_flm = False
+    next_line_add_exr = False
+    next_line_add_png = False
+    next_line_add_tga = False
+
     for l in scene_file_src.splitlines():
         if next_line_add_halt:
             next_line_add_halt = False
@@ -93,6 +101,16 @@ def regenerate_lux_file(scene_file_src, xres, yres, halttime, haltspp, writeinte
         if next_line_add_write_resume_flm:
             next_line_add_write_resume_flm = False
             out += '\t"bool write_resume_flm" ["true"]\n'
+        if next_line_add_exr:
+            next_line_add_exr = False
+            out += '\t"bool write_exr" ["true"]\n'
+        if next_line_add_png:
+            next_line_add_png = False
+            out += '\t"bool write_png" ["true"]\n'
+        if next_line_add_tga:
+            next_line_add_tga = False
+            out += '\t"bool write_tga" ["true"]\n'
+
         line = re.sub(r'("integer\s+xresolution"\s*)(\[\s*\d*\s*\])', r'\1[{}]'.format(xres), l)
         line = re.sub(r'("integer\s+yresolution"\s*)(\[\s*\d*\s*\])', r'\1[{}]'.format(yres), line)
         line = re.sub(r'("integer\s+halttime"\s*)(\[\s*\d*\s*\])', r'\1[{}]'.format(halttime), line)
@@ -113,6 +131,12 @@ def regenerate_lux_file(scene_file_src, xres, yres, halttime, haltspp, writeinte
             next_line_add_haltspp = True
         if add_write_resume_flm and 'Film' in line:
             next_line_add_write_resume_flm = True
+        if add_exr and 'Film' in line:
+            next_line_add_exr = True
+        if add_png and 'Film' in line:
+            next_line_add_png = True
+        if add_tga and 'Film' in line:
+            next_line_add_tga = True
 
     return out
 
