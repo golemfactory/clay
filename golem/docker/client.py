7cf564f5d6b8d03f7326ba777ac49feb20207ce2
mfrancis <mfranciszkiewicz@gmail.com>
2016-05-09 19:49:20 +0200

    Set a fixed docker-py version in requirements.txt Removed the fixed API version from golem.docker.client

--- golem/docker/client.py
+++ golem/docker/client.py
@@ -1,13 +1,11 @@
 from docker import Client
 from docker.utils import kwargs_from_env
 
-VERSION = '1.19'  # 'auto' will increase the number of API requests
-
 
 def local_client():
     """Returns an instance of docker.Client for communicating with
     local docker daemon.
     :returns docker.Client:
     """
-    client = Client(version=VERSION, **kwargs_from_env(assert_hostname=False))
+    client = Client(**kwargs_from_env(assert_hostname=False))
     return client
