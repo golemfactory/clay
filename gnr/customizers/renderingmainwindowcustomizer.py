3abd77205b0a764bcca522624f77fccc93941fcd
Adam Banasiak <banasiakadam60@gmail.com>
2016-07-01 13:20:07 +0200

    Better names for frames #398
    Better frames numbers

--- gnr/customizers/renderingmainwindowcustomizer.py
+++ gnr/customizers/renderingmainwindowcustomizer.py
@@ -159,7 +159,7 @@ class AbsRenderingMainWindowCustomizer(object):
     def __get_frame_name(self, definition, num):
         output_name, ext = os.path.splitext(definition.output_file)
         frame_num = definition.renderer_options.frames[num]
-        padding = len(output_name.split('_')[-1])
+        padding = len(output_name.split('_')[-2])
         output_name += str(frame_num).zfill(padding)
         return output_name + ext
 
