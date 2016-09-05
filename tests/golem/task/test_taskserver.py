0227a54a38683b3bd83b1b0043477ff2addc3959
badb <aleksandra.skrzypczak@imapp.pl>
2016-09-05 17:37:02 +0200

    remove task headers that end with an error

--- tests/golem/task/test_taskserver.py
+++ tests/golem/task/test_taskserver.py
@@ -34,15 +34,23 @@ class TestTaskServer(TestWithKeysAuth, LogTestCase):
                         use_docker_machine_manager=False)
         self.ts = ts
         ts.client.get_suggested_addr.return_value = "10.10.10.10"
+        ts.client.get_suggested_conn_reverse.return_value = False
         self.assertIsInstance(ts, TaskServer)
-        self.assertEqual(0, ts.request_task())
+        assert ts.request_task() is None
         n2 = Node()
         n2.prv_addr = "10.10.10.10"
         n2.port = 10101
         task_header = self.__get_example_task_header()
         task_header["task_owner"] = n2
         ts.add_task_header(task_header)
-        self.assertEqual("uvw", ts.request_task())
+        assert ts.request_task() == "uvw"
+        ts.remove_task_header("uvw")
+        task_header["port"] = 0
+        task_header["id"] = "uvw2"
+        assert ts.add_task_header(task_header)
+        assert ts.task_keeper.task_headers["uvw2"] is not None
+        assert ts.request_task() is None
+        assert ts.task_keeper.task_headers.get("uvw2") is None
 
     def test_send_results(self):
         ccd = ClientConfigDescriptor()
