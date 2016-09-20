cf6c5272686e2d026eec48509c9fb408210c7bab
Paul Peregud <paulperegud@gmail.com>
2016-09-20 16:44:09 +0200

    replace gosu with su-exec
    Check the checksum for downloaded binary.
    
    Reasons for su-exec - it's tiny! (and we can't use chroot since we drop
    capabilities before launching container)

--- gnr/task/images/entrypoint.sh
+++ gnr/task/images/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$LOCAL_USER_ID" != "" ]; then
     useradd --shell /bin/bash -u "$LOCAL_USER_ID" -o -c "" -m task
     export HOME=/home/task
-    exec /usr/local/bin/gosu task /bin/sh -c "/usr/bin/python $@"
+    exec /usr/local/bin/su-exec task /bin/sh -c "/usr/bin/python $@"
 else
     /usr/bin/python $@
 fi
