5ba0ed511411e22804bf8237763d92f22260a1d8
badb <aleksandra.skrzypczak@imapp.pl>
2016-11-30 19:10:58 +0100

    move tests

--- tests/apps/lux/test_luxenvironment.py
+++ tests/apps/lux/test_luxenvironment.py
@@ -1,27 +1,10 @@
 import unittest
 
-from apps.blender.blenderenvironment import BlenderEnvironment
 from apps.lux.luxenvironment import LuxRenderEnvironment
 from golem.clientconfigdescriptor import ClientConfigDescriptor
 
 
-class BlenderEnvTest(unittest.TestCase):
-
-    def test_blender(self):
-        env = BlenderEnvironment()
-        assert bool(env.supported()) == bool(env.check_software())
-        
-    def test_get_performance(self):
-        env = BlenderEnvironment()
-        perf = 2345.2
-        cfg_desc = ClientConfigDescriptor()
-        cfg_desc.estimated_blender_performance = perf
-        result = env.get_performance(cfg_desc)
-        self.assertTrue(result == perf)
-        
-
 class TestLuxRenderEnvironment(unittest.TestCase):
-
     def test_lux(self):
         env = LuxRenderEnvironment()
         self.assertIsInstance(env, LuxRenderEnvironment)
