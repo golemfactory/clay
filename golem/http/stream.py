d49705b5c11390f7b28e7a2be845f90062babc09
Adam Banasiak <banasiakadam60@gmail.com>
2016-12-13 16:59:48 +0100

    Add error messages to the empty try-except blocks
    Signed-off-by: Adam Banasiak <banasiakadam60@gmail.com>

--- golem/http/stream.py
+++ golem/http/stream.py
@@ -305,8 +305,10 @@ class ChunkStream:
             # read remaining data
             try:
                 self.__read(drain=True)
-            except:
-                pass
+            except StopIteration as ste:
+                logger.debug("Cannot read remaining data: {}".format(ste.message))
+            except socket.error:
+                logger.debug("Socket error")
             logger.debug("Disconnecting socket: closing socket")
             # dispose of the socket
             self.sock.close()
