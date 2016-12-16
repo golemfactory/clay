a0f54c8da58e764feac8b5581763ec8bacaf561f
Adam Banasiak <banasiakadam60@gmail.com>
2016-12-16 17:52:00 +0100

    Make padding function even better
    Signed-off-by: Adam Banasiak <banasiakadam60@gmail.com>

--- golem/ethereum/client.py
+++ golem/ethereum/client.py
@@ -181,8 +181,10 @@ class Client(object):
         :return: Padded address
         """
         from eth_abi.utils import zpad
-        if address is None or address.startswith('0x'):
+        if address is None:
             return address
-        if not isinstance(address, basestring):
-            raise TypeError('Address must be a string')
-        return '0x' + zpad(address, 32)
+        elif isinstance(address, basestring):
+            if address.startswith('0x'):
+                return address
+            return '0x' + zpad(address, 32)
+        raise TypeError('Address must be a string')
