4ac16967e613023964adcd48c7b2154192f4d491
grzegorz <borowik.grzegorz@gmail.com>
2017-01-03 17:53:32 +0100

    trust manager four methods added

--- golem/ranking/manager/trust_manager.py
+++ golem/ranking/manager/trust_manager.py
@@ -1,8 +1,11 @@
 from golem.ranking.helper.min_max_utility import count_trust
-from golem.ranking.helper.trust_const import UNKNOWN_TRUST
+from golem.ranking.helper.trust_const import UNKNOWN_TRUST, NEIGHBOUR_WEIGHT_BASE, NEIGHBOUR_WEIGHT_POWER
 from golem.ranking.manager.database_manager import get_neighbour_loc_rank, get_local_rank
 
 
+############
+# computed #
+############
 def __computed_trust_positive(rank):
     return rank.positive_computed
 
@@ -20,11 +23,31 @@ def computed_node_trust_local(node_id):
     return computed_trust_local(get_local_rank(node_id))
 
 
-def computed_neighbour_trust_local(neighbour, about):
+def __computed_neighbour_trust_local(neighbour, about):
     rank = get_neighbour_loc_rank(neighbour, about)
     return rank.computing_trust_value if rank is not None else UNKNOWN_TRUST
 
 
+def __computed_neighbour_weight(node_id):
+    local_trust = computed_node_trust_local(node_id)
+    return NEIGHBOUR_WEIGHT_BASE ** (NEIGHBOUR_WEIGHT_POWER * local_trust) \
+        if local_trust is not None else NEIGHBOUR_WEIGHT_BASE ** (NEIGHBOUR_WEIGHT_POWER * UNKNOWN_TRUST)
+
+
+def computed_neighbours_rank(node_id, neighbours):
+    sum_weight = 0.0
+    sum_trust = 0.0
+    for neighbour in [x for x in neighbours if x != node_id]:
+        trust = __computed_neighbour_trust_local(neighbour, node_id)
+        weight = __requested_neighbour_weight(neighbour)
+        sum_trust += (weight - 1) * trust
+        sum_weight += weight
+    return sum_trust, sum_weight
+
+
+#############
+# requested #
+#############
 def __requested_trust_positive(rank):
     return rank.positive_payment
 
@@ -42,6 +65,23 @@ def requested_node_trust_local(node_id):
     return requested_trust_local(get_local_rank(node_id))
 
 
-def requested_neighbour_trust_local(neighbour, about):
+def __requested_neighbour_trust_local(neighbour, about):
     rank = get_neighbour_loc_rank(neighbour, about)
     return rank.requesting_trust_value if rank is not None else UNKNOWN_TRUST
+
+
+def __requested_neighbour_weight(node_id):
+    local_trust = requested_node_trust_local(node_id)
+    return NEIGHBOUR_WEIGHT_BASE ** (NEIGHBOUR_WEIGHT_POWER * local_trust) \
+        if local_trust is not None else NEIGHBOUR_WEIGHT_BASE ** (NEIGHBOUR_WEIGHT_POWER * UNKNOWN_TRUST)
+
+
+def requested_neighbours_rank(node_id, neighbours):
+    sum_weight = 0.0
+    sum_trust = 0.0
+    for neighbour in [x for x in neighbours if x != node_id]:
+        trust = __requested_neighbour_trust_local(neighbour, node_id)
+        weight = __computed_neighbour_weight(neighbour)
+        sum_trust += (weight - 1) * trust
+        sum_weight += weight
+    return sum_trust, sum_weight
