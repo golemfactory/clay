4a5714d2da88df385244f3eeeea3b07acd6eb920
Dariusz Rybi <dariusz.rybi@imapp.pl>
2017-01-05 14:05:59 +0100

    Removed SIGINT handler.
    SIGINT is already handled by Twisted Resctor.

--- golemapp.py
+++ golemapp.py
@@ -1,7 +1,6 @@
 from multiprocessing import freeze_support
 
 import click
-import signal
 import sys
 
 from golem.core.common import config_logging
@@ -42,11 +41,6 @@ def start(gui, payments, datadir, node_address, rpc_address, peer, task, multipr
     if rpc_address:
         config['rpc_address'] = rpc_address.address
         config['rpc_port'] = rpc_address.port
-
-    def sigint_handler(signum, frame):
-        print(u"SIGINT received. Attempting graceful shutdown.")
-        sys.exit()
-    signal.signal(signal.SIGINT, sigint_handler)
 
     # Crossbar
     if m == 'crossbar.worker.process':
