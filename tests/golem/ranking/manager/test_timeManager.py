500b946a0017b7f7ae8fceb4b33f136db5f877fc
grzegorz <borowik.grzegorz@gmail.com>
2017-01-09 11:00:22 +0100

    assert replaced

--- tests/golem/ranking/manager/test_timeManager.py
+++ tests/golem/ranking/manager/test_timeManager.py
@@ -9,28 +9,28 @@ class TestTimeManager(TestCase):
     @patch("golem.ranking.manager.time_manager.time")
     def test_oracle(self, mock_time):
         oracle = TimeManager(200, 50, 110, 1000)
-        assert oracle.break_time == 200
-        assert oracle.round_time == 50
-        assert oracle.end_round_time == 110
-        assert oracle.stage_time == 1000
+        self.assertEqual(oracle.break_time, 200)
+        self.assertEqual(oracle.round_time, 50)
+        self.assertEqual(oracle.end_round_time, 110)
+        self.assertEqual(oracle.stage_time, 1000)
 
         # During round
         mock_time.time.return_value = 1475850990.931
-        assert int(oracle.sec_to_round()) == 329
-        assert int(oracle.sec_to_end_round()) == 19
-        assert int(oracle.sec_to_break()) == 129
-        assert int(oracle.sec_to_new_stage()) == 9
+        self.assertAlmostEqual(oracle.sec_to_round(), 329, 0)
+        self.assertAlmostEqual(oracle.sec_to_end_round(), 19, 0)
+        self.assertAlmostEqual(oracle.sec_to_break(), 129, 0)
+        self.assertAlmostEqual(oracle.sec_to_new_stage(), 9, 0)
 
         # During end round
         mock_time.time.return_value = 1475851010.931
-        assert int(oracle.sec_to_round()) == 309
-        assert int(oracle.sec_to_end_round()) == 359
-        assert int(oracle.sec_to_break()) == 109
-        assert int(oracle.sec_to_new_stage()) == 989
+        self.assertAlmostEqual(oracle.sec_to_round(), 309, 0)
+        self.assertAlmostEqual(oracle.sec_to_end_round(), 359, 0)
+        self.assertAlmostEqual(oracle.sec_to_break(), 109, 0)
+        self.assertAlmostEqual(oracle.sec_to_new_stage(), 989, 0)
 
         # During break
         mock_time.time.return_value = 1475851140.931
-        assert int(oracle.sec_to_round()) == 179
-        assert int(oracle.sec_to_end_round()) == 229
-        assert int(oracle.sec_to_break()) == 339
-        assert int(oracle.sec_to_new_stage()) == 859
+        self.assertAlmostEqual(oracle.sec_to_round(), 179, 0)
+        self.assertAlmostEqual(oracle.sec_to_end_round(), 229, 0)
+        self.assertAlmostEqual(oracle.sec_to_break(), 339, 0)
+        self.assertAlmostEqual(oracle.sec_to_new_stage(), 859, 0)
