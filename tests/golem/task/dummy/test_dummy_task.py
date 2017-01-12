3a3be5060505a102665b885da2e22adb60213958
Marek Franciszkiewicz <marek.franciszkiewicz@imapp.pl>
2017-01-12 16:40:16 +0100

    DummyTask test reverted to improved assignment and increased timeout

--- tests/golem/task/dummy/test_dummy_task.py
+++ tests/golem/task/dummy/test_dummy_task.py
@@ -1,6 +1,5 @@
-import unittest
-
 import runner
+import unittest
 
 
 class TestDummyTask(unittest.TestCase):
@@ -8,16 +7,16 @@ class TestDummyTask(unittest.TestCase):
 
     def test_dummy_task_computation(self, *mocks):
         error_msg = runner.run_simulation(
-            num_computing_nodes=2, num_subtasks=3, timeout=300)
-        assert error_msg.startswith("Computation finished")
+            num_computing_nodes=2, num_subtasks=3, timeout=420)
+        self.assertIsNone(error_msg)
 
     def test_dummy_task_computation_timeout(self, *mocks):
         error_msg = runner.run_simulation(timeout=5)
-        assert error_msg.startswith("Computation timed out")
+        self.assertEqual(error_msg, "Computation timed out")
 
     def test_dummy_task_computation_subprocess_error(self, *mocks):
         # Make the first computing node fail after approx. 5 secs
         error_msg = runner.run_simulation(
             num_computing_nodes=2, num_subtasks=10, timeout=120,
             node_failure_times=[5])
-        assert error_msg.startswith("Node failure")
+        self.assertTrue(error_msg.startswith("Node exited with return code"))
