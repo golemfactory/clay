c59e70e279a82553b2bd208f6f4266f4a3cd9d52
badb <aleksandra.skrzypczak@imapp.pl>
2017-01-16 17:55:06 +0100

    remove wrong import after rebase

--- tests/apps/lux/task/test_luxrendertask.py
+++ tests/apps/lux/task/test_luxrendertask.py
@@ -8,7 +8,6 @@ from golem.tools.assertlogs import LogTestCase
 from golem.task.taskbase import ComputeTaskDef
 
 from apps.lux.task.luxrendertask import LuxRenderDefaults, LuxRenderTaskBuilder, LuxRenderOptions, logger
-from apps.rendering.task.renderingtask import AcceptClientVerdict
 
 from apps.rendering.task.renderingtaskstate import RenderingTaskDefinition
 
