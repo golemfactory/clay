67fd9549d19b19d7d99243d0b9c64a3e365ef616
grzegorz <borowik.grzegorz@gmail.com>
2017-01-16 17:53:18 +0100

    doc strings update

--- tests/golem/ranking/helper/test_min_max_utility.py
+++ tests/golem/ranking/helper/test_min_max_utility.py
@@ -6,6 +6,8 @@ from golem.ranking.helper.trust_const import MIN_TRUST, MAX_TRUST
 
 class TestMinMaxUtility(TestCase):
     def test_should_count_trust(self):
+        """Should calculate trust for various combinations of pos and neg using formula
+        min(MAX_TRUST, max(MIN_TRUST, (pos * POS_WEIGHT - neg * NEG_WEIGHT) / max(pos + neg, MIN_OPERATION_NUMBER)))."""
         pos = -1.3
         neg = 1.3
         self.assertEqual(count_trust(pos, neg), min(MAX_TRUST, max(MIN_TRUST, (pos * POS_WEIGHT - neg * NEG_WEIGHT)
@@ -22,11 +24,15 @@ class TestMinMaxUtility(TestCase):
         self.assertAlmostEqual(0.102, count_trust(5.1, 0.0), 7)
 
     def test_should_count_trust_throw_exception(self):
+        """Should throw exception for non float parameter values."""
         with self.assertRaises(TypeError):
             count_trust("alpha", 0.5)
             count_trust("alpha", "beta")
 
     def test_should_convert_vec_to_trust(self):
+        """Should convert two dimensional vector (a,b) to trust_value specified by formula:
+            min(MAX_TRUST, max(MIN_TRUST, float(a) / float(b)))
+        """
         vec = None
         self.assertEqual(0.0, vec_to_trust(vec))
 
@@ -45,6 +51,7 @@ class TestMinMaxUtility(TestCase):
         self.assertEqual(0.0, vec_to_trust(vec))
 
     def test_should_vec_to_trust_throw_exception(self):
+        """Should throw exception for non float parameter values."""
         vec = "beta", "alpha"
         self.assertEqual(None, vec_to_trust(vec))
 
