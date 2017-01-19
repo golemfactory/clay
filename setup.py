8d074aa00205ef1596a9c34401ac1e76bcb3b02e
badb <aleksandra.skrzypczak@imapp.pl>
2017-01-19 15:25:00 +0100

    write ignored errors at the end of setup.py

--- setup.py
+++ setup.py
@@ -22,20 +22,34 @@ except ImportError:
 from gui.view.generateui import generate_ui_files
 
 
-generate_ui_files()
+ui_err = ""
+
+try:
+    generate_ui_files()
+except EnvironmentError as err:
+    ui_err = \
+            """
+            ***************************************************************
+            Generating UI elements was not possible.
+            Golem will work only in command line mode
+            Generate_ui_files function returned {}
+            ***************************************************************
+            """.format(err)
 
 
 def try_building_docker_images():
     try:
         subprocess.check_call(["docker", "info"])
     except Exception as err:
-        print("""
-              ***************************************************************"
-              Docker not available, not building images."
-              Command 'docker info' returned {}"
-              ***************************************************************"
-              """.format(err))
-        return
+        return \
+              """
+              ***************************************************************
+              Docker not available, not building images.
+              Golem will not be able to compute anything.
+              Command 'docker info' returned {}
+              ***************************************************************
+              """.format(err)
+
     images_dir = 'apps'
     cwd = os.getcwdu()
 
@@ -65,7 +79,7 @@ def try_building_docker_images():
             finally:
                 os.chdir(cwd)
 
-try_building_docker_images()
+docker_err = try_building_docker_images()
 
 
 class PyTest(TestCommand):
@@ -184,3 +198,12 @@ setup(
     test_suite='tests',
     tests_require=test_requirements
 )
+
+
+def print_errors(ui_err, docker_err):
+    if ui_err:
+        print(ui_err)
+    if docker_err:
+        print(docker_err)
+
+print_errors(ui_err, docker_err)
