f8b1503406c3af2b8572d6069f21a003bd37e62a
Dariusz Rybi <dariusz.rybi@imapp.pl>
2017-02-01 11:08:56 +0100

    Monitor: send task time spent

--- golem/monitor/monitor.py
+++ golem/monitor/monitor.py
@@ -9,6 +9,7 @@ from model.loginlogoutmodel import LoginModel, LogoutModel
 from model.statssnapshotmodel import StatsSnapshotModel, VMSnapshotModel, P2PSnapshotModel
 from model.taskcomputersnapshotmodel import TaskComputerSnapshotModel
 from model.paymentmodel import ExpenditureModel, IncomeModel
+from model.statssnapshotmodel import ComputationTime
 from transport.sender import DefaultJSONSender as Sender
 
 log = logging.getLogger('golem.monitor')
@@ -28,7 +29,6 @@ class SenderThread(threading.Thread):
         self.queue.put(o)
 
     def run(self):
-
         while not self.stop_request.isSet():
             try:
                 msg = self.queue.get(True, self.monitor_sender_thread_timeout)
@@ -51,17 +51,17 @@ class SystemMonitor(object):
         self.meta_data = meta_data
         self.node_info = NodeInfoModel(meta_data.cliid, meta_data.sessid)
         self.config = monitor_config
-        self.queue = None
-        self.sender_thread = None
+
+        self.sender_thread = self.create_sender_thread()
 
         dispatcher.connect(self.dispatch_listener, signal='golem.monitor')
 
-    # Private interface
-
-    def _send_with_args(self, obj_type, *args):
-        log.debug('_send_with_args(%r, %r)', obj_type, args)
-        obj = obj_type(*args)
-        return self.sender_thread.send(obj)
+    def create_sender_thread(self):
+        host = self.config['HOST']
+        request_timeout = self.config['REQUEST_TIMEOUT']
+        sender_thread_timeout = self.config['SENDER_THREAD_TIMEOUT']
+        proto_ver = self.config['PROTO_VERSION']
+        return SenderThread(self.node_info, host, request_timeout, sender_thread_timeout, proto_ver)
 
     @log_error()
     def dispatch_listener(self, sender, signal, event='default', **kwargs):
@@ -75,12 +75,6 @@ class SystemMonitor(object):
     # Initialization
 
     def start(self):
-        host = self.config['HOST']
-        request_timeout = self.config['REQUEST_TIMEOUT']
-        sender_thread_timeout = self.config['SENDER_THREAD_TIMEOUT']
-        proto_ver = self.config['PROTO_VERSION']
-
-        self.sender_thread = SenderThread(self.node_info, host, request_timeout, sender_thread_timeout, proto_ver)
         self.sender_thread.start()
 
     def shut_down(self):
@@ -89,32 +83,33 @@ class SystemMonitor(object):
     # Public interface
 
     def on_login(self):
-        self._send_with_args(LoginModel, self.meta_data)
+        self.sender_thread.send(LoginModel(self.meta_data))
 
     def on_config_update(self, meta_data):
         self.meta_data = meta_data
         self.node_info = NodeInfoModel(meta_data.cliid, meta_data.sessid)
-        return self._send_with_args(LoginModel, self.meta_data)
+        self.sender_thread.send(LoginModel(self.meta_data))
+
+    def on_computation_time_spent(self, success, value):
+        self.sender_thread.send(ComputationTime(self.meta_data, success, value))
 
     def on_logout(self):
-        return self._send_with_args(LogoutModel, self.meta_data)
+        self.sender_thread.send(LogoutModel(self.meta_data))
 
     def on_stats_snapshot(self, known_tasks, supported_tasks, computed_tasks, tasks_with_errors, tasks_with_timeout):
-        return self._send_with_args(StatsSnapshotModel, self.meta_data.cliid, self.meta_data.sessid, known_tasks,
-                                    supported_tasks, computed_tasks, tasks_with_errors, tasks_with_timeout)
+        self.sender_thread.send(StatsSnapshotModel(self.meta_data.cliid, self.meta_data.sessid, known_tasks, supported_tasks, computed_tasks, tasks_with_errors, tasks_with_timeout))
 
     def on_vm_snapshot(self, vm_data):
-        return self._send_with_args(VMSnapshotModel, self.meta_data.cliid, self.meta_data.sessid, vm_data)
+        self.sender_thread.send(VMSnapshotModel(self.meta_data.cliid, self.meta_data.sessid, vm_data))
 
     def on_peer_snapshot(self, p2p_data):
-        return self._send_with_args(P2PSnapshotModel, self.meta_data.cliid, self.meta_data.sessid, p2p_data)
+        self.sender_thread.send(P2PSnapshotModel(self.meta_data.cliid, self.meta_data.sessid, p2p_data))
 
     def on_task_computer_snapshot(self, waiting_for_task, counting_task, task_requested, comput_task, assigned_subtasks):
-        return self._send_with_args(TaskComputerSnapshotModel, self.meta_data.cliid, self.meta_data.sessid,
-                                    waiting_for_task, counting_task, task_requested, comput_task, assigned_subtasks)
+        self.sender_thread.send(TaskComputerSnapshotModel(self.meta_data.cliid, self.meta_data.sessid, waiting_for_task, counting_task, task_requested, comput_task, assigned_subtasks))
 
     def on_payment(self, addr, value):
-        return self._send_with_args(ExpenditureModel, self.meta_data.cliid, self.meta_data.sessid, addr, value)
+        self.sender_thread.send(ExpenditureModel(self.meta_data.cliid, self.meta_data.sessid, addr, value))
 
     def on_income(self, addr, value):
-        return self._send_with_args(IncomeModel, self.meta_data.cliid, self.meta_data.sessid, addr, value)
+        self.sender_thread.send(IncomeModel(self.meta_data.cliid, self.meta_data.sessid, addr, value))
