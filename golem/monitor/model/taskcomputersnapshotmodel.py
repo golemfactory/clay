f8c7ec7aa150682aac9fbaa73ef49be41ad5cd39
Dariusz Rybi <dariusz.rybi@imapp.pl>
2017-02-03 12:46:48 +0100

    Typo fix

--- golem/monitor/model/taskcomputersnapshotmodel.py
+++ golem/monitor/model/taskcomputersnapshotmodel.py
@@ -9,5 +9,5 @@ class TaskComputerSnapshotModel(BasicModel):
         self.waiting_for_task = task_computer.waiting_for_task
         self.counting_task = task_computer.counting_task
         self.task_requested = task_computer.task_requested
-        self.compute_task = task_computer.compute_task
+        self.compute_tasks = task_computer.compute_task
         self.assigned_subtasks = task_computer.assigned_subtasks.keys()
