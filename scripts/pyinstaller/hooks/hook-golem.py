654a130de8089260e69108c1056740a4f70f5e49
Marek Franciszkiewicz <marek.franciszkiewicz@imapp.pl>
2017-03-09 13:54:48 +0100

    Pyinstaller golem-hook updated

--- scripts/pyinstaller/hooks/hook-golem.py
+++ scripts/pyinstaller/hooks/hook-golem.py
@@ -5,7 +5,7 @@ hiddenimports = collect_submodules('golem') + \
                 collect_submodules('apps')
 
 datas = collect_data_files('gui') + [
-    ('logging.ini', '.'),
+    ('loggingconfig.py', '.'),
     ('gui/view/*', 'gui/view'),
     ('apps/*.ini', 'apps'),
     ('apps/blender/resources/scripts/blendercrop.py.template', 'apps/blender/resources/scripts/'),
