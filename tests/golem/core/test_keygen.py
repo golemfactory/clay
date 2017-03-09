00cb60c3ebf58292a49abcc7fb8f53bba54f10c1
grzegorz <borowik.grzegorz@gmail.com>
2017-03-09 17:04:05 +0100

    test vectors changed sha3_256->keccak_256

--- tests/golem/core/test_keygen.py
+++ tests/golem/core/test_keygen.py
@@ -3,46 +3,50 @@ from unittest import TestCase
 from golem.core.keysauth import mk_privkey, privtopub
 
 
+def key_in_hex_with_colon_sep(key):
+    return ':'.join(x.encode('hex') for x in key)
+
+
 def key_in_hex(key):
-    return ''.join(x.encode('hex') for x in key)
+    return key.encode('hex')
 
 
 class TestKeygenHelper(TestCase):
-    """First three test vectors from http://www.di-mgt.com.au/sha_testvectors.html"""
+    """Test vectors for keccak"""
     def test_mk_privkey(self):
         key = key_in_hex(mk_privkey(str("")))
-        self.assertEqual(key, "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a")
+        self.assertEqual(key, "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470")
 
         key = key_in_hex(mk_privkey(str("abc")))
-        self.assertEqual(key, "3a985da74fe225b2045c172d6bd390bd855f086e3e9d525b46bfe24511431532")
+        self.assertEqual(key, "4e03657aea45a94fc7d47ba826c8d667c0d1e6e33a64a036ec44f58fa12d6c45")
 
         key = key_in_hex(mk_privkey(str("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq")))
-        self.assertEqual(key, "41c0dba2a9d6240849100376a8235e2c82e1b9998a999e21db32dd97496d3376")
+        self.assertEqual(key, "45d3b367a6904e6e8d502ee04999a7c27647f91fa845d456525fd352ae3d7371")
 
         key = key_in_hex(mk_privkey(str("ala")))
-        self.assertEqual(key, "dd8a216367a5616f25cef9dc48eda86d6ea68cb59f9f0df69af34914e3ce7750")
+        self.assertEqual(key, "f5e06954a3a7c1ccaed8675b3fb429e23a839b5b406a72432bd6532dcd400d18")
 
         key = key_in_hex(mk_privkey(str("102")))
-        self.assertEqual(key, "3672ce6ff46ae74b281fcb27881affd18e54e3efacc11031364b69416c9264b1")
+        self.assertEqual(key, "2a9063ed52b7d417e441f378325359c9ce274f5d6c3ecf11b7d42f24b9c90b7f")
 
         key = key_in_hex(mk_privkey(str("-4")))
-        self.assertEqual(key, "738d11181b63c51590e9b6c74266b803094ef05436748edf0b265c757d77a576")
+        self.assertEqual(key, "702525ac629be223a9df40afdd83ad491268dd60af4e6703d2f71ef8a0b52408")
 
         key = key_in_hex(mk_privkey(str("0.234")))
-        self.assertEqual(key, "6fad32c106f730f4b483d2d357525d6379b4ebadae1e00061a95abc5b37ae678")
+        self.assertEqual(key, "2eac655c86354907b5a79706be2803d449fa3da2c66ee9cc0ee0e1b00e69f9da")
 
     def test_privtopub(self):
         key = key_in_hex(privtopub(mk_privkey(str(""))))
-        self.assertEqual(key, "c988c298ec514516121176a5117c8a4f9692914b0e197d2d425d733f83e41b891ed9b82459328c1ab7e3c20507c86e8bead9320b950c565609f860ebb5377088")
+        self.assertEqual(key, "a63a07e888061ec9e8b64a3dc2937805c76089af36459305920373cd98a9f4ce15c27dbbe60928161eb62ae19f94ea48f399ce85e6db698520f3bcd4a9257157")
 
         key = key_in_hex(privtopub(mk_privkey(str("ala"))))
-        self.assertEqual(key, "88aaea31f42c37a429ba5a7a522d0a006fb681ce20cdfc852f618ee680228a2ed0aeaa72e29c059d589ed08c6425c3d740056860f369c8be0e3c8b6970d17ab4")
+        self.assertEqual(key, "17c5529fc8eefc8689e466b93db2b84513db56813ce9a3404287633814d8b499fa4b667bb339d66d1cd724f4a3385ecbdb8c5038f5e04bfca6e6b4026fd7b9db")
 
         key = key_in_hex(privtopub(mk_privkey(str("102"))))
-        self.assertEqual(key, "78b9325c466c77f4ac350fee377503e70d49d2924a54bdeb214c3c6fdad4e2c80f160b69f23dc60e1eda4b30529ea11113b7a5c0331c87faa07c2984fa9ecaaa")
+        self.assertEqual(key, "1ec23d1ac518015777e4600bc617144b0d04f3d8f4f69c945d553a40362a661a5dee4fcf75cfa274f8bee984e8d517ef3d23fa6a03e29b48e430dc7c983b2a89")
 
         key = key_in_hex(privtopub(mk_privkey(str("-4"))))
-        self.assertEqual(key, "9d36a5fe87b28aaf8867a431672a4ff337f751ab74e8c9bf9bcbd5ee3354ab91749089aa4725b81bcd97d2e81dae3d74e1e01844387498c77d9f0d9b36b5c4c1")
+        self.assertEqual(key, "eaf8b04c5310fd1d9d614403d3450778f7bc2e141091499406963e02460a62edb8b25fc39a18fc840451fbeb83cf4a9d1570f158ab4d541d725bc135ed5e8c84")
 
         key = key_in_hex(privtopub(mk_privkey(str("0.234"))))
-        self.assertEqual(key, "2cb335f6f2a5262cbab585104737d6b156fb6e2073f191e435b6340fd6903bd5d3f19135db082843881e4096d75db4c35484a5fd8acc3085ca0884f5a45cfa64")
+        self.assertEqual(key, "20c9df380ad77338a517b6df725d04e46eaa6d52e83385df4ae249dcdb50bebc11e5e4fd303949e2caa55abf62fff7945a3d710b8ed1cd955798b571e598ccf9")
