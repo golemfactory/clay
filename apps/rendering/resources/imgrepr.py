469f89a6ef248fdd5113b069d3c3c2d0ff93385d
badb <aleksandra.skrzypczak@imapp.pl>
2017-03-15 18:41:56 +0100

    imgrepr refactoring & tests

--- apps/rendering/resources/imgrepr.py
+++ apps/rendering/resources/imgrepr.py
@@ -29,6 +29,10 @@ class ImgRepr(object):
     def get_size(self):
         return
 
+    @abc.abstractmethod
+    def copy(self):
+        return
+
 
 class PILImgRepr(ImgRepr):
     def __init__(self):
@@ -49,6 +53,9 @@ class PILImgRepr(ImgRepr):
         color = tuple(int(c) for c in color)
         self.img.putpixel((i, j), color)
 
+    def copy(self):
+        return deepcopy(self)
+
 
 class EXRImgRepr(ImgRepr):
     def __init__(self):
@@ -59,11 +66,15 @@ class EXRImgRepr(ImgRepr):
         self.rgb = None
         self.min = 0.0
         self.max = 1.0
+        self.file_path = None
 
     def load_from_file(self, file_):
         self.img = OpenEXR.InputFile(file_)
         self.dw = self.img.header()['dataWindow']
-        self.rgb = [Image.frombytes("F", self.get_size(), self.img.channel(c, self.pt)) for c in "RGB"]
+        self.rgb = [Image.frombytes("F", self.get_size(),
+                                    self.img.channel(c, self.pt))
+                    for c in "RGB"]
+        self.file_path = file_
 
     def get_size(self):
         return self.dw.max.x - self.dw.min.x + 1, self.dw.max.y - self.dw.min.y + 1
@@ -86,6 +97,15 @@ class EXRImgRepr(ImgRepr):
 
         rgb8 = [im.point(normalize_0_255).convert("L") for im in self.rgb]
         return Image.merge("RGB", rgb8)
+
+    def copy(self):
+        e = EXRImgRepr()
+        e.load_from_file(self.file_path)
+        e.dw = deepcopy(self.dw)
+        e.rgb = deepcopy(self.rgb)
+        e.min = self.min
+        e.max = self.max
+        return e
 
 
 def load_img(file_):
@@ -113,9 +133,10 @@ def advance_verify_img(file_, res_x, res_y, start_box, box_size, compare_file, c
         logger.error("Wrong box size for advanced verification {}".format(box_size))
 
     if isinstance(img, PILImgRepr) and isinstance(cmp_img, PILImgRepr):
-        return __compare_imgs(img, cmp_img, start1=start_box, start2=cmp_start_box, box=box_size)
+        return compare_imgs(img, cmp_img, start1=start_box, start2=cmp_start_box, box=box_size)
     else:
-        return __compare_imgs(img, cmp_img, max_col=1, start1=start_box, start2=cmp_start_box, box=box_size)
+        return compare_imgs(img, cmp_img, max_col=1, start1=start_box, start2=cmp_start_box,
+                            box=box_size)
 
 
 def verify_img(file_, res_x, res_y):
@@ -133,7 +154,7 @@ def compare_pil_imgs(file1, file2):
         img1.load_from_file(file1)
         img2 = PILImgRepr()
         img2.load_from_file(file2)
-        return __compare_imgs(img1, img2)
+        return compare_imgs(img1, img2)
     except Exception as err:
         logger.info("Can't compare images {}, {}: {}".format(file1, file2, err))
         return False
@@ -145,7 +166,7 @@ def compare_exr_imgs(file1, file2):
         img1.load_from_file(file1)
         img2 = EXRImgRepr()
         img2.load_from_file(file2)
-        return __compare_imgs(img1, img2, 1)
+        return compare_imgs(img1, img2, 1)
     except Exception as err:
         logger.info("Can't compare images {}, {}: {}".format(file1, file2, err))
         return False
@@ -157,13 +178,13 @@ def blend(img1, img2, alpha):
         logger.error("Both images must have the same size.")
         return
 
-    img = deepcopy(img1)
+    img = img1.copy()
 
     for x in range(0, res_x):
         for y in range(0, res_y):
             p1 = img1.get_pixel((x, y))
             p2 = img2.get_pixel((x, y))
-            p = map(lambda x, y: x * (1 - alpha) + y * alpha, p1, p2)
+            p = map(lambda c1, c2: c1 * (1 - alpha) + c2 * alpha, p1, p2)
             img.set_pixel((x, y), p)
 
     return img
@@ -172,22 +193,27 @@ def blend(img1, img2, alpha):
 PSNR_ACCEPTABLE_MIN = 30
 
 
-def __compare_imgs(img1, img2, max_col=255, start1=(0, 0), start2=(0, 0), box=None):
-    mse = __count_mse(img1, img2, start1, start2, box)
+def compare_imgs(img1, img2, max_col=255, start1=(0, 0), start2=(0, 0), box=None):
+    mse = count_mse(img1, img2, start1, start2, box)
     logger.debug("MSE = {}".format(mse))
     if mse == 0:
         return True
-    psnr = __count_psnr(mse, max_col)
+    psnr = count_psnr(mse, max_col)
     logger.debug("PSNR = {}".format(psnr))
     return psnr >= PSNR_ACCEPTABLE_MIN
 
 
-def __count_psnr(mse, max=255):
-    return 20 * math.log10(max) - 10 * math.log10(mse)
+def count_psnr(mse, max_=255):
+    if mse <= 0 or max_ <= 0:
+        raise ValueError("MSE & MAX_ must be higher than 0")
+    return 20 * math.log10(max_) - 10 * math.log10(mse)
 
 
-def __count_mse(img1, img2, start1=(0, 0), start2=(0, 0), box=None):
+def count_mse(img1, img2, start1=(0, 0), start2=(0, 0), box=None):
     mse = 0
+    if not isinstance(img1, ImgRepr) or not isinstance(img2, ImgRepr):
+        raise TypeError("img1 and img2 must be ImgRepr")
+
     if box is None:
         (res_x, res_y) = img1.get_size()
     else:
@@ -198,5 +224,7 @@ def __count_mse(img1, img2, start1=(0, 0), start2=(0, 0), box=None):
             [r2, g2, b2] = img2.get_pixel((start2[0] + i, start2[1] + j))
             mse += (r1 - r2) * (r1 - r2) + (g1 - g2) * (g1 - g2) + (b1 - b2) * (b1 - b2)
 
+    if res_x <= 0 or res_y <= 0:
+        raise ValueError("Image or box resolution must be greater than 0")
     mse /= res_x * res_y * 3
     return mse
--- tests/apps/rendering/resources/test_imgrepr.py
+++ tests/apps/rendering/resources/test_imgrepr.py
@@ -7,8 +7,9 @@ from PIL import Image
 from golem.testutils import TempDirFixture
 from golem.tools.assertlogs import LogTestCase
 
-from apps.rendering.resources.imgrepr import (blend, EXRImgRepr, ImgRepr, load_img, logger,
-                                              PILImgRepr, verify_img)
+from apps.rendering.resources.imgrepr import (blend, compare_exr_imgs, compare_imgs,
+                                              compare_pil_imgs, count_mse, count_psnr, EXRImgRepr,
+                                              ImgRepr, load_img, logger, PILImgRepr, verify_img)
 
 
 class TImgRepr(ImgRepr):
@@ -25,6 +26,9 @@ class TImgRepr(ImgRepr):
     def set_pixel(self, (i, j), color):
         super(TImgRepr, self).set_pixel((i, j), color)
 
+    def copy(self):
+        super(TImgRepr, self).copy()
+
 
 class TestImgRepr(unittest.TestCase):
 
@@ -33,12 +37,20 @@ class TestImgRepr(unittest.TestCase):
         t.load_from_file("file_")
         t.get_pixel((0, 0))
         t.get_size()
+        t.copy()
 
 
 def make_test_img(img_path, size=(10, 10), color=(255, 0, 0)):
     img = Image.new('RGB', size, color)
     img.save(img_path)
     img.close()
+
+
+def get_pil_img_repr(path, size=(10, 10), color=(255, 0, 0)):
+    make_test_img(path, size, color)
+    p = PILImgRepr()
+    p.load_from_file(path)
+    return p
 
 
 class TestPILImgRepr(TempDirFixture):
@@ -60,11 +72,7 @@ class TestPILImgRepr(TempDirFixture):
 
     def test_pil_repr(self):
         img_path = self.temp_file_name('img.png')
-        make_test_img(img_path)
-
-        p = PILImgRepr()
-
-        p.load_from_file(img_path)
+        p = get_pil_img_repr(img_path)
         assert isinstance(p.img, Image.Image)
         assert p.get_size() == (10, 10)
         assert p.get_pixel((0, 0)) == [255, 0, 0]
@@ -73,8 +81,15 @@ class TestPILImgRepr(TempDirFixture):
         with self.assertRaises(Exception):
             p.get_pixel((10, 10))
 
+        p_copy = p.copy()
+
         p.set_pixel((3, 5), [10, 11, 12])
         assert p.get_pixel((3, 5)) == [10, 11, 12]
+        assert p_copy.get_pixel((3, 5)) == [255, 0, 0]
+
+        p_copy.set_pixel((5, 3), [200, 210, 220])
+        assert p_copy.get_pixel((5, 3)) == [200, 210, 220]
+        assert p.get_pixel((5, 3)) == [255, 0, 0]
 
 
 def _get_test_exr(alt=False):
@@ -84,6 +99,22 @@ def _get_test_exr(alt=False):
         filename = 'testfile2.EXR'
 
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+
+
+def get_exr_img_repr(alt=False):
+    exr = EXRImgRepr()
+    exr_file = _get_test_exr(alt)
+    exr.load_from_file(exr_file)
+    return exr
+
+
+def almost_equal(v1, v2):
+    assert abs(v1 - v2) < 0.001
+
+
+def almost_equal_pixels(pix1, pix2):
+    for c1, c2 in zip(pix1, pix2):
+        almost_equal(c1, c2)
 
 
 class TestExrImgRepr(TempDirFixture):
@@ -97,6 +128,7 @@ class TestExrImgRepr(TempDirFixture):
         assert img.rgb is None
         assert img.min == 0.0
         assert img.max == 1.0
+        assert img.file_path is None
 
     def test_errors(self):
         e = EXRImgRepr()
@@ -109,9 +141,7 @@ class TestExrImgRepr(TempDirFixture):
             e.get_pixel((0, 0))
 
     def test_exr_repr(self):
-        e = EXRImgRepr()
-        img_file = _get_test_exr()
-        e.load_from_file(img_file)
+        e = get_exr_img_repr()
         assert e.img is not None
         assert e.dw is not None
         assert e.rgb is not None
@@ -126,37 +156,33 @@ class TestExrImgRepr(TempDirFixture):
             e.get_pixel((10, 10))
 
     def test_set_pixel(self):
-        e = EXRImgRepr()
-        img_file = _get_test_exr()
-        e.load_from_file(img_file)
+        e = get_exr_img_repr()
+
         val1 = [0.4, 0.3, 0.2]
         e.set_pixel((0, 0), val1)
         val2 = [0.1, 0.131, 0.001]
         e.set_pixel((4, 4), val2)
 
-        def almost_compare(v1, v2):
-            assert abs(v1 - v2) < 0.001
+        almost_equal_pixels(e.get_pixel((0, 0)), val1)
+        almost_equal_pixels(e.get_pixel((4, 4)), val2)
 
-        def almost_compare_pixels(pix1, pix2):
-            for c1, c2 in zip(pix1, pix2):
-                almost_compare(c1, c2)
-
-        almost_compare_pixels(e.get_pixel((0, 0)), val1)
-        almost_compare_pixels(e.get_pixel((4, 4)), val2)
+        e_copy = e.copy()
 
         e.min = 0.5
         e.set_pixel((0, 0), val1)
-        almost_compare_pixels(e.get_pixel((0, 0)), [0.5, 0.5, 0.5])
+        almost_equal_pixels(e.get_pixel((0, 0)), [0.5, 0.5, 0.5])
 
         e.max = 0.8
         val3 = [0.1, 0.6, 0.9]
         e.set_pixel((0, 0), val3)
-        almost_compare_pixels(e.get_pixel((0, 0)), [0.5, 0.6, 0.8])
+        almost_equal_pixels(e.get_pixel((0, 0)), [0.5, 0.6, 0.8])
+
+        almost_equal_pixels(e_copy.get_pixel((0, 0)), val1)
+        assert e_copy.min == 0.0
+        assert e_copy.max == 1.0
 
     def test_to_pil(self):
-        e = EXRImgRepr()
-        img_file = _get_test_exr()
-        e.load_from_file(img_file)
+        e = get_exr_img_repr()
 
         img = e.to_pil()
         img_file = os.path.join(self.path, "img1.jpg")
@@ -184,9 +210,7 @@ class TestImgFunctions(TempDirFixture, LogTestCase):
         assert isinstance(exr_img, EXRImgRepr)
         assert exr_img.get_size() == (10, 10)
 
-        img_path = self.temp_file_name("img.jpg")
-        make_test_img(img_path)
-        pil_img = load_img(img_path)
+        pil_img = get_pil_img_repr(img_path)
         assert isinstance(pil_img, PILImgRepr)
         assert pil_img.get_size() == (10, 10)
 
@@ -202,6 +226,7 @@ class TestImgFunctions(TempDirFixture, LogTestCase):
         img2 = PILImgRepr()
         img1.load_from_file(img_path1)
         img2.load_from_file(img_path2)
+        img1 = get_pil_img_repr(img_path1)
 
         img = blend(img1, img2, 0.5)
         assert isinstance(img, PILImgRepr)
@@ -233,16 +258,9 @@ class TestImgFunctions(TempDirFixture, LogTestCase):
         assert img.get_pixel((3, 2)) == [229, 25, 3]
 
     def test_blend_exr(self):
-        exr1 = EXRImgRepr()
-        exr2 = EXRImgRepr()
+        exr1 = get_exr_img_repr()
+        exr2 = get_exr_img_repr(alt=True)
 
-        exr1_file = _get_test_exr()
-        exr2_file = _get_test_exr(alt=True)
-
-        exr1.load_from_file(exr1_file)
-        exr2.load_from_file(exr2_file)
-
-        print exr1.get_pixel((3, 2))
         exr = blend(exr1, exr2, 0)
         assert exr.get_pixel((3, 2)) == exr1.get_pixel((3, 2))
 
@@ -251,10 +269,186 @@ class TestImgFunctions(TempDirFixture, LogTestCase):
 
         assert exr2.get_pixel((3, 2)) == [0, 0, 0]
 
-        print exr1.get_pixel((3, 2))
-        assert False
+        assert exr1.get_pixel((3, 2)) == [0.381103515625, 0.412353515625, 0.42236328125]
+        assert exr2.get_pixel((3, 2)) == [0,  0, 0]
 
         exr = blend(exr1, exr2, 0.5)
+        almost_equal_pixels(exr.get_pixel((3, 2)), [0.1905, 0.206, 0.211])
+
+        exr = blend(exr1, exr2, 0.9)
+        almost_equal_pixels(exr.get_pixel((3, 2)), [0.0381, 0.0412, 0.0422])
+
+    def test_advance_verify_imgs(self):
+        pass
+
+    def test_compare_pil_imgs(self):
+        img_path1 = self.temp_file_name("img1.jpg")
+        img_path2 = self.temp_file_name("img2.jpg")
+        make_test_img(img_path1)
+        make_test_img(img_path2)
+
+        assert compare_pil_imgs(img_path1, img_path2)
+
+        img = load_img(img_path1)
+        img.set_pixel((0, 0), [253, 1, 1])
+        img.img.save(img_path1)
+        assert compare_pil_imgs(img_path1, img_path2)
+
+        make_test_img(img_path1, color=(200, 0, 0))
+        assert not compare_pil_imgs(img_path1, img_path2)
+
+        make_test_img(img_path1, size=[11, 10], color=(255, 0, 0))
+        with self.assertLogs(logger, level="INFO"):
+            assert not compare_pil_imgs(img_path1, img_path2)
+
+        with self.assertLogs(logger, level="INFO"):
+            assert not compare_pil_imgs(img_path2, _get_test_exr())
+
+    def test_compar_exr_imgs(self):
+        assert compare_exr_imgs(_get_test_exr(), _get_test_exr())
+        assert not compare_exr_imgs(_get_test_exr(), _get_test_exr(alt=True))
+        with self.assertLogs(logger, level="INFO"):
+            assert not compare_exr_imgs(_get_test_exr(), "Not existing")
+
+        pil_img_path = self.temp_file_name("img1.png")
+        make_test_img(pil_img_path)
+        with self.assertLogs(logger, level="INFO"):
+            assert not compare_exr_imgs(_get_test_exr(), pil_img_path)
+
+    def test_count_psnr(self):
+        assert count_psnr(10, 10) == 10
+        assert count_psnr(10, 1) == -10
+        assert count_psnr(1, 10) == 20
+        assert int(count_psnr(0.5, 10)) == 23
+        assert count_psnr(100, 20)
+        with self.assertRaises(ValueError):
+            count_psnr(0, 10)
+
+        with self.assertRaises(ValueError):
+            count_psnr(10, 0)
+
+        with self.assertRaises(ValueError):
+            count_psnr(-1, 10)
+
+        with self.assertRaises(ValueError):
+            count_psnr(10, -1)
+
+        assert round(count_psnr(1514.6), 2) == 16.33
+        assert round(count_psnr(703.4), 2) == 19.66
+        assert round(count_psnr(710.4), 2) == 19.62
+        assert round(count_psnr(396.1), 2) == 22.15
+        assert round(count_psnr(326.0), 2) == 23.00
+        assert round(count_psnr(221.2), 2) == 24.68
+        assert round(count_psnr(164.7), 2) == 25.96
+        assert round(count_psnr(113.8), 2) == 27.57
+        assert round(count_psnr(82.5), 2) == 28.97
+        assert round(count_psnr(78.5), 2) == 29.18
+        assert round(count_psnr(66.1), 2) == 29.93
+        assert round(count_psnr(64.4), 2) == 30.04
+        assert round(count_psnr(57.4437), 2) == 30.54
+
+    def test_count_mse(self):
+
+        # Both arguments are not ImgRepr
+        with self.assertRaises(TypeError):
+            count_mse("notanimgrepr1", "notanimgrepr2")
+
+        img_path = self.temp_file_name("img.png")
+        img1 = get_pil_img_repr(img_path)
+
+        # Only one argument is ImgRepr
+        with self.assertRaises(TypeError):
+            count_mse("notanimgrepr", img1)
+
+        with self.assertRaises(TypeError):
+            count_mse(img1, "notanimgrepr")
+
+        # ImgRepr before being image is loaded
+        img_before_loaded = PILImgRepr()
+        with self.assertRaises(Exception):
+            count_mse(img_before_loaded, img1)
+
+        with self.assertRaises(Exception):
+            count_mse(img1, img_before_loaded)
+
+        # Proper comparison (same images)
+        img2_path = self.temp_file_name("img2.png")
+        img2 = get_pil_img_repr(img2_path)
+
+        assert count_mse(img1, img2) == 0
+
+        # Wrong box values
+        with self.assertRaises(Exception):
+            count_mse(img1, img2, box="Not box")
+
+        with self.assertRaises(ValueError):
+            count_mse(img1, img2, box=(0, 1))
+
+        with self.assertRaises(ValueError):
+            count_mse(img1, img2, box=(1, 0))
+
+        with self.assertRaises(ValueError):
+            count_mse(img1, img2, box=(0, 0))
+
+        # Img2 too small
+        img2 = get_pil_img_repr(img2_path, (5, 5))
+        with self.assertRaises(Exception):
+            count_mse(img1, img2)
+
+        # Proper execution with smaller img2
+        assert count_mse(img1, img2, box=(5, 5)) == 0
+        assert count_mse(img1, img2, start1=(5, 5), box=(5, 5)) == 0
+
+        img2 = get_pil_img_repr(img2_path, (10, 10), (253, 0, 0))
+        assert count_mse(img1, img2) == 1
+
+        img2 = get_pil_img_repr(img2_path, (10, 10))
+        img2.set_pixel((0, 0), (0, 0, 0))
+        assert count_mse(img1, img2) == 216
+
+        assert count_mse(img1, img2, start1=(0, 0), start2=(2, 2), box=(7, 7)) == 0
+
+    def test_compare_imgs(self):
+        img1_path = self.temp_file_name("img1.png")
+        img2_path = self.temp_file_name("img2.png")
+
+        img1 = get_pil_img_repr(img1_path)
+        img2 = get_pil_img_repr(img2_path)
+
+        assert compare_imgs(img1, img2)
+        assert compare_imgs(img1, img2, start1=(4, 4), box=(5, 5))
+
+        with self.assertRaises(Exception):
+            compare_imgs(img1, img2, box=(0, 0))
+
+        with self.assertRaises(Exception):
+            compare_imgs(img1, img2, start2=(3, 3))
+
+        exr_img1 = get_exr_img_repr()
+        exr_img2 = get_exr_img_repr(alt=True)
+
+        assert not compare_imgs(exr_img1, exr_img2, max_col=1)
+        assert compare_imgs(exr_img1, exr_img1, max_col=1)
+        exr_img2_copy = exr_img2.copy()
+
+        for i in range(10):
+            for j in range(10):
+                exr_img2.set_pixel((j, i), [0.1, 0.1, 0.1])
+
+        assert compare_imgs(exr_img2, exr_img2_copy)
+        assert not compare_imgs(exr_img2, exr_img2_copy, max_col=1)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
