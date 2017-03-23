7a4a7bb91e224b56c866496a928041c0a059b0e1
Marek Franciszkiewicz <marek.franciszkiewicz@imapp.pl>
2017-03-23 12:14:36 +0100

    Replace the twisted.internet.defer.DebugInfo.__del__ logging method

--- golem/core/deferred.py
+++ golem/core/deferred.py
@@ -1,20 +1,14 @@
-from twisted.logger import LogLevel
-from twisted.logger import globalLogPublisher
+
+from twisted.internet.defer import DebugInfo
 
 
 def install_event_logger():
     import logging
     logger = logging.getLogger('golem')
-    event_logger = _create_event_logger(logger)
-    globalLogPublisher.addObserver(event_logger)
 
+    def delete(self):
+        if self.failResult is not None:
+            logger.error("Unhandled error in Deferred:\n{}"
+                         .format(self.failResult))
 
-def _create_event_logger(logger):
-    def event_logger(event):
-        level = event.get("log_level")
-        failure = event.get("log_failure")
-
-        if level == LogLevel.critical and failure:
-            logger.error("Twisted: unhandled error in Deferred: {}"
-                         .format(failure))
-    return event_logger
+    setattr(DebugInfo, '__del__', delete)
