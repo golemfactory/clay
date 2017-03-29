bd8c20a0c4e7b5f0d1907190927c5a687c4140c0
Lukasz Foniok <lukaszfoniok@gmail.com>
2017-03-29 13:04:12 +0200

    Adding and running discovery, peermanager services from pydevp2p
    Signed-off-by: Lukasz Foniok <lukaszfoniok@gmail.com>

--- golem/client.py
+++ golem/client.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import atexit
 import logging
 from pydispatch import dispatcher
@@ -30,8 +31,8 @@ from golem.monitor.monitor import SystemMonitor
 from golem.monitorconfig import MONITOR_CONFIG
 from golem.network.hyperdrive.daemon_manager import HyperdriveDaemonManager
 from golem.network.p2p.node import Node
-from golem.network.p2p.p2pservice import P2PService
 from golem.network.p2p.peersession import PeerSessionInfo
+from golem.network.p2p.peersession import PeerMonitor
 from golem.network.transport.message import init_messages
 from golem.network.transport.tcpnetwork import SocketAddress
 from golem.ranking.ranking import Ranking
@@ -49,6 +50,15 @@ from golem.task.tasktester import TaskTester
 from golem.tools import filelock
 from golem.transactions.ethereum.ethereumtransactionsystem import EthereumTransactionSystem
 
+from devp2p.app import BaseApp
+from devp2p.discovery import NodeDiscovery
+from devp2p.peermanager import PeerManager
+from devp2p.service import BaseService
+from ethereum.utils import encode_hex
+import ethereum.slogging as slogging
+from golem.network.p2p.golemservice import GolemService
+
+devp2plog = slogging.get_logger('app')
 log = logging.getLogger("golem.client")
 
 
@@ -63,9 +73,16 @@ class ClientTaskComputerEventListener(object):
         self.client.config_changed()
 
 
-class Client(object):
+class Client(BaseApp):
+    client_name = 'golem'
+    default_config = dict(BaseApp.default_config)
+    services = [NodeDiscovery, PeerManager, GolemService]
+
     def __init__(self, datadir=None, transaction_system=False, connect_to_known_hosts=True,
                  use_docker_machine_manager=True, use_monitor=True, **config_overrides):
+
+        slogging.configure(u':info')
+        devp2plog.info('starting')
 
         # TODO: Should we init it only once?
         init_messages()
@@ -91,6 +108,15 @@ class Client(object):
         self.keys_auth = EllipticalKeysAuth(self.datadir)
         self.config_approver = ConfigApprover(self.config_desc)
 
+        from golem.p2pconfig import p2pconfig
+        configp2p = p2pconfig
+        configp2p['node'] = {}
+        configp2p['node']['privkey_hex'] = encode_hex(self.keys_auth._private_key)
+        configp2p['node']['pubkey_hex'] = encode_hex(self.keys_auth.public_key)
+        configp2p['node']['id'] = encode_hex(self.keys_auth.public_key)
+
+        BaseApp.__init__(self, configp2p)
+
         # NETWORK
         self.node = Node(node_name=self.config_desc.node_name,
                          key=self.keys_auth.get_key_id(),
@@ -109,7 +135,7 @@ class Client(object):
 
         self.nodes_manager_client = None
 
-        self.do_work_task = task.LoopingCall(self.__do_work)
+        self.do_work_task = None
 
         self.cfg = config
         self.send_snapshot = False
@@ -179,7 +205,9 @@ class Client(object):
         except Exception:
             log.critical('Can\'t start network. Giving up.', exc_info=True)
             sys.exit(1)
+        self.do_work_task = task.LoopingCall(self.__do_work)
         self.do_work_task.start(1, False)
+        BaseApp.start(self)
 
     def start_network(self):
         log.info("Starting network ...")
@@ -187,43 +215,41 @@ class Client(object):
                                        use_ipv6=self.config_desc.use_ipv6)
         log.debug("Is super node? %s", self.node.is_super_node())
 
-        # self.ipfs_manager = IPFSDaemonManager(connect_to_bootstrap_nodes=self.connect_to_known_hosts)
-        # self.ipfs_manager.store_client_info()
 
         self.daemon_manager = HyperdriveDaemonManager(self.datadir)
         self.daemon_manager.start()
 
-        self.p2pservice = P2PService(self.node, self.config_desc, self.keys_auth,
-                                     connect_to_known_hosts=self.connect_to_known_hosts)
+        for service in Client.services:
+            assert issubclass(service, BaseService)
+            assert service.name not in self.services
+            service.register_with_app(self)
+            assert hasattr(self.services, service.name)
+
         self.task_server = TaskServer(self.node, self.config_desc, self.keys_auth, self,
                                       use_ipv6=self.config_desc.use_ipv6,
                                       use_docker_machine_manager=self.use_docker_machine_manager)
+
+        self.services.golemservice.set_task_server(self.task_server)
 
         dir_manager = self.task_server.task_computer.dir_manager
 
         self.resource_server = BaseResourceServer(HyperdriveResourceManager(dir_manager),
                                                   dir_manager, self.keys_auth, self)
-
-        log.info("Starting p2p server ...")
-        self.p2pservice.start_accepting()
         time.sleep(1.0)
 
         log.info("Starting resource server...")
         self.resource_server.start_accepting()
         time.sleep(1.0)
 
-        self.p2pservice.set_resource_server(self.resource_server)
-        self.p2pservice.set_metadata_manager(self)
-
         log.info("Starting task server ...")
         self.task_server.start_accepting()
 
-        self.p2pservice.task_server = self.task_server
         self.task_server.task_computer.register_listener(ClientTaskComputerEventListener(self))
-        self.p2pservice.connect_to_network()
+
+        self.peermonitor = PeerMonitor(self.services.peermanager)
 
         if self.monitor:
-            self.diag_service.register(self.p2pservice, self.monitor.on_peer_snapshot)
+            self.diag_service.register(self.peermonitor, self.monitor.on_peer_snapshot)
             self.monitor.on_login()
 
     def init_monitor(self):
@@ -239,7 +265,7 @@ class Client(object):
             socket_address = SocketAddress(socket_address[0], int(socket_address[1]))
 
         log.debug("P2pservice connecting to %s on port %s", socket_address.address, socket_address.port)
-        self.p2pservice.connect(socket_address)
+        #self.p2pservice.connect(socket_address)
 
     def quit(self):
         if self.do_work_task.running:
@@ -260,11 +286,11 @@ class Client(object):
     def key_changed(self):
         self.node.key = self.keys_auth.get_key_id()
         self.task_server.key_changed()
-        self.p2pservice.key_changed()
+        #self.p2pservice.key_changed()
 
     def stop_network(self):
         # FIXME: Implement this method properly - send disconnect package, close connections etc.
-        self.p2pservice = None
+        #self.p2pservice = None
         self.task_server = None
         self.nodes_manager_client = None
 
@@ -286,7 +312,7 @@ class Client(object):
 
     def set_resource_port(self, resource_port):
         self.resource_port = resource_port
-        self.p2pservice.set_resource_peer(self.node.prv_addr, self.resource_port)
+        #self.p2pservice.set_resource_peer(self.node.prv_addr, self.resource_port)
 
     def run_test_task(self, t_dict):
         if self.task_tester is None:
@@ -361,26 +387,32 @@ class Client(object):
         return self.config_desc.node_name
 
     def get_neighbours_degree(self):
-        return self.p2pservice.get_peers_degree()
+        #return self.p2pservice.get_peers_degree()
+        pass
 
     def get_suggested_addr(self, key_id):
-        return self.p2pservice.suggested_address.get(key_id)
+        #return self.p2pservice.suggested_address.get(key_id)
+        pass
 
     def get_suggested_conn_reverse(self, key_id):
-        return self.p2pservice.get_suggested_conn_reverse(key_id)
+        return self.services.golemservice.get_suggested_conn_reverse(key_id)
 
     def get_resource_peers(self):
-        self.p2pservice.send_get_resource_peers()
+        #self.p2pservice.send_get_resource_peers()
+        pass
 
     def get_peers(self):
-        return self.p2pservice.peers.values()
+        #return self.p2pservice.peers.values()
+        pass
 
     def get_known_peers(self):
-        peers = self.p2pservice.free_peers or []
+        #peers = self.p2pservice.free_peers or []
+        peers = []
         return [DictSerializer.dump(PeerSessionInfo(p), typed=False) for p in peers]
 
     def get_connected_peers(self):
-        peers = self.get_peers() or []
+        peers = self.services.peermanager.peers
+        #peers = self.get_peers() or []
         return [DictSerializer.dump(PeerSessionInfo(p), typed=False) for p in peers]
 
     def get_public_key(self):
@@ -432,7 +464,8 @@ class Client(object):
         return self.datadir
 
     def get_p2p_port(self):
-        return self.p2pservice.cur_port
+        #return self.p2pservice.cur_port
+        pass
 
     def get_task_server_port(self):
         return self.task_server.cur_port
@@ -545,13 +578,15 @@ class Client(object):
         return bool(self.ranking)
 
     def want_to_start_task_session(self, key_id, node_id, conn_id):
-        self.p2pservice.want_to_start_task_session(key_id, node_id, conn_id)
+        self.services.golemservice.want_to_start_task_session(key_id, node_id, conn_id)
 
     def inform_about_task_nat_hole(self, key_id, rv_key_id, addr, port, ans_conn_id):
-        self.p2pservice.inform_about_task_nat_hole(key_id, rv_key_id, addr, port, ans_conn_id)
+        #self.p2pservice.inform_about_task_nat_hole(key_id, rv_key_id, addr, port, ans_conn_id)
+        pass
 
     def inform_about_nat_traverse_failure(self, key_id, res_key_id, conn_id):
-        self.p2pservice.inform_about_nat_traverse_failure(key_id, res_key_id, conn_id)
+        #self.p2pservice.inform_about_nat_traverse_failure(key_id, res_key_id, conn_id)
+        pass
 
     # CLIENT CONFIGURATION
     def set_rpc_server(self, rpc_server):
@@ -561,7 +596,7 @@ class Client(object):
     def change_config(self, new_config_desc, run_benchmarks=False):
         self.config_desc = self.config_approver.change_config(new_config_desc)
         self.cfg.change_config(self.config_desc)
-        self.p2pservice.change_config(self.config_desc)
+        #self.p2pservice.change_config(self.config_desc)
         if self.task_server:
             self.task_server.change_config(self.config_desc, run_benchmarks=run_benchmarks)
         dispatcher.send(signal='golem.monitor', event='config_update', meta_data=self.__get_nodemetadatamodel())
@@ -631,7 +666,8 @@ class Client(object):
         dir_manager.clear_dir(self.get_received_files_dir())
 
     def remove_task(self, task_id):
-        self.p2pservice.remove_task(task_id)
+        #self.p2pservice.remove_task(task_id)
+        pass
 
     def remove_task_header(self, task_id):
         self.task_server.remove_task_header(task_id)
@@ -684,19 +720,24 @@ class Client(object):
         self.environments_manager.change_accept_tasks(env_id, False)
 
     def send_gossip(self, gossip, send_to):
-        return self.p2pservice.send_gossip(gossip, send_to)
+        #return self.p2pservice.send_gossip(gossip, send_to)
+        pass
 
     def send_stop_gossip(self):
-        return self.p2pservice.send_stop_gossip()
+        #return self.p2pservice.send_stop_gossip()
+        pass
 
     def collect_gossip(self):
-        return self.p2pservice.pop_gossips()
+        #return self.p2pservice.pop_gossips()
+        pass
 
     def collect_stopped_peers(self):
-        return self.p2pservice.pop_stop_gossip_form_peers()
+        #return self.p2pservice.pop_stop_gossip_form_peers()
+        pass
 
     def collect_neighbours_loc_ranks(self):
-        return self.p2pservice.pop_neighbours_loc_ranks()
+        #return self.p2pservice.pop_neighbours_loc_ranks()
+        return []
 
     def push_local_rank(self, node_id, loc_rank):
         self.p2pservice.push_local_rank(node_id, loc_rank)
@@ -732,14 +773,10 @@ class Client(object):
         return new_value
 
     def __do_work(self):
-        if self.p2pservice:
-            if self.config_desc.send_pings:
-                self.p2pservice.ping_peers(self.config_desc.pings_interval)
-
             try:
-                self.p2pservice.sync_network()
-            except Exception:
-                log.exception("p2pservice.sync_network failed")
+                self.services.golemservice.get_tasks()
+            except:
+                log.exception("golemservice task broadcast failed")
             try:
                 self.task_server.sync_network()
             except Exception:
@@ -781,8 +818,8 @@ class Client(object):
                     self.rpc_publisher.publish(Network.evt_connection, self.connection_status())
 
     def __make_node_state_snapshot(self, is_running=True):
-        peers_num = len(self.p2pservice.peers)
-        last_network_messages = self.p2pservice.get_last_messages()
+        peers_num = 0 #len(self.p2pservice.peers)
+        last_network_messages = '' #self.p2pservice.get_last_messages()
 
         if self.task_server:
             tasks_num = len(self.task_server.task_keeper.task_headers)
@@ -793,8 +830,8 @@ class Client(object):
                                                               self.config_desc.node_name,
                                                               peers_num,
                                                               tasks_num,
-                                                              self.p2pservice.node.pub_addr,
-                                                              self.p2pservice.node.pub_port,
+                                                              '',#self.p2pservice.node.pub_addr,
+                                                              '',#self.p2pservice.node.pub_port,
                                                               last_network_messages,
                                                               last_task_messages,
                                                               remote_tasks_progresses,
@@ -847,7 +884,7 @@ class Client(object):
         else:
             msg = "Not accepting tasks\n"
 
-        peers = self.p2pservice.get_peers()
+        peers = self.services.peermanager.peers
 
         msg += "Active peers in network: {}\n".format(len(peers))
         return msg
---
+++ golem/network/p2p/golemprotocol.py
@@ -0,0 +1,78 @@
+import rlp
+from devp2p.protocol import BaseProtocol, SubProtocolError
+from ethereum import slogging
+from rlp.sedes import big_endian_int, binary, List, CountableList
+from golem.docker.image import DockerImage
+from golem.task.taskbase import TaskHeader
+from golem.network.p2p.node import Node
+from golem.core.simpleserializer import CBORSedes
+log = slogging.get_logger('golem.protocol')
+
+
+class GolemProtocol(BaseProtocol):
+    protocol_id = 18317  # just a random number; not sure what to put here
+
+    def __init__(self, peer, service):
+        # required by P2PProtocol
+        self.config = peer.config
+        BaseProtocol.__init__(self, peer, service)
+
+    class get_tasks(BaseProtocol.command):
+        """
+        Peer want tasks information
+        """
+        cmd_id = 0
+
+        structure = [
+        ]
+
+    class task_headers(BaseProtocol.command):
+        """
+        Sends tasks descriptors
+        """
+        cmd_id = 1
+
+        structure = rlp.sedes.CountableList(TaskHeader)
+
+        def create(self, proto, task_headers):
+            self.sent = True
+            t = []
+            for th in task_headers:
+                t.append(th)
+            return t
+
+        @classmethod
+        def decode_payload(cls, rlp_data):
+            ll = rlp.decode_lazy(rlp_data)
+            theaders = []
+            for th in ll:
+                theaders.append(TaskHeader.deserialize(th, mutable=True))
+
+            return theaders
+
+    class want_to_start_task_session(BaseProtocol.command):
+        """
+        Send invitation for connection if requestor is behind NAT
+        """
+
+        cmd_id = 2
+
+        structure = [('node', Node),
+                     ('connection_id', rlp.sedes.binary),
+                     ('super_node', CBORSedes)
+                     ]
+
+    class set_task_session(BaseProtocol.command):
+        """
+        Someone else is trying to connect with node to which direct connection is impossible
+        and to which there is no p2p connection. Send this messages to neighboring nodes in order to forward it
+        to reach recipient.
+        """
+
+        cmd_id = 3
+
+        structure = [('key', rlp.sedes.binary),
+                     ('node', Node),
+                     ('connection_id', rlp.sedes.binary),
+                     ('super_node', CBORSedes)
+                     ]
