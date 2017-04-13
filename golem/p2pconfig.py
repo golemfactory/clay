b6e8759778c221db367b908284e3d7887fdb09fe
Lukasz Foniok <lukaszfoniok@gmail.com>
2017-04-13 11:15:41 +0200

    Add pebble bootstrap node to config
    Signed-off-by: Lukasz Foniok <lukaszfoniok@gmail.com>

--- golem/p2pconfig.py
+++ golem/p2pconfig.py
@@ -3,7 +3,7 @@ p2pconfig = {
         "listen_host": "0.0.0.0",
         "listen_port": 20170,
         "bootstrap_nodes": [
-            "enode://ceb6294a907dd32a4e8a5622903868178cdd788976d9e7338b29735be0c436114a5828063ddddfdeda7e22ac1bf88ba195238590709a83a0ce791b9765f5be26@127.0.0.1:20170"]
+            "enode://a9ac7cb82f08929e3253372b0c7fab7589fd37c9ee5cc2ad1cc9da75ffa8b78398450aa5a25f1836d4984700570f69aa8356a94ca9bd55c5d226d051c5bd6742@94.23.17.170:20170"]
      },
      "p2p": {
         "listen_host": "0.0.0.0",
