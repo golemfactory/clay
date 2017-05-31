92600b0a61780e382db53941de4fc51310cd4d11
Dariusz Rybi <jiivanq@gmail.com>
2017-05-31 11:27:38 +0200

    blender benchmark styling & refactoring

--- apps/blender/benchmark/benchmark.py
+++ apps/blender/benchmark/benchmark.py
@@ -1,30 +1,26 @@
-import os
+import pathlib
 import tempfile
 
 from apps.core.benchmark.benchmark import Benchmark
 from apps.blender.task.blenderrendertask import BlenderRendererOptions
 from apps.blender.blenderenvironment import BlenderEnvironment
 
-from golem.core.common import get_golem_path
-from golem.resource.dirmanager import find_task_script
-
 
 class BlenderBenchmark(Benchmark):
     def __init__(self):
-        
-        Benchmark.__init__(self)
-        
+        super(BlenderBenchmark, self).__init__()
         self.normalization_constant = 9360
-        
-        self.blender_task_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_task")
-        
-        self.task_definition.output_file = os.path.join(tempfile.gettempdir(), "blender_benchmark.png")
-        self.task_definition.task_type = "Blender"
-        self.task_definition.output_format = "png"
-        self.task_definition.options = BlenderRendererOptions()
-        self.task_definition.options.frames = [1]
-        self.task_definition.task_id = u"{}".format("blender_benchmark")
-        self.task_definition.main_scene_file = os.path.join(self.blender_task_path, "scene-Helicopter-27-cycles.blend")
-        self.task_definition.main_program_file = BlenderEnvironment().main_program_file
-
-        self.task_definition.resources.add(os.path.normpath(self.task_definition.main_scene_file))
+        this_dir = pathlib.Path(__file__).resolve().parent
+        self.blender_task_path = str(this_dir / "test_task")
+        task_def = self.task_definition
+        task_def.output_file = tempfile.mkstemp("blender_benchmark.png")[1]
+        task_def.task_type = "Blender"
+        task_def.output_format = "png"
+        task_def.options = BlenderRendererOptions()
+        task_def.options.frames = [1]
+        task_def.task_id = u"blender_benchmark"
+        main_scene_file = pathlib.Path(self.blender_task_path)
+        main_scene_file /= "scene-Helicopter-27-cycles.blend"
+        task_def.main_scene_file = str(main_scene_file)
+        task_def.main_program_file = BlenderEnvironment().main_program_file
+        task_def.resources.add(str(main_scene_file.resolve()))
