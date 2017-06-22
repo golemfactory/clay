eb1f73134e51c2686e4df2ba92aaedd8c2f82e88
Adam Banasiak <banasiakadam60@gmail.com>
2017-06-22 11:53:18 +0200

    Fix getting dir path in lux benchmark
    Signed-off-by: Adam Banasiak <banasiakadam60@gmail.com>

--- apps/lux/benchmark/benchmark.py
+++ apps/lux/benchmark/benchmark.py
@@ -1,39 +1,46 @@
-import os
+import sys
 import tempfile
-
-from golem.core.common import get_golem_path
-from golem.resource.dirmanager import find_task_script
+from os import walk
+from os.path import abspath, dirname, join
 
 from apps.core.benchmark.benchmark import Benchmark
-from apps.lux.task.luxrendertask import LuxRenderOptions
 from apps.lux.luxenvironment import LuxRenderEnvironment
+from apps.lux.task.luxrendertask import LuxRenderOptions
+from golem.core.common import get_golem_path
 
-APP_DIR = os.path.join(get_golem_path(), 'apps', 'lux')
+APP_DIR = join(get_golem_path(), 'apps', 'lux')
 
 
 class LuxBenchmark(Benchmark):
     def __init__(self):
-        
+
         Benchmark.__init__(self)
-        
+
         self.normalization_constant = 9910
-        
-        self.lux_task_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_task")
-        
-        self.task_definition.output_file = os.path.join(tempfile.gettempdir(), "lux_benchmark.png")
+
+        if hasattr(sys, 'frozen') and sys.frozen:
+            self.lux_task_path = join(dirname(sys.executable), 'apps', 'lux',
+                                      'benchmark')
+        else:
+            self.lux_task_path = join(dirname(abspath(__file__)), "test_task")
+
+        self.task_definition.output_file = join(tempfile.gettempdir(),
+                                                "lux_benchmark.png")
         self.task_definition.task_type = "LuxRender"
         self.task_definition.output_format = "png"
         self.task_definition.options = LuxRenderOptions()
         self.task_definition.options.haltspp = 5
         self.task_definition.options.halttime = 0
         self.task_definition.task_id = u"{}".format("lux_benchmark")
-        self.task_definition.main_scene_file = os.path.join(self.lux_task_path, "schoolcorridor.lxs")
-        self.task_definition.main_program_file = LuxRenderEnvironment().main_program_file
+        self.task_definition.main_scene_file = join(self.lux_task_path,
+                                                    "schoolcorridor.lxs")
+        self.task_definition.main_program_file =\
+            LuxRenderEnvironment().main_program_file
         self.task_definition.resources = self.find_resources()
 
     def find_resources(self):
         selection = []
-        for root, dirs, files in os.walk(self.lux_task_path):
+        for root, dirs, files in walk(self.lux_task_path):
             for name in files:
-                selection.append(os.path.join(root, name))
+                selection.append(join(root, name))
         return set(selection)
