f584f77edc6d2a66ac45fb3a4038fc8f0c27d7b9
maaktweluit <maaktweluit@gitgub.com>
2017-06-25 09:45:32 +0200

    Added argument in coverage test

--- tests/golem/test_opt_node.py
+++ tests/golem/test_opt_node.py
@@ -71,7 +71,8 @@ class TestNode(TestWithDatabase):
         mock_client.assert_called_with(node_address=node_address,
                                        datadir=self.path,
                                        transaction_system=True,
-                                       use_docker_machine_manager=True)
+                                       use_docker_machine_manager=True,
+                                       use_monitor=True)
 
     def test_node_address_invalid(self, *_):
         runner = CliRunner()
