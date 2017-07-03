23d47f8d955ea4e5423c1bcf742a5e56f2768d44
Marek Franciszkiewicz <marek.franciszkiewicz@imapp.pl>
2017-07-03 14:49:06 +0200

    Bump app and protocol versions

--- golem/task/tasksession.py
+++ golem/task/tasksession.py
@@ -24,7 +24,7 @@ from golem.transactions.ethereum.ethereumpaymentskeeper import EthAccountInfo
 logger = logging.getLogger(__name__)
 
 
-TASK_PROTOCOL_ID = 14
+TASK_PROTOCOL_ID = 15
 
 
 def drop_after_attr_error(*args, **kwargs):
