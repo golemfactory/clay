4372cd2a11e6cbf7d6c78f2032c7feff118ed8f4
Marek Franciszkiewicz <marek.franciszkiewicz@imapp.pl>
2017-07-12 15:43:26 +0200

    Report geth start and stop

--- golem/ethereum/node.py
+++ golem/ethereum/node.py
@@ -20,6 +20,7 @@ from web3 import Web3, IPCProvider
 from golem.core.common import is_windows, DEVNULL, is_frozen
 from golem.core.crypto import privtopub
 from golem.environments.utils import find_program
+from golem.report import report_calls, Component
 from golem.utils import find_free_net_port
 
 log = logging.getLogger('golem.ethereum')
@@ -99,6 +100,7 @@ class NodeProcess(object):
     def is_running(self):
         return self.__ps is not None
 
+    @report_calls(Component.ethereum, 'node.start')
     def start(self):
         if self.__ps is not None:
             raise RuntimeError("Ethereum node already started by us")
@@ -170,6 +172,7 @@ class NodeProcess(object):
 
         log.info("Node started in %ss: `%s`", wait_time, " ".join(args))
 
+    @report_calls(Component.ethereum, 'node.stop')
     def stop(self):
         if self.__ps:
             start_time = time.clock()
