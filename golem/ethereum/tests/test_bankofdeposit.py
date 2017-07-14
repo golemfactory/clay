c698b1a9fb1bc33f9f8009edf1deb7ca6386b4f8
mfranciszkiewicz <mfranciszkiewicz@gmail.com>
2017-07-14 14:09:05 +0200

    Ethereum, continued

--- golem/ethereum/tests/test_bankofdeposit.py
+++ golem/ethereum/tests/test_bankofdeposit.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from ethereum import tester
 tester.serpent = True  # tester tries to load serpent module, prevent that.
@@ -9,6 +10,7 @@ try:
 except ImportError:
     from BankOfDeposit import BankOfDeposit
 
+BANK_OF_DEPOSIT_ABI = json.loads(BankOfDeposit.ABI)
 eth = denoms.ether
 
 
@@ -40,7 +42,7 @@ class BankContractTest(unittest.TestCase):
         owner = self.monitor(owner_idx)
         addr = self.state.evm(decode_hex(BankOfDeposit.INIT_HEX),
                               sender=owner.key)
-        self.c = tester.ABIContract(self.state, BankOfDeposit.ABI, addr)
+        self.c = tester.ABIContract(self.state, BANK_OF_DEPOSIT_ABI, addr)
         return addr, owner.gas()
 
     def contract_balance(self):
@@ -228,7 +230,7 @@ class BankContractTest(unittest.TestCase):
         self.deploy_contract()
         v = 100
         p = 55 + 44 + 33 + 22
-        d = p / 2
+        d = p // 2
         self.deposit(1, d*eth)  # Deposit half of payments
         self.transfer(1, [(5, 55*eth), (4, 44*eth), (3, 33*eth), (2, 22*eth)],
                       value=(d + 100)*eth)  # Transfer another half + v
