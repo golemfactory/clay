bbd63c6bc3c193ad2fab29b3fd4e5abe44f783d4
Paul Peregud <paulperegud@gmail.com>
2017-08-11 17:44:32 +0200

    fix indentation, fix pylint warning

--- golemapp.py
+++ golemapp.py
@@ -90,7 +90,7 @@ def start(gui, payments, monitor, datadir, node_address, rpc_address, peer,
         install_reactor()
 
         node = OptNode(peers=peer, node_address=node_address,
-		               use_monitor=monitor, **config)
+                       use_monitor=monitor, **config)
         node.run(use_rpc=True)
 
 
