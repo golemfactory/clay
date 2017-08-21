c1d31ac509d6adb952af47e28ac78191c272aed6
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-08-21 18:50:48 +0200

    squashed changes after first review / catchup with Aleksandra
    added postfix to all py files when calling them
    
    typo, replaced the wrong word.
    
    switch travis to python3
    
    error in travis with argument
    
    debug travis
    
    debug travis
    
    fix slow-argument.py on travis
    
    Added comments to get-slow-argument.py
    
    remove test line
    
    cleanup travis after debug
    
    comment cleanup

--- scripts/get-slow-argument.py
+++ scripts/get-slow-argument.py
@@ -1,3 +1,8 @@
+# Check if this build can skip slow tests
+# Skipping happens when build is a PR with < 2 approvals
+# - input: pull_id from CI ( argv ) 
+# - output: argument to use for this test ( stdout )
+
 import sys
 import json
 import urllib.request;
@@ -5,20 +10,23 @@ import urllib.request;
 pullRequestID = sys.argv[1]
 runSlow = True
 
-if pullRequestID is not "" and pullRequestID is not "false":
+# When build is not a PR the input is: "" or "false"
+if pullRequestID not in ["", "false"]:
     baseUrl = "https://api.github.com/repos/golemfactory/golem/pulls/{}/reviews"
     url = baseUrl.format(pullRequestID)
+    
+    # Github API requires user agent.
     req = urllib.request.Request(url,headers={'User-Agent':'build-bot'})
     with urllib.request.urlopen(req,timeout=10) as f:
         data = f.read().decode('utf-8')
 
     jsonData = json.loads(data)
-
     result = [a for a in jsonData if a["state"] is not "APPROVED"]
     approvals = len(result)
     runSlow = approvals >= 2
 
 if runSlow:
+    # Space in front is important for more arguments later.
     print(" --runslow")
 else:
     print("")
