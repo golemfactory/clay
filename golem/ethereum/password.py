Spencerx/golem

Add helper function to mock password for Ethereum account

golem/ethereum/password.py
@@ -1,0 +1,22 @@
+import os
+import stat
+
+PASSWORD_LENGH = 50
+
+
+def get_saved_password(datadir):
+    password_file = os.path.join(datadir, 'password')
+    if os.path.exists(password_file):
+        with open(password_file, 'rb') as f:
+            password = f.read(PASSWORD_LENGH)
+            if len(password) < PASSWORD_LENGH:
+                raise IOError(
+                    "Invalid password in file {}".format(password_file)
+                )
+            return password
+
+    password = os.urandom(PASSWORD_LENGH)
+    fd = os.open(password_file, os.O_WRONLY | os.O_CREAT, stat.S_IREAD)
+    with os.fdopen(fd, 'wb') as f:
+        f.write(password)
+    return password

tests/golem/ethereum/test_ethereum_password.py
@@ -1,0 +1,41 @@
+import os
+import stat
+from golem.testutils import TempDirFixture
+
+from golem.ethereum.password import get_saved_password, PASSWORD_LENGH
+
+
+class EthereumPasswordTest(TempDirFixture):
+
+    def test_write_and_read(self):
+        password1 = get_saved_password(self.tempdir)
+        assert type(password1) is bytes
+        assert len(password1) == PASSWORD_LENGH
+        password2 = get_saved_password(self.tempdir)
+        assert type(password2) is bytes
+        assert password1 == password2
+        os.chmod(os.path.join(self.tempdir, 'password'), stat.S_IWRITE)
+
+    def test_passwords_are_different(self):
+        dir1 = os.path.join(self.tempdir, '1')
+        dir2 = os.path.join(self.tempdir, '2')
+        os.mkdir(dir1)
+        os.mkdir(dir2)
+        pass1 = get_saved_password(dir1)
+        pass2 = get_saved_password(dir2)
+        assert len(pass1) == PASSWORD_LENGH
+        assert len(pass2) == PASSWORD_LENGH
+        assert pass1 != pass2
+        os.chmod(os.path.join(dir1, 'password'), stat.S_IWRITE)
+        os.chmod(os.path.join(dir2, 'password'), stat.S_IWRITE)
+
+    def test_empty_password_file(self):
+        password_file = os.path.join(self.tempdir, 'password')
+        with open(password_file, 'w'):
+            pass
+        assert os.path.exists(password_file)
+
+        with self.assertRaises(IOError):
+            get_saved_password(self.tempdir)
+
+        os.chmod(password_file, stat.S_IWRITE)  # Allow deleting the file.

golemfactory/clay

golem/ethereum/password.py
@@ -1,0 +1,22 @@
+import os
+import stat
+
+PASSWORD_LENGTH = 50
+
+
+def get_saved_password(datadir):
+    password_file = os.path.join(datadir, 'password')
+    if os.path.exists(password_file):
+        with open(password_file, 'rb') as f:
+            password = f.read(PASSWORD_LENGTH)
+            if len(password) < PASSWORD_LENGTH:
+                raise IOError(
+                    "Invalid password in file {}".format(password_file)
+                )
+            return password
+
+    password = os.urandom(PASSWORD_LENGTH)
+    fd = os.open(password_file, os.O_WRONLY | os.O_CREAT, stat.S_IREAD)
+    with os.fdopen(fd, 'wb') as f:
+        f.write(password)
+    return password

tests/golem/ethereum/test_ethereum_password.py
@@ -1,0 +1,41 @@
+import os
+import stat
+from golem.testutils import TempDirFixture
+
+from golem.ethereum.password import get_saved_password, PASSWORD_LENGTH
+
+
+class EthereumPasswordTest(TempDirFixture):
+
+    def test_write_and_read(self):
+        password1 = get_saved_password(self.tempdir)
+        assert type(password1) is bytes
+        assert len(password1) == PASSWORD_LENGTH
+        password2 = get_saved_password(self.tempdir)
+        assert type(password2) is bytes
+        assert password1 == password2
+        os.chmod(os.path.join(self.tempdir, 'password'), stat.S_IWRITE)
+
+    def test_passwords_are_different(self):
+        dir1 = os.path.join(self.tempdir, '1')
+        dir2 = os.path.join(self.tempdir, '2')
+        os.mkdir(dir1)
+        os.mkdir(dir2)
+        pass1 = get_saved_password(dir1)
+        pass2 = get_saved_password(dir2)
+        assert len(pass1) == PASSWORD_LENGTH
+        assert len(pass2) == PASSWORD_LENGTH
+        assert pass1 != pass2
+        os.chmod(os.path.join(dir1, 'password'), stat.S_IWRITE)
+        os.chmod(os.path.join(dir2, 'password'), stat.S_IWRITE)
+
+    def test_empty_password_file(self):
+        password_file = os.path.join(self.tempdir, 'password')
+        with open(password_file, 'w'):
+            pass
+        assert os.path.exists(password_file)
+
+        with self.assertRaises(IOError):
+            get_saved_password(self.tempdir)
+
+        os.chmod(password_file, stat.S_IWRITE)  # Allow deleting the file.