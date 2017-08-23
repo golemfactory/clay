bf524e4acf5b2d1f7459ae49996afa18a61aeb98
Marek Franciszkiewicz <marek.franciszkiewicz@imapp.pl>
2017-08-23 19:01:41 +0200

    HyperdriveClientOptions tests

--- tests/golem/network/hyperdrive/test_hyperdrive_client.py
+++ tests/golem/network/hyperdrive/test_hyperdrive_client.py
@@ -3,7 +3,8 @@ import uuid
 
 import mock
 
-from golem.network.hyperdrive.client import HyperdriveClient
+from golem.network.hyperdrive.client import HyperdriveClient, \
+    HyperdriveClientOptions
 
 
 class TestHyperdriveClient(unittest.TestCase):
@@ -69,5 +70,93 @@ class TestHyperdriveClient(unittest.TestCase):
                                return_value=self.response):
 
             assert client.pin_rm(multihash) == self.response['hash']
+
+
+class TestHyperdriveClientOptions(unittest.TestCase):
+
+    def test_clone(self):
+        peers = [dict(
+            TCP=dict(
+                address='192.168.1.2',
+                port=3282
+            ),
+            uTP=dict(
+                address='192.168.1.2',
+                port=3283
+            )
+        )]
+
+        options = HyperdriveClientOptions('client_id', 1.0,
+                                          options=dict(peers=peers))
+
+        cloned = options.clone()
+        assert cloned is not options
+        assert cloned.client_id == options.client_id
+        assert cloned.version == options.version
+        assert cloned.options == options.options
+        assert cloned.options is not options.options
+
+    def test_filtered(self):
+        peers = [
+            dict(
+                TCP=dict(
+                    address='1.2.3.4',
+                    port=3282
+                ),
+                uTP=dict(
+                    address='::1.2.3.4',
+                    port=3283
+                )
+            ),
+        ]
+
+        client = HyperdriveClient.CLIENT_ID
+        version = HyperdriveClient.VERSION
+
+        options = HyperdriveClientOptions(client, version,
+                                          options=dict(peers=None))
+
+        assert options.filtered(client, version) is None
+        assert options.filtered() is None
+
+        options = HyperdriveClientOptions(client, version,
+                                          options=dict(peers=peers))
+        filtered = options.filtered()
+
+        assert isinstance(filtered, HyperdriveClientOptions)
+        assert filtered.options['peers'] == peers
+        assert options.filtered('invalid client', version) is None
+
+    def test_filter_peers(self):
+        peers_local_ip = [
+            dict(
+                TCP=dict(address='192.168.1.2', port=3282),
+                uTP=dict(address='192.168.1.2', port=3283)
+            ),
+            dict(
+                TCP=dict(address='::1', port=3282),
+                uTP=dict(address='127.0.0.1', port=3283)
+            ),
+        ]
+        peers_remote_ip = [
+            dict(
+                TCP=dict(address='1.2.3.4', port=3282),
+                uTP=dict(address='::1.2.3.4', port=3283)
+            )
+        ]
+        peers_mixed_ip = [
+            dict(
+                TCP=dict(address='1.2.3.4', port=3282),
+                uTP=dict(address='127.0.0.1', port=3283)
+            )
+        ]
+
+        filtered = HyperdriveClientOptions.filter_peers(
+            peers_local_ip + peers_remote_ip)
+        assert filtered == peers_remote_ip
+
+        filtered = HyperdriveClientOptions.filter_peers(
+            peers_mixed_ip)
+        assert filtered == [dict(TCP=dict(address='1.2.3.4', port=3282))]
 
 
--- tests/golem/resource/test_resourceclient.py
+++ tests/golem/resource/test_resourceclient.py
@@ -1,16 +1,14 @@
-import types
+import time
 import unittest
 import uuid
 
-import time
 import twisted
-from golem.tools.testwithreactor import TestWithReactor
-from mock import Mock, patch
-from twisted.internet.defer import Deferred
+from mock import Mock
 
+from golem.core.async import AsyncRequest, async_run
 from golem.resource.client import ClientHandler, ClientCommands, ClientError, \
     ClientOptions, ClientConfig
-from golem.core.async import AsyncRequest, async_run
+from golem.tools.testwithreactor import TestWithReactor
 
 
 class MockClientHandler(ClientHandler):
@@ -69,18 +67,50 @@ class TestClientHandler(unittest.TestCase):
 
 class TestClientOptions(unittest.TestCase):
 
+    def test_init(self):
+        with self.assertRaises(AssertionError):
+            ClientOptions(None, 1.0)
+        with self.assertRaises(AssertionError):
+            ClientOptions('client_id', None)
+
     def test_get(self):
         option = 'test_option'
-
-        options = ClientOptions('valid_id', 'valid_version', {})
+        options = ClientOptions('valid_id', 1.0, {})
         options.options[option] = True
 
         with self.assertRaises(ClientError):
-            options.get('valid_id', 'invalid_version', option)
+            options.get('valid_id', 0.5, option)
         with self.assertRaises(ClientError):
-            options.get('invalid_id', 'valid_version', option)
+            options.get('invalid_id', 1.0, option)
 
-        assert options.get('valid_id', 'valid_version', option)
+        assert options.get('valid_id', 1.0, option)
+
+    def test_clone(self):
+        dict_options = dict(key='val')
+        options = ClientOptions('client_id', 1.0, options=dict_options)
+        cloned = options.clone()
+
+        assert isinstance(cloned, ClientOptions)
+        assert cloned.options == dict_options
+        assert cloned.options is not dict_options
+
+    def test_filtered(self):
+        dict_options = dict(key='val')
+        options = ClientOptions('client_id', 1.0, options=dict_options)
+
+        filtered = options.filtered('client_id', 1.0)
+        assert isinstance(filtered, ClientOptions)
+        assert filtered is not options
+        assert filtered.client_id == options.client_id
+        assert filtered.version == options.version
+        assert filtered.options == options.options
+        assert filtered.options is not options.options
+
+        filtered = options.filtered(None, 1.0)
+        assert filtered is None
+
+        filtered = options.filtered('client_id', None)
+        assert isinstance(filtered, ClientOptions)
 
 
 class TestAsyncRequest(TestWithReactor):
