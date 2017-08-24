260f6231359d6476a8b9ee5773d76895ee7bfa4a
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-08-24 19:12:51 +0200

    CamelCase variables to lower case with underscores.
    added check for empty file
    
    typo and added test
    
    Added try catch to script for better error handling
    
    typo
    
    write to stderr
    
    removed raise: it is a appveyor specific error
    
    error substitution is invalid, cleanup %TMP_FILE%
    
    test without if
    
    solve exit code 1
    
    exit 0

--- scripts/get-slow-argument.py
+++ scripts/get-slow-argument.py
@@ -7,25 +7,28 @@ import sys
 import json
 import urllib.request;
 
-pullRequestID = sys.argv[1]
-runSlow = True
+pull_request_id = sys.argv[1]
+run_slow = True
 
 # When build is not a PR the input is: "" or "false"
-if pullRequestID not in ["", "false"]:
-    baseUrl = "https://api.github.com/repos/golemfactory/golem/pulls/{}/reviews"
-    url = baseUrl.format(pullRequestID)
+if pull_request_id not in ["", "false"]:
+    base_url = "https://api.github.com/repos/golemfactory/golem/pulls/{}/reviews"
+    url = base_url.format(pull_request_id)
     
-    # Github API requires user agent.
-    req = urllib.request.Request(url,headers={'User-Agent':'build-bot'})
-    with urllib.request.urlopen(req,timeout=10) as f:
-        data = f.read().decode('utf-8')
+    try:
+        # Github API requires user agent.
+        req = urllib.request.Request(url,headers={'User-Agent':'build-bot'})
+        with urllib.request.urlopen(req,timeout=10) as f:
+            data = f.read().decode('utf-8')
 
-    jsonData = json.loads(data)
-    result = [a for a in jsonData if a["state"] is not "APPROVED"]
-    approvals = len(result)
-    runSlow = approvals >= 2
+        json_data = json.loads(data)
+        result = [a for a in json_data if a["state"] is not "APPROVED"]
+        approvals = len(result)
+        run_slow = approvals >= 2
+    except:
+        sys.stderr.write("Error calling github, run all tests. {}".format(url))
 
-if runSlow:
+if run_slow:
     # Space in front is important for more arguments later.
     print(" --runslow")
 else:
