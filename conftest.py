be4f49024d5a02154137b728f4e03eac1b14ee07
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-08-26 15:21:21 +0200

    exit on fast test
    pylint fixes
    
    last pylint, not sure how to fix...
    
    found it!

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
 import pytest
+
+
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true",
                      default=False, help="run slow tests")
+
 
 def pytest_collection_modifyitems(config, items):
     if config.getoption("--runslow"):
--- scripts/get-slow-argument.py
+++ scripts/get-slow-argument.py
@@ -1,31 +1,37 @@
 # Check if this build can skip slow tests
 # Skipping happens when build is a PR with < 2 approvals
-# - input: pull_id from CI ( argv ) 
+# - input: pull_id from CI ( argv )
 # - output: argument to use for this test ( stdout )
 
 import sys
 import json
-import urllib.request;
+import urllib.request
+import urllib.error
 
+# input / ouput vars
 pull_request_id = sys.argv[1]
 run_slow = True
 
+# config vars
+required_approvals = 2
+
 # When build is not a PR the input is: "" or "false"
 if pull_request_id not in ["", "false"]:
-    base_url = "https://api.github.com/repos/golemfactory/golem/pulls/{}/reviews"
+    base_url = "https://api.github.com/" \
+        "repos/golemfactory/golem/pulls/{}/reviews"
     url = base_url.format(pull_request_id)
-    
+
     try:
         # Github API requires user agent.
-        req = urllib.request.Request(url,headers={'User-Agent':'build-bot'})
-        with urllib.request.urlopen(req,timeout=10) as f:
+        req = urllib.request.Request(url, headers={'User-Agent': 'build-bot'})
+        with urllib.request.urlopen(req, timeout=10) as f:
             data = f.read().decode('utf-8')
 
         json_data = json.loads(data)
-        result = [a for a in json_data if a["state"] is not "APPROVED"]
+        result = [a for a in json_data if a["state"] != "APPROVED"]
         approvals = len(result)
-        run_slow = approvals >= 2
-    except:
+        run_slow = approvals >= required_approvals
+    except urllib.error.HTTPError:
         sys.stderr.write("Error calling github, run all tests. {}".format(url))
 
 if run_slow:
