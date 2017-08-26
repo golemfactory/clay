97549fb8e578845e38e6bffd2ce8abfc058326b0
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-08-26 19:43:07 +0200

    added types to conftest.py
    Changed ; to && it seems better in cmd

--- conftest.py
+++ conftest.py
@@ -1,12 +1,14 @@
 import pytest
+import _pytest
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser: _pytest.config.Parser) -> None:
     parser.addoption("--runslow", action="store_true",
                      default=False, help="run slow tests")
 
 
-def pytest_collection_modifyitems(config, items):
+def pytest_collection_modifyitems(config: _pytest.config.Config,
+                                  items: list) -> None:
     if config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
         return
