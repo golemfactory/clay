29c35eec6897cbc0a920daa34f4a55ad98ed210b
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2017-08-28 11:08:11 +0200

    Fix changes being lost if they were to be overwritten by switching branches

--- lintdiff.sh
+++ lintdiff.sh
@@ -6,55 +6,80 @@ CURRENT_BRANCH=$(git symbolic-ref --short -q HEAD)
 
 REF_OUT=/tmp/ref-lint.out
 CURRENT_OUT=/tmp/current-lint.out
+FETCH=true
 
 usage() {
-	echo "Usage: $0 [-b reference-branch] command"
+	echo "Usage: $0 [-b branch] [-o] command"
+	echo "    -b branch			use branch \`branch\` for comparison"
+	echo "    -o 				offline mode, do not fetch origin"
 	exit 1
 }
 
-while getopts "b:" opt; do
+# Check if diff supports colored output
+# Ubuntu Trusty has an ancient version of diffutils, 3.3,
+# which doesn't handle that yet
+if diff --color /dev/null /dev/null; then
+	DIFF="diff --color"
+else
+	DIFF=colordiff
+fi
+
+while getopts "b:o" opt; do
 	case $opt in
 		b)
 			REF_BRANCH=$OPTARG
 			;;
-		*) ;;
+		o)
+			FETCH=false
+			;;
+		*)
+			usage
+			;;
 	esac
 done
 shift $((OPTIND - 1))
 
 # get new changes from develop, GitHub doesn't integrate them on its own
-echo "Fetching new changes from develop..."
-git fetch origin || exit 1
+if $FETCH; then
+	echo "Fetching new changes from develop..."
+	git fetch origin || exit 1
+fi
 
 cleanup_artifacts() {
 	git reset --hard HEAD
 	git checkout "$CURRENT_BRANCH" || exit 1
 }
-trap cleanup_artifacts EXIT
 
-commit=$(git rev-parse HEAD)
-echo "Checking branch $CURRENT_BRANCH, commit: $commit..."
-echo "$@"
-"$@" >$CURRENT_OUT
-
+# we checkout the reference branch first in case there are
+# uncommitted changes to be overwritten by the merge
 git checkout "$REF_BRANCH" || exit 1
+trap cleanup_artifacts EXIT
 commit=$(git rev-parse HEAD)
 # We need to take files responsible for the linting configuration from
 # the new commit.
 git checkout "$CURRENT_BRANCH" .pylintrc setup.cfg
 echo "Checking branch $REF_BRANCH, commit: $commit..."
 echo "$@"
-"$@" >$REF_OUT
+"$@" || exit 1 >$REF_OUT
+
+# Now take back the checked out config, go back to the new branch
+git reset --hard HEAD
+git checkout "$CURRENT_BRANCH" || exit 1
+# The trap is no longer needed
+trap - EXIT
+commit=$(git rev-parse HEAD)
+echo "Checking branch $CURRENT_BRANCH, commit: $commit..."
+echo "$@"
+"$@" || exit 1 >$CURRENT_OUT
 
 # Remove the trap
-trap - EXIT
 cleanup_artifacts
 
 diff=$(diff --old-line-format="" --unchanged-line-format="" -w <(sort $REF_OUT) <(sort $CURRENT_OUT))
 # There's always a newline, so -gt 1
 if [ -n "$diff" ]; then
 	echo "New findings! The error diff is:"
-	diff --unified --color $REF_OUT $CURRENT_OUT
+	$DIFF --unified $REF_OUT $CURRENT_OUT
 	exit 1
 else
 	echo "Check OK, no new findings..."
