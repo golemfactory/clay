99cf80e2a0c7b03f01e12324c050928b6f650b62
Adam Banasiak <banasiakadam60@gmail.com>
2017-08-29 16:16:33 +0200

    Init ClientConfigDescriptor with default values from AppConfig
    Signed-off-by: Adam Banasiak <banasiakadam60@gmail.com>

--- golem/clientconfigdescriptor.py
+++ golem/clientconfigdescriptor.py
@@ -8,55 +8,63 @@ class ClientConfigDescriptor(object):
 
     def __init__(self):
         """ Create new basic empty configuration scheme """
-        from golem.appconfig import END_PORT
+        from golem.appconfig import (START_PORT, END_PORT, RPC_ADDRESS, RPC_PORT, OPTIMAL_PEER_NUM, SEND_PINGS,
+                                     PINGS_INTERVALS, USE_IP6, GETTING_PEERS_INTERVAL, GETTING_TASKS_INTERVAL,
+                                     TASK_REQUEST_INTERVAL, USE_WAITING_FOR_TASK_TIMEOUT, WAITING_FOR_TASK_TIMEOUT,
+                                     WAITING_FOR_TASK_SESSION_TIMEOUT, FORWARDED_SESSION_REQUEST_TIMEOUT,
+                                     P2P_SESSION_TIMEOUT, TASK_SESSION_TIMEOUT, RESOURCE_SESSION_TIMEOUT,
+                                     ESTIMATED_DEFAULT, NODE_SNAPSHOT_INTERVAL, NETWORK_CHECK_INTERVAL,
+                                     MAX_SENDING_DELAY, MIN_CPU_CORES, MIN_DISK_SPACE, MIN_MEMORY_SIZE,
+                                     DEFAULT_HARDWARE_PRESET_NAME, REQUESTING_TRUST, COMPUTING_TRUST, MIN_PRICE,
+                                     MAX_PRICE, ACCEPT_TASKS)
         self.node_name = ""
         self.node_address = ""
-        self.start_port = 0
+        self.start_port = START_PORT
         self.end_port = END_PORT
-        self.rpc_address = ""
-        self.rpc_port = 0
-        self.opt_peer_num = 0
-        self.send_pings = 0
-        self.pings_interval = 0.0
-        self.use_ipv6 = 0
+        self.rpc_address = RPC_ADDRESS
+        self.rpc_port = RPC_PORT
+        self.opt_peer_num = OPTIMAL_PEER_NUM
+        self.send_pings = SEND_PINGS
+        self.pings_interval = PINGS_INTERVALS
+        self.use_ipv6 = USE_IP6
 
         self.seed_host = ""
         self.seed_port = 0
 
-        self.getting_peers_interval = 0.0
-        self.getting_tasks_interval = 0.0
-        self.task_request_interval = 0.0
-        self.use_waiting_for_task_timeout = 0
-        self.waiting_for_task_timeout = 0.0
-        self.waiting_for_task_session_timeout = 0.0
-        self.forwarded_session_request_timeout = 0.0
-        self.p2p_session_timeout = 0
-        self.task_session_timeout = 0
-        self.resource_session_timeout = 0
+        self.getting_peers_interval = GETTING_PEERS_INTERVAL
+        self.getting_tasks_interval = GETTING_TASKS_INTERVAL
+        self.task_request_interval = TASK_REQUEST_INTERVAL
+        self.use_waiting_for_task_timeout = USE_WAITING_FOR_TASK_TIMEOUT
+        self.waiting_for_task_timeout = WAITING_FOR_TASK_TIMEOUT
+        self.waiting_for_task_session_timeout = WAITING_FOR_TASK_SESSION_TIMEOUT
+        self.forwarded_session_request_timeout = FORWARDED_SESSION_REQUEST_TIMEOUT
+        self.p2p_session_timeout = P2P_SESSION_TIMEOUT
+        self.task_session_timeout = TASK_SESSION_TIMEOUT
+        self.resource_session_timeout = RESOURCE_SESSION_TIMEOUT
 
-        self.estimated_performance = 0.0
-        self.estimated_lux_performance = 0.0
-        self.estimated_blender_performance = 0.0
-        self.node_snapshot_interval = 0.0
-        self.network_check_interval = 0.0
-        self.max_results_sending_delay = 0.0
+        self.estimated_performance = ESTIMATED_DEFAULT
+        self.estimated_lux_performance = ESTIMATED_DEFAULT
+        self.estimated_blender_performance = ESTIMATED_DEFAULT
+        self.node_snapshot_interval = NODE_SNAPSHOT_INTERVAL
+        self.network_check_interval = NETWORK_CHECK_INTERVAL
+        self.max_results_sending_delay = MAX_SENDING_DELAY
 
-        self.num_cores = 0
-        self.max_resource_size = 0
-        self.max_memory_size = 0
-        self.hardware_preset_name = ""
+        self.num_cores = MIN_CPU_CORES
+        self.max_resource_size = MIN_DISK_SPACE
+        self.max_memory_size = MIN_MEMORY_SIZE
+        self.hardware_preset_name = DEFAULT_HARDWARE_PRESET_NAME
 
         self.use_distributed_resource_management = 1
 
-        self.requesting_trust = 0.0
-        self.computing_trust = 0.0
+        self.requesting_trust = REQUESTING_TRUST
+        self.computing_trust = COMPUTING_TRUST
 
         self.eth_account = ""
-        self.min_price = 0
-        self.max_price = 0
+        self.min_price = MIN_PRICE
+        self.max_price = MAX_PRICE
         self.public_address = ""
 
-        self.accept_tasks = 1
+        self.accept_tasks = ACCEPT_TASKS
 
     def init_from_app_config(self, app_config):
         """Initializes config parameters based on the specified AppConfig
