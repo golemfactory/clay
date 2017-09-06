7a2f21b0f0f27df4551963ae4ef229f6c89a50ae
badb <a.skrzy@gmail.com>
2017-09-06 19:45:33 +0200

    Add description to subtasktask state dict

--- tests/golem/task/test_taskstate.py
+++ tests/golem/task/test_taskstate.py
@@ -1,6 +1,8 @@
+import time
 import unittest
 
-from golem.task.taskstate import SubtaskState
+from golem.core.common import timeout_to_deadline, deadline_to_timeout
+from golem.task.taskstate import SubtaskState, SubtaskStatus
 
 
 class TestSubtaskState(unittest.TestCase):
@@ -13,4 +15,43 @@ class TestSubtaskState(unittest.TestCase):
         ss2.results.append(2)
         self.assertEqual(ss.results, [1])
         self.assertEqual(ss2.results, [2])
+
+    def test_to_dictionary(self):
+        ss = SubtaskState()
+        ss.subtask_definition = "My long task definition"
+        ss.subtask_id = "ABCDEF"
+        ss.subtask_progress = 0.92
+        ss.time_started = time.time()
+        ss.deadline = timeout_to_deadline(ss.time_started + 5)
+        ss.extra_data = {"param1": 1323, "param2": "myparam"}
+        ss.subtask_rem_time = deadline_to_timeout(ss.deadline) - ss.time_started
+        ss.subtask_status = SubtaskStatus.starting
+        ss.value = 138
+        ss.stdout = "path/to/file"
+        ss.stderr = "path/to/file2"
+        ss.results = ["path/to/file3", "path/to/file4"]
+        ss.computation_time = 130
+        ss.computer.node_name = "NODE1"
+        ss.computer.node_id = "abc131"
+        ss.computer.performance = 180
+        ss.computer.ip_address = "10.10.10.1"
+        ss.computer.port = 1311
+
+        ss_dict = ss.to_dictionary()
+        assert ss_dict['subtask_id'] == "ABCDEF"
+        assert ss_dict['node_name'] == "NODE1"
+        assert ss_dict['node_id'] == "abc131"
+        assert ss_dict['node_performance'] == "180"
+        assert ss_dict['node_ip_address'] == "10.10.10.1"
+        assert ss_dict['node_port'] == 1311
+        assert ss_dict['status'] == SubtaskStatus.starting
+        assert ss_dict['progress'] == 0.92
+        assert time.time() - ss_dict['time_started'] >= 0
+        assert ss_dict['time_remaining'] <= 5
+        assert ss_dict['results'] == ["path/to/file3", "path/to/file4"]
+        assert ss_dict['stderr'] == "path/to/file2"
+        assert ss_dict['stdout'] == "path/to/file"
+        assert ss_dict['description'] == "My long task definition"
+
+
 
