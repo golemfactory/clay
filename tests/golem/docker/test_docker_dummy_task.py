ae0033e359a50f72b9a852ce03ce8df2081eada2
Jacek Karwowski <inexxt@users.noreply.github.com>
2017-09-13 18:11:53 +0200

    Fixing macos docker dummy task 2

--- tests/golem/docker/test_docker_dummy_task.py
+++ tests/golem/docker/test_docker_dummy_task.py
@@ -45,7 +45,7 @@ class TestDockerDummyTask(TempDirFixture, DockerTestCase):
 
     @classmethod
     def setUpClass(cls):
-        TempDirFixture.setUpClass()
+        super().setUpClass()
         data_dir = os.path.join(get_golem_path(),
                                 "apps",
                                 "dummy",
