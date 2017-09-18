b25b06a80492573b13d228007e5c883da17b441a
Jacek Karwowski <inexxt@users.noreply.github.com>
2017-09-18 18:07:54 +0200

    Style and annotation fix

--- apps/core/task/coretask.py
+++ apps/core/task/coretask.py
@@ -185,9 +185,10 @@ class CoreTask(Task):
                and self.header.docker_images \
                and len(self.header.docker_images) > 0
 
-    def initialize(self, dir_manager):
+    def initialize(self, dir_manager: DirManager) -> None:
         dir_manager.clear_temporary(self.header.task_id)
-        self.tmp_dir = dir_manager.get_task_temporary_dir(self.header.task_id, create=True)
+        self.tmp_dir = dir_manager.get_task_temporary_dir(self.header.task_id,
+                                                          create=True)
         self.verificator.tmp_dir = self.tmp_dir
 
     def needs_computation(self):
