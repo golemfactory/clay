b52ab0844c773cad6266a6d41d212d09a47870c4
ggruszczynski <ggruszczynski@golem.network>
2017-10-12 11:46:40 +0200

    GNTW_Faucet in PaymentProcessor
    Signed-off-by: ggruszczynski <ggruszczynski@golem.network>

--- golem/ethereum/contracts/GNTW_Faucet.py
+++ golem/ethereum/contracts/GNTW_Faucet.py
@@ -1,39 +1,37 @@
 
-class GNTW_Faucet:
+class GNTW_Faucet(object):
     ABI = """
-            [
+            [{
+                "constant": false,
+                "inputs": [],
+                "name": "create",
+                "outputs": [],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+            {
+                "constant": true,
+                "inputs": [],
+                "name": "token",
+                "outputs": [
                     {
-                        "constant": false,
-                        "inputs": [],
-                        "name": "create",
-                        "outputs": [],
-                        "payable": false,
-                        "stateMutability": "nonpayable",
-                        "type": "function"
-                    },
-                    {
-                        "constant": true,
-                        "inputs": [],
-                        "name": "token",
-                        "outputs": [
-                            {
-                                "name": "",
-                                "type": "address"
-                            }
-                        ],
-                        "payable": false,
-                        "stateMutability": "view",
-                        "type": "function"
-                    },
-                    {
-                        "inputs": [
-                            {
-                                "name": "_token",
-                                "type": "address"
-                            }
-                        ],
-                        "payable": false,
-                        "stateMutability": "nonpayable",
-                        "type": "constructor"
+                        "name": "",
+                        "type": "address"
                     }
-            ]"""  # noqa
+                ],
+                "payable": false,
+                "stateMutability": "view",
+                "type": "function"
+            },
+            {
+                "inputs": [
+                    {
+                        "name": "_token",
+                        "type": "address"
+                    }
+                ],
+                "payable": false,
+                "stateMutability": "nonpayable",
+                "type": "constructor"
+            }]"""  # noqa
