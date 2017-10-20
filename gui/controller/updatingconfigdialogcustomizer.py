db9e475418645cd176b32e29566b7c4e936a321e
badb <a.skrzy@gmail.com>
2017-10-20 19:53:57 +0200

    No gui (#1487)

--- gui/controller/updatingconfigdialogcustomizer.py
+++
@@ -1,9 +0,0 @@
-from .customizer import Customizer
-
-
-class UpdatingConfigDialogCustomizer(Customizer):
-    def show_message(self, msg):
-        self.gui.ui.message.setText(msg)
-
-    def close(self):
-        self.gui.close()
--- gui/view/widget.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QWidget
-
-
-class TaskWidget(QWidget):
-
-    def __init__(self, widget_class):
-        super(TaskWidget, self).__init__()
-        self.ui = widget_class()
-        self.ui.setupUi(self)
