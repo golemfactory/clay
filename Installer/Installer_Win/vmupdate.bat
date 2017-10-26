127c1f408b15a0ac0e6f6053a298d1c81f50733f
badb <a.skrzy@gmail.com>
2017-10-26 20:06:49 +0200

    make installator 64 bit

--- Installer/Installer_Win/vmupdate.bat
+++ Installer/Installer_Win/vmupdate.bat
@@ -1,3 +1,6 @@
 docker-machine inspect golem 2>&1 > NUL
-IF "%ERRORLEVEL%"=="0" ( docker-machine rm golem -y )
+IF "%ERRORLEVEL%"=="0" ( 
+ docker-machine stop golem 
+ docker-machine rm golem -y 
+)
 %WinDir%\System32\pnputil.exe -i -a "C:\Program Files\Oracle\VirtualBox\drivers\vboxdrv\VBoxDrv.inf"
