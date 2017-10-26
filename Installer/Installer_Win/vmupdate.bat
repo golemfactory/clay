eec131104cea11c814b729cfe8ca38df879ae50f
badb <a.skrzy@gmail.com>
2017-10-26 17:42:05 +0200

    Full path to pnputil

--- Installer/Installer_Win/vmupdate.bat
+++ Installer/Installer_Win/vmupdate.bat
@@ -1,2 +1,3 @@
-docker-machine rm golem -y
-pnputil.exe -i -a "C:\Program Files\Oracle\VirtualBox\drivers\vboxdrv\VBoxDrv.inf"
+docker-machine inspect golem 2>&1 > NUL
+IF "%ERRORLEVEL%"=="0" ( docker-machine rm golem -y )
+%WinDir%\System32\pnputil.exe -i -a "C:\Program Files\Oracle\VirtualBox\drivers\vboxdrv\VBoxDrv.inf"
