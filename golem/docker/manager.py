dcc74d47495a8aa976c79f60435468b5e4d7ef93
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-11-07 17:56:22 +0100

    Check hypervisor existance for unix

--- golem/docker/manager.py
+++ golem/docker/manager.py
@@ -424,8 +424,9 @@ class DockerManager(DockerConfigManager):
                     return self._set_docker_machine_env(retried=True)
 
                 try:
-                    if self.hypervisor.remove(self.docker_machine):
-                        self.hypervisor.create(self.docker_machine)
+                    if self.hypervisor:
+                        if self.hypervisor.remove(self.docker_machine):
+                            self.hypervisor.create(self.docker_machine)
                 except subprocess.CalledProcessError as e:
                     logger.warning("DockerMachine:"
                                    " failed to re-create the VM: %s -- %s",
