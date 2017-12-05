bc5f8ba8539187bace1be785fa8b044fec6694b1
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-12-05 11:42:09 +0100

    Split out control worker role to be run 4x on the same host

--- scripts/ci/roles/buildbot-master/files/src/golem_buildbot/workers.py
+++ scripts/ci/roles/buildbot-master/files/src/golem_buildbot/workers.py
@@ -3,7 +3,10 @@ from buildbot.plugins import worker
 from .settings import local_settings
 
 workers = [
-    worker.Worker('control', local_settings['worker_pass']),
+    worker.Worker('control_01', local_settings['worker_pass']),
+    worker.Worker('control_02', local_settings['worker_pass']),
+    worker.Worker('control_03', local_settings['worker_pass']),
+    worker.Worker('control_04', local_settings['worker_pass']),
     worker.Worker('macOS', local_settings['worker_pass']),
     worker.Worker('linux', local_settings['worker_pass']),
     worker.Worker('windows_server_2016', local_settings['worker_pass']),
