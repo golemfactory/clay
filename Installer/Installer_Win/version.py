538f71b020229e3a77befb43f85f83e7e7947126
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-12-20 12:34:10 +0100

    Quick fix the tag matching, can be better with semver

--- Installer/Installer_Win/version.py
+++ Installer/Installer_Win/version.py
@@ -19,8 +19,13 @@ def get_tag():
     """
     repo = Repo(get_golem_path())
     tag = repo.tags[-1]  # get latest tag
-    tag_id = tag.commit.hexsha  # get commit id from tag
     commit_id = repo.head.commit.hexsha  # get last commit id
+    for rel_tag in repo.tags:
+        if rel_tag.commit.hexsha == commit_id:
+            tag = rel_tag
+            break
+
+    tag_id = tag.commit.hexsha  # get commit id from tag
     if commit_id != tag_id:  # devel package
         return "{}-0x{}{}".format(tag.name, commit_id[:4], commit_id[-4:])
     else:  # release package
