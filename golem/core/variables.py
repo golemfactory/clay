701e3f62185e6ef07c6a0a75e9150972a84531f0
Igor Adamski <igor@golem.network>
2017-12-29 16:04:15 +0100

    Start using GNTW

--- golem/core/variables.py
+++ golem/core/variables.py
@@ -40,10 +40,10 @@ TASK_HEADERS_LIMIT = 20
 #       over class container.
 class PROTOCOL_CONST(object):
     """
-    https://docs.python.org/3/faq/programming.html#how-do-i-share-global-variables-across-modules #noqa
-    https://bytes.com/topic/python/answers/19859-accessing-updating-global-variables-among-several-modules #noqa
+    https://docs.python.org/3/faq/programming.html#how-do-i-share-global-variables-across-modules # noqa
+    https://bytes.com/topic/python/answers/19859-accessing-updating-global-variables-among-several-modules # noqa
     """
-    ID = 19
+    ID = 20
 
     @staticmethod
     def patch_protocol_id(ctx, param, value):
