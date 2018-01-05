933ccc72b501e3d701ea0f3a6da8dd85d102cd23
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2018-01-05 17:01:04 +0100

    Swapped 2 files switching from windows to mac

--- scripts/test_bin/windows/golem_source_run.bat
+++ scripts/test_bin/windows/golem_source_run.bat
@@ -1,15 +1,12 @@
 
-set PROJECT_DIR="%systemdrive%%homepath%\projects"
+set PROJECT_DIR=%systemdrive%%homepath%\projects
 
-echo "Ensure projects directory exists"
-mkdir -p "%PROJECT_DIR%"
+echo "Activate golem-env"
+call "%PROJECT_DIR%\golem-env\Scripts\activate.bat"
 
+echo "Change to source directory"
+cd "%PROJECT_DIR%\golem"
 
-echo "Setup venv in ~/projects/golem-env"
-python -m venv "%PROJECT_DIR%\golem-env"
-
-echo "Clone into ~/projects/golem"
-git clone https://github.com/golemfactory/golem "%PROJECT_DIR%\golem"
-
-call ".\golem_source_update.bat"
+echo "Running golem"
+python golemapp.py
 
