1f1a0430a03154021b4598b60e6f17e8a038432f
Karol Tomala <ktomala@golem.network>
2018-01-18 16:41:20 +0100

    After discussing with Gert-Jan we have decided to merge check-daemon to test-start-daemon. #1900

--- scripts/test-daemon-start.ps1
+++ scripts/test-daemon-start.ps1
@@ -1,13 +1,13 @@
-
 "Ensure hyperg is started"
 
 $HyperdriveProcess = Get-Process hyperg -ErrorAction SilentlyContinue
 
-if (!$HyperdriveProcess)
-{
-  "Starting..."
-  $HyperdriveProcess = Start-Process C:\BuildResources\hyperg\hyperg.exe -WorkingDirectory C:\Users\buildbot-worker -PassThru
+if ($HyperdriveProcess) {
+  Stop-Process -Id $HyperdriveProcess
 }
+
+"Starting..."
+$HyperdriveProcess = Start-Process C:\BuildResources\hyperg\hyperg.exe -WorkingDirectory C:\Users\buildbot-worker -PassThru
 
 $HyperdriveProcess
 
--- scripts/test-daemon-start.sh
+++ scripts/test-daemon-start.sh
@@ -1,9 +1,36 @@
-#!/bin/sh
+#!/bin/bash
+
+let DEBUG=1
+H_FILE=./.test-daemon-hyperg.pid
+[ -e $H_FILE ] && H_PID=$(cat $H_FILE)
+
+find_and_kill_pid () {
+  [ $DEBUG -eq 1 ] && echo "Checking for pid $H_PID" >&2
+  if ps -p $H_PID > /dev/null; then
+    [ $DEBUG -eq 1 ] && echo "Killing pid $H_PID" >&2
+    kill -9 $H_PID
+    let res=$?
+    [ $res -eq 1 ] && echo "Unable to kill pid $H_PID" >&2 && exit 1
+  fi
+}
+
+kill_running_hyperg () {
+  RUNNING_PID=`pidof hyperg`
+  let res=$?
+  [ $res -eq 1 ] && exit 0
+  kill -9 $RUNNING_PID
+  let res=$?
+  [ $res -eq 1 ] && echo "Unable to kill pid $RUNNING_PID" >&2 && exit 1
+}
+
+[ -e $H_FILE ] && find_and_kill_pid
+
+kill_running_hyperg
 
 echo "Starting hyperg"
 hyperg > /dev/null 2>&1 &
 H_PID=$!
 
-echo $H_PID > ./.test-daemon-hyperg.pid
+echo $H_PID > $H_FILE
 
 exit 0
