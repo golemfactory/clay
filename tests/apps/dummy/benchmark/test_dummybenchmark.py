f0257dd3b0bc0274f51870f32a91c05e2784be5b
Adam Mizerski <amizerski@golem.network>
2018-01-18 11:26:01 +0100

    fix DummyBenchmark.verify_result

--- tests/apps/dummy/benchmark/test_dummybenchmark.py
+++ tests/apps/dummy/benchmark/test_dummybenchmark.py
@@ -1,13 +1,15 @@
 import os
-import tempfile
-import unittest
+from unittest.mock import patch
 
 from apps.dummy.benchmark.benchmark import DummyTaskBenchmark
 from apps.dummy.task.dummytaskstate import DummyTaskDefinition, DummyTaskOptions
 
+from golem.testutils import TempDirFixture
 
-class TestDummyBenchmark(unittest.TestCase):
+
+class TestDummyBenchmark(TempDirFixture):
     def setUp(self):
+        super().setUp()
         self.db = DummyTaskBenchmark()
 
     def test_is_instance(self):
@@ -28,3 +30,20 @@ class TestDummyBenchmark(unittest.TestCase):
         self.assertTrue(self.db.task_definition.result_size == 256)
         self.assertTrue(self.db.task_definition.options.subtask_data_size == 128)
         sizes = sum(os.stat(x).st_size for x in self.db.task_definition.shared_data_files)
+
+    def test_verify_result(self):
+        # given
+        files = [
+            self.new_path / "benchmark.result",
+            self.new_path / "benchmark.log"
+        ]
+        for f in files:
+            f.touch()
+
+        # when
+        with patch("apps.dummy.task.verifier.DummyTaskVerifier._verify_result",
+                   returns=True):
+            ret = self.db.verify_result([str(f) for f in files])
+
+        # then
+        assert ret
