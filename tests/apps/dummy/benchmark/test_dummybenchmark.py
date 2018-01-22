d05b0a781696e9016bd71e1479d5786ac8d64474
Adam Mizerski <amizerski@golem.network>
2018-01-22 16:25:36 +0100

    cleanup TestDummyBenchmark.test_task_settings

--- tests/apps/dummy/benchmark/test_dummybenchmark.py
+++ tests/apps/dummy/benchmark/test_dummybenchmark.py
@@ -20,16 +20,19 @@ class TestDummyBenchmark(TempDirFixture):
     def test_task_settings(self):
         self.assertTrue(os.path.isdir(self.db.dummy_task_path))
 
-        self.assertTrue(self.db.task_definition.out_file_basename == "out")
-        self.assertTrue(self.db.task_definition.task_id == u"{}".format("dummy_benchmark"))
+        self.assertEquals(self.db.task_definition.out_file_basename, "out")
+        self.assertEquals(self.db.task_definition.task_id, "dummy_benchmark")
 
-        self.assertTrue(all(os.path.isfile(x) for x in self.db.task_definition.shared_data_files))
-        self.assertTrue(os.path.isfile(self.db.task_definition.main_program_file))
+        self.assertTrue(all(os.path.isfile(x) for x
+                            in self.db.task_definition.shared_data_files))
+        self.assertTrue(os.path.isfile(
+            self.db.task_definition.main_program_file))
 
-        self.assertTrue(self.db.task_definition.options.difficulty == 0xffff0000)
-        self.assertTrue(self.db.task_definition.result_size == 256)
-        self.assertTrue(self.db.task_definition.options.subtask_data_size == 128)
-        sizes = sum(os.stat(x).st_size for x in self.db.task_definition.shared_data_files)
+        self.assertEquals(self.db.task_definition.options.difficulty,
+                          0xffff0000)
+        self.assertEquals(self.db.task_definition.result_size, 256)
+        self.assertEquals(self.db.task_definition.options.subtask_data_size,
+                          128)
 
     def test_verify_result(self):
         # given
