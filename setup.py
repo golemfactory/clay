4848e5e274bebffda1470fde14eb2779cdede4a4
Piotr Chromiec <tworec@golem.network>
2018-02-01 14:51:11 +0100

    bump-up python version in setup: 2.7 -> 3.5

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
     ],
     zip_safe=False,
     keywords='golem',
