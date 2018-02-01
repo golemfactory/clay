b891a003e573a64d9917e4978be7ec2938491bfa
Dariusz Rybi <jiivanq@gmail.com>
2018-02-01 16:52:04 +0100

    lints

--- tests/factories/p2p.py
+++ tests/factories/p2p.py
@@ -1,14 +1,15 @@
+# pylint: disable=too-few-public-methods
 import faker
 import factory
 
-from golem.network.p2p.node import Node
+from golem.network.p2p import node
 
 fake = faker.Faker()
 
 
 class Node(factory.Factory):
     class Meta:
-        model = Node
+        model = node.Node
 
     node_name = factory.Faker('name')
-    key = factory.LazyAttribute(lambda o: format(fake.pyint(), '02x'))
+    key = factory.LazyAttribute(lambda o: format(fake.pyint(), '02x'))  # noqa pylint: disable=no-member
