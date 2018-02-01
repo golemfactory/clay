56239265c4439062e6710128d7f5cb192c57e361
Marek Franciszkiewicz <marek.franciszkiewicz@imapp.pl>
2018-02-01 15:08:44 +0100

    golem.db logging configuration

--- loggingconfig.py
+++ loggingconfig.py
@@ -74,6 +74,10 @@ LOGGING = {
             'level': 'INFO',
             'propagate': True,
         },
+        'golem.db': {
+            'level': 'INFO',
+            'propagate': True,
+        },
         'golem.pay': {
             'level': 'INFO',
             'propagate': True,
