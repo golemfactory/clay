8a97578b6c372ed57044891310700b0f2a264ee7
Marek Franciszkiewicz <marek.franciszkiewicz@imapp.pl>
2018-02-01 21:01:31 +0100

    Working migration

--- golem/database/migration/migrate.py
+++ golem/database/migration/migrate.py
@@ -1,8 +1,10 @@
-from golem.database.migration import default_migrate_dir
+from peewee_migrate import Migrator
+
+from golem.database.migration import default_migrate_dir, patch_peewee
 from golem.database.migration.router import Router
 
 
-def migrate_schema(db: 'peewee.Database',
+def migrate_schema(database: 'Database',
                    from_version: int,
                    to_version: int,
                    migrate_dir: str = default_migrate_dir()):
@@ -10,21 +12,41 @@ def migrate_schema(db: 'peewee.Database',
     if from_version == to_version:
         return
 
-    router = Router(db, migrate_dir, schema_version=to_version)
-    migrator = router.migrator
-    environment = Router.Environment()
-
     downgrade = from_version > to_version
     reference_version = to_version if downgrade else from_version
 
-    scripts = filter(
-        lambda f: Router.Environment.version_from_name(f) > reference_version,
-        environment.scripts
-    )
+    router = Router(database.db, migrate_dir, schema_version=to_version)
+    environment = router.environment
+    all_scripts = environment.scripts
+
+    start_idx = -1
+
+    for idx, script in enumerate(all_scripts):
+        version = environment.version_from_name(script)
+        if version > reference_version:
+            start_idx = idx
+            break
+
+    scripts = all_scripts[start_idx:]
+    previous_scripts = all_scripts[:start_idx]
 
     if downgrade:
-        scripts = reversed(list(scripts))
+        previous_scripts += scripts
+        scripts = list(reversed(scripts))
 
-    with db.transaction():
+    with patch_peewee():
+        migrator = Migrator(router.database)
+
+        # Teach migrator previous changes
+        for script in previous_scripts:
+            router.run_one(script, migrator, fake=True, downgrade=downgrade)
+
         for script in scripts:
             router.run_one(script, migrator, fake=False, downgrade=downgrade)
+            # Teach migrator the changes
+            router.run_one(script, migrator, fake=True, downgrade=downgrade)
+
+    database.set_user_version(to_version)
+
+
+
