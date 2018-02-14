16b0731826b5d70aecc50ced968a275d4a8d6f05
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2018-02-14 19:10:44 +0100

    Try removing ci_skip

--- tests/golem/transactions/test_paymentskeeper.py
+++ tests/golem/transactions/test_paymentskeeper.py
@@ -16,7 +16,6 @@ from golem.utils import encode_hex
 from golem.tools.ci import ci_skip
 
 
-@ci_skip  # Windows gives random failures #1738
 class TestPaymentsDatabase(LogTestCase, TestWithDatabase):
 
     def test_init(self):
@@ -108,7 +107,6 @@ class TestPaymentsDatabase(LogTestCase, TestWithDatabase):
         self.assertEqual(res[2].subtask, "xyz17")
 
 
-@ci_skip  # Windows gives random failures #1738
 class TestPaymentsKeeper(TestWithDatabase):
     def test_init(self):
         pk = PaymentsKeeper()
