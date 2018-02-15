0443850710fdd96daa24b80d776ea0abe308b68d
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2018-02-15 15:55:40 +0100

    indentation

--- golem/transactions/ethereum/ethereumtransactionsystem.py
+++ golem/transactions/ethereum/ethereumtransactionsystem.py
@@ -18,13 +18,8 @@ log = logging.getLogger('golem.pay')
 class EthereumTransactionSystem(TransactionSystem):
     """ Transaction system connected with Ethereum """
 
-    def __init__(
-        self,
-        datadir,
-        node_priv_key,
-        start_geth=False,  # noqa pylint: disable=too-many-arguments
-        start_port=None,
-        address=None):
+    def __init__(self, datadir, node_priv_key, start_geth=False,  # noqa pylint: disable=too-many-arguments
+                 start_port=None, address=None):
         """ Create new transaction system instance for node with given id
         :param node_priv_key str: node's private key for Ethereum account(32b)
         """
