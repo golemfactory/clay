2e4a6bcb058bff460dcb71617a74232a2db8be45
badb <a.skrzy@gmail.com>
2018-03-15 15:30:55 +0100

    remove imunes tests

--- tests/imunes/build-images.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DOCKERFILE_DIR=`dirname $0`
-
-# Build imapp/blender based on imunes/vroot
-docker build -t imapp/blender -f ${DOCKERFILE_DIR}/Dockerfile.blender . | tee docker-blender.log
-
-# Build imapp/core beased on imapp/blender
-docker build -t imapp/core -f ${DOCKERFILE_DIR}/Dockerfile.core . | tee docker-core.log
