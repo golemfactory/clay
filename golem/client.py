a300ddd04dce52627aae81dac23ed84d36a9bccf
shadeofblue <blue@wave460.net>
2018-03-21 12:04:06 +0100

    2203 concent/forced download provider files upload (#2378)
    * initial implementation of the `ConcentFiletransferService`
    * start `ConcentFiletransferService` on golem client start
    * + update upload according to the new directive from the Concent team
    * + factory, test
    * + reaction to `ForceGetTaskResultUpload`
    * + tests
    * bump messages

--- golem/client.py
+++ golem/client.py
@@ -40,6 +40,7 @@ from golem.monitor.model.nodemetadatamodel import NodeMetadataModel
 from golem.monitor.monitor import SystemMonitor
 from golem.monitorconfig import MONITOR_CONFIG
 from golem.network.concent.client import ConcentClientService
+from golem.network.concent.filetransfers import ConcentFiletransferService
 from golem.network.history import MessageHistoryService
 from golem.network.hyperdrive.daemon_manager import HyperdriveDaemonManager
 from golem.network.p2p.node import Node
@@ -139,6 +140,9 @@ class Client(HardwarePresetsMixin):
         self.use_concent = use_concent
         self.concent_service = ConcentClientService(
             enabled=self.use_concent,
+            keys_auth=self.keys_auth,
+        )
+        self.concent_filetransfers = ConcentFiletransferService(
             keys_auth=self.keys_auth,
         )
 
@@ -252,6 +256,7 @@ class Client(HardwarePresetsMixin):
         logger.debug('Starting client services ...')
         self.environments_manager.load_config(self.datadir)
         self.concent_service.start()
+        self.concent_filetransfers.start()
 
         if self.use_monitor and not self.monitor:
             self.init_monitor()
@@ -275,6 +280,8 @@ class Client(HardwarePresetsMixin):
             if service.running:
                 service.stop()
         self.concent_service.stop()
+        if self.concent_filetransfers.running:
+            self.concent_filetransfers.stop()
         if self.task_server:
             self.task_server.task_computer.quit()
         if self.use_monitor and self.monitor:
