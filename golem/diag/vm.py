b87a87d3eb7ab775282c8b1335ea864373eb74f9
RD Projekt <info@rdprojekt.pl>
2018-03-21 12:04:51 +0100

    Resources in stats #2329
    Added hardware_memory_size and hardware_num_cores to VM diagnostics
    so that the golem-monitor can receive them.
    
    Change-Id: I70b6cf1588a400289e245ad33aea6d03e5741db7

--- golem/diag/vm.py
+++ golem/diag/vm.py
@@ -6,10 +6,11 @@ from golem.core.common import is_windows, is_osx
 from golem.diag.service import DiagnosticsProvider
 
 
+# pylint: disable=too-few-public-methods
 class VMDiagnosticsProvider(DiagnosticsProvider):
     def __init__(self):
         self.process = psutil.Process(os.getpid())
-        self.attrs = [
+        self.process_attrs = [
             'connections', 'cpu_percent', 'cpu_times', 'create_time',
             'memory_info', 'memory_percent',
             'nice', 'num_ctx_switches', 'num_threads', 'status',
@@ -17,13 +18,16 @@ class VMDiagnosticsProvider(DiagnosticsProvider):
         ]
 
         if is_windows():
-            self.attrs += ['num_handles']
+            self.process_attrs += ['num_handles']
         else:
-            self.attrs += ['uids', 'num_fds']
+            self.process_attrs += ['uids', 'num_fds']
 
         if not is_osx():
-            self.attrs += ['cpu_affinity', 'io_counters']
+            self.process_attrs += ['cpu_affinity', 'io_counters']
 
     def get_diagnostics(self, output_format):
-        data = self.process.as_dict(attrs=self.attrs)
+        data = self.process.as_dict(attrs=self.process_attrs)
+        data['hardware_num_cores'] = psutil.cpu_count()
+        # In KiB, to be consistent with max_memory_size (settings)
+        data['hardware_memory_size'] = psutil.virtual_memory().total // 1024
         return self._format_diagnostics(data, output_format)
--- tests/golem/diag/test_diagvm.py
+++ tests/golem/diag/test_diagvm.py
@@ -1,11 +1,13 @@
 import json
 from unittest import TestCase
+import psutil
 
 from golem.diag.service import DiagnosticsOutputFormat
 from golem.diag.vm import VMDiagnosticsProvider
 
 
 class TestVMDiagnosticProvider(TestCase):
+
     def test_format_outputs(self):
         provider = VMDiagnosticsProvider()
         diag = provider.get_diagnostics(DiagnosticsOutputFormat.string)
@@ -17,3 +19,15 @@ class TestVMDiagnosticProvider(TestCase):
 
         with(self.assertRaises(ValueError)):
             provider.get_diagnostics("UNKNOWN FORMAT")
+
+    def test_diagnostics_contain_hardware_info(self):
+        provider = VMDiagnosticsProvider()
+        diag = provider.get_diagnostics(DiagnosticsOutputFormat.data)
+        self.assertIn('hardware_memory_size', diag)
+        self.assertIn('hardware_num_cores', diag)
+        self.assertAlmostEqual(
+            diag['hardware_memory_size'],
+            psutil.virtual_memory().total // 1024,
+            delta=1
+        )
+        self.assertEqual(diag['hardware_num_cores'], psutil.cpu_count())
