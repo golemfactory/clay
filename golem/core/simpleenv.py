340beed1dccbc20bca308b8fdf778bf4f15806c5
badb <a.skrzy@gmail.com>
2018-03-22 17:59:58 +0100

    Remove SimpleEnv

--- golem/core/simpleenv.py
+++ golem/core/simpleenv.py
@@ -11,21 +11,3 @@ def get_local_datadir(name):
         a dir the upper layer (like Client instance).
         """
     return os.path.join(appdirs.user_data_dir('golem'), name)
-
-
-class SimpleEnv(object):
-    """ Metaclass that keeps information about golem configuration files location. """
-
-    @staticmethod
-    def env_file_name(filename):
-        """ Return full configuration file name adding configuration files location to the filename
-        :param str filename: name of a file
-        :return str: name of a file connected with path
-        """
-        # FIXME: Deprecated!
-
-        datadir = get_local_datadir('SimpleEnv')
-        if not os.path.exists(datadir):
-            os.makedirs(datadir)
-
-        return os.path.join(datadir, filename)
