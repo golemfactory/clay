648a41a29ef8a697cbd04716ca1103df97b72792
badb <a.skrzy@gmail.com>
2018-03-22 15:08:47 +0100

    Add issues to FIXME in tests

--- tests/golem/transactions/ethereum/test_paymentprocessor.py
+++ tests/golem/transactions/ethereum/test_paymentprocessor.py
@@ -57,7 +57,7 @@ class PaymentProcessorInternalTest(DatabaseFixture):
         self.sci.get_gnt_balance.return_value = 0
         self.sci.get_gntb_balance.return_value = 0
         self.sci.get_eth_address.return_value = self.addr
-        # FIXME: PaymentProcessor should be started and stopped!
+        # FIXME: PaymentProcessor should be started and stopped! #2455
         self.pp = PaymentProcessor(self.sci)
         self.pp._loopingCall.clock = Clock()  # Disable looping call.
         self.pp._gnt_converter = mock.Mock()
