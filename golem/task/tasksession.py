43502f832ee55c5206d10d1fa24f9353192f9948
mfranciszkiewicz <marek.franciszkiewicz@imapp.pl>
2018-03-25 21:16:14 +0200

    Allow private addresses in TaskSession._check_ctd_params

--- golem/task/tasksession.py
+++ golem/task/tasksession.py
@@ -831,16 +831,20 @@ class TaskSession(BasicSafeSession, ResourceHandshakeSessionMixin):
     def _check_ctd_params(self, ctd):
         header = self.task_manager.comp_task_keeper.get_task_header(
             ctd['task_id'])
+        owner = header.task_owner
+
         reasons = message.CannotComputeTask.REASON
-        if header.task_owner.key != self.key_id:
+        if owner.key != self.key_id:
             self.err_msg = reasons.WrongKey
             return False
 
-        address_valid = tcpnetwork.SocketAddress.is_proper_address(
-            header.task_owner.pub_addr,
-            header.task_owner.pub_port)
+        addresses = [
+            (owner.pub_addr, owner.pub_port),
+            (owner.prv_addr, owner.prv_port)
+        ]
 
-        if not address_valid:
+        if not any(tcpnetwork.SocketAddress.is_proper_address(addr, port)
+                   for addr, port in addresses):
             self.err_msg = reasons.WrongAddress
             return False
         return True
