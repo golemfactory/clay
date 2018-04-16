aac1ed50d92e90209f324efa69d2fc12fb5938f0
Marek Franciszkiewicz <marek@golem.network>
2018-04-16 18:47:45 +0200

    Handshake share timeout

--- golem/resource/resourcehandshake.py
+++ golem/resource/resourcehandshake.py
@@ -223,10 +223,14 @@ class ResourceHandshakeSessionMixin:
 
     def _share_handshake_nonce(self, key_id):
         handshake = self._get_handshake(key_id)
+
         options = self.task_server.get_share_options(handshake.nonce,
                                                      self.address)
+        options.timeout = self.HANDSHAKE_TIMEOUT
+
         deferred = self.resource_manager.add_file(handshake.file,
                                                   self.NONCE_TASK,
+                                                  client_options=options,
                                                   async_=True)
         deferred.addCallbacks(
             lambda res: self._nonce_shared(key_id, res, options),
