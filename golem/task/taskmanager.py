ae3a7ad441e565c2a29b82c79d6d45e3ca380fb7
Adam Mizerski <amizerski@golem.network>
2018-04-24 12:00:18 +0200

    taskmanager: fix subtask_status default value

--- golem/task/taskmanager.py
+++ golem/task/taskmanager.py
@@ -803,7 +803,7 @@ class TaskManager(TaskEventListener):
         ss.subtask_definition = ctd['short_description']
         ss.subtask_id = ctd['subtask_id']
         ss.extra_data = ctd['extra_data']
-        ss.subtask_status = TaskStatus.starting
+        ss.subtask_status = SubtaskStatus.starting
         ss.value = 0
 
         (self.tasks_states[ctd['task_id']].
