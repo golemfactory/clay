c199d0c08b6c120e0e870161e61c865af8a088bb
Marek Franciszkiewicz <marek@golem.network>
2018-04-27 14:35:33 +0200

    Linter

--- golem/config/active.py
+++ golem/config/active.py
@@ -3,7 +3,8 @@ import os
 
 from golem_sci.chains import MAINNET
 
-from golem.config.environments import GOLEM_ENVIRONMENT_VARIABLE
+from golem.config.environments import GOLEM_ENVIRONMENT_VARIABLE, \
+    CONCENT_ENVIRONMENT_VARIABLE
 from golem.core import variables
 
 if os.environ.get(GOLEM_ENVIRONMENT_VARIABLE) == MAINNET:
