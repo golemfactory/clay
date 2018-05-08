319cb28f1031e14bfde2d26b1326e106bcac90f6
Piotr Chromiec <tworec@golem.network>
2018-05-08 23:24:49 +0200

    key gen speedup

--- tests/golem/core/keygen_benchmark.py
+++ tests/golem/core/keygen_benchmark.py
@@ -5,9 +5,7 @@ from golem.core.keysauth import KeysAuth
 
 
 def skip_benchmarks():
-    if os.environ.get('benchmarks', False):
-        return False
-    return True
+    return not os.environ.get('benchmarks', False)
 
 
 def key_gen(d: int):
@@ -15,7 +13,7 @@ def key_gen(d: int):
 
 
 @pytest.mark.skipif(skip_benchmarks(), reason="skip benchmarks by default")
-@pytest.mark.parametrize("d", [10, 11, 12, 13, 14, 15, 16])
+@pytest.mark.parametrize("d", [10, 11, 12, 13, 14])
 @pytest.mark.benchmark(min_rounds=20, warmup=False)
 def test_key_gen_speed(benchmark, d: int):
     benchmark(key_gen, d)
