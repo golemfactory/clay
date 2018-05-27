19b1e95a38e503608ceda31420f15ef510883398
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2018-05-27 15:27:25 +0200

    Export magic to fixture

---
+++ tests_dist/ProcTestFixture.py
@@ -0,0 +1,72 @@
+import subprocess
+import time
+import unittest
+
+def _clean_line(line):
+    return line.decode('utf-8').replace('\n', '')
+
+def _empty_stream(_out, _in):
+    line = _in.readline()
+    while line and len(line) > 0:
+        _out.append(_clean_line(line))
+        line = _in.readline()
+
+class ProcTestFixture(unittest.TestCase):
+
+    def do_magic(self, config, args, exp_err, exp_out):
+
+        exit_code = None
+        log_err = []
+        log_out = []
+        
+        proc = subprocess.Popen(
+            args,
+            cwd='dist/' + config["dist_dir"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+
+        check_err_log = 0
+        check_err_exp = 0
+        check_out_log = 0
+        check_out_exp = 0
+        while True:
+            # streams to logs
+            _empty_stream(log_err, proc.stderr)
+            _empty_stream(log_out, proc.stdout)
+
+            # check logs for next assert
+            while len(log_err) > check_err_log:
+                if exp_err:
+                    cur_err = log_err[check_err_log]
+                    if cur_err == exp_err[check_err_exp]:
+                        # foundd expected line
+                        check_err_exp+=1
+                check_err_log+=1
+
+            while len(log_out) > check_out_log:
+                print("DEBUG: Checking log line: " + str(check_out_log))
+                if exp_out:
+                    cur_out = log_out[check_out_log]
+                    print("DEBUG: compare")
+                    print(cur_out)
+                    print(exp_out[check_out_exp])
+                    if cur_out == exp_out[check_out_exp]:
+                        # foundd expected line
+                        print("P: Found match '{}'".format(cur_out))
+                        check_out_exp+=1
+                check_out_log+=1
+
+            tick = proc.poll()
+            if tick is not None:
+                exit_code = tick
+                print("DEBUG: Exit code=" + str(tick))
+                break
+            time.sleep(0.1)
+
+        assert check_out_exp == len(exp_out)
+        print("P: All expected out lines have been found")
+
+        assert check_err_exp == len(exp_err)
+        print("P: All expected err lines have been found")
+
+        return exit_code, log_err, log_out
--- tests_dist/tests/test_version.py
+++ tests_dist/tests/test_version.py
@@ -1,89 +1,25 @@
 import json
-import time
-import subprocess
 
-def run(args, config):
-    proc = subprocess.Popen(
-        args,
-        cwd='dist/' + config["dist_dir"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
+from tests_dist.ProcTestFixture import ProcTestFixture
 
-    return proc
+class TestVersion(ProcTestFixture):
 
-class TestVersion:
-
-    @staticmethod
-    def test_golemapp_version():
+    def test_golemapp_version(self):
         print()
         print("DEBUG: Hello World")
         config = {}
         with open('./tests_dist/tests/config.json') as f:
             config = json.load(f)
-        log_err = []
-        log_out = []
-        exit_code = None
         # do magic
-        proc = run(['golemapp', '--version'], config)
+        args = ['golemapp', '--version']
         
         # assert logs in right order
-        expect_err = []
-        expect_out = [
+        exp_err = []
+        exp_out = [
             'GOLEM version: ' + config['version']
         ]
 
-        check_err_log = 0
-        check_err_exp = 0
-        check_out_log = 0
-        check_out_exp = 0
-        while True:
-            # streams to logs
-            err_line = proc.stderr.readline()
-            while err_line and len(err_line) > 0:
-                if err_line:
-                    log_err.append(err_line.decode('utf-8').replace('\n',''))
-                err_line = proc.stderr.readline()
-
-            out_line = proc.stdout.readline()
-            while out_line and len(out_line) > 0:
-                if out_line:
-                    log_out.append(out_line.decode('utf-8').replace('\n',''))
-                out_line = proc.stdout.readline()
-
-            # check logs for next assert
-            while len(log_err) > check_err_log:
-                if expect_err:
-                    cur_err = log_err[check_err_log]
-                    if cur_err == expect_err[check_err_exp]:
-                        # foundd expected line
-                        check_err_exp+=1
-                check_err_log+=1
-
-            while len(log_out) > check_out_log:
-                print("DEBUG: Checking log line: " + str(check_out_log))
-                if expect_out:
-                    cur_out = log_out[check_out_log]
-                    print("DEBUG: compare")
-                    print(cur_out)
-                    print(expect_out[check_out_exp])
-                    if cur_out == expect_out[check_out_exp]:
-                        # foundd expected line
-                        print("P: Found match '{}'".format(cur_out))
-                        check_out_exp+=1
-                check_out_log+=1
-
-            tick = proc.poll()
-            if tick is not None:
-                exit_code = tick
-                print("DEBUG: Exit code=" + str(tick))
-                break
-            time.sleep(0.1)
-
-        assert check_out_exp == len(expect_out)
-        print("P: All expected out lines have been found")
-
-        assert check_err_exp == len(expect_err)
-        print("P: All expected err lines have been found")
+        exit_code, log_err, log_out = self.do_magic(config, args, exp_err, exp_out)
 
         print("DEBUG: OUT:" + str(len(log_out)))
         print(log_out)
