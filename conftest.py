75323d0d0fa183f580e7390575e3968b9774e99d
RD Projekt <info@rdprojekt.pl>
2018-05-30 18:10:12 +0200

    PoC p.9 Add firejail test
    Change-Id: I3a479d3c6a84bf7305401ef37d46b30f78baab23

--- conftest.py
+++ conftest.py
@@ -6,14 +6,19 @@ import _pytest
 def pytest_addoption(parser: _pytest.config.Parser) -> None:
     parser.addoption("--runslow", action="store_true",
                      default=False, help="run slow tests")
+    parser.addoption("--runfirejail", action="store_true",
+                     default=False, help="run firejail tests")
 
 
 def pytest_collection_modifyitems(config: _pytest.config.Config,
                                   items: List[_pytest.main.Item]) -> None:
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
+    if not config.getoption("--runfirejail"):
+        skip_firejail = pytest.mark.skip(reason="need --runfirejail option to run")
+        for item in items:
+            if "firejail" in item.keywords:
+                item.add_marker(skip_firejail)
+    if not config.getoption("--runslow"):
+        skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+        for item in items:
+            if "slow" in item.keywords:
+                item.add_marker(skip_slow)
