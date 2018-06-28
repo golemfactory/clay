8269cd678d2dcb3ba054c6c3ae71631dc0b51e1c
Piotr Chromiec <tworec@golem.network>
2018-06-28 15:04:25 +0200

    hardware config change detection fix

--- tests/golem/core/test_hardware_presets.py
+++ tests/golem/core/test_hardware_presets.py
@@ -1,6 +1,7 @@
 from golem import testutils
-from golem.appconfig import DEFAULT_HARDWARE_PRESET_NAME, \
-    CUSTOM_HARDWARE_PRESET_NAME, MIN_MEMORY_SIZE, MIN_DISK_SPACE, MIN_CPU_CORES
+from golem.appconfig import DEFAULT_HARDWARE_PRESET_NAME as DEFAULT, \
+    CUSTOM_HARDWARE_PRESET_NAME as CUSTOM, MIN_MEMORY_SIZE, MIN_DISK_SPACE, \
+    MIN_CPU_CORES
 from golem.clientconfigdescriptor import ClientConfigDescriptor
 from golem.core.hardware import HardwarePresets
 from golem.model import HardwarePreset
@@ -14,8 +15,8 @@ class TestHardwarePresets(testutils.DatabaseFixture):
         self.config = ClientConfigDescriptor()
 
     def test_initialize(self):
-        assert HardwarePreset.get(name=DEFAULT_HARDWARE_PRESET_NAME)
-        assert HardwarePreset.get(name=CUSTOM_HARDWARE_PRESET_NAME)
+        assert HardwarePreset.get(name=DEFAULT)
+        assert HardwarePreset.get(name=CUSTOM)
 
     def test_caps(self):
         caps = HardwarePresets.caps()
@@ -25,15 +26,14 @@ class TestHardwarePresets(testutils.DatabaseFixture):
 
     def test_update_config_to_default(self):
         # given
-        _, default_preset = HardwarePresets.values(DEFAULT_HARDWARE_PRESET_NAME)
+        _, default_preset = HardwarePresets.values(DEFAULT)
 
         # when
-        config_changed = HardwarePresets.update_config(
-            DEFAULT_HARDWARE_PRESET_NAME, self.config)
+        config_changed = HardwarePresets.update_config(DEFAULT, self.config)
 
         # then
-        assert config_changed
-        assert self.config.hardware_preset_name == DEFAULT_HARDWARE_PRESET_NAME
+        assert not config_changed
+        assert self.config.hardware_preset_name == DEFAULT
         assert self.config.num_cores == default_preset['cpu_cores']
         assert self.config.max_memory_size == default_preset['memory']
         assert self.config.max_resource_size == default_preset['disk']
@@ -46,7 +46,7 @@ class TestHardwarePresets(testutils.DatabaseFixture):
         config_changed = HardwarePresets.update_config('min', self.config)
 
         # then
-        assert config_changed
+        assert not config_changed
         assert self.config.hardware_preset_name == 'min'
         assert self.config.num_cores == MIN_CPU_CORES
         assert self.config.max_memory_size == MIN_MEMORY_SIZE
@@ -61,7 +61,7 @@ class TestHardwarePresets(testutils.DatabaseFixture):
         config_changed = HardwarePresets.update_config('foo', self.config)
 
         # then
-        assert config_changed
+        assert not config_changed
         assert self.config.hardware_preset_name == 'foo'
         assert self.config.num_cores == 1
         assert self.config.max_memory_size == 1200000
@@ -76,20 +76,23 @@ class TestHardwarePresets(testutils.DatabaseFixture):
         config_changed = HardwarePresets.update_config('max', self.config)
 
         # then
-        assert config_changed
+        assert not config_changed
         assert self.config.hardware_preset_name == 'max'
         assert self.config.num_cores == caps['cpu_cores']
         assert self.config.max_memory_size == caps['memory']
         assert self.config.max_resource_size == caps['disk']
 
     def test_update_config_not_changed(self):
-        assert HardwarePresets.update_config(
-            DEFAULT_HARDWARE_PRESET_NAME, self.config)
-        assert not HardwarePresets.update_config(
-            DEFAULT_HARDWARE_PRESET_NAME, self.config)
-        assert not HardwarePresets.update_config(
-            DEFAULT_HARDWARE_PRESET_NAME, self.config)
-        assert HardwarePresets.update_config(
-            CUSTOM_HARDWARE_PRESET_NAME, self.config)
-        assert not HardwarePresets.update_config(
-            CUSTOM_HARDWARE_PRESET_NAME, self.config)
+        # given
+        HardwarePreset.create(name='foo', cpu_cores=1, memory=1200000,
+                              disk=2000000)
+
+        # than
+        assert not HardwarePresets.update_config(DEFAULT, self.config)
+        assert not HardwarePresets.update_config(DEFAULT, self.config)
+        assert not HardwarePresets.update_config(DEFAULT, self.config)
+        assert HardwarePresets.update_config(CUSTOM, self.config)
+        assert not HardwarePresets.update_config(CUSTOM, self.config)
+        assert HardwarePresets.update_config(DEFAULT, self.config)
+        assert HardwarePresets.update_config('foo', self.config)
+        assert not HardwarePresets.update_config('foo', self.config)
