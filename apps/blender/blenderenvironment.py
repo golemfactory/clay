051bd181e312e02b185ac9f25ce23446db0b9eb6
Marek Franciszkiewicz <marek@golem.network>
2018-07-12 20:53:20 +0200

    Blender NVGPU environment

--- apps/blender/blenderenvironment.py
+++ apps/blender/blenderenvironment.py
@@ -1,49 +1,56 @@
-from os import environ, path
-from typing import Tuple, List, Dict
+from os import path
+from typing import List, Dict, Optional
 
 from apps.core import nvgpu
-from golem.core.common import get_golem_path
+from golem.core.common import get_golem_path, posix_path
 from golem.docker.environment import DockerEnvironment
 
 
-def _get_image_and_tag(gpu_supported: bool) -> Tuple[str, str]:
-    if gpu_supported:
-        return "golemfactory/blender_nvgpu", "1.0"
-    return "golemfactory/blender", "1.4"
+class BlenderEnvironmentChooser:
+
+    def __new__(cls, *args, **kwargs):
+        if nvgpu.is_supported():
+            instance = BlenderNVGPUEnvironment.__new__(BlenderNVGPUEnvironment)
+        else:
+            instance = BlenderEnvironment.__new__(BlenderEnvironment)
+
+        instance.__init__()
+        return instance
 
 
 class BlenderEnvironment(DockerEnvironment):
+    DOCKER_IMAGE = "golemfactory/blender"
+    DOCKER_TAG = "1.4"
     ENV_ID = "BLENDER"
-
-    GPU_SUPPORTED = nvgpu.is_supported()
-    GPU_ENV = {
-        'DISPLAY': environ.get("DISPLAY"),
-    }
-    GPU_VOLUMES = [
-        '/tmp/.X11-unix:/tmp/.X11-unix',
-    ]
-    GPU_DEVICES = [
-        '/dev/nvidia0:/dev/nvidia0',
-        '/dev/nvidiactl:/dev/nvidiactl',
-        '/dev/nvidia-uvm:/dev/nvidia-uvm',
-    ]
-
-    DOCKER_IMAGE, DOCKER_TAG = _get_image_and_tag(GPU_SUPPORTED)
     APP_DIR = path.join(get_golem_path(), 'apps', 'blender')
     SCRIPT_NAME = "docker_blendertask.py"
     SHORT_DESCRIPTION = "Blender (www.blender.org)"
 
+
+class BlenderNVGPUEnvironment(BlenderEnvironment):
+    DOCKER_IMAGE = "golemfactory/blender_nvgpu"
+    DOCKER_TAG = "1.0"
+    SHORT_DESCRIPTION = "Blender NVGPU (www.blender.org)"
+
     def get_volumes(self) -> List[str]:
-        if not self.GPU_SUPPORTED:
-            return super().get_volumes()
-        return self.GPU_VOLUMES
+        return [
+            '/tmp/.X11-unix',
+        ]
+
+    def get_binds(self) -> Dict[str, Dict[str, str]]:
+        return {
+            posix_path('/tmp/.X11-unix'): {
+                "bind": '/tmp/.X11-unix',
+                "mode": "rw"
+            }
+        }
 
     def get_devices(self) -> List[str]:
-        if not self.GPU_SUPPORTED:
-            return super().get_devices()
-        return self.GPU_DEVICES
+        return [
+            '/dev/nvidia0:/dev/nvidia0',
+            '/dev/nvidiactl:/dev/nvidiactl',
+            '/dev/nvidia-uvm:/dev/nvidia-uvm',
+        ]
 
-    def get_environment_variables(self) -> Dict[str, str]:
-        if not self.GPU_SUPPORTED:
-            return super().get_environment_variables()
-        return self.GPU_ENV
+    def get_runtime(self) -> Optional[str]:
+        return 'nvidia'
