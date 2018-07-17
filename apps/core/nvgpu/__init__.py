27e5a86d83b8c90de25910214dfcab1dff415c51
Marek Franciszkiewicz <marek@golem.network>
2018-07-17 13:32:03 +0200

    Comprehensive checklist

--- apps/core/nvgpu/__init__.py
+++ apps/core/nvgpu/__init__.py
@@ -10,11 +10,12 @@ def is_supported(*_) -> bool:
     except Exception as exc:  # pylint: disable=broad-except
         import logging
         logger = logging.getLogger(__name__)
-        logger.debug("Error executing is_supported: %r", exc)
+        logger.warning("NVGPU Docker environment is not supported: %r", exc)
         return False
 
 
 def _is_supported(*_) -> bool:
+    # soft fail section
     if not is_linux():
         return False
 
@@ -22,13 +23,31 @@ def _is_supported(*_) -> bool:
     if not dev_nvidia:
         return False
 
+    # hard fail section
     mod_nouveau = _pipe(['lsmod'], ['grep', '-i', 'nouveau'])
     if mod_nouveau:
-        return False
+        raise RuntimeError('nouveau driver is not compatible with '
+                           'nvidia-docker')
 
     mod_nvidia = _pipe(['lsmod'], ['grep', '-i', 'nvidia'])
     if not mod_nvidia:
-        return False
+        raise RuntimeError('nvidia kernel module not loaded')
+
+    mod_nvidia_uvm = _pipe(['lsmod'], ['grep', '-i', 'nvidia_uvm'])
+    if not mod_nvidia_uvm:
+        raise RuntimeError('nvidia_uvm kernel module was not loaded')
+
+    dev_nvidia_0 = _pipe(['ls', '-l', '/dev'], ['grep', '-i', 'nvidia0'])
+    if not dev_nvidia_0:
+        raise RuntimeError('nvidia0 device not found')
+
+    dev_nvidia_uvm = _pipe(['ls', '-l', '/dev'], ['grep', '-i', 'nvidia-uvm'])
+    if not dev_nvidia_uvm:
+        raise RuntimeError('nvidia-uvm device not found')
+
+    dev_nvidia_ctl = _pipe(['ls', '-l', '/dev'], ['grep', '-i', 'nvidiactl'])
+    if not dev_nvidia_ctl:
+        raise RuntimeError('nvidiactl device not found')
 
     return True
 
