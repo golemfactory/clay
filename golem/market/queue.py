967761f3ee2ed4c3178a8bb850b6c05a8501a26c
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2018-08-16 16:54:47 +0200

    first queue

---
+++ golem/market/queue.py
@@ -0,0 +1,10 @@
+class MarketQueue:
+
+    def __init__():
+        self.queue = []
+
+    def push(msg):
+        self.queue.push(msg)
+    
+    def pop():
+        return self.queue.pop()        
