f099a60e4cd25f392b5884d05050d6c34594e557
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2018-08-17 11:05:50 +0200

    WIP singleton

--- golem/market/queue.py
+++ golem/market/queue.py
@@ -1,10 +1,20 @@
 class MarketQueue:
+    class __MarketQueue:
+        def __init__():
+            self.queue = queue.Queue()
 
-    def __init__():
-        self.queue = []
+    instance = None
 
-    def push(msg):
-        self.queue.push(msg)
+    def __new__(cls): # __new__ always a classmethod
+        if not MarketQueue.instance:
+            MarketQueue.instance = MarketQueue.__MarketQueue()
+
+    @classmethod
+    def push(cls, msg):
+        instance.queue.put(msg)
     
-    def pop():
-        return self.queue.pop()        
+    @classmethod
+    def tick(cls):
+        if not instance.queue.empty():
+            return instance.queue.get()        
+        return None
