b8c0e8515ee248dfa00fc2f289e8b33dd460144c
Marek Franciszkiewicz <marek@golem.network>
2018-08-30 15:18:51 +0200

    take_when method

--- golem/market/offer_pool.py
+++ golem/market/offer_pool.py
@@ -1,5 +1,6 @@
 import logging
-from typing import List, Dict, Any
+import time
+from typing import List, Dict, Any, Optional, Union
 
 from twisted.internet import task, defer
 from twisted.internet.defer import Deferred
@@ -9,13 +10,23 @@ from golem.core.common import each
 logger = logging.getLogger(__name__)
 
 
+PoolType = List[Any]
+
+
 class OfferPool:
 
-    _pools: Dict[str, List[Any]] = dict()
+    _TAKE_INTERVAL: float = 1.0  # s
+    _pools: Dict[str, PoolType] = dict()
 
     @classmethod
     def contains(cls, key: str) -> bool:
         return key in cls._pools
+
+    @classmethod
+    def size(cls, key: str) -> int:
+        if not cls.contains(key):
+            return 0
+        return len(cls._pools[key])
 
     @classmethod
     def add(cls, key: str, *items) -> None:
@@ -23,25 +34,69 @@ class OfferPool:
         each(pool.append, items)
 
     @classmethod
-    def get(cls, key: str) -> List[Any]:
-        if key in cls._pools:
-            return cls._pools[key][:]
-        return []
+    def peek(cls, key: str, count: int = 0) -> PoolType:
+        if not cls.contains(key):
+            return []
+
+        pool = cls._pools[key]
+        idx = cls._rev_index(pool, count)
+        return pool[:idx]
 
     @classmethod
-    def drain(cls, key: str) -> List[Any]:
-        elements: List[Any] = cls.get(key)
-        if key in cls._pools:
+    def drain(cls, key: str) -> PoolType:
+        elements = cls.peek(key)
+        if cls.contains(key):
             del cls._pools[key]
         return elements
 
     @classmethod
-    def drain_after(cls, key: str, timeout: float) -> Deferred:
+    def drain_after(cls, key: str, timeout: Union[int, float]) -> Deferred:
         from twisted.internet import reactor
         return task.deferLater(reactor, timeout, cls.drain, key)
 
     @classmethod
-    def _pool(cls, key: str) -> List[Any]:
-        if key not in cls._pools:
+    def take_when(cls,
+                  key: str,
+                  count: int,
+                  timeout: Union[int, float]) -> Deferred:
+
+        result = Deferred()
+        cls._take(result, key, count, deadline=time.time() + timeout)
+        return result
+
+    @classmethod
+    def _take(cls,
+              result: Deferred,
+              key: str,
+              count: int,
+              deadline: Union[int, float] = 0) -> None:
+
+        if deadline and time.time() >= deadline:
+            result.errback(TimeoutError('OfferPool._take timed out'))
+        elif cls.size(key) >= count:
+            offers = cls._shift(key, count)
+            result.callback(offers)
+        else:
+            from twisted.internet import reactor
+            reactor.callLater(cls._TAKE_INTERVAL, cls._take,
+                              result, key, count, deadline)
+
+    @classmethod
+    def _pool(cls, key: str) -> PoolType:
+        if not cls.contains(key):
             cls._pools[key] = list()
         return cls._pools[key]
+
+    @classmethod
+    def _shift(cls, key: str, count: int) -> PoolType:
+        pool = cls._pools[key]
+        idx = cls._rev_index(pool, count)
+        elements, cls._pools[key] = pool[:idx], pool[idx:]
+        return elements
+
+    @classmethod
+    def _rev_index(cls, pool: PoolType, count: int) -> int:
+        size = len(pool)
+        if count in (0, size):
+            return size
+        return min(0, count - size) or size
--- tests/golem/market/test_offer_pool.py
+++ tests/golem/market/test_offer_pool.py
@@ -1,6 +1,7 @@
 import uuid
 from unittest import TestCase, mock
 
+from freezegun import freeze_time
 from twisted.internet.task import Clock
 
 from golem.core.common import each
@@ -9,12 +10,25 @@ from golem.market.offer_pool import OfferPool
 
 class TestOfferPool(TestCase):
 
+    def test_peek(self):
+        key = str(uuid.uuid4())
+        data = list(range(1, 11))
+
+        OfferPool.add(key, *data)
+
+        assert OfferPool.peek(key) == data
+        assert OfferPool.peek(key, 0) == data
+        assert OfferPool.peek(key, 1000) == data
+        assert OfferPool.peek(key, -1) == []
+        assert OfferPool.peek(key, 1) == data[:1]
+        assert OfferPool.peek(key, 5) == data[:5]
+
     def test_add_and_drain(self):
         key = str(uuid.uuid4())
         data = list(range(1, 11))
 
         OfferPool.add(key, *data)
-        assert OfferPool.get(key) == data
+        assert OfferPool.peek(key) == data
         assert OfferPool.contains(key)
 
         assert OfferPool.drain(key) == data
@@ -27,13 +41,56 @@ class TestOfferPool(TestCase):
         data = list(range(1, 11))
         result = []
 
-        def callback(r):
-            each(result.append, *r)
-
-        OfferPool.add(key, data)
+        OfferPool.add(key, *data)
         deferred = OfferPool.drain_after(key, 3.50)
-        deferred.addCallback(callback)
+        deferred.addCallback(lambda r: each(result.append, r))
+        deferred.addErrback(self.fail)
+
         clock.advance(10)
 
         assert deferred.called
         assert result == data
+
+    @mock.patch('twisted.internet.reactor', new_callable=Clock, create=True)
+    def test_take(self, clock):
+        key = str(uuid.uuid4())
+        data = list(range(1, 11))
+
+        count = 3
+        result = []
+
+        deferred = OfferPool.take_when(key, count, 60.0)
+        deferred.addCallback(lambda r: each(result.append, r))
+        deferred.addErrback(self.fail)
+
+        clock.advance(1)
+        assert not result
+
+        OfferPool.add(key, data[0])
+        clock.advance(1)
+        assert not result
+
+        OfferPool.add(key, data[1])
+        clock.advance(1)
+        assert not result
+
+        OfferPool.add(key, data[2])
+        clock.advance(1)
+        assert deferred.called
+        assert result == data[:count]
+
+    @freeze_time("2020-01-01 00:00:00")
+    @mock.patch('twisted.internet.reactor', new_callable=Clock, create=True)
+    def test_take_timeout(self, clock):
+        key = str(uuid.uuid4())
+        result = []
+
+        deferred = OfferPool.take_when(key, 10, 5.0)
+        deferred.addCallback(self.fail)
+        deferred.addErrback(lambda _: result.append(True))
+
+        with freeze_time("2020-01-01 00:01:00"):
+            clock.advance(10)
+
+        assert result
+        assert result[0] is True
