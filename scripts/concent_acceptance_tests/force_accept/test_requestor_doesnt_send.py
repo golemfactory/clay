52d713376afb196c0798951cfe4251fb08a110b7
Karol Beker <karolbeker@gmail.com>
2018-09-12 12:47:04 +0200

    Add sleep to force_accept Concent's test

--- scripts/concent_acceptance_tests/force_accept/test_requestor_doesnt_send.py
+++ scripts/concent_acceptance_tests/force_accept/test_requestor_doesnt_send.py
@@ -1,4 +1,6 @@
 # pylint: disable=protected-access,no-member
+import time
+
 import calendar
 import datetime
 import logging
@@ -90,6 +92,7 @@ class RequestorDoesntSendTestCase(SCIBaseTest):
             rct_kwargs = {}
         price = random.randint(1 << 20, 10 << 20)
         self.requestor_put_deposit(price)
+        time.sleep(120)
         rct_kwargs['task_to_compute__price'] = price
         report_computed_task = self.prepare_report_computed_task(
             mode=mode,
