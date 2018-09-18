2bc7530e59eb9ca93e2bc7f0e41e2b6196a84063
inexxt <jac.karwowski@gmail.com>
2018-09-18 19:16:56 +0200

    original_dir update

--- apps/runf/resources/scripts/docker_runf.py
+++ apps/runf/resources/scripts/docker_runf.py
@@ -2,15 +2,23 @@ import os
 
 # pylint: disable=import-error
 import params  # This module is generated before this script is run
-from golem_remote import decode_str_to_obj, encode_obj_to_str
+import golem_remote as golem
+from golem_remote.runf_helpers import SubtaskData
 
 # sys.path.append(os.path.join(params.RESOURCES_DIR, "code"))
 # sys.path.append(os.path.join(params.WORK_DIR))  # for messages.py
 
-data = decode_str_to_obj(params.data)
+data: SubtaskData = golem.decode_str_to_obj(params.data)
+
+
+# black magic
+import builtins  # pylint: disable=wrong-import-position
+golem.open_file.orig_open = open
+builtins.open = golem.open_file.open_file(original_dir=data.original_dir)
+
 
 solution = data.function(*data.args, **data.kwargs)
-solution = encode_obj_to_str(solution)
+solution = golem.encode_obj_to_str(solution)
 
 result_path = os.path.join(params.OUTPUT_DIR, f"out.{params.RESULT_EXT}")
 
