1fa71f4ad35af8bf0c668508a5dfa3e75f789010
inexxt <jac.karwowski@gmail.com>
2018-09-21 11:38:59 +0200

    Attempting to remove pyssim

--- apps/rendering/resources/imgverifier.py
+++ apps/rendering/resources/imgverifier.py
@@ -4,88 +4,8 @@ import math
 
 from apps.rendering.resources.imgrepr import (ImgRepr, PILImgRepr)
 
-from golem_verificator.verifier import SubtaskVerificationState
-
-from ssim import compute_ssim
 
 logger = logging.getLogger("apps.rendering")
-
-
-class ImgStatistics(object):
-    def __init__(self, base_img: ImgRepr, img: ImgRepr):
-        if base_img.get_size() != img.get_size():
-            raise ValueError('base_img and img are of different sizes.')
-
-        self.img = img
-        self.ssim = compute_ssim(base_img.to_pil(), self.img.to_pil())
-        self.mse, self.norm_mse = \
-            self._calculate_color_normalized_mse(base_img, self.img)
-        self.mse_bw, norm_mse_bw = \
-            self._calculate_greyscale_normalized_mse(base_img, self.img)
-        self.psnr = self._calculate_psnr(self.mse)
-
-    @property
-    def name(self) -> str:
-        name = None
-        if isinstance(self.img, PILImgRepr):
-            name = self.img.get_name()
-
-        return name
-
-    def _calculate_greyscale_normalized_mse(self, img1: ImgRepr, img2: ImgRepr):
-        (res_x, res_y) = img1.get_size()
-
-        img1_bw = img1.to_pil().convert('L')  # makes it greyscale
-        img2_bw = img2.to_pil().convert('L')  # makes it greyscale
-
-        import numpy
-        npimg1 = numpy.array(img1_bw)
-        npimg2 = numpy.array(img2_bw)
-
-        npimg1 = npimg1.astype(numpy.float32, copy=False)
-        npimg2 = npimg2.astype(numpy.float32, copy=False)
-
-        mse_bw = 0
-        for i in range(len(npimg1)):
-            for j in range(len(npimg1[0])):
-                mse_bw += (npimg1[i][j] - npimg2[i][j]) \
-                          * (npimg1[i][j] - npimg2[i][j])
-
-        mse_bw /= res_x * res_y
-
-        # max value of pixel is 255
-        max_possible_mse = res_x * res_y * 255
-        norm_mse = mse_bw / max_possible_mse
-
-        return mse_bw, norm_mse
-
-    def _calculate_color_normalized_mse(self, img1, img2):
-        mse = 0
-        (res_x, res_y) = img1.get_size()
-
-        for i in range(0, res_x):
-            for j in range(0, res_y):
-                [r1, g1, b1] = img1.get_pixel((i, j))
-                [r2, g2, b2] = img2.get_pixel((i, j))
-                mse += (r1 - r2) * (r1 - r2) + \
-                       (g1 - g2) * (g1 - g2) + \
-                       (b1 - b2) * (b1 - b2)
-
-        mse /= res_x * res_y * 3
-
-        # max value of pixel is 255
-        max_possible_mse = res_x * res_y * 3 * 255
-        norm_mse = mse / max_possible_mse
-
-        return mse, norm_mse
-
-    def _calculate_psnr(self, mse, max_=255):
-        if mse <= 0 or max_ <= 0:
-            raise ValueError("MSE & MAX_ must be higher than 0")
-        return 20 * math.log10(max_) - 10 * math.log10(mse)
-
-    def get_stats(self):
-        return self.ssim, self.mse, self.norm_mse, self.mse_bw, self.psnr
 
 
 class ImgVerifier(object):
@@ -136,19 +56,3 @@ class ImgVerifier(object):
 
         crop_window = (start[0], start[0] + dx, start[1], start[1] + dy)
         return crop_window
-
-    def is_valid_against_reference(self,
-                                   imgStat, reference_imgStat,
-                                   acceptance_ratio=0.75, maybe_ratio=0.65):
-
-        if not isinstance(imgStat, ImgStatistics) \
-                and not isinstance(reference_imgStat, ImgStatistics):
-            raise TypeError("imgStatistics be instance of ImgStatistics")
-
-        if imgStat.ssim > acceptance_ratio * reference_imgStat.ssim:
-            return SubtaskVerificationState.VERIFIED
-
-        if imgStat.ssim > maybe_ratio * reference_imgStat.ssim:
-            return SubtaskVerificationState.UNKNOWN
-
-        return SubtaskVerificationState.WRONG_ANSWER
