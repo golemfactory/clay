4736bff6e24a5239c767fc7f499ccc3b2091bd7c
Igor Adamski <igor@golem.network>
2018-09-28 15:31:04 +0200

    mute pylint

--- golem/marketplace/__init__.py
+++ golem/marketplace/__init__.py
@@ -1,1 +1,1 @@
-from rust.golem import marketplace__pick_provider as pick_provider  # noqa
+from rust.golem import marketplace__pick_provider as pick_provider  # noqa pylint: disable=no-name-in-module,import-error
