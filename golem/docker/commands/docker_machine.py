ee6fa1dd14588eb038b203fe5d102a13785124c1
Adam Wierzbicki <awierzbicki@golem.network>
2018-10-01 15:43:19 +0200

    Start VM means `start` not `restart`
    Changed the handling of `start` command in DockerMachineCommandHandler
    so that it calls `docker-machine start` instead of `restart`. It was
    not working correctly because Hyper-V driver for docker-machine tries
    to connect to a VM via SSH before stopping it and this would hang up
    forever if the VM is not running in the first place.
    
    Added a separate `restart` command which is used in `_recover()`.

--- golem/docker/commands/docker_machine.py
+++ golem/docker/commands/docker_machine.py
@@ -6,14 +6,15 @@ class DockerMachineCommandHandler(DockerCommandHandler):
     commands: CommandDict = dict(
         create=['docker-machine', 'create'],
         rm=['docker-machine', 'rm', '-y'],
-        start=['docker-machine', 'restart'],
+        start=['docker-machine', 'start'],
         stop=['docker-machine', 'stop'],
         active=['docker-machine', 'active'],
         list=['docker-machine', 'ls', '-q'],
         env=['docker-machine', 'env'],
         status=['docker-machine', 'status'],
         inspect=['docker-machine', 'inspect'],
-        regenerate_certs=['docker-machine', 'regenerate-certs', '--force']
+        regenerate_certs=['docker-machine', 'regenerate-certs', '--force'],
+        restart=['docker-machine', 'restart'],
     )
 
     commands.update(DockerCommandHandler.commands)
