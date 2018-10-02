f6d42000bb9018fcedb968a6b2d9e38e2909e7c4
Marek Franciszkiewicz <marek@golem.network>
2018-10-02 14:44:12 +0200

    ProviderComputeTimer -> instantiable ComputeTimer

--- golem/task/timer.py
+++ golem/task/timer.py
@@ -1,40 +1,38 @@
 import logging
 import time
-from typing import ClassVar, Optional
+from typing import Optional
 
 
 logger = logging.getLogger(__name__)
 
 
-class ProviderComputeTimer:
-    """ Keeps track of provider's computation / idle time per Golem session """
+class ComputeTimer:
+    """ Keeps track of computation / idle time per Golem session """
 
-    _time_comp: ClassVar[float] = 0
-    _time_idle: ClassVar[float] = 0
+    def __init__(self):
+        self._time_comp: float = 0.
+        self._time_idle: float = 0.
 
-    _time_started: ClassVar[Optional[float]] = None
-    _time_finished: ClassVar[Optional[float]] = time.time()
+        self._time_started: Optional[float] = None
+        self._time_finished: Optional[float] = time.time()
 
-    @classmethod
-    def time_computing(cls) -> float:
+    def time_computing(self) -> float:
         """ Returns the total computation time. If computing, returns the
             accumulated value enlarged by the time since computation started.
         """
-        if cls._time_started is None:
-            return cls._time_comp
-        return cls._time_comp + time.time() - cls._time_started
+        if self._time_started is None:
+            return self._time_comp
+        return self._time_comp + time.time() - self._time_started
 
-    @classmethod
-    def time_idle(cls) -> float:
+    def time_idle(self) -> float:
         """ Returns the total idle time. If not computing, returns the
             accumulated value enlarged by the time since last computation.
         """
-        if cls._time_finished is None:
-            return cls._time_idle
-        return cls._time_idle + time.time() - cls._time_finished
+        if self._time_finished is None:
+            return self._time_idle
+        return self._time_idle + time.time() - self._time_finished
 
-    @classmethod
-    def start(cls) -> None:
+    def start(self) -> None:
         """ Updates the state to keep track of computation time and increases
             the accumulated idle time.
 
@@ -43,26 +41,28 @@ class ProviderComputeTimer:
 
         now = time.time()
 
-        if cls._time_finished is None:
+        if self._time_finished is None:
             logger.error("Computation was not finished")
         else:
-            cls._time_idle += now - cls._time_finished
+            self._time_idle += now - self._time_finished
 
-        cls._time_started = now
-        cls._time_finished = None
+        self._time_started = now
+        self._time_finished = None
 
-    @classmethod
-    def stop(cls) -> None:
+    def stop(self) -> None:
         """ Updates the state to keep track of idle time and increases
             the accumulated computation time.
         """
 
-        if cls._time_started is None:
+        if self._time_started is None:
             logger.debug("Computation is not running")
             return
 
         now = time.time()
 
-        cls._time_comp += now - cls._time_started
-        cls._time_started = None
-        cls._time_finished = now
+        self._time_comp += now - self._time_started
+        self._time_started = None
+        self._time_finished = now
+
+
+ProviderComputeTimer = ComputeTimer()  # noqa
