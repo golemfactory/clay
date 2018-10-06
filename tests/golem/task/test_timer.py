a377d48b4aa929d489128da79cc05e0439bda943
Marek Franciszkiewicz <marek@golem.network>
2018-10-06 16:27:45 +0200

    IdleTimer now only stores the start and finished computation timestamps

--- tests/golem/task/test_timer.py
+++ tests/golem/task/test_timer.py
@@ -13,43 +13,29 @@ class TestIdleTimer(unittest.TestCase):
     @freeze_time("2018-01-01 00:00:00", as_arg=True)
     def test_comp_started(frozen_time, _):
         timer = IdleTimer()
-        assert timer._last_comp_finished
+        assert not timer.last_comp_started
+        assert timer.last_comp_finished
 
         timer.comp_started()
-        assert not timer._last_comp_finished
+        assert timer.last_comp_started
+        assert not timer.last_comp_finished
 
         frozen_time.tick(timedelta(seconds=5))
         timer.comp_started()
-        assert not timer._last_comp_finished
+        assert timer.last_comp_started
+        assert not timer.last_comp_finished
 
     @freeze_time("2018-01-01 00:00:00", as_arg=True)
     def test_comp_finished(frozen_time, _):
         timer = IdleTimer()
-        finished = timer._last_comp_finished
+        finished = timer.last_comp_finished
 
         timer.comp_finished()
 
-        assert timer._last_comp_finished == finished
+        assert timer.last_comp_finished == finished
 
         timer.comp_started()
         frozen_time.tick(timedelta(seconds=5))
         timer.comp_finished()
 
-        assert timer._last_comp_finished == finished + 5.
-
-    @freeze_time("2018-01-01 00:00:00", as_arg=True)
-    def test_idle_time(frozen_time, _):
-        timer = IdleTimer()
-        frozen_time.tick(timedelta(seconds=5))
-
-        timer.comp_started()
-        assert timer.time_idle() == 5.
-
-        frozen_time.tick(timedelta(seconds=5))
-        timer.comp_finished()
-
-        assert timer.time_idle() == 5.
-
-        frozen_time.tick(timedelta(seconds=5))
-        timer.comp_started()
-        assert timer.time_idle() == 10.
+        assert timer.last_comp_finished == finished + 5.
