5a065730debe9914364a0be06d0d75f9bc2a3771
Marek Franciszkiewicz <mfranciszkiewicz@users.noreply.github.com>
2018-10-12 11:38:02 +0200

    Marketplace: requestor tracks providers' computation time (#3384)

--- golem/task/timer.py
+++ golem/task/timer.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from typing import Optional
+from typing import Optional, Dict, Tuple
 
 logger = logging.getLogger(__name__)
 
@@ -42,4 +42,62 @@ class IdleTimer:
             logger.warning("Computation is not running")
 
 
+ComputeTimeTuple = Tuple[float, Optional[float]]
+
+
+class ComputeTimers:
+    """ Keeps track of subtask computation time per Golem session """
+
+    def __init__(self):
+        self._comp_history: Dict[str, ComputeTimeTuple] = dict()
+
+    def time_computing(self, identifier: str) -> Optional[float]:
+        """ Returns computation time per subtask. Returns None if computation
+            hasn't finished yet. Throws a KeyError if identifier is not known.
+        """
+
+        return self._comp_time(self._comp_history[identifier])
+
+    def comp_started(self, identifier: str) -> None:
+        """ Initializes the start and finished (= None) computation points in
+            time.
+        """
+        logger.debug("ComputeTimers: started computation of %s at %r",
+                     identifier, time.time())
+
+        self._comp_history[identifier] = (time.time(), None)
+
+    def comp_finished(self, identifier: str) -> None:
+        """ Updates the finished (= None) computation point in time
+            for an identifier and returns the computation time.
+        """
+
+        if identifier not in self._comp_history:
+            return
+
+        logger.debug("ComputeTimers: finished computation of %s at %r",
+                     identifier, time.time())
+
+        entry = self._comp_history[identifier]
+        if entry[1] is not None:
+            return
+
+        self._comp_history[identifier] = (entry[0], time.time())
+
+    def remove(self, identifier: str) -> Optional[float]:
+        """ Removes the identifier from history. Throws a KeyError if identifier
+            is not known.
+        """
+
+        entry = self._comp_history.pop(identifier)
+        return self._comp_time(entry)
+
+    @staticmethod
+    def _comp_time(entry: ComputeTimeTuple) -> Optional[float]:
+        if entry[1] is None:
+            return None
+        return entry[1] - entry[0]
+
+
 ProviderIdleTimer = IdleTimer()  # noqa
+ProviderComputeTimers = ComputeTimers()  # noqa
--- tests/golem/task/test_timer.py
+++ tests/golem/task/test_timer.py
@@ -1,9 +1,11 @@
+import time
 import unittest
+import uuid
 from datetime import timedelta
 
 from freezegun import freeze_time
 
-from golem.task.timer import IdleTimer
+from golem.task.timer import IdleTimer, ComputeTimers
 
 
 class TestIdleTimer(unittest.TestCase):
@@ -39,3 +41,63 @@ class TestIdleTimer(unittest.TestCase):
         timer.comp_finished()
 
         assert timer.last_comp_finished == finished + 5.
+
+
+class TestComputeTimers(unittest.TestCase):
+
+    # pylint: disable=no-member,no-self-argument
+
+    @freeze_time("2018-01-01 00:00:00", as_arg=True)
+    def test_comp_started(frozen_time, _):
+        timer = ComputeTimers()
+        identifier = str(uuid.uuid4())
+
+        frozen_time.tick(timedelta(seconds=5))
+        timer.comp_started(identifier)
+
+        entry = timer._comp_history[identifier]
+        assert entry == (time.time(), None)
+
+        frozen_time.tick(timedelta(seconds=5))
+        timer.comp_started(identifier)
+
+        entry = timer._comp_history[identifier]
+        assert entry == (time.time(), None)
+
+    @freeze_time("2018-01-01 00:00:00", as_arg=True)
+    def test_comp_finished(frozen_time, self):
+        timer = ComputeTimers()
+        identifier = str(uuid.uuid4())
+
+        with self.assertRaises(KeyError):
+            assert timer.time_computing(identifier) is None
+
+        timer.comp_started(identifier)
+        frozen_time.tick(timedelta(seconds=5))
+        timer.comp_finished(identifier)
+        assert timer.time_computing(identifier) == 5
+
+        # second call should return the same value
+        timer.comp_finished(identifier)
+        assert timer.time_computing(identifier) == 5
+
+    @freeze_time("2018-01-01 00:00:00", as_arg=True)
+    def test_remove(frozen_time, self):
+        timer = ComputeTimers()
+        identifier = str(uuid.uuid4())
+
+        with self.assertRaises(KeyError):
+            timer.remove(identifier)
+
+        timer.comp_started(identifier)
+        frozen_time.tick(timedelta(seconds=5))
+        assert timer.remove(identifier) is None
+
+        with self.assertRaises(KeyError):
+            timer.remove(identifier)
+
+        timer.comp_started(identifier)
+        frozen_time.tick(timedelta(seconds=5))
+        timer.comp_finished(identifier)
+
+        assert timer.remove(identifier) == 5
