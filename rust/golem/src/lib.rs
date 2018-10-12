cad04e875fb6354b787af1400c2276c56054dd6f
Igor Adamski <igor@golem.network>
2018-10-12 14:02:00 +0200

    Simplify

--- rust/golem/src/lib.rs
+++ rust/golem/src/lib.rs
@@ -1,16 +1,12 @@
 #[macro_use]
 extern crate cpython;
 
-use cpython::{PyList, PyResult, Python};
+use cpython::{PyResult, Python};
 
 mod marketplace;
 
 #[allow(non_snake_case)]
-fn marketplace__order_providers(py: Python, pyoffers: PyList) -> PyResult<Vec<usize>> {
-    let mut offers = Vec::with_capacity(pyoffers.len(py));
-    for pyoffer in pyoffers.iter(py) {
-        offers.push(pyoffer.extract::<f64>(py)?);
-    }
+fn marketplace__order_providers(_py: Python, offers: Vec<f64>) -> PyResult<Vec<usize>> {
     Ok(marketplace::order_providers(&offers))
 }
 
@@ -19,7 +15,7 @@ py_module_initializer!(libgolem, initlibgolem, PyInit_golem, |py, m| {
     try!(m.add(
         py,
         "marketplace__order_providers",
-        py_fn!(py, marketplace__order_providers(offers: PyList))
+        py_fn!(py, marketplace__order_providers(offers: Vec<f64>))
     ));
     Ok(())
 });
