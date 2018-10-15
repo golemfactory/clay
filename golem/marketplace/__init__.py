4801ca1a7c94c5776a22463c0d286c9e51977750
Igor Adamski <igor@golem.network>
2018-10-15 15:58:23 +0200

    Implement provider ordering algorithm

--- golem/marketplace/__init__.py
+++ golem/marketplace/__init__.py
@@ -1,1 +1,8 @@
 from rust.golem import marketplace__order_providers as order_providers  # noqa pylint: disable=no-name-in-module,import-error
+
+
+class Offer:
+    def __init__(self, scaled_price: float) -> None:
+        self.scaled_price = scaled_price
+        self.reputation = 0.0
+        self.quality = (0.0, 0.0, 0.0, 0.0)
