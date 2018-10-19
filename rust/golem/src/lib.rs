376382a5f4531f828bf20f5f6d3b996a3ec8f66a
Krigpl <3904252+Krigpl@users.noreply.github.com>
2018-10-19 16:05:34 +0200

    Implement provider ordering algorithm (#3453)

--- rust/golem/src/lib.rs
+++ rust/golem/src/lib.rs
@@ -1,25 +1,39 @@
 #[macro_use]
 extern crate cpython;
 
-use cpython::{PyResult, Python};
+use cpython::{FromPyObject, ObjectProtocol, PyObject, PyResult, Python};
 
 mod marketplace;
 
-#[allow(non_snake_case)]
-fn marketplace__order_providers(_py: Python, offers: Vec<f64>) -> PyResult<Vec<usize>> {
-    let offers: Vec<marketplace::Offer> = offers
-        .iter()
-        .map(|price| marketplace::Offer::new(*price))
-        .collect();
-    Ok(marketplace::order_providers(&offers))
+impl<'source> FromPyObject<'source> for marketplace::Offer {
+    fn extract(py: Python, obj: &'source PyObject) -> PyResult<Self> {
+        let quality = obj
+            .getattr(py, "quality")?
+            .extract::<(f64, f64, f64, f64)>(py)?;
+        Ok(marketplace::Offer {
+            scaled_price: obj.getattr(py, "scaled_price")?.extract::<f64>(py)?,
+            reputation: obj.getattr(py, "reputation")?.extract::<f64>(py)?,
+            quality: marketplace::Quality {
+                s: quality.0,
+                t: quality.1,
+                f: quality.2,
+                r: quality.3,
+            },
+        })
+    }
 }
 
-py_module_initializer!(libgolem, initlibgolem, PyInit_golem, |py, m| {
-    try!(m.add(py, "__doc__", "Parts of Golem core implemented in Rust"));
+py_module_initializer!(libgolem, initlibgolem, PyInit_golem, |_py, m| {
+    try!(m.add(_py, "__doc__", "Parts of Golem core implemented in Rust"));
     try!(m.add(
-        py,
+        _py,
         "marketplace__order_providers",
-        py_fn!(py, marketplace__order_providers(offers: Vec<f64>))
+        py_fn!(
+            _py,
+            marketplace__order_providers(offers: Vec<marketplace::Offer>) -> PyResult<Vec<usize>> {
+                Ok(marketplace::order_providers(offers))
+            }
+        )
     ));
     Ok(())
 });
--- tests/golem/marketplace/test_marketplace.py
+++ tests/golem/marketplace/test_marketplace.py
@@ -1,5 +1,10 @@
-from golem.marketplace import order_providers
+from golem.marketplace import order_providers, Offer
 
 
 def test_order_providers():
-    assert order_providers([2.0, 1.8, 5.0, 4.4]) == [1, 0, 3, 2]
+    offer0 = Offer(scaled_price=2.0)
+    offer1 = Offer(scaled_price=1.8)
+    offer2 = Offer(scaled_price=4.4)
+    res = order_providers([offer0, offer1, offer2])
+    # Actual order is not important, just that it is a permutation
+    assert sorted(res) == list(range(3))
