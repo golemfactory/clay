9ac026cfa36024d4fcbf58f3a2a029b996849c0c
Marek Franciszkiewicz <marek@golem.network>
2018-10-21 21:05:42 +0200

    Refactor timer and timer method names

--- tests/golem/task/test_timer.py
+++ tests/golem/task/test_timer.py
@@ -1,119 +1,125 @@
-import time
 import unittest
 import uuid
 from datetime import timedelta
 
 from freezegun import freeze_time
 
-from golem.task.timer import IdleTimer, ComputeTimers
+from golem.task.timer import ActionTimer, ActionTimers, ThirstTimer
 
 
-class TestIdleTimer(unittest.TestCase):
+class TestActionTimer(unittest.TestCase):
 
     # pylint: disable=no-member,no-self-argument
 
     @freeze_time("2018-01-01 00:00:00", as_arg=True)
-    def test_comp_started(frozen_time, _):
-        timer = IdleTimer()
-        assert not timer.last_comp_started
-        assert timer.last_comp_finished
+    def test_started(frozen_time, _):
+        timer = ActionTimer()
+        assert not timer._started
+        assert timer._finished
 
-        timer.comp_started()
-        assert timer.last_comp_started
-        assert not timer.last_comp_finished
+        timer.start()
+        assert timer._started
+        assert not timer._finished
 
         frozen_time.tick(timedelta(seconds=5))
-        timer.comp_started()
-        assert timer.last_comp_started
-        assert not timer.last_comp_finished
+        timer.start()
+        assert timer._started
+        assert not timer._finished
 
     @freeze_time("2018-01-01 00:00:00", as_arg=True)
-    def test_comp_finished(frozen_time, _):
-        timer = IdleTimer()
-        finished = timer.last_comp_finished
+    def test_finished(frozen_time, _):
+        timer = ActionTimer()
+        finished = timer._finished
 
-        timer.comp_finished()
+        timer.finish()
 
-        assert timer.last_comp_finished == finished
+        assert timer._finished == finished
 
-        timer.comp_started()
+        timer.start()
         frozen_time.tick(timedelta(seconds=5))
-        timer.comp_finished()
+        timer.finish()
 
-        assert timer.last_comp_finished == finished + 5.
+        assert timer._finished == finished + 5.
+
+
+class TestThirstTimer(unittest.TestCase):
 
     @freeze_time("2018-01-01 00:00:00", as_arg=True)
     def test_thirst_decrease(frozen_time, _):
-        timer = IdleTimer()
+        timer = ThirstTimer()
         thirst = timer.thirst
         frozen_time.tick(timedelta(seconds=5))
         assert thirst > timer.thirst
 
     @freeze_time("2018-01-01 00:00:00", as_arg=True)
     def test_thirst_increase(frozen_time, _):
-        timer = IdleTimer()
+        timer = ThirstTimer()
         thirst = timer.thirst
-        timer.comp_started()
+        timer.start()
         frozen_time.tick(timedelta(seconds=5))
-        timer.comp_finished()
+        timer.finish()
         assert thirst < timer.thirst
 
 
-class TestComputeTimers(unittest.TestCase):
+class TestActionTimers(unittest.TestCase):
 
     # pylint: disable=no-member,no-self-argument
 
     @freeze_time("2018-01-01 00:00:00", as_arg=True)
-    def test_comp_started(frozen_time, _):
-        timer = ComputeTimers()
+    def test_started(frozen_time, _):
+        timers = ActionTimers()
         identifier = str(uuid.uuid4())
 
         frozen_time.tick(timedelta(seconds=5))
-        timer.comp_started(identifier)
+        timers.start(identifier)
 
-        entry = timer._comp_history[identifier]
-        assert entry == (time.time(), None)
+        timer = timers._history[identifier]
+        assert isinstance(timer, ActionTimer)
+        assert timer._started
+        assert not timer.finished
 
         frozen_time.tick(timedelta(seconds=5))
-        timer.comp_started(identifier)
+        timers.start(identifier)
 
-        entry = timer._comp_history[identifier]
-        assert entry == (time.time(), None)
+        timer = timers._history[identifier]
+        assert isinstance(timer, ActionTimer)
+        assert timer._started
+        assert not timer.finished
 
     @freeze_time("2018-01-01 00:00:00", as_arg=True)
-    def test_comp_finished(frozen_time, self):
-        timer = ComputeTimers()
+    def test_finished(frozen_time, self):
+        timer = ActionTimers()
         identifier = str(uuid.uuid4())
 
         with self.assertRaises(KeyError):
-            assert timer.time_computing(identifier) is None
+            assert timer.time(identifier) is None
 
-        timer.comp_started(identifier)
+        timer.start(identifier)
         frozen_time.tick(timedelta(seconds=5))
-        timer.comp_finished(identifier)
-        assert timer.time_computing(identifier) == 5
+        timer.finish(identifier)
+        assert timer.time(identifier) == 5
 
         # second call should return the same value
-        timer.comp_finished(identifier)
-        assert timer.time_computing(identifier) == 5
+        timer.finish(identifier)
+        assert timer.time(identifier) == 5
 
     @freeze_time("2018-01-01 00:00:00", as_arg=True)
     def test_remove(frozen_time, self):
-        timer = ComputeTimers()
+        timer = ActionTimers()
         identifier = str(uuid.uuid4())
 
         with self.assertRaises(KeyError):
             timer.remove(identifier)
 
-        timer.comp_started(identifier)
+        timer.start(identifier)
         frozen_time.tick(timedelta(seconds=5))
         assert timer.remove(identifier) is None
 
         with self.assertRaises(KeyError):
             timer.remove(identifier)
 
-        timer.comp_started(identifier)
+        timer.start(identifier)
         frozen_time.tick(timedelta(seconds=5))
-        timer.comp_finished(identifier)
+        timer.finish(identifier)
 
         assert timer.remove(identifier) == 5
