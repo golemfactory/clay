d6d4d738544aa20b90b4e52a52a3ce222b29aae9
Michal Plebanski <michal.plebanski@golem.network>
2018-10-25 15:45:55 +0200

    linter fixup

--- golem/task/taskmanager.py
+++ golem/task/taskmanager.py
@@ -26,8 +26,8 @@ from golem.resource.hyperdrive.resourcesmanager import \
     HyperdriveResourceManager
 from golem.rpc import utils as rpc_utils
 from golem.task.result.resultmanager import EncryptedResultPackageManager
-from golem.task.taskbase import TaskEventListener, Task, TaskHeader, TaskPurpose, \
-    AcceptClientVerdict
+from golem.task.taskbase import TaskEventListener, Task, TaskHeader,\
+    TaskPurpose, AcceptClientVerdict
 from golem.task.taskkeeper import CompTaskKeeper
 from golem.task.taskrequestorstats import RequestorTaskStatsManager
 from golem.task.taskstate import TaskState, TaskStatus, SubtaskStatus, \
