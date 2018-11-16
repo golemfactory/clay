678297aedd07b9997e0db9d62de896bea0ada4f4
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2018-11-16 15:16:21 +0100

    Fix `deposit_contract_address` values for 'dev' and 'staging'

--- golem/core/variables.py
+++ golem/core/variables.py
@@ -36,14 +36,14 @@ CONCENT_CHOICES = {
         'url': 'http://devel.concent.golem.network',
         'pubkey': b'\xf3\x97\x19\xcdX\xda\x86tiP\x1c&\xd39M\x9e\xa4\xddb\x89\xb5,]O\xd5cR\x84\xb85\xed\xc9\xa17e,\xb2s\xeb\n1\xcaN.l\xba\xc3\xb7\xc2\xba\xff\xabN\xde\xb3\x0b\xa6l\xbf6o\x81\xe0;',  # noqa pylint: disable=line-too-long
         'deposit_contract_address':
-            '0x694667D7787CFca1892606E81734860a617537B2',
+            '0xcfB81A6EE3ae6aD4Ac59ddD21fB4589055c13DaD',
     },
     'staging': {
         'url': 'https://staging.concent.golem.network',
         'pubkey': b'b\x9b>\xf3\xb3\xefW\x92\x93\xfeIW\xd1\n\xf0j\x91\t\xdf\x95\x84\x81b6C\xe8\xe0\xdb\\.P\x00;rZM\xafQI\xf7G\x95\xe3\xe3.h\x19\xf1\x0f\xfa\x8c\xed\x12:\x88\x8aK\x00C9 \xf0~P',  # noqa pylint: disable=line-too-long
         'certificate': str(CONCENT_CERTIFICATES_DIR / 'staging.crt'),
         'deposit_contract_address':
-            '0x694667D7787CFca1892606E81734860a617537B2',
+            '0xA172A4B929Ae9589E3228F723CB99508b8c0709a',
     },
     'test': {
         'url': 'https://test.concent.golem.network',
