62404da6c410c47cbd8994dfcbcc6309779ae835
shadeofblue <blue@golem.network>
2018-12-12 20:01:17 +0100

    additional success criteria for uc2/forced download and uc3/force accept

--- scripts/node_integration_tests/playbooks/concent/force_accept.py
+++ scripts/node_integration_tests/playbooks/concent/force_accept.py
@@ -1,4 +1,8 @@
+import datetime
+import time
+
 from scripts.node_integration_tests import helpers
+
 from ..base import NodeTestPlaybook
 
 
@@ -6,20 +10,72 @@ class ForceAccept(NodeTestPlaybook):
     provider_node_script = 'provider/debug'
     requestor_node_script = 'requestor/no_sra'
 
+    task_finished = False
+    sra_received = False
+    sra_deadline = None
+
     def step_clear_provider_output(self):
         helpers.clear_output(self.provider_output_queue)
         self.next()
 
-    def step_wait(self):
-        concent_fail = helpers.search_output(
+    def step_wait_task_finished_and_sra_received(self):
+        def on_success(result):
+            if result['status'] == 'Finished':
+                print("Task finished.")
+                self.task_finished = True
+                self.sra_deadline = \
+                    datetime.datetime.now() + datetime.timedelta(minutes=30)
+            elif result['status'] == 'Timeout':
+                self.fail("Task timed out :( ... ")
+            else:
+                print("{} ... ".format(result['status']))
+                time.sleep(10)
+
+        concent_fail_triggers = [
+            'Concent request failed',
+            'Problem interpreting',
+        ]
+
+        sra_trigger = [
+            'ForceSubtaskResultsResponse'
+        ]
+
+        log_match_pattern = \
+            '.*' + '.*|.*'.join(concent_fail_triggers + sra_trigger) + '.*'
+
+        log_match = helpers.search_output(
             self.provider_output_queue,
-            '.*Concent request failed.*',
+            log_match_pattern,
         )
 
-        if concent_fail:
-            print("Provider: ", concent_fail.group(0))
-            self.fail()
+        if log_match:
+            match = log_match.group(0)
+            if any([t in match for t in concent_fail_triggers]):
+                self.fail("Provider<->Concent comms failure: %s " % match)
+                return
+            if any([t in match and 'Concent Message received' in match
+                    for t in sra_trigger]):
+                print("Forced SRA received.")
+                self.sra_received = True
+
+        if self.task_finished and self.sra_received:
+            print("Task finished and the relayed SRA received, great! :)")
+            self.next()
             return
+
+        if ((not self.sra_received) and
+                self.sra_deadline and
+                datetime.datetime.now() > self.sra_deadline
+        ):
+            self.fail("Forced SRA timeout...")
+            return
+
+        if not self.task_finished:
+            return self.call_requestor(
+                'comp.task', self.task_id,
+                on_success=on_success,
+                on_error=self.print_error
+            )
 
     steps = (
         NodeTestPlaybook.step_get_provider_key,
@@ -34,7 +90,7 @@ class ForceAccept(NodeTestPlaybook):
         NodeTestPlaybook.step_create_task,
         NodeTestPlaybook.step_get_task_id,
         NodeTestPlaybook.step_get_task_status,
-        step_wait,
+        step_wait_task_finished_and_sra_received,
         NodeTestPlaybook.step_get_subtasks,
         NodeTestPlaybook.step_verify_provider_income,
     )
--- scripts/node_integration_tests/playbooks/concent/force_download.py
+++ scripts/node_integration_tests/playbooks/concent/force_download.py
@@ -6,6 +6,8 @@ class ForceDownload(NodeTestPlaybook):
     provider_node_script = 'provider/debug'
     requestor_node_script = 'requestor/fail_results'
 
+    forced_download_successful = False
+
     def step_clear_requestor_output(self):
         helpers.clear_output(self.requestor_output_queue)
         self.next()
@@ -15,15 +17,25 @@ class ForceDownload(NodeTestPlaybook):
         self.next()
 
     def step_wait_task_finished(self):
-        concent_fail = helpers.search_output(
+        fail_triggers = ['Concent request failed', ]
+        download_trigger = ['Concent results download successful', ]
+
+        log_match_pattern = \
+            '.*' + '.*|.*'.join(fail_triggers + download_trigger) + '.*'
+
+        log_match = helpers.search_output(
             self.requestor_output_queue,
-            '.*Concent request failed.*',
+            log_match_pattern,
         )
 
-        if concent_fail:
-            print("Requestor: ", concent_fail.group(0))
-            self.fail()
-            return
+        if log_match:
+            match = log_match.group(0)
+            if any([t in match for t in fail_triggers]):
+                self.fail("Requestor<->Concent comms failure: %s " % match)
+                return
+            if any([t in match for t in download_trigger]):
+                print("Concent download successful.")
+                self.forced_download_successful = True
 
         concent_fail = helpers.search_output(
             self.provider_output_queue,
@@ -35,7 +47,16 @@ class ForceDownload(NodeTestPlaybook):
             self.fail()
             return
 
-        super().step_wait_task_finished()
+        return super().step_wait_task_finished()
+
+    def step_verify_forced_download_happened(self):
+        if self.forced_download_successful:
+            self.next()
+        else:
+            self.fail(
+                "The task might have finished but "
+                "we didn't notice the Requestor<->Concent communicating."
+            )
 
     steps = (
         NodeTestPlaybook.step_get_provider_key,
@@ -52,6 +73,7 @@ class ForceDownload(NodeTestPlaybook):
         NodeTestPlaybook.step_get_task_id,
         NodeTestPlaybook.step_get_task_status,
         step_wait_task_finished,
+        step_verify_forced_download_happened,
         NodeTestPlaybook.step_verify_output,
         NodeTestPlaybook.step_get_subtasks,
         NodeTestPlaybook.step_verify_provider_income,
