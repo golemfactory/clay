936c8c04797253434fd4184214dbb007a5a484dc
Dariusz Rybi <dariusz@golem.network>
2018-12-13 14:53:57 +0100

    CLI concent soft switch

--- golem/interface/client/concent.py
+++ golem/interface/client/concent.py
@@ -3,21 +3,36 @@ import typing
 import html2text
 
 from golem.core.deferred import sync_wait
-from golem.interface.command import group, command
+from golem.interface.command import (
+    Argument,
+    command,
+    group,
+)
 
 if typing.TYPE_CHECKING:
     from golem.rpc.session import ClientProxy  # noqa pylint: disable=unused-import
+
+
+on_off_arg = Argument(
+    "on_off",
+    choices=[True, False],
+)
 
 
 @group(help="Concent Service")
 class Concent:
     client: 'ClientProxy'
 
+    @command(help="Soft switch", arguments=(on_off_arg, ))
+    def switch(self, on_off):
+        return self.client._call(  # pylint: disable=protected-access
+            "golem.concent.switch",
+            on_off,
+        )
+
 
 @group(parent=Concent, help="Terms of Use")
 class Terms:
-    client: 'ClientProxy'
-
     @classmethod
     def _call(cls, uri, *args, **kwargs):
         return Concent.client._call(  # pylint: disable=protected-access
