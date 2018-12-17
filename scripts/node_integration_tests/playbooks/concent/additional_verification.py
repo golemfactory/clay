25161aa045f307208df033f87f924251ad14e43c
shadeofblue <blue@wave460.net>
2018-12-17 10:23:15 +0100

    add checks to the `AdditionalVerification` test

--- scripts/node_integration_tests/playbooks/concent/additional_verification.py
+++ scripts/node_integration_tests/playbooks/concent/additional_verification.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from scripts.node_integration_tests import helpers
@@ -8,6 +9,7 @@ from ..base import NodeTestPlaybook
 class AdditionalVerification(NodeTestPlaybook):
     provider_node_script = 'provider/debug'
     requestor_node_script = 'requestor/reject_results'
+    concent_verification_timeout = None
 
     def step_clear_provider_output(self):
         helpers.clear_output(self.provider_output_queue)
@@ -43,22 +45,70 @@ class AdditionalVerification(NodeTestPlaybook):
                     return
                 if srr in verification_match:
                     print("Results rejected as expected.")
+
+                    #
+                    # putting some arbitrary value here,
+                    # @todo make the value something more sensible
+                    #
+                    self.concent_verification_timeout = (
+                            datetime.datetime.now() +
+                            datetime.timedelta(minutes=15)
+                    )
+
                     self.next()
                     return
+        else:
+            def on_success(result):
+                if result['status'] == 'Timeout':
+                    self.fail("Task timed out without either "
+                              "an acceptance or rejection...")
+                else:
+                    print("{} ... ".format(result['status']))
+                    time.sleep(10)
 
-        print("...")
-        time.sleep(10)
+            return self.call_requestor('comp.task', self.task_id,
+                           on_success=on_success, on_error=self.print_error)
 
-    def step_wait(self):
-        concent_fail = helpers.search_output(
+    def step_wait_settled(self):
+        if not self.concent_verification_timeout:
+            self.fail("No Concent verification timeout? ... ")
+
+        fail_triggers = [
+            "Concent request failed",
+            "Can't receive message from Concent",
+            "Concent service exception",
+            # "SubtaskResultsRejected",
+            # @todo need to add some additional condition
+            # so as not to catch the original SRR
+            # and only catch the Concent's one
+        ]
+
+        settled_trigger = [
+            "SubtaskResultsSettled",
+        ]
+
+        log_match_pattern = \
+            '.*' + '.*|.*'.join(fail_triggers + settled_trigger) + '.*'
+
+        log_match = helpers.search_output(
             self.provider_output_queue,
-            '.*Concent service exception.*',
+            log_match_pattern,
         )
 
-        if concent_fail:
-            print("Provider: ", concent_fail.group(0))
-            self.fail()
-            return
+        if log_match:
+            match = log_match.group(0)
+            if any([t in match for t in fail_triggers]):
+                self.fail(
+                    "Provider's Additional Verification failed: %s " % match
+                )
+                return
+            if any([t in match for t in settled_trigger]):
+                print("Concent verification successful.")
+                self.success()
+                return
+
+        if datetime.datetime.now() > self.concent_verification_timeout:
+            self.fail("Concent verification timed out... ")
 
     steps = NodeTestPlaybook.initial_steps + (
         step_clear_provider_output,
@@ -67,10 +117,8 @@ class AdditionalVerification(NodeTestPlaybook):
         NodeTestPlaybook.step_get_task_status,
         step_wait_task_started,
         step_wait_task_rejected,
-        step_wait,
+        step_wait_settled,
 
-        # @TODO something here
-        # ...?
-        # profit!
+        # @todo add some concent payment check here
 
     )
