146e7adbfccec0d033ed60d46435fe475e802a79
Marek Franciszkiewicz <marek@golem.network>
2018-12-20 14:03:54 +0100

    Check for Node._docker_manager

--- golem/node.py
+++ golem/node.py
@@ -448,8 +448,13 @@ class Node(HardwarePresetsMixin):
         self.client.sync()
 
         try:
-            with self._docker_manager.locked_config():
+            if self._docker_manager:
+                # pylint: disable=no-member
+                with self._docker_manager.locked_config():
+                    self.client.start()
+            else:
                 self.client.start()
+
             for peer in self._peers:
                 self.client.connect(peer)
         except SystemExit:
