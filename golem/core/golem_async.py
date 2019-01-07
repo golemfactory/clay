e71095afd2bb7022d3bb984504a61b78a5b6aaea
Dariusz Rybi <jiivanq@gmail.com>
2019-01-07 23:43:28 +0100

    Twisted and asyncio in a single app

--- golem/core/golem_async.py
+++ golem/core/golem_async.py
@@ -1,8 +1,13 @@
+import asyncio
 import functools
 import logging
+import queue
+import threading
 from typing import Callable, Optional
 
+from pydispatch import dispatcher
 from twisted.internet import defer
+from twisted.internet import task as twisted_task
 from twisted.internet import threads
 from twisted.web.iweb import IBodyProducer
 from zope.interface import implementer
@@ -97,3 +102,98 @@ def deferred_run():
             return execute(f, *args, **kwargs)
         return curry
     return wrapped
+
+
+##
+# DISPATCHING
+##
+
+
+def dispatch_from(queue_: queue.Queue):
+    try:
+        kwargs = queue_.get_nowait()
+    except queue.Empty:
+        pass
+    else:
+        dispatcher.send(**kwargs)
+
+
+def asyncio_dispatch(**kwargs):
+    global _TO_ASYNCIO
+    _TO_ASYNCIO.put_nowait(kwargs)
+
+
+def twisted_dispatch(**kwargs):
+    global _TO_TWISTED
+    _TO_TWISTED.put_nowait(kwargs)
+
+
+def asyncio_listen():
+    def wrapper(f):
+        @functools.wraps(f)
+        def curry(**kwargs):
+            if threading.current_thread().name != _ASYNCIO_ID:
+                asyncio_dispatch(**kwargs)
+                return
+            f(**kwargs)
+        return curry
+    return wrapper
+
+
+##
+# ASYNCIO
+##
+
+_ASYNCIO_ID = 'Thread-aio'
+_RUN = asyncio.Event()
+_TO_ASYNCIO = queue.Queue()
+_TO_TWISTED = queue.Queue()
+
+
+@defer.inlineCallbacks
+def start_asyncio_thread():
+    asyncio_thread = threading.Thread(
+        target=asyncio_start,
+        name=_ASYNCIO_ID,
+    )
+    counter = 0
+    def listener(**kwargs):
+        nonlocal counter
+        print('IN TWISTED', threading.current_thread().name, kwargs)
+        counter += 1
+        dispatcher.send(signal='asyncio', nonce=counter)
+    dispatcher.connect(listener, signal='twisted', weak=False)
+    asyncio_thread.start()
+    loop = twisted_task.LoopingCall(
+        functools.partial(dispatch_from, _TO_TWISTED),
+    )
+    yield loop.start(1.0)
+
+
+def asyncio_start():
+    @asyncio_listen()
+    def listener(**kwargs):
+        print('IN ASYNCIO', threading.current_thread().name, kwargs)
+    dispatcher.connect(listener, signal='asyncio')
+    _RUN.set()
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(asyncio_main())
+
+
+def asyncio_stop():
+    global _RUN
+    print('Stopping ASYNCIO')
+    _RUN.clear()
+
+
+async def asyncio_main():
+    counter = 0
+    global _RUN
+    print('ASYNCIO started', threading.current_thread().name)
+    while _RUN.is_set():
+        print(str(counter)+'*'*80, threading.current_thread().name)
+        twisted_dispatch(signal="twisted", nonce=counter)
+        counter += 1
+        dispatch_from(_TO_ASYNCIO)
+        await asyncio.sleep(1)
+    print("ASYNCIO finished")
