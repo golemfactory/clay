e233e681758a59f34fc8816088d60e56b27ab8de
Kuba Mazurek <zakaprov@gmail.com>
2019-01-16 15:43:38 +0100

    Fixed indentation and line lengths

--- golem/core/windows.py
+++ golem/core/windows.py
@@ -16,8 +16,9 @@ def run_powershell(
     :param script: path of PowerShell script to execute
     :param command: PowerShell command to execute
     :param args: optional extra arguments
-    :param use_profile: when set to True, PowerShell will first load all profile scripts present on the machine before
-    running the actual command or script.
+    :param use_profile: when set to True, PowerShell will first load all
+    profile scripts present on the machine before running the actual command
+    or script.
     :param timeout: timeout for the script or command, in seconds
     """
     cmd = ['powershell.exe']
@@ -43,15 +44,15 @@ def run_powershell(
     try:
         return subprocess \
             .run(
-            cmd,
-            timeout=timeout,  # seconds
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        ) \
+                cmd,
+                timeout=timeout,  # seconds
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            ) \
             .stdout \
             .decode('utf8') \
             .strip()
-    except (subprocess.CalledProcessError, \
+    except (subprocess.CalledProcessError,
             subprocess.TimeoutExpired) as exc:
         raise RuntimeError(exc.stderr.decode('utf8') if exc.stderr else '')
