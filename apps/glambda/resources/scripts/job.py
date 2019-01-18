6b740943d0c9276ff87bb833909d7466f6f77f48
Michal Plebanski <michal.plebanski@golem.network>
2019-01-18 16:42:22 +0100

    Use base64 encoding to send function object in glambda taska

--- apps/glambda/resources/scripts/job.py
+++ apps/glambda/resources/scripts/job.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import base64
 import cloudpickle
 import functools
 import json
@@ -7,7 +8,7 @@ import os
 import traceback
 
 with open('params.json', 'rb') as params_file:
-    params = cloudpickle.load(params_file)
+    params = json.load(params_file)
 
 result_obj = {}
 
@@ -21,8 +22,8 @@ def write_path(path, content):
 write_result = functools.partial(write_path, result_path)
 
 try:
-    method_code = cloudpickle.loads(params['method'])
-    args = cloudpickle.loads(params['args'])
+    method_code = cloudpickle.loads(base64.b64decode(params['method']))
+    args = cloudpickle.loads(base64.b64decode(params['args']))
     result = method_code(args)
 except Exception as e:
     result_obj['error'] = str(e)
