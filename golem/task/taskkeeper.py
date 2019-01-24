b2334c4311a56b9a51cbb6dd6b696aaceda04141
shadeofblue <blue@golem.network>
2019-01-24 12:56:35 +0100

    additional debug logs for `Tasks` message exchange et al

--- golem/task/taskkeeper.py
+++ golem/task/taskkeeper.py
@@ -593,12 +593,15 @@ class TaskHeaderKeeper:
         :param exclude: Task ids to exclude
         :return: None if there are no tasks that this node may want to compute
         """
+        logger.debug("`get_task` called. exclude=%r", exclude)
         tasks = self.supported_tasks
         if exclude:
             tasks = [t for t in tasks if t not in exclude]
         if not tasks:
+            logger.debug("`get_task`: no potential task candidates found.")
             return None
         task_id = random.choice(tasks)
+        logger.debug("`get_task`: task candidate found. task_id=%r", task_id)
         return self.task_headers[task_id]
 
     def remove_old_tasks(self):
