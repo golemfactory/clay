f16a0202dd20bb79f2a0d1e6d86fbcbd8dda9480
shadeofblue <blue@golem.network>
2019-02-14 10:54:38 +0100

    replace `.format` with logger's built-in formatting

--- golem/task/taskmanager.py
+++ golem/task/taskmanager.py
@@ -754,8 +754,8 @@ class TaskManager(TaskEventListener):
 
         if not subtask_status.is_computed():
             logger.warning(
-                "TaskFailure for subtask {} when subtask state is {}"
-                .format(subtask_id, subtask_status.value)
+                "TaskFailure for subtask %s when subtask state is %s",
+                subtask_id, subtask_status.value
             )
             self.notice_task_updated(task_id,
                                      subtask_id=subtask_id,
