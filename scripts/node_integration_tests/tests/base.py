c1eaf8e96bfdf446f2ed607ad1117f81885790d2
pawel.kisielewicz@codepoets.it <pawel.kisielewicz@codepoets.it>
2019-02-26 09:21:58 +0100

    Add singleton to remember if test is first in set and to prepare folders for reusable files

--- scripts/node_integration_tests/tests/base.py
+++ scripts/node_integration_tests/tests/base.py
@@ -1,8 +1,69 @@
 import os
 import pathlib
+import shutil
 import subprocess
+import time
+from shutil import copyfile
+from threading import Thread
 
 from ..helpers import get_testdir
+
+
+class ReuseNodeKeys(object):
+    instance = None
+
+    class __ReuseNodeKeys:
+        first_test_in_set = True
+
+        def _copy_keystores(self):
+            time.sleep(30)
+            self._prepare_keystore_reuse_folders()
+            self._copy_keystore(self.provider_datadir, self.provider_reuse_dir)
+            self._copy_transaction_system(
+                self.provider_datadir, self.provider_reuse_dir)
+            self._copy_keystore(
+                self.requestor_datadir, self.requestor_reuse_dir)
+            self._copy_transaction_system(
+                self.requestor_datadir, self.requestor_reuse_dir)
+
+        def _prepare_keystore_reuse_folders(self) -> None:
+            self.provider_reuse_dir = '/tmp/provider_reuse_keystore'
+            self.requestor_reuse_dir = '/tmp/requestor_reuse_keystore'
+            shutil.rmtree(self.provider_reuse_dir, ignore_errors=True)
+            shutil.rmtree(self.requestor_reuse_dir, ignore_errors=True)
+            print('Old temporary folders for reuse keystore.json removed')
+            try:
+                os.mkdir(self.provider_reuse_dir)
+                os.mkdir(self.requestor_reuse_dir)
+            except OSError:
+                print('Unexpected problem with creating folders for keystore')
+                raise
+
+        @staticmethod
+        def _copy_keystore(datadir: str, reuse_dir: str) -> None:
+            src = str(datadir) + '/rinkeby/keys/keystore.json'
+            dst = str(reuse_dir) + '/keystore.json'
+            copyfile(src, dst)
+
+        @staticmethod
+        def _copy_transaction_system(datadir: str, reuse_dir: str) -> None:
+            src = str(datadir) + '/rinkeby/transaction_system/wallet.json'
+            dst = str(reuse_dir) + '/wallet.json'
+            copyfile(src, dst)
+
+        def __init__(self, provider_datadir, requestor_datadir):
+            self.provider_datadir = provider_datadir
+            self.requestor_datadir = requestor_datadir
+            thread = Thread(target=self._copy_keystores)
+            thread.start()
+
+    def __new__(cls, provider_datadir: str, requestor_datadir: str):
+        if cls.instance is None:
+            cls.instance = ReuseNodeKeys.__ReuseNodeKeys(
+                provider_datadir, requestor_datadir)
+        else:
+            cls.instance.first_test_in_set = False
+        return ReuseNodeKeys.instance
 
 
 class NodeTestBase:
@@ -12,6 +73,8 @@ class NodeTestBase:
         self.requestor_datadir = test_dir / 'requestor'
         os.makedirs(self.provider_datadir)
         os.makedirs(self.requestor_datadir)
+        self.first_test_in_set = ReuseNodeKeys(
+            self.provider_datadir, self.requestor_datadir).first_test_in_set
 
     def _relative_id(self):
         from . import __name__ as parent_name
