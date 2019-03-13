3cb09fddf7819be6d65dc975964723ea88b6a34d
shadeofblue <blue@golem.network>
2019-03-13 18:47:31 +0100

    + proper singleton + code cleanup

--- scripts/node_integration_tests/conftest.py
+++ scripts/node_integration_tests/conftest.py
@@ -1,19 +1,52 @@
 from typing import List
 import _pytest
 
-DISABLE_KEY_REUSE_COMMAND_LINE = False
+REUSE_KEYS = True
+
+
+class NodeKeyReuseException(Exception):
+    pass
+
+
+class NodeKeyReuse:
+    instance = None
+    _first_test = True
+
+    @classmethod
+    def get(cls):
+        if not cls.instance:
+            cls.instance = cls()
+        return cls.instance
+
+    @property
+    def keys_ready(self):
+        return not self._first_test
+
+    def mark_keys_ready(self):
+        if not self.enabled:
+            raise NodeKeyReuseException("Key reuse disabled.")
+        self._first_test = False
+
+    def disable(self):
+        global REUSE_KEYS
+        REUSE_KEYS = False
+
+    @property
+    def enabled(self):
+        return REUSE_KEYS
 
 
 def pytest_addoption(parser: _pytest.config.Parser) -> None:
 
-    parser.addoption("--disable-key-reuse", action="store_true",
-                     help="Parameter disables reusing of provider's and "
-                          "requestor's node keys. All node_integration_tests"
-                          "run with new, fresh keys ")
+    parser.addoption(
+        "--disable-key-reuse", action="store_true",
+        help="Parameter disables reusing of provider's and "
+             "requestor's node keys. All node_integration_tests "
+             "run with new, fresh keys."
+    )
 
 
 def pytest_collection_modifyitems(config: _pytest.config.Config,
                                   items: List[_pytest.main.Item]) -> None:
-    if config.getoption("--disable-key-reuse") is True:
-        global DISABLE_KEY_REUSE_COMMAND_LINE
-        DISABLE_KEY_REUSE_COMMAND_LINE = True
+    if config.getoption("--disable-key-reuse"):
+        NodeKeyReuse().disable()
--- scripts/node_integration_tests/tests/base.py
+++ scripts/node_integration_tests/tests/base.py
@@ -5,23 +5,19 @@ import subprocess
 from functools import wraps
 from typing import Callable
 
+from scripts.node_integration_tests.conftest import NodeKeyReuse
+
 from ..helpers import get_testdir
+
+KEYSTORE_DIR = 'rinkeby/keys'
 
 
 def disable_key_reuse(test_function: Callable)-> Callable:
     @wraps(test_function)
     def wrap(*args, **kwargs) -> None:
-        args[0].disable_keys_decorator = True
+        args[0].reuse_keys = False
         test_function(*args, **kwargs)
     return wrap
-
-
-class ReuseNodeKeys:
-    first_test = True
-
-    @classmethod
-    def next_test(cls):
-        cls.first_test = False
 
 
 class NodeTestBase:
@@ -31,30 +27,24 @@ class NodeTestBase:
         self.requestor_datadir = test_dir / 'requestor'
         os.makedirs(self.provider_datadir)
         os.makedirs(self.requestor_datadir)
-        self.disable_keys_decorator = False
+        self.reuse_keys = True
+
+        self.key_reuse_dir = test_dir.parent / 'key_reuse'
+        self.provider_reuse_dir = self.key_reuse_dir / 'provider'
+        self.requestor_reuse_dir = self.key_reuse_dir / 'requestor'
 
     def tearDown(self):
-        if ReuseNodeKeys.first_test:
-            ReuseNodeKeys.next_test()
+        key_reuse = NodeKeyReuse.get()
+        if key_reuse.enabled and not key_reuse.keys_ready:
             self._copy_keystores()
+            key_reuse.mark_keys_ready()
 
     def _relative_id(self):
         from . import __name__ as parent_name
         return self.id().replace(parent_name + '.', '')
 
-    def _should_node_keys_be_reused(self) -> bool:
-        # It should be imported locally because may be changed in command line
-        # and we need to get here updated value
-        from scripts.node_integration_tests.conftest import \
-            DISABLE_KEY_REUSE_COMMAND_LINE
-        if any([
-            ReuseNodeKeys.first_test,
-            DISABLE_KEY_REUSE_COMMAND_LINE,
-            self.disable_keys_decorator
-        ]) is True:
-            return False
-        else:
-            return True
+    def _can_recycle_keys(self) -> bool:
+        return all([NodeKeyReuse.get().keys_ready, self.reuse_keys])
 
     def _run_test(self, playbook_class_path: str, *args, **kwargs):
         cwd = pathlib.Path(os.path.realpath(__file__)).parent.parent
@@ -69,50 +59,47 @@ class NodeTestBase:
             test_args.append('--' + k)
             test_args.append(v)
 
-        if self._should_node_keys_be_reused():
-            self._replace_files_to_used_before()
+        if self._can_recycle_keys():
+            self._recycle_keys()
 
         return subprocess.call(args=test_args)
 
     @staticmethod
-    def _replace_keystore(destination: pathlib.PosixPath) -> None:
-        src = pathlib.Path(get_testdir()).parent / 'provider_reuse_keystore/' \
-            'keystore.json' if 'provider' in str(destination) else pathlib.Path(
-            get_testdir()).parent/'requestor_reuse_keystore/keystore.json'
+    def _replace_keystore(src: pathlib.Path, dst: pathlib.Path) -> None:
+        src_file = src / 'keystore.json'
+        dst_file = dst / KEYSTORE_DIR / 'keystore.json'
+        os.makedirs(str(dst / KEYSTORE_DIR))
+        shutil.copyfile(str(src_file), str(dst_file))
 
-        dst = destination/'rinkeby/keys/keystore.json'
-        os.makedirs(str(destination/'rinkeby/keys'))
-        shutil.copyfile(str(src), str(dst))
-
-    def _replace_files_to_used_before(self):
-        self._replace_keystore(self.provider_datadir)
-        self._replace_keystore(self.requestor_datadir)
+    def _recycle_keys(self):
+        self._replace_keystore(
+            self.provider_reuse_dir, self.provider_datadir
+        )
+        self._replace_keystore(
+            self.requestor_reuse_dir, self.requestor_datadir
+        )
 
     def _copy_keystores(self):
         self._prepare_keystore_reuse_folders()
-        self._copy_keystore(self.provider_datadir, self.provider_reuse_dir)
         self._copy_keystore(
-            self.requestor_datadir, self.requestor_reuse_dir)
+            self.provider_datadir, self.provider_reuse_dir
+        )
+        self._copy_keystore(
+            self.requestor_datadir, self.requestor_reuse_dir
+        )
 
     def _prepare_keystore_reuse_folders(self) -> None:
-        self.provider_reuse_dir = pathlib.Path(get_testdir()).parent \
-                                  / 'provider_reuse_keystore'
-        self.requestor_reuse_dir = pathlib.Path(get_testdir()).parent \
-                                   / 'requestor_reuse_keystore'
         shutil.rmtree(self.provider_reuse_dir, ignore_errors=True)
         shutil.rmtree(self.requestor_reuse_dir, ignore_errors=True)
-        print('Old temporary folders for reuse keystore.json removed')
         try:
-            os.mkdir(self.provider_reuse_dir)
-            os.mkdir(self.requestor_reuse_dir)
+            os.makedirs(self.provider_reuse_dir)
+            os.makedirs(self.requestor_reuse_dir)
         except OSError:
             print('Unexpected problem with creating folders for keystore')
             raise
 
     @staticmethod
-    def _copy_keystore(
-            datadir: pathlib.PosixPath,
-            reuse_dir: pathlib.PosixPath) -> None:
-        src = str(datadir / 'rinkeby/keys/keystore.json')
+    def _copy_keystore(datadir: pathlib.Path, reuse_dir: pathlib.Path) -> None:
+        src = str(datadir / KEYSTORE_DIR / 'keystore.json')
         dst = str(reuse_dir / 'keystore.json')
         shutil.copyfile(src, dst)
