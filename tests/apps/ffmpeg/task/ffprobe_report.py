20c0e3067a9748c3d1bd691a0ad2fbf2379196b1
pawel.kisielewicz@codepoets.it <pawel.kisielewicz@codepoets.it>
2019-03-27 10:22:50 +0100

    ffprobe_report: Implement FfprobeFormatReport

--- tests/apps/ffmpeg/task/ffprobe_report.py
+++ tests/apps/ffmpeg/task/ffprobe_report.py
@@ -1,4 +1,109 @@
-from typing import Any, Union
+import json
+import os
+from typing import Any, Dict, List, Optional, Union
+
+from apps.transcoding.ffmpeg.utils import StreamOperator
+
+
+class FfprobeFormatReport:
+    ATTRIBUTES_TO_COMPARE = {
+        'format_name',
+        'stream_types',
+        'duration',
+        'start_time',
+        'program_count',
+    }
+
+    def __init__(self, raw_report: dict) -> None:
+        self._raw_report = raw_report
+
+    @property
+    def stream_types(self) -> Dict[str, int]:
+        streams = self._raw_report['streams']
+        streams_dict: Dict[str, int] = {}
+
+        for stream in streams:
+            codec_type = stream['codec_type']
+            if codec_type in streams_dict:
+                streams_dict[codec_type] = streams_dict[codec_type] + 1
+            else:
+                streams_dict.update({codec_type: 1})
+        return streams_dict
+
+    @property
+    def format_name(self) -> Optional[str]:
+        return self._raw_report.get('format', {}).get('format_name', None)
+
+    @property
+    def duration(self) -> 'FuzzyDuration':
+        return FuzzyDuration(
+            self._raw_report.get('format', {}).get('duration', None),
+            0.1,
+        )
+
+    @property
+    def start_time(self) -> Optional['FuzzyDuration']:
+        return FuzzyDuration(
+            self._raw_report.get('format', {}).get('start_time', None),
+            0.1,
+        )
+
+    @property
+    def program_count(self) -> Optional[str]:
+        return self._raw_report.get('format', {}).get('nb_programs', None)
+
+    def diff(self, format_report: dict, overrides: Optional[dict] = None):
+        if overrides is None:
+            overrides = {}
+
+        differences = list()
+        for attr in self.ATTRIBUTES_TO_COMPARE:
+            original_value = getattr(self, attr)
+            modified_value = getattr(format_report, attr)
+
+            if 'streams' in overrides and attr in overrides['streams']:
+                modified_value = overrides['streams'][attr]
+
+            if 'format' in overrides and attr in overrides['format']:
+                modified_value = overrides['format'][attr]
+
+            if modified_value != original_value:
+                diff_dict = {
+                    'location': 'format',
+                    'attribute': attr,
+                    'original value': original_value,
+                    'modified value': modified_value,
+                }
+                differences.append(diff_dict)
+        return differences
+
+    def __eq__(self, other):
+        return len(self.diff(other)) == 0
+
+    @classmethod
+    def build(cls, *video_paths: str) -> List['FfprobeFormatReport']:
+        dirs_and_basenames: dict = {}
+        for path in video_paths:
+            dirname, basename = os.path.split(path)
+            dirs_and_basenames[dirname] = (
+                dirs_and_basenames.get(dirname, []) +
+                [basename]
+            )
+
+        list_of_reports = []
+        stream_operator = StreamOperator()
+
+        for key in dirs_and_basenames:
+            metadata = stream_operator.get_metadata(
+                dirs_and_basenames[key],
+                key
+            )
+            for path in metadata['data']:
+                with open(path) as metadata_file:
+                    list_of_reports.append(FfprobeFormatReport(
+                        json.loads(metadata_file.read())
+                    ))
+        return list_of_reports
 
 
 class FuzzyDuration:
