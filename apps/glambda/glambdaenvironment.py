a90e66575524b3e38eea59a72963d770b9582982
Michal Plebanski <michal.plebanski@golem.network>
2019-03-28 14:19:43 +0100

    Add gVisor secure container runtime to GLambda App

--- apps/glambda/glambdaenvironment.py
+++ apps/glambda/glambdaenvironment.py
@@ -1,4 +1,9 @@
+import shutil
+from typing import Dict
+
+from golem.core.common import is_linux
 from golem.docker.environment import DockerEnvironment
+from golem.environments.environment import SupportStatus, UnsupportReason
 
 
 class GLambdaTaskEnvironment(DockerEnvironment):
@@ -6,3 +11,20 @@ class GLambdaTaskEnvironment(DockerEnvironment):
     DOCKER_TAG = "1.3"
     ENV_ID = "glambda"
     SHORT_DESCRIPTION = "GLambda PoC"
+
+    def check_support(self) -> SupportStatus:
+        GVISOR_SECURE_RUNTIME = 'runsc'
+        if is_linux() and not shutil.which(GVISOR_SECURE_RUNTIME):
+            return SupportStatus.err({
+                UnsupportReason.ENVIRONMENT_NOT_SECURE: self.ENV_ID
+            })
+        return super().check_support()
+
+    def get_container_config(self) -> Dict:
+        return dict(
+            runtime='runsc' if is_linux() else None,
+            volumes=[],
+            binds={},
+            devices=[],
+            environment={}
+        )
