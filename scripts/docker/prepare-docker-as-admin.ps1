17fa73fa688a5166ac0bda25075420db086bffc7
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2019-04-02 13:05:28 +0200

    Get LocalAppData folder from the temp scripts

--- scripts/docker/prepare-docker-as-admin.ps1
+++ scripts/docker/prepare-docker-as-admin.ps1
@@ -1,16 +1,19 @@
 $ErrorActionPreference = "Stop"
 [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 
-$CurrentUserName = (AI_GetMsiProperty USERNAME)
-# FIXME: LocalAppDataFolder property points to admin's AppData folder
-$AppDataDir = (AI_GetMsiProperty LocalAppDataFolder)
-
 $RunAsAdminScript = (AI_GetMsiProperty AI_RUN_AS_ADMIN_FILE)
 "RunAsAdminScript: " + $RunAsAdminScript
 $PrepareDockerScript = (AI_GetMsiProperty AI_PREPARE_DOCKER_FILE)
 "PrepareDockerScript: " + $PrepareDockerScript
 $CreateShareScript = (AI_GetMsiProperty AI_CREATESHARE_FILE)
 "CreateShareScript: " + $CreateShareScript
+
+# FIXME: LocalAppDataFolder property points to admin's AppData folder
+# $AppDataDir = (AI_GetMsiProperty LocalAppDataFolder)
+# For now we use the default temp folder used by the installer
+$pathList = $RunAsAdminScript -split "\\Temp"
+$AppDataDir = $pathList[0]
+"AppDataDir: " + $AppDataDir
 
 &"$RunAsAdminScript" -ScriptPath "$PrepareDockerScript" `
     -ScriptParams "-createShareScript `"$CreateShareScript`" -appDataDir `"$AppDataDir`" -currentUserName `"$currentUserName`""
