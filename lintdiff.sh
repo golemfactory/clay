67ca385670c4110e335ebc9dbdd727cd11db501d
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-04-04 17:27:04 +0200

    lintdiff.sh: Add comments explaining the regexes in diff-lines()
    - The regexes are much easier to understand if we can see examples of the lines they're supposed to match.

--- lintdiff.sh
+++ lintdiff.sh
@@ -63,16 +63,38 @@ cleanup_artifacts() {
 list-added-lines-from-diff() {
     # Function taken from: https://stackoverflow.com/questions/8259851/using-git-diff-how-can-i-get-added-and-modified-lines-numbers
     # Adjusted the 'echo' line to not print the line content.
+    # EDIT 2019-04-04: A few more fixes and tweaks but I won't be listing them here. See git log.
     local path=
     local line=
     while read; do
         if [[ $REPLY =~ ^---\ (a/)?.* ]]; then
+            # A line that looks like one of these:
+            # --- a/apps/rendering/resources/utils.py
+            # --- /dev/null
+            #
+            # Skip it. It does not provide any useful information here.
             continue
         elif [[ $REPLY =~ ^\+\+\+\ (b/)?([^[:blank:]]+).* ]]; then
+            # A line that looks like this:
+            # +++ a/apps/rendering/resources/utils.py
+            # +++ /dev/null
+            #
+            # It tells us which file now contains lines marked as added in the diff.
+            # If it's /dev/null, the file has been removed. We should just ignore the
+            # lines from a removed file but this will happen automatically because they
+            # all start with a - sign.
             path=${BASH_REMATCH[2]}
         elif [[ $REPLY =~ ^@@\ -[0-9]+(,[0-9]+)?\ \+([0-9]+)(,[0-9]+)?\ @@.* ]]; then
+            # A line that looks like this:
+            # @@ -6,2 +4,0 @@ from typing import Optional
+            #
+            # It tells us, among other things, the line number that the next line
+            # marked as added in the diff is located now at.
             line=${BASH_REMATCH[2]}
         elif [[ $REPLY =~ ^\+ ]]; then
+            # All the other lines should start either with + or -
+            # We're only interested in thos starting with + because only they exist
+            # in the code that has been fed to the linter.
             echo "$path:$line:"
             ((line++))
         fi
