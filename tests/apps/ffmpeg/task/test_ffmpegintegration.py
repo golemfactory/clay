a982f2e6aa4d8fa7531547af695b77a8d9ddfffa
pawel.kisielewicz@codepoets.it <pawel.kisielewicz@codepoets.it>
2019-04-09 12:08:18 +0200

    test_ffmpegintegration: Add optional parameters to _create_task_def_for_transcoding

--- tests/apps/ffmpeg/task/test_ffmpegintegration.py
+++ tests/apps/ffmpeg/task/test_ffmpegintegration.py
@@ -22,25 +22,29 @@ class FfmpegIntegrationTestCase(TestTaskIntegration):
         self.tt = ffmpegTaskTypeInfo()
 
     @classmethod
-    def _create_task_def_for_transcoding(cls, resource_stream, result_file):
-        return {
+    def _create_task_def_for_transcoding(
+            cls,
+            resource_stream,
+            result_file,
+            video_options=None,
+            subtasks_count=2,
+    ):
+        task_def_for_transcoding = {
             'type': 'FFMPEG',
             'name': os.path.splitext(os.path.basename(result_file))[0],
             'timeout': '0:10:00',
             'subtask_timeout': '0:09:50',
-            'subtasks_count': 2,
+            'subtasks_count': subtasks_count,
             'bid': 1.0,
             'resources': [resource_stream],
             'options': {
                 'output_path': os.path.dirname(result_file),
-                'video': {
-                    'codec': 'h265',
-                    'resolution': [320, 240],
-                    'frame_rate': "25"
-                },
+                'video': video_options if video_options is not None else {},
                 'container': os.path.splitext(result_file)[1][1:]
             }
         }
+
+        return task_def_for_transcoding
 
 
 @ci_skip
@@ -52,7 +56,12 @@ class TestffmpegIntegration(FfmpegIntegrationTestCase):
         result_file = os.path.join(self.root_dir, 'test_simple_case.mp4')
         task_def = self._create_task_def_for_transcoding(
             resource_stream,
-            result_file)
+            result_file,
+            video_options={
+                'codec': 'h265',
+                'resolution': [320, 240],
+                'frame_rate': "25",
+            })
 
         task = self.execute_task(task_def)
         result = task.task_definition.output_file
@@ -65,7 +74,12 @@ class TestffmpegIntegration(FfmpegIntegrationTestCase):
                                    'test_invalid_task_definition.mp4')
         task_def = self._create_task_def_for_transcoding(
             resource_stream,
-            result_file)
+            result_file,
+            video_options={
+                'codec': 'h265',
+                'resolution': [320, 240],
+                'frame_rate': "25",
+            })
 
         task = self.execute_task(task_def)
 
@@ -82,7 +96,12 @@ class TestffmpegIntegration(FfmpegIntegrationTestCase):
         result_file = os.path.join(self.root_dir, 'test_nonexistent_video.mp4')
         task_def = self._create_task_def_for_transcoding(
             resource_stream,
-            result_file)
+            result_file,
+            video_options={
+                'codec': 'h265',
+                'resolution': [320, 240],
+                'frame_rate': "25",
+            })
 
         with self.assertRaises(TranscodingTaskBuilderException):
             self.execute_task(task_def)
@@ -95,7 +114,12 @@ class TestffmpegIntegration(FfmpegIntegrationTestCase):
 
         task_def = self._create_task_def_for_transcoding(
             resource_stream,
-            result_file)
+            result_file,
+            video_options={
+                'codec': 'h265',
+                'resolution': [320, 240],
+                'frame_rate': "25",
+            })
 
         with self.assertRaises(ffmpegException):
             self.execute_task(task_def)
@@ -106,8 +130,12 @@ class TestffmpegIntegration(FfmpegIntegrationTestCase):
         result_file = os.path.join(self.root_dir, 'test_invalid_params.mp4')
         task_def = self._create_task_def_for_transcoding(
             resource_stream,
-            result_file)
-        task_def['options']['video']['codec'] = 'abcd'
+            result_file,
+            video_options={
+                'codec': 'abcd',
+                'resolution': [320, 240],
+                'frame_rate': "25",
+            })
 
         with self.assertRaises(UnsupportedVideoCodec):
             self.execute_task(task_def)
