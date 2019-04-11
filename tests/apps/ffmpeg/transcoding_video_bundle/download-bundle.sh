33f4c9ba43a9e52f61c8e44d6dba57c4e4d3c687
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-04-11 12:42:28 +0200

    Helper script for downloading and unpacking test videos

---
+++ tests/apps/ffmpeg/transcoding_video_bundle/download-bundle.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+bundle_version=3
+resource_dir="$(dirname "${BASH_SOURCE[0]}")/../resources"
+package_name="transcoding-video-bundle-v$bundle_version.zip"
+package_url="http://builder.concent.golem.network/download/$package_name"
+
+mkdir --parents "$resource_dir/videos/"
+cd "$resource_dir"
+
+curl --remote-name "$package_url"
+unzip "$package_name" -d videos/
+
+# The unpacked files are inside a transcoding-video-bundle/ directory.
+# Move them out of it.
+mv "videos/transcoding-video-bundle/"* "videos/"
+rm -r "videos/transcoding-video-bundle/"
+
+# TODO: Add an option to allow user to tell us not to delete the archive.
+rm "$package_name"
