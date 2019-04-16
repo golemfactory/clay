lancekrogers/golem

separate config class for integration tests

scripts/node_integration_tests/playbooks/golem/regular_run.py
@@ -1,6 +1,5 @@
-from ..base import NodeTestPlaybook
+from ..test_config_base import DebugTestConfig
 
 
-class RegularRun(NodeTestPlaybook):
-    provider_node_script = 'provider/debug'
-    requestor_node_script = 'requestor/debug'
+class TestConfig(DebugTestConfig):
+    pass

scripts/node_integration_tests/playbooks/golem/rpc_test/no_concent/test_config.py
@@ -1,4 +1,4 @@
-from ..test_config_base import DebugTestConfig
+from ....test_config_base import DebugTestConfig
 
 
 class TestConfig(DebugTestConfig):

scripts/node_integration_tests/playbooks/golem/zero_price.py
@@ -1,17 +1,13 @@
-from ..base import NodeTestPlaybook
+from ..test_config_base import DebugTestConfig
 
 
-class ZeroPrice(NodeTestPlaybook):
-    provider_node_script = 'provider/debug'
-    requestor_node_script = 'requestor/debug'
-
-    provider_opts = {
-        'min_price': 0,
-    }
-    requestor_opts = {
-        'max_price': 0,
-    }
-
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
+class TestConfig(DebugTestConfig):
+    def __init__(self):
+        super().__init__()
+        self.provider.opts = {
+            'min_price': 0,
+        }
+        self.requestor.opts = {
+            'max_price': 0,
+        }
         self.task_dict['bid'] = 0

golemfactory/clay

scripts/node_integration_tests/playbooks/golem/regular_run.py
@@ -1,6 +1,5 @@
-from ..base import NodeTestPlaybook
+from ..test_config_base import TestConfigBase
 
 
-class RegularRun(NodeTestPlaybook):
-    provider_node_script = 'provider/debug'
-    requestor_node_script = 'requestor/debug'
+class TestConfig(TestConfigBase):
+    pass

scripts/node_integration_tests/playbooks/golem/rpc_test/no_concent/test_config.py
@@ -1,4 +1,4 @@
-from ..test_config_base import TestConfigBase
+from ....test_config_base import TestConfigBase
 
 
 class TestConfig(TestConfigBase):

scripts/node_integration_tests/playbooks/golem/zero_price.py
@@ -1,17 +1,13 @@
-from ..base import NodeTestPlaybook
+from ..test_config_base import TestConfigBase
 
 
-class ZeroPrice(NodeTestPlaybook):
-    provider_node_script = 'provider/debug'
-    requestor_node_script = 'requestor/debug'
-
-    provider_opts = {
-        'min_price': 0,
-    }
-    requestor_opts = {
-        'max_price': 0,
-    }
-
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
+class TestConfig(TestConfigBase):
+    def __init__(self):
+        super().__init__()
+        self.provider.opts = {
+            'min_price': 0,
+        }
+        self.requestor.opts = {
+            'max_price': 0,
+        }
         self.task_dict['bid'] = 0