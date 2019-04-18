66e0f5fd04c1650d13ded85c6b629bf45f66d3f5
Karol Beker <karolbeker@gmail.com>
2019-04-18 10:53:08 +0200

    Fixes in image_metrics_calculator

--- apps/blender/resources/images/entrypoints/scripts/verifier_tools/image_metrics_calculator.py
+++ apps/blender/resources/images/entrypoints/scripts/verifier_tools/image_metrics_calculator.py
@@ -1,5 +1,4 @@
 import cv2
-import itertools
 import os
 import sys
 import numpy as np
@@ -14,22 +13,13 @@ from .image_format_converter import convert_tga_to_png, convert_exr_to_png
 from .imgmetrics import ImgMetrics
 
 
-# todo review: refer to crops as "providers_result_crop" and "reference_crop",
-#  now it's not clear which one was cut out from image and which one was
-#  rendered locally. Analogically use "providers_result_image" instead of
-#  "rendered_scene". Variables which are pointing to uncropped provider's result
-#  should contain this information in name ("providers_result_uncropped", for
-#  instance)
-
-
-# todo review: rename this variable and the file so they indicate which
-#  (reference or provider's result) crop they are
-CROP_NAME = "scene_crop.png"
+PROVIDERS_RESULT_CROP_NAME = "scene_crop.png"
 VERIFICATION_SUCCESS = "TRUE"
 VERIFICATION_FAIL = "FALSE"
 PKT_FILENAME = "tree35_[crr=87.71][frr=0.92].pkl"
 TREE_PATH = Path(os.path.dirname(os.path.realpath(__file__))) / PKT_FILENAME
-# todo review: this is image_metrics_calculator.py, it's not a good place for
+# todo review: WILL BE CHANGED DURING TEST CHANGE
+#  this is image_metrics_calculator.py, it's not a good place for
 #  the alternative verification method used only in tests.
 WHITE = 0
 BLACK = 765
@@ -43,7 +33,6 @@ def calculate_metrics(
         top_left_corner_y,
         metrics_output_filename='metrics.txt'
 ):
-    # todo review: remove the workaround instead of commenting it
     """
     This is the entry point for calculation of metrics between the
     rendered_scene and the sample(cropped_image) generated for comparison.
@@ -54,7 +43,7 @@ def calculate_metrics(
     :param metrics_output_filename:
     :return:
     """
-    (cropped_image, scene_crops) = \
+    (cropped_image, providers_result_crop) = \
         _load_and_prepare_images_for_comparison(
             reference_crop_path,
             providers_result_image,
@@ -66,8 +55,6 @@ def calculate_metrics(
 
     (classifier, labels, available_metrics) = get_metrics()
 
-    # TODO this shouldn't depend on the crops' ordering
-    providers_result_crop = scene_crops[0]
     print(f"default_crop: {providers_result_crop.getbbox()}")
     default_metrics = compare_images(
         cropped_image,
@@ -81,7 +68,7 @@ def calculate_metrics(
         print("There were errors %r" % e, file=sys.stderr)
         default_metrics['Label'] = VERIFICATION_FAIL
 
-    providers_result_crop.save(CROP_NAME)
+    providers_result_crop.save(PROVIDERS_RESULT_CROP_NAME)
     return ImgMetrics(default_metrics).write_to_file(
         metrics_output_filename
     )
@@ -100,7 +87,6 @@ def classify_with_tree(metrics, classifier, feature_labels):
     return results[0].decode('utf-8')
 
 
-# todo review: this should generate 1 crop instead of 9
 def _load_and_prepare_images_for_comparison(
         reference_crop_path,
         result_image_path,
@@ -108,7 +94,7 @@ def _load_and_prepare_images_for_comparison(
         top_left_corner_y
 ):
     """
-    This function prepares (i.e. crops) the rendered_scene so that it will
+    This function prepares (i.e. crops) the providers_result_image so that it will
     fit the sample(cropped_image) generated for comparison.
 
     :param reference_crop_path:
@@ -117,13 +103,22 @@ def _load_and_prepare_images_for_comparison(
     :param top_left_corner_y: y position of crop (left, top)
     :return:
     """
-    rendered_scene = convert_to_png_if_needed(result_image_path)
-    # todo review: rename to "reference_crop"
-    reference_image = convert_to_png_if_needed(reference_crop_path)
-    (crop_width, crop_height) = reference_image.size
-    print(f"top_left_corner_x={top_left_corner_x}, top_left_corner_y={top_left_corner_y}, width={crop_width}, height={crop_height}")
-    crops = get_providers_result_crop(rendered_scene, top_left_corner_x, top_left_corner_y, crop_width, crop_height)
-    return reference_image, crops
+    providers_result_image = convert_to_png_if_needed(result_image_path)
+    reference_crop = convert_to_png_if_needed(reference_crop_path)
+    (crop_width, crop_height) = reference_crop.size
+    print(
+        f"top_left_corner_x={top_left_corner_x}, "
+        f"top_left_corner_y={top_left_corner_y}, "
+        f"width={crop_width}, height={crop_height}"
+    )
+    provider_crop = get_providers_result_crop(
+        providers_result_image,
+        top_left_corner_x,
+        top_left_corner_y,
+        crop_width,
+        crop_height
+    )
+    return (reference_crop, provider_crop)
 
 
 def get_file_extension_lowercase(file_path):
@@ -146,20 +141,15 @@ def convert_to_png_if_needed(image_path):
     return Image.open(file_name)
 
 
-# todo review: should return only one crop
-def get_providers_result_crop(rendered_scene, x, y, width, height):
-    offsets = itertools.product([0, -1, 1], repeat=2)
-    crops = [rendered_scene.crop((x + x_offset, y + y_offset,
-                                  x + width + x_offset,
-                                  y + height + y_offset))
-             for x_offset, y_offset in offsets]
-    return crops
+def get_providers_result_crop(providers_result_image, x, y, width, height):
+    return providers_result_image.crop((x, y, x + width, y + height))
 
 
 def get_metrics():
     classifier, feature_labels = load_classifier()
     available_metrics = ImgMetrics.get_metric_classes()
-    # todo review: effective_metrics isn't used after filling it with values
+    # todo review: DONE IN DOCS
+    #  effective_metrics isn't used after filling it with values
     #  in the loops below
     effective_metrics = []
     for metric in available_metrics:
@@ -167,7 +157,7 @@ def get_metrics():
             for label_part in metric.get_labels():
                 if label_part == label and metric not in effective_metrics:
                     effective_metrics.append(metric)
-    return classifier, feature_labels, available_metrics
+    return (classifier, feature_labels, available_metrics)
 
 
 def get_labels_from_metrics(metrics):
@@ -177,13 +167,13 @@ def get_labels_from_metrics(metrics):
     return labels
 
 
-# todo review: docstring is missing "metrics" parameter
 def compare_images(image_a, image_b, metrics) -> Dict:
     """
     This the entry point for calculating metrics between image_a, image_b
     once they are cropped to the same size.
     :param image_a:
     :param image_b:
+    :param metrics:
     :return: ImgMetrics
     """
 
@@ -201,9 +191,11 @@ def compare_images(image_a, image_b, metrics) -> Dict:
     return data
 
 
-# todo review: functions below are used only in tests, they should be moved to
+# todo review: WILL BE CHANGED DURING TEST CHANGE
+#  functions below are used only in tests, they should be moved to
 #  a file in the tests directory (check comment in line 36)
-# todo review: function's name should describe what it actually does, so it
+# todo review: WILL BE CHANGED DURING TEST CHANGE
+#  function's name should describe what it actually does, so it
 #  should contain information about squashing pixel values to black and white
 def convert_image_to_simple_array(image: np.ndarray) -> np.ndarray:
     """
@@ -223,7 +215,8 @@ def convert_image_to_simple_array(image: np.ndarray) -> np.ndarray:
     return simple_array
 
 
-# todo review: why are you implementing another function for cropping instead of
+# todo review: WILL BE CHANGED DURING TEST CHANGE
+#  why are you implementing another function for cropping instead of
 #  using previously written functions used for real verification?
 def cut_out_crop_from_whole_image(
         top_left_corner_x: int,
@@ -267,6 +260,7 @@ def get_raw_verification(
         stub_data['Label'] = VERIFICATION_SUCCESS
     else:
         stub_data['Label'] = VERIFICATION_FAIL
-    # todo review: writing to file won't be necessary after moving raw
+    # todo review: WILL BE CHANGED DURING TEST CHANGE
+    #  writing to file won't be necessary after moving raw
     #  verification to tests
     return ImgMetrics(stub_data).write_to_file(metrics_output_filename)
