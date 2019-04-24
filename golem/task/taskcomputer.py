dc29569f1c222cb82fc81f468a31a974bbbd35b9
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2019-04-24 11:24:21 +0200

    Never enter success with no task_header available

--- golem/task/taskcomputer.py
+++ golem/task/taskcomputer.py
@@ -139,6 +139,7 @@ class TaskComputer(object):
 
         work_wall_clock_time = task_thread.end_time - task_thread.start_time
         fallback_error = "Wrong result format"
+        has_header = False
         try:
             subtask = self.assigned_subtask
             assert subtask is not None
@@ -149,6 +150,7 @@ class TaskComputer(object):
             # get paid for max working time,
             # thus task withholding won't make profit
             work_time_to_be_paid = task_header.subtask_timeout
+            has_header = True
 
         except KeyError:
             fallback_error = "Task header not found in task keeper"
@@ -169,7 +171,7 @@ class TaskComputer(object):
                     task_thread.error_msg,
                 )
 
-        elif task_thread.result and 'data' in task_thread.result:
+        elif has_header and task_thread.result and 'data' in task_thread.result:
 
             logger.info("Task %r computed, work_wall_clock_time %s",
                         subtask_id,
