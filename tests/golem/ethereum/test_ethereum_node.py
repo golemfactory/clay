fab86f2a1ba7118c992c1c652a9735bf007f6fb8
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2019-05-01 17:03:47 +0200

    Moved responsibility for web3 retry to the ProxyProvider

--- tests/golem/ethereum/test_ethereum_node.py
+++ tests/golem/ethereum/test_ethereum_node.py
@@ -1,4 +1,3 @@
-# pylint: disable=protected-access
 from unittest.mock import patch, Mock
 from unittest import TestCase
 
@@ -10,28 +9,7 @@ class TestPublicNodeList(TestCase):
     def test_node_start(self):
         node = NodeProcess(['addr1', 'addr2'])
         node.web3 = Mock()
-        node.is_connected = Mock()
-        node._handle_remote_rpc_provider_failure = Mock()
+        node.web3.isConnected = Mock()
 
-        assert node.addr_list is None
         node.start()
-        assert node.addr_list
-        assert node.is_connected.called
-
-    @classmethod
-    @patch('golem.core.golem_async.async_run',
-           side_effect=lambda r, *_: r.method(*r.args, **r.kwargs))
-    def test_handle_remote_rpc_provider(cls, _async_run):
-        node = NodeProcess(['addr'])
-        node.start = Mock()
-
-        assert node.provider_proxy
-        assert node.initial_addr_list
-        assert node.addr_list is None
-
-        node.provider_proxy.provider = Mock()
-        node.addr_list = []
-        node._handle_remote_rpc_provider_failure()
-
-        assert node.provider_proxy.provider is None
-        assert node.start.called
+        node.web3.isConnected.assert_called_once()
