rbs-pli/golem

Add file upload

golem/node.py
@@ -1,6 +1,7 @@
 from enum import IntEnum
 import functools
 import logging
+from pathlib import Path
 import time
 from typing import (
     Any,
@@ -12,7 +13,7 @@ from typing import (
     TypeVar,
 )
 
-from pathlib import Path
+from fs.tempfs import TempFS
 from twisted.internet import threads
 from twisted.internet.defer import gatherResults, Deferred, succeed, fail, \
     FirstError
@@ -44,6 +45,8 @@ from golem.rpc.session import (
     Session,
 )
 from golem import terms
+from golem.tools.uploadcontroller import UploadController
+from golem.tools.remotefs import RemoteFS
 
 F = TypeVar('F', bound=Callable[..., Any])
 logger = logging.getLogger(__name__)
@@ -80,8 +83,8 @@ class Node(HardwarePresetsMixin):
                  # SEE golem.core.variables.CONCENT_CHOICES
                  concent_variant: dict,
                  peers: Optional[List[SocketAddress]] = None,
-                 use_monitor: bool = None,
-                 use_talkback: bool = None,
+                 use_monitor: bool = False,
+                 use_talkback: bool = False,
                  use_docker_manager: bool = True,
                  geth_address: Optional[str] = None,
                  password: Optional[str] = None
@@ -142,6 +145,9 @@ class Node(HardwarePresetsMixin):
             task_finished_cb=self._try_shutdown,
             update_hw_preset=self.upsert_hw_preset
         )
+
+        self.tempfs = TempFS()
+        self.remotefs = RemoteFS(self.tempfs, UploadController(self.tempfs))
 
         if password is not None:
             if not self.set_password(password):
@@ -229,6 +235,33 @@ class Node(HardwarePresetsMixin):
     @rpc_utils.expose('golem.password.unlocked')
     def is_account_unlocked(self) -> bool:
         return self._keys_auth is not None
+
+    @rpc_utils.expose('comp.task.results_purge')
+    def purge_task_results(self, task_id):
+        path = self.get_temp_results_path_for_task(task_id)
+        self.tempfs.removetree(path)
+
+    @staticmethod
+    def get_temp_results_path_for_task(task_id):
+        return 'results-{task_id}'.format(task_id=task_id)
+
+    @rpc_utils.expose('comp.task.subtask_results')
+    def query_subtask_results(self, task_id, subtask_id):
+        task = self.client.task_server.task_manager.tasks[task_id]
+        results = task.get_results(subtask_id)
+        res_path = self.get_temp_results_path_for_task(subtask_id)
+        # Create a directory there results will be held temporarily
+        outs = self.remotefs.copy_files_to_tmp_location(results, res_path)
+        return outs
+
+    @rpc_utils.expose('comp.task.result')
+    def get_task_results(self, task_id):
+        # FIXME Obtain task state in less hacky way
+        state = self.client.task_server.task_manager.query_task_state(task_id)
+        res_path = self.get_temp_results_path_for_task(task_id)
+        outs = self.remotefs.copy_files_to_tmp_location(state.outputs,
+                                                        res_path)
+        return outs
 
     @rpc_utils.expose('golem.mainnet')
     @classmethod
@@ -347,7 +380,8 @@ class Node(HardwarePresetsMixin):
         rpc_providers = (
             self,
             virtualization,
-            self.rpc_session
+            self.rpc_session,
+            self.remotefs
         )
 
         for provider in rpc_providers:

golem/tools/remotefs.py
@@ -1,0 +1,97 @@
+import os
+from pathlib import PurePath
+import traceback
+from typing import List, Dict, Any
+
+import fs
+from fs.osfs import OSFS
+
+from golem.rpc import utils as rpc_utils
+
+
+class RemoteFS:
+    def __init__(self, _fs, upload_ctrl):
+        self._fs = _fs
+        self.upload_ctrl = upload_ctrl
+
+    @rpc_utils.expose('fs.isdir')
+    def fs_isdir(self, path) -> bool:
+        path = str(PurePath(path))
+        return self._fs.getinfo(path).is_dir
+
+    @rpc_utils.expose('fs.isfile')
+    def fs_isfile(self, path) -> bool:
+        path = str(PurePath(path))
+        return self._fs.getinfo(path).is_file
+
+    @rpc_utils.expose('fs.listdir')
+    def fs_listdir(self, path) -> List[str]:
+        try:
+            return [
+                str(PurePath(f)) for f in self._fs.listdir(path)
+            ]
+        except (fs.errors.DirectoryExpected, fs.errors.ResourceNotFound):
+            traceback.print_stack()
+            return []
+
+    @rpc_utils.expose('fs.mkdir')
+    def fs_mkdir(self, path) -> None:
+        path = str(PurePath(path))
+        try:
+            self._fs.makedir(path, recreate=True)
+        except (fs.errors.DirectoryExpected, fs.errors.ResourceNotFound):
+            traceback.print_stack()
+
+    @rpc_utils.expose('fs.remove')
+    def fs_remove(self, path) -> None:
+        path = str(PurePath(path))
+        return self._fs.remove(path)
+
+    @rpc_utils.expose('fs.removetree')
+    def fs_removetree(self, path) -> None:
+        path = str(PurePath(path))
+        try:
+            return self._fs.removetree(path)
+        except fs.errors.ResourceNotFound:
+            pass
+
+    @rpc_utils.expose('fs.meta')
+    def fs_meta(self) -> Dict[str, Any]:
+        return self.upload_ctrl.meta
+
+    @rpc_utils.expose('fs.upload_id')
+    def fs_upload_id(self, path) -> List[str]:
+        path = str(PurePath(path))
+        return self.upload_ctrl.open(path, 'wb')
+
+    @rpc_utils.expose('fs.upload')
+    def fs_upload(self, _id, data) -> List[str]:
+        return self.upload_ctrl.upload(_id, data)
+
+    @rpc_utils.expose('fs.download_id')
+    def fs_download_id(self, path) -> List[str]:
+        path = str(PurePath(path))
+        return self.upload_ctrl.open(path, 'rb')
+
+    @rpc_utils.expose('fs.download')
+    def fs_download(self, _id) -> List[str]:
+        return self.upload_ctrl.download(_id)
+
+    def copy_files_to_tmp_location(self,
+                                   files: List[str],
+                                   dest: str) -> List[str]:
+        outs = []
+        osfs = OSFS('/')
+        self._fs.makedir(dest)
+        for output in files:
+            out_path = os.path.join(
+                dest,
+                os.path.basename(os.path.normpath(output)))
+            if os.path.isfile(output):
+                fs.copy.copy_file(osfs, output, self._fs, out_path)
+            elif os.path.isdir(output):
+                fs.copy.copy_dir(osfs, output, self._fs, out_path)
+            else:
+                pass
+            outs.append(str(PurePath(out_path)))
+        return outs

golemfactory/clay

golem/node.py
@@ -1,6 +1,7 @@
 from enum import IntEnum
 import functools
 import logging
+from pathlib import Path
 import time
 from typing import (
     Any,
@@ -12,7 +13,7 @@ from typing import (
     TypeVar,
 )
 
-from pathlib import Path
+from fs.tempfs import TempFS
 from twisted.internet import threads
 from twisted.internet.defer import gatherResults, Deferred, succeed, fail, \
     FirstError
@@ -44,6 +45,8 @@ from golem.rpc.session import (
     Session,
 )
 from golem import terms
+from golem.tools.uploadcontroller import UploadController
+from golem.tools.remotefs import RemoteFS
 
 F = TypeVar('F', bound=Callable[..., Any])
 logger = logging.getLogger(__name__)
@@ -80,8 +83,8 @@ class Node(HardwarePresetsMixin):
                  # SEE golem.core.variables.CONCENT_CHOICES
                  concent_variant: dict,
                  peers: Optional[List[SocketAddress]] = None,
-                 use_monitor: bool = None,
-                 use_talkback: bool = None,
+                 use_monitor: bool = False,
+                 use_talkback: bool = False,
                  use_docker_manager: bool = True,
                  geth_address: Optional[str] = None,
                  password: Optional[str] = None
@@ -142,6 +145,9 @@ class Node(HardwarePresetsMixin):
             task_finished_cb=self._try_shutdown,
             update_hw_preset=self.upsert_hw_preset
         )
+
+        self.tempfs = TempFS()
+        self.remotefs = RemoteFS(self.tempfs, UploadController(self.tempfs))
 
         if password is not None:
             if not self.set_password(password):
@@ -230,6 +236,33 @@ class Node(HardwarePresetsMixin):
     @rpc_utils.expose('golem.password.unlocked')
     def is_account_unlocked(self) -> bool:
         return self._keys_auth is not None
+
+    @rpc_utils.expose('comp.task.results_purge')
+    def purge_task_results(self, task_id):
+        path = self.get_temp_results_path_for_task(task_id)
+        self.tempfs.removetree(path)
+
+    @staticmethod
+    def get_temp_results_path_for_task(task_id):
+        return 'results-{task_id}'.format(task_id=task_id)
+
+    @rpc_utils.expose('comp.task.subtask_results')
+    def get_subtask_results(self, task_id, subtask_id):
+        task = self.client.task_server.task_manager.tasks[task_id]
+        results = task.get_results(subtask_id)
+        res_path = self.get_temp_results_path_for_task(subtask_id)
+        # Create a directory there results will be held temporarily
+        outs = self.remotefs.copy_files_to_tmp_location(results, res_path)
+        return outs
+
+    @rpc_utils.expose('comp.task.result')
+    def get_task_results(self, task_id):
+        # FIXME Obtain task state in less hacky way
+        state = self.client.task_server.task_manager.query_task_state(task_id)
+        res_path = self.get_temp_results_path_for_task(task_id)
+        outs = self.remotefs.copy_files_to_tmp_location(state.outputs,
+                                                        res_path)
+        return outs
 
     @rpc_utils.expose('golem.mainnet')
     @classmethod
@@ -348,7 +381,8 @@ class Node(HardwarePresetsMixin):
         rpc_providers = (
             self,
             virtualization,
-            self.rpc_session
+            self.rpc_session,
+            self.remotefs
         )
 
         for provider in rpc_providers:

golem/tools/remotefs.py
@@ -1,0 +1,97 @@
+import os
+from pathlib import PurePath
+import traceback
+from typing import List, Dict, Any
+
+import fs
+from fs.osfs import OSFS
+
+from golem.rpc import utils as rpc_utils
+
+
+class RemoteFS:
+    def __init__(self, filesystem, upload_ctrl):
+        self._fs = filesystem
+        self.upload_ctrl = upload_ctrl
+
+    @rpc_utils.expose('fs.isdir')
+    def fs_isdir(self, path) -> bool:
+        path = str(PurePath(path))
+        return self._fs.getinfo(path).is_dir
+
+    @rpc_utils.expose('fs.isfile')
+    def fs_isfile(self, path) -> bool:
+        path = str(PurePath(path))
+        return self._fs.getinfo(path).is_file
+
+    @rpc_utils.expose('fs.listdir')
+    def fs_listdir(self, path) -> List[str]:
+        try:
+            return [
+                str(PurePath(f)) for f in self._fs.listdir(path)
+            ]
+        except (fs.errors.DirectoryExpected, fs.errors.ResourceNotFound):
+            traceback.print_stack()
+            return []
+
+    @rpc_utils.expose('fs.mkdir')
+    def fs_mkdir(self, path) -> None:
+        path = str(PurePath(path))
+        try:
+            self._fs.makedir(path, recreate=True)
+        except (fs.errors.DirectoryExpected, fs.errors.ResourceNotFound):
+            traceback.print_stack()
+
+    @rpc_utils.expose('fs.remove')
+    def fs_remove(self, path) -> None:
+        path = str(PurePath(path))
+        return self._fs.remove(path)
+
+    @rpc_utils.expose('fs.removetree')
+    def fs_removetree(self, path) -> None:
+        path = str(PurePath(path))
+        try:
+            return self._fs.removetree(path)
+        except fs.errors.ResourceNotFound:
+            pass
+
+    @rpc_utils.expose('fs.meta')
+    def fs_meta(self) -> Dict[str, Any]:
+        return self.upload_ctrl.meta
+
+    @rpc_utils.expose('fs.upload_id')
+    def fs_upload_id(self, path) -> List[str]:
+        path = str(PurePath(path))
+        return self.upload_ctrl.open(path, 'wb')
+
+    @rpc_utils.expose('fs.upload')
+    def fs_upload(self, id_, data) -> List[str]:
+        return self.upload_ctrl.upload(id_, data)
+
+    @rpc_utils.expose('fs.download_id')
+    def fs_download_id(self, path) -> List[str]:
+        path = str(PurePath(path))
+        return self.upload_ctrl.open(path, 'rb')
+
+    @rpc_utils.expose('fs.download')
+    def fs_download(self, id_) -> List[str]:
+        return self.upload_ctrl.download(id_)
+
+    def copy_files_to_tmp_location(self,
+                                   files: List[str],
+                                   dest: str) -> List[str]:
+        outs = []
+        osfs = OSFS('/')
+        self._fs.makedir(dest)
+        for output in files:
+            out_path = os.path.join(
+                dest,
+                os.path.basename(os.path.normpath(output)))
+            if os.path.isfile(output):
+                fs.copy.copy_file(osfs, output, self._fs, out_path)
+            elif os.path.isdir(output):
+                fs.copy.copy_dir(osfs, output, self._fs, out_path)
+            else:
+                pass
+            outs.append(str(PurePath(out_path)))
+        return outs