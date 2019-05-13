ea1feebe33293aa588b804b95fd528144a996aab
Kuba Mazurek <zakaprov@gmail.com>
2019-05-13 13:45:02 +0200

    Improved crop file detection in Blender verifier
    Improves the logic for finding crop files by looking for files with the
    expected Blender output format in cases when the original path does not
    point to an existing file.

---
+++ apps/blender/resources/images/entrypoints/scripts/verifier_tools/file_extension/matcher.py
@@ -0,0 +1,30 @@
+import typing
+
+from . import types
+
+TYPES = [
+    types.Bmp(),
+    types.Jpeg(),
+    types.Tga()
+]
+
+
+def get_expected_extension(extension: str) -> str:
+    """
+    Based on the provided file extension string, returns the expected alias
+    extension that Blender will use for its output. This can be used to avoid
+    output file names mismatch (e.g. .jpg vs .jpeg extensions). The check is
+    based on a predefined list of file types.
+    :param extension: file extension string (with leading dot) to check
+    against.
+    :return: expected output file extension (lowercase, with leading dot).
+    Returns the provided file extension if no alias was found.
+    """
+    lower_extension = extension.lower()
+
+    for file_type in TYPES:
+        if lower_extension in file_type.extensions:
+            return file_type.output_format
+
+    return lower_extension
+
---
+++ apps/blender/resources/images/entrypoints/scripts/verifier_tools/file_extension/types.py
@@ -0,0 +1,69 @@
+import abc
+import typing
+
+
+class FileType(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def extensions(self) -> typing.AbstractSet[str]:
+        """
+        List of possible extensions (aliases) for the represented file type.
+        :return: a set of strings, each prefixed with a dot.
+        """
+        pass
+
+    @property
+    @abc.abstractmethod
+    def output_format(self) -> str:
+        """
+        Returns the file extension expected to be used by Blender for its
+        output files of the represented file type.
+        :return: a file extension string, prefixed with a dot.
+        """
+        pass
+
+
+class Bmp(FileType):
+    @property
+    def extensions(self) -> typing.AbstractSet[str]:
+        return frozenset([
+            '.bmp',
+            '.dib'
+        ])
+
+    @property
+    def output_format(self) -> str:
+        return '.bmp'
+
+
+class Jpeg(FileType):
+    @property
+    def extensions(self) -> typing.AbstractSet[str]:
+        return frozenset([
+            '.jpg',
+            '.jpeg',
+            '.jpe',
+            '.jif',
+            '.jfif',
+            '.jfi'
+        ])
+
+    @property
+    def output_format(self) -> str:
+        return '.jpg'
+
+
+class Tga(FileType):
+    @property
+    def extensions(self) -> typing.AbstractSet[str]:
+        return frozenset([
+            '.tga',
+            '.icb',
+            '.vda',
+            '.vst'
+        ])
+
+    @property
+    def output_format(self) -> str:
+        return '.tga'
+
