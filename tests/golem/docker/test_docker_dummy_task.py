25864fe4dd43a7c6ddebb34e5702197e5566053b
shadeofblue <blue@golem.network>
2019-05-14 15:35:10 +0200

    unskip tests

--- tests/golem/docker/test_docker_dummy_task.py
+++ tests/golem/docker/test_docker_dummy_task.py
@@ -29,7 +29,6 @@ logging.getLogger("peewee").setLevel("INFO")
 WAIT_TIMEOUT = 60
 
 
-@skip("Disabled because it leaves zombie processes #4165")
 class TestDockerDummyTask(
         DockerTaskTestCase[DummyTask, DummyTaskBuilder], TestWithReactor
 ):
