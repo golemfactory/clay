eb36d9de1f2905a01618c34b6da924798cb806e3
Adam Wierzbicki <awierzbicki@golem.network>
2019-05-14 19:04:18 +0200

    Unit tests for Runtime events

--- tests/golem/envs/test_runtime.py
+++ tests/golem/envs/test_runtime.py
@@ -2,7 +2,7 @@ from logging import Logger
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
-from golem.envs import RuntimeStatus, Runtime
+from golem.envs import RuntimeStatus, Runtime, RuntimeEventType, RuntimeEvent
 
 
 class TestRuntime(TestCase):
@@ -35,33 +35,65 @@ class TestChangeStatus(TestRuntime):
         self.assertEqual(self.runtime.status(), RuntimeStatus.RUNNING)
 
 
-class TestWrapStatusChange(TestRuntime):
+class TestEmitEvents(TestRuntime):
 
-    def test_error(self):
-        func = Mock(side_effect=ValueError)
-        error_msg = "test error"
-        wrapper = self.runtime._wrap_status_change(
-            success_status=RuntimeStatus.RUNNING,
-            error_msg=error_msg
+    def test_emit_event(self):
+        started_listener1 = Mock()
+        started_listener2 = Mock()
+        stopped_listener = Mock()
+
+        self.runtime.listen(RuntimeEventType.STARTED, started_listener1)
+        self.runtime.listen(RuntimeEventType.STARTED, started_listener2)
+        self.runtime.listen(RuntimeEventType.STOPPED, stopped_listener)
+
+        event = RuntimeEvent(
+            type=RuntimeEventType.STARTED,
+            details={"key": "value"}
         )
-        wrapped = wrapper(func)
 
-        with self.assertRaises(ValueError):
-            wrapped()
+        self.runtime._emit_event(event.type, event.details)
 
-        self.logger.exception.assert_called_once_with(error_msg)
-        self.assertEqual(self.runtime.status(), RuntimeStatus.FAILURE)
+        started_listener1.assert_called_once_with(event)
+        started_listener2.assert_called_once_with(event)
+        stopped_listener.assert_not_called()
 
-    def test_success(self):
-        func = Mock()
-        success_msg = "test success"
-        wrapper = self.runtime._wrap_status_change(
-            success_status=RuntimeStatus.RUNNING,
-            success_msg=success_msg
-        )
-        wrapped = wrapper(func)
+    @patch('golem.envs.Runtime._emit_event')
+    def test_prepared(self, emit):
+        self.runtime._prepared()
+        self.assertEqual(self.runtime.status(), RuntimeStatus.PREPARED)
+        self.logger.info.assert_called_once_with('Runtime prepared.')
+        emit.assert_called_once_with(RuntimeEventType.PREPARED)
 
-        wrapped()
-
-        self.logger.info.assert_called_once_with(success_msg)
+    @patch('golem.envs.Runtime._emit_event')
+    def test_started(self, emit):
+        self.runtime._started()
         self.assertEqual(self.runtime.status(), RuntimeStatus.RUNNING)
+        self.logger.info.assert_called_once_with('Runtime started.')
+        emit.assert_called_once_with(RuntimeEventType.STARTED)
+
+    @patch('golem.envs.Runtime._emit_event')
+    def test_stopped(self, emit):
+        self.runtime._stopped()
+        self.assertEqual(self.runtime.status(), RuntimeStatus.STOPPED)
+        self.logger.info.assert_called_once_with('Runtime stopped.')
+        emit.assert_called_once_with(RuntimeEventType.STOPPED)
+
+    @patch('golem.envs.Runtime._emit_event')
+    def test_torn_down(self, emit):
+        self.runtime._torn_down()
+        self.assertEqual(self.runtime.status(), RuntimeStatus.TORN_DOWN)
+        self.logger.info.assert_called_once_with('Runtime torn down.')
+        emit.assert_called_once_with(RuntimeEventType.TORN_DOWN)
+
+    @patch('golem.envs.Runtime._emit_event')
+    def test_error_occurred(self, emit):
+        error = RuntimeError("test")
+        message = "error message"
+        self.runtime._error_occurred(error, message)
+        self.assertEqual(self.runtime.status(), RuntimeStatus.FAILURE)
+        self.logger.error.assert_called_once_with(message, exc_info=error)
+        emit.assert_called_once_with(
+            RuntimeEventType.ERROR_OCCURRED, {
+                'error': error,
+                'message': message
+            })
