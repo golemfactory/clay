7a4b6dfd3d53868b3f7b482e8423a9a2541655f2
Kamil Śliwak <kamil.sliwak@codepoets.it>
2019-05-24 16:35:10 +0200

    ffmpeg.task: Update supported format and codec lists to include all values from ffmpeg_tools
    - I'm not sure if these should all be marked as supported but these lists seem unused and until now they contained all possible values so I'm just keeping it that way.

--- apps/transcoding/ffmpeg/task.py
+++ apps/transcoding/ffmpeg/task.py
@@ -83,13 +83,30 @@ class ffmpegDefaults(TaskDefaults):
 
 
 class ffmpegTaskBuilder(TranscodingTaskBuilder):
-    SUPPORTED_FILE_TYPES = [Container.c_MKV, Container.c_AVI,
-                            Container.c_MP4, Container.c_MOV, Container.c_MPEG,
-                            Container.c_3GP, Container.c_3G2, Container.c_F4V]
-    SUPPORTED_VIDEO_CODECS = [VideoCodec.MPEG_1, VideoCodec.MPEG_2,
-                              VideoCodec.H_264, VideoCodec.H_265,
-                              VideoCodec.HEVC, VideoCodec.H_264]
-    SUPPORTED_AUDIO_CODECS = [AudioCodec.MP3, AudioCodec.AAC]
+    SUPPORTED_FILE_TYPES = [
+        Container.c_3G2,
+        Container.c_3GP,
+        Container.c_AVI,
+        Container.c_F4V,
+        Container.c_MATROSKA,
+        Container.c_MOV,
+        Container.c_MP4,
+        Container.c_MPEG,
+        Container.c_WEBM,
+    ]
+    SUPPORTED_VIDEO_CODECS = [
+        VideoCodec.H_264,
+        VideoCodec.H_265,
+        VideoCodec.HEVC,
+        VideoCodec.MPEG_1,
+        VideoCodec.MPEG_2,
+        VideoCodec.MPEG_4,
+    ]
+    SUPPORTED_AUDIO_CODECS = [
+        AudioCodec.AAC,
+        AudioCodec.MP3,
+        AudioCodec.OPUS,
+    ]
     TASK_CLASS = ffmpegTask
     DEFAULTS = ffmpegDefaults
 
