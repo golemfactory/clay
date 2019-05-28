wilsonfonseca/golem

ffprobe_report: Implement stream report classes with a common base class

tests/apps/ffmpeg/task/ffprobe_report.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Collection, Dict, List, Optional, Tuple, Union
 
 from apps.transcoding.ffmpeg.utils import StreamOperator
 
@@ -174,3 +174,164 @@ class FuzzyInt:
 
     def __repr__(self):
         return f'FuzzyInt({self._value}, {self.tolerance_percent})'
+
+
+class FfprobeStreamReport:
+    ATTRIBUTES_TO_COMPARE = {
+        'codec_type',
+        'codec_name',
+        'start_time'
+    }
+
+    def __init__(self, raw_report: dict) -> None:
+        assert 'codec_type' in raw_report
+
+        self._raw_report = raw_report
+
+    @property
+    def codec_type(self) -> Optional[str]:
+        return self._raw_report.get('codec_type', None)
+
+    @property
+    def codec_name(self)-> Optional[str]:
+        return self._raw_report.get('codec_name', None)
+
+    @property
+    def start_time(self) -> FuzzyDuration:
+        return FuzzyDuration(
+            self._raw_report.get('start_time'),
+            0.1,
+        )
+
+    def __eq__(self, other):
+        return len(self.diff(other, {})) == 0
+
+
+class FfprobeAudioAndVideoStreamReport(FfprobeStreamReport):
+    ATTRIBUTES_TO_COMPARE = FfprobeStreamReport.ATTRIBUTES_TO_COMPARE | {
+        'duration',
+        'bitrate',
+        'frame_count',
+    }
+
+    @property
+    def duration(self) -> FuzzyDuration:
+        return FuzzyDuration(self._raw_report.get('duration'), 0.1)
+
+    @property
+    def bitrate(self) -> FuzzyInt:
+        return FuzzyInt(self._raw_report.get('bit_rate'), 5)
+
+    @property
+    def frame_count(self) -> Union[str, Any]:
+        return self._raw_report.get('nb_frames')
+
+
+class FfprobeVideoStreamReport(FfprobeAudioAndVideoStreamReport):
+    ATTRIBUTES_TO_COMPARE = \
+        FfprobeAudioAndVideoStreamReport.ATTRIBUTES_TO_COMPARE | {
+            'resolution',
+            'pixel_format',
+            'frame_rate',
+        }
+
+    def __init__(self, raw_report: dict) -> None:
+        assert raw_report['codec_type'] == 'video'
+        super().__init__(raw_report)
+
+    @property
+    def resolution(self) -> Union[Collection, Tuple[Collection, Any, Any]]:
+        return (
+            self._raw_report.get('resolution', None),
+            self._raw_report.get('width', None),
+            self._raw_report.get('height', None),
+        )
+
+    @property
+    def pixel_format(self) -> Optional[str]:
+        return self._raw_report.get('pix_fmt')
+
+    @property
+    def frame_rate(self)-> Union[float, str, None]:
+        frame_rate = self._raw_report.get('r_frame_rate')
+        if isinstance(frame_rate, (int, float)):
+            return frame_rate
+        elif isinstance(frame_rate, str):
+            splited = frame_rate.split('/')
+            try:
+                return float(splited[0])/float(splited[1])
+            except (ValueError, TypeError):
+                pass
+        return self._raw_report.get('r_frame_rate')
+
+    def diff(self,
+             format_report: dict,
+             overrides: Optional[dict] = None) -> list:
+
+        if overrides is None:
+            overrides = {}
+
+        differences = list()
+        for attr in self.ATTRIBUTES_TO_COMPARE:
+            original_value = getattr(self, attr)
+            modified_value = getattr(format_report, attr)
+
+            if modified_value != original_value:
+                diff_dict = {
+                    'location': 'video',
+                    'attribute': attr,
+                    'original value': original_value,
+                    'modified value': modified_value,
+                }
+                differences.append(diff_dict)
+        return differences
+
+
+class FfprobeAudioStreamReport(FfprobeAudioAndVideoStreamReport):
+    def __init__(self, raw_report: dict) -> None:
+        assert raw_report['codec_type'] == 'audio'
+        super().__init__(raw_report)
+
+    ATTRIBUTES_TO_COMPARE = \
+        FfprobeAudioAndVideoStreamReport.ATTRIBUTES_TO_COMPARE | {
+            'sample_rate',
+            'sample_format',
+            'channel_count',
+            'channel_layout',
+        }
+
+    @property
+    def sample_rate(self)-> Union[int, Any]:
+        return int(self._raw_report.get('sample_rate'))
+
+    @property
+    def sample_format(self) -> Optional[str]:
+        return self._raw_report.get('sample_format')
+
+    @property
+    def channel_count(self)-> Optional[int]:
+        return self._raw_report.get('channels')
+
+    @property
+    def channel_layout(self)-> Optional[str]:
+        return self._raw_report.get('channel_layout')
+
+
+class FfprobeSubtitleStreamReport(FfprobeStreamReport):
+    def __init__(self, raw_report: dict) -> None:
+        assert raw_report['codec_type'] == 'subtitle'
+        super().__init__(raw_report)
+
+    ATTRIBUTES_TO_COMPARE = FfprobeStreamReport.ATTRIBUTES_TO_COMPARE | {
+        'language',
+    }
+
+    @property
+    def language(self):
+        return self._raw_report.get('tags', {}).get('language')
+
+
+class FfprobeDataStreamReport(FfprobeStreamReport):
+    def __init__(self, raw_report: dict) -> None:
+        assert raw_report['codec_type'] == 'data'
+        super().__init__(raw_report)

golemfactory/clay

tests/apps/ffmpeg/task/ffprobe_report.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Collection, Dict, List, Optional, Tuple, Union
 
 from apps.transcoding.ffmpeg.utils import StreamOperator
 
@@ -174,3 +174,164 @@ class FuzzyInt:
 
     def __repr__(self):
         return f'FuzzyInt({self._value}, {self.tolerance_percent})'
+
+
+class FfprobeStreamReport:
+    ATTRIBUTES_TO_COMPARE = {
+        'codec_type',
+        'codec_name',
+        'start_time'
+    }
+
+    def __init__(self, raw_report: dict) -> None:
+        assert 'codec_type' in raw_report
+
+        self._raw_report = raw_report
+
+    @property
+    def codec_type(self) -> Optional[str]:
+        return self._raw_report.get('codec_type', None)
+
+    @property
+    def codec_name(self)-> Optional[str]:
+        return self._raw_report.get('codec_name', None)
+
+    @property
+    def start_time(self) -> FuzzyDuration:
+        return FuzzyDuration(
+            self._raw_report.get('start_time'),
+            0.1,
+        )
+
+    def __eq__(self, other):
+        return len(self.diff(other)) == 0
+
+
+class FfprobeAudioAndVideoStreamReport(FfprobeStreamReport):
+    ATTRIBUTES_TO_COMPARE = FfprobeStreamReport.ATTRIBUTES_TO_COMPARE | {
+        'duration',
+        'bitrate',
+        'frame_count',
+    }
+
+    @property
+    def duration(self) -> FuzzyDuration:
+        return FuzzyDuration(self._raw_report.get('duration'), 0.1)
+
+    @property
+    def bitrate(self) -> FuzzyInt:
+        return FuzzyInt(self._raw_report.get('bit_rate'), 5)
+
+    @property
+    def frame_count(self) -> Union[str, Any]:
+        return self._raw_report.get('nb_frames')
+
+
+class FfprobeVideoStreamReport(FfprobeAudioAndVideoStreamReport):
+    ATTRIBUTES_TO_COMPARE = \
+        FfprobeAudioAndVideoStreamReport.ATTRIBUTES_TO_COMPARE | {
+            'resolution',
+            'pixel_format',
+            'frame_rate',
+        }
+
+    def __init__(self, raw_report: dict) -> None:
+        assert raw_report['codec_type'] == 'video'
+        super().__init__(raw_report)
+
+    @property
+    def resolution(self) -> Union[Collection, Tuple[Collection, Any, Any]]:
+        return (
+            self._raw_report.get('resolution', None),
+            self._raw_report.get('width', None),
+            self._raw_report.get('height', None),
+        )
+
+    @property
+    def pixel_format(self) -> Optional[str]:
+        return self._raw_report.get('pix_fmt')
+
+    @property
+    def frame_rate(self)-> Union[float, str, None]:
+        frame_rate = self._raw_report.get('r_frame_rate')
+        if isinstance(frame_rate, (int, float)):
+            return frame_rate
+        elif isinstance(frame_rate, str):
+            split = frame_rate.split('/')
+            try:
+                return float(split[0]) / float(split[1])
+            except (ValueError, TypeError):
+                pass
+        return self._raw_report.get('r_frame_rate')
+
+    def diff(self,
+             format_report: dict,
+             overrides: Optional[dict] = None) -> list:
+
+        if overrides is None:
+            overrides = {}
+
+        differences = list()
+        for attr in self.ATTRIBUTES_TO_COMPARE:
+            original_value = getattr(self, attr)
+            modified_value = getattr(format_report, attr)
+
+            if modified_value != original_value:
+                diff_dict = {
+                    'location': 'video',
+                    'attribute': attr,
+                    'original value': original_value,
+                    'modified value': modified_value,
+                }
+                differences.append(diff_dict)
+        return differences
+
+
+class FfprobeAudioStreamReport(FfprobeAudioAndVideoStreamReport):
+    def __init__(self, raw_report: dict) -> None:
+        assert raw_report['codec_type'] == 'audio'
+        super().__init__(raw_report)
+
+    ATTRIBUTES_TO_COMPARE = \
+        FfprobeAudioAndVideoStreamReport.ATTRIBUTES_TO_COMPARE | {
+            'sample_rate',
+            'sample_format',
+            'channel_count',
+            'channel_layout',
+        }
+
+    @property
+    def sample_rate(self)-> Union[int, Any]:
+        return int(self._raw_report.get('sample_rate'))
+
+    @property
+    def sample_format(self) -> Optional[str]:
+        return self._raw_report.get('sample_format')
+
+    @property
+    def channel_count(self)-> Optional[int]:
+        return self._raw_report.get('channels')
+
+    @property
+    def channel_layout(self)-> Optional[str]:
+        return self._raw_report.get('channel_layout')
+
+
+class FfprobeSubtitleStreamReport(FfprobeStreamReport):
+    def __init__(self, raw_report: dict) -> None:
+        assert raw_report['codec_type'] == 'subtitle'
+        super().__init__(raw_report)
+
+    ATTRIBUTES_TO_COMPARE = FfprobeStreamReport.ATTRIBUTES_TO_COMPARE | {
+        'language',
+    }
+
+    @property
+    def language(self):
+        return self._raw_report.get('tags', {}).get('language')
+
+
+class FfprobeDataStreamReport(FfprobeStreamReport):
+    def __init__(self, raw_report: dict) -> None:
+        assert raw_report['codec_type'] == 'data'
+        super().__init__(raw_report)