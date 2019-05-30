86c727626c855992a457510baae3d84b18bc9ade
radek <radoslaw.kondziolka@golem.network>
2019-05-30 13:39:54 +0200

    linter fixes

--- golem/verificator/blender_verifier.py
+++ golem/verificator/blender_verifier.py
@@ -74,11 +74,12 @@ class BlenderVerifier(FrameRenderingVerifier):
         self.finished.addErrback(failure)
 
         subtask_info = self.verification_data['subtask_info']
-        work_dir = Path(os.path.dirname(self.verification_data['results'][0])).parent
+        work_dir = Path(os.path.dirname(
+            self.verification_data['results'][0])).parent
         res_dir = os.path.join(work_dir, 'resources')
         tmp_dir = os.path.join(work_dir, "tmp")
 
-        assert(len(self.verification_data['resources']) > 0)
+        assert self.verification_data['resources']
 
         shutil.copy(self.verification_data['resources'][0], res_dir)
         shutil.copy(self.verification_data['results'][0], work_dir)
