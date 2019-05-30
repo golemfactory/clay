8ba0c12934cc75ffaf342609b67f39182ff49dc3
Piotr Dybowski <piotr.dybowski@codepoets.it>
2019-05-30 12:24:48 +0200

    Fix path generation for provider result's crop

--- apps/blender/resources/images/entrypoints/scripts/verifier_tools/image_metrics_calculator.py
+++ apps/blender/resources/images/entrypoints/scripts/verifier_tools/image_metrics_calculator.py
@@ -60,11 +60,16 @@ def calculate_metrics(
         print("There were errors %r" % e, file=sys.stderr)
         compare_metrics['Label'] = VERIFICATION_FAIL
     providers_result_crop.save(
-        PROVIDER_RESULT_CROP_NAME_PREFIX + os.path.basename(reference_crop_path)
+        _generate_path_for_providers_result_crop(reference_crop_path)
     )
     return ImgageMetrics(compare_metrics).write_to_file(
         metrics_output_filename
     )
+
+
+def _generate_path_for_providers_result_crop(reference_crop_path):
+    return PROVIDER_RESULT_CROP_NAME_PREFIX + os.path.splitext(os.path.basename(
+        reference_crop_path))[0]
 
 
 def load_classifier():
