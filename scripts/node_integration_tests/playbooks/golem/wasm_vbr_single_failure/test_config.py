46b834366e38526d6307e602e4acad3b29860909
Michal Plebanski <michal.plebanski@golem.network>
2019-06-28 16:01:31 +0200

    Fix node integration tests

--- scripts/node_integration_tests/playbooks/golem/wasm_vbr_single_failure/test_config.py
+++ scripts/node_integration_tests/playbooks/golem/wasm_vbr_single_failure/test_config.py
@@ -7,6 +7,7 @@ from ...test_config_base import (
 
 import tasks
 from pathlib import Path
+from scripts.node_integration_tests import helpers
 
 extend_enum(NodeId, 'provider2', 'provider2')
 extend_enum(NodeId, 'provider3', 'provider3')
@@ -20,16 +21,7 @@ class TestConfig(TestConfigBase):
     def __init__(self, *, task_settings: str = 'WASM_g_flite') -> None:
         super().__init__(task_settings=task_settings)
         self.task_package = 'test_wasm_task_0'
-        self.task_settings = 'WASM_g_flite'
-        settings = tasks.get_settings(task_settings)
-        cwd = Path(__file__).resolve().parent
-        settings['options']['input_dir'] = str((cwd / '..' / '..' / '..' /
-                                                'tasks' / self.task_package /
-                                                'in').resolve())
-        settings['options']['output_dir'] = str((cwd / '..' / '..' / '..' /
-                                                 'tasks' / self.task_package /
-                                                 'out').resolve())
-
+        self.update_task_dict()
         self.nodes[NodeId.provider2] = make_node_config_from_env(
             NodeId.provider2.value, 2
         )
@@ -48,4 +40,15 @@ class TestConfig(TestConfigBase):
             NodeId.provider5.value, 5
         )
 
-        self.task_dict = settings
+    def update_task_dict(self):
+        self.task_dict = helpers.construct_test_task(
+            task_package_name=self.task_package,
+            task_settings=self.task_settings,
+        )
+        cwd = Path(__file__).resolve().parent
+        self.task_dict['options']['input_dir'] = str((cwd / '..' / '..' / '..' /
+                                                       'tasks' / self.task_package /
+                                                       'in').resolve())
+        self.task_dict['options']['output_dir'] = str((cwd / '..' / '..' / '..' /
+                                                       'tasks' / self.task_package /
+                                                       'out').resolve())
