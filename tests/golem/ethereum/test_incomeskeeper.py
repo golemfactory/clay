93bb04a90bdee869af5b3994996440d4cd104495
Dariusz Rybi <dariusz@golem.network>
2019-07-02 13:59:10 +0200

    [review] Remove duplicated test

--- tests/golem/ethereum/test_incomeskeeper.py
+++ tests/golem/ethereum/test_incomeskeeper.py
@@ -290,26 +290,13 @@ class TestIncomesKeeper(TestWithDatabase):
             model.WalletOperation.STATUS.overdue,
         )
 
-    def test_received_eth_transfer(self):
+    def test_received_transfer(self):
         self.incomes_keeper.received_transfer(
             tx_hash=f"0x{'0'*64}",
             sender_address=random_eth_address(),
             recipient_address=random_eth_address(),
             amount=1,
             currency=model.WalletOperation.CURRENCY.ETH,
-        )
-        self.assertEqual(
-            model.WalletOperation.select().count(),
-            1,
-        )
-
-    def test_received_gnt_transfer(self):
-        self.incomes_keeper.received_transfer(
-            tx_hash=f"0x{'0'*64}",
-            sender_address=random_eth_address(),
-            recipient_address=random_eth_address(),
-            amount=1,
-            currency=model.WalletOperation.CURRENCY.GNT,
         )
         self.assertEqual(
             model.WalletOperation.select().count(),
