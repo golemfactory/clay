121802b135a71bdb2af76e790c1045c63c5ede74
Marek Franciszkiewicz <marek@golem.network>
2019-07-02 15:37:00 +0200

    Buildable and runnable binary

--- golem/node.py
+++ golem/node.py
@@ -164,6 +164,7 @@ class Node(HardwarePresetsMixin):
         self._crossbar_serializer = crossbar_serializer
 
     def start(self) -> None:
+        from autobahn.wamp import ApplicationError
 
         HardwarePresets.initialize(self._datadir)
         HardwarePresets.update_config(self._config_desc.hardware_preset_name,
@@ -179,15 +180,21 @@ class Node(HardwarePresetsMixin):
                 return gatherResults([terms_, keys, docker], consumeErrors=True)
 
             def on_start_error(failure: FirstError):
-                sub_failure: Failure = failure.value.subFailure
-                sub_failure.trap(EnvironmentError)
+                exception = failure.value
+
+                if isinstance(exception, ApplicationError):
+                    error_message = exception.error_message()
+                else:
+                    sub_failure: Failure = exception.subFailure
+                    sub_failure.trap(EnvironmentError)
+                    error_message = sub_failure.getErrorMessage()
 
                 logger.error(
                     """
                     There was a problem setting up the environment: {}
                     Golem will run with limited functionality to support
                     communication with local clients.
-                    """.format(sub_failure.getErrorMessage())
+                    """.format(error_message)
                 )
 
             chain_function(rpc, on_rpc_ready).addCallbacks(
@@ -546,6 +553,8 @@ class Node(HardwarePresetsMixin):
         # pylint: disable=no-member
         self.rpc_session.add_procedures(methods).addCallback(  # type: ignore
             rpc_ready,
+        ).addErrback(
+            self._error('Registering RPC procedures failed')
         )
         # pylint: enable=no-member
 
