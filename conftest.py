2390a06ebc4d840a307c5aca8dd1f8cd3ae6316e
Krigpl <3904252+Krigpl@users.noreply.github.com>
2019-07-10 12:42:40 +0200

    Fix typehints of conftest files (#4446)

--- conftest.py
+++ conftest.py
@@ -1,12 +1,15 @@
+from typing import List
+import _pytest
 import pytest
 
 
-def pytest_addoption(parser) -> None:
+def pytest_addoption(parser: _pytest.config.argparsing.Parser) -> None:
     parser.addoption("--runslow", action="store_true",
                      default=False, help="run slow tests")
 
 
-def pytest_collection_modifyitems(config, items) -> None:
+def pytest_collection_modifyitems(config: _pytest.config.Config,
+                                  items: List[pytest.Item]) -> None:
     if config.getoption("--runslow"):
         # --runslow given in cli: do not skip slow tests
         return
