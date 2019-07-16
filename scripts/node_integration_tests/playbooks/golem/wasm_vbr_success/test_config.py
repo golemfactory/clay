6832462943f813b8278ba0e2acc601bc7f32f846
Jakub Konka <jakub.konka@golem.network>
2019-07-16 18:26:57 +0200

    Wasm verification by redundancy - backported to b0.20.1 (#4475)
    * Draft out skeleton for Wasm VbR
    
    * Add should_accept_client with description
    
    * Update apps/wasm/task.py
    
    Co-Authored-By: Marcin Benke <marcin.benke@gmail.com>
    
    * Update apps/wasm/task.py
    
    Co-Authored-By: Marcin Benke <marcin.benke@gmail.com>
    
    * Add pseudocode to lay out potential flow
    
    * Copy methods from CoreTask to WasmTask
    
    * Add draft VbrSubtaskQueue and VbrSubtask
    
    * Simplify the queueing mechanism
    
    * Refactor
    
    * Add classes from vbr.py
    
    * Add draft impl of a couple required methods
    
    * Mark unfinished funcs
    
    * Modularise
    
    * Check if verification is finished in computation_finished
    
    * Align should_accept_client with VbrSubtask
    
    * Add complete draft of computation_finished
    
    * Refactor
    
    * Wasm task fixes
    
    * Fix payment problem related to subtasks_count
    
    * Update comments in vbr.py
    
    * Remove copied comment from BucketVerifier
    
    * Update apps/wasm/vbr.py
    
    Change actor - result order.
    
    Co-Authored-By: Marcin Benke <marcin.benke@gmail.com>
    
    * Update apps/wasm/vbr.py
    
    Remove debug message.
    
    Co-Authored-By: Marcin Benke <marcin.benke@gmail.com>
    
    * Remove SimpleSubtaskVerifier from vbr.py
    
    * Fixed progress reporting and allowing duplicate Actor
    
    * Update apps/wasm/task.py
    
    Use named parameter referee_count
    
    Co-Authored-By: Marcin Benke <marcin.benke@gmail.com>
    
    * Add a wiser approach to finding custom class instance in a list
    
    * Add basic integration test for WASM
    
    * Update apps/wasm/vbr.py
    
    Co-Authored-By: Marcin Benke <marcin.benke@gmail.com>
    
    * Add description to VbrSubtask
    
    * Fix typing in linter
    
    * Fix type returned by _find_vbrsubtask_by_id
    
    * Fix linter
    
    * More linter fixes
    
    * Fix some linter errors
    
    * Remove unused WasmTaskVerifier
    
    * Fixed verification failure case
    
    * Add single failure integration test
    
    * Add missing test task for wasm integrations tests
    
    * Linter fixes for WASM task
    
    * More linter fixes for WASM task
    
    * Add crash integration test case and fixes WasmTask
    
    * Linter fixes for WasmTask integration test
    
    * Fix integration test
    
    * Fix WasmTask assigning orphaned task
    
    * Fix integrations test in WASM task
    
    * Task logic fixes in Golem
    
    * Fix node integration tests
    
    * Fix remaining integration tests
    
    * Fix linters in WASM integration tests
    
    * Remove no longer applicable test for WASM task
    
    * More linter fixes
    
    * Fix logs in WasmTask.cmp_results
    
    * Logging fixes
    
    * Refactor stub methods in WasmTask
    
    * Revert "Remove no longer applicable test for WASM task"
    
    This reverts commit c7d624ac0f848dc46957b688545826da1bc6c437.
    
    * WasmTask remove non applicable unit tests
    
    * Remove nonappliacle WASM unit tests
    
    * Small linter fix
    
    * Add WASM verifier unit tests
    
    * Linter fixes to unit tests
    
    * Fix build on b0.20.1
    
    * Silence log in finished_computation

---
+++ scripts/node_integration_tests/playbooks/golem/wasm_vbr_success/test_config.py
@@ -0,0 +1,34 @@
+from aenum import extend_enum
+from ...test_config_base import (
+    TestConfigBase,
+    make_node_config_from_env,
+    NodeId
+)
+
+import pathlib
+from scripts.node_integration_tests import helpers
+
+extend_enum(NodeId, 'provider2', 'provider2')
+
+
+class TestConfig(TestConfigBase):
+    def __init__(self, *, task_settings: str = 'WASM_g_flite') -> None:
+        super().__init__(task_settings=task_settings)
+        self.task_package = 'test_wasm_task_0'
+        self.update_task_dict()
+        self.nodes[NodeId.provider2] = make_node_config_from_env(
+            NodeId.provider2.value, 2
+        )
+
+    def update_task_dict(self):
+        self.task_dict = helpers.construct_test_task(
+            task_package_name=self.task_package,
+            task_settings=self.task_settings,
+        )
+        cwd = pathlib.Path(__file__).resolve().parent
+        self.task_dict['options']['input_dir'] =\
+            str((cwd / '..' / '..' / '..' / 'tasks' / self.task_package /
+                'in').resolve())
+        self.task_dict['options']['output_dir'] =\
+            str((cwd / '..' / '..' / '..' / 'tasks' / self.task_package /
+                'out').resolve())
