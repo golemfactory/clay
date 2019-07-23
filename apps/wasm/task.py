f86cf05395dcad9aacaa59aebde6cfff620222a3
Michal Plebanski <michal.plebanski@golem.network>
2019-07-23 12:02:57 +0200

    Move WasmBenchmark to apps/wasm/task.py

--- apps/wasm/task.py
+++ apps/wasm/task.py
@@ -1,5 +1,8 @@
 from copy import deepcopy
+import logging
+import os
 from pathlib import Path, PurePath
+from tempfile import TemporaryDirectory
 from typing import (
     Any,
     Dict,
@@ -12,7 +15,7 @@ from typing import (
     Callable,
     Set
 )
-import logging
+import uuid
 
 from golem_messages.message import ComputeTaskDef
 from golem_messages.datastructures.p2p import Node
@@ -22,9 +25,10 @@ from apps.core.task.coretask import (
     CoreTaskBuilder,
     CoreTaskTypeInfo
 )
+from apps.core.benchmark.benchmarkrunner import CoreBenchmark
 from apps.core.task.coretaskstate import Options, TaskDefinition
 from apps.wasm.environment import WasmTaskEnvironment
-from apps.wasm.benchmark import WasmTaskBenchmark
+from golem.core.common import get_golem_path
 from golem.marketplace.marketplace import RequestorMarketStrategy
 from golem.marketplace.wasm_marketplace import RequestorWasmMarketStrategy
 from golem.model import Performance
@@ -523,5 +527,52 @@ class WasmTaskTypeInfo(CoreTaskTypeInfo):
             Performance.environment_id == WasmTaskEnvironment.ENV_ID
         )
         self.MARKET_STRATEGY.set_performance(
-            WasmTaskBenchmark.normalization_constant / perf_db_item.value
+            WasmBenchmark.normalization_constant / perf_db_item.value
         )
+
+
+class WasmBenchmark(CoreBenchmark):
+    EXPECTED_OUTPUT = 'Hello world!\ntest_input\ntest_arg\n'
+
+    def __init__(self):
+        self._normalization_constant = 1000
+        self.test_data_dir = os.path.join(
+            get_golem_path(), 'apps', 'wasm', 'test_data'
+        )
+        self.output_dir = TemporaryDirectory()
+
+        opts = WasmTaskOptions()
+        opts.input_dir = os.path.join(self.test_data_dir, 'input')
+        opts.output_dir = self.output_dir.name
+        opts.js_name = 'test.js'
+        opts.wasm_name = 'test.wasm'
+        opts.subtasks = {
+            'test_subtask': WasmTaskOptions.SubtaskOptions(
+                'test_subtask', ['test_arg'], ['out.txt']
+            )
+        }
+
+        self._task_definition = WasmTaskDefinition()
+        self._task_definition.task_id = str(uuid.uuid4())
+        self._task_definition.options = opts
+        self._task_definition.subtasks_count = 1
+        self._task_definition.add_to_resources()
+
+    @property
+    def normalization_constant(self):
+        return self._normalization_constant
+
+    @property
+    def task_definition(self):
+        return self._task_definition
+
+    def verify_result(self, result):
+        for result_file in result:
+            if os.path.basename(result_file) == 'out.txt':
+                actual_output_path = result_file
+                break
+        else:
+            return False
+
+        with open(actual_output_path, 'r') as f_act:
+            return f_act.read() == self.EXPECTED_OUTPUT
