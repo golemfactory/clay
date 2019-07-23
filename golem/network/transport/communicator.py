9ce423469938727888497d10b7e190758788dd08
radek <radoslaw.kondziolka@golem.network>
2019-07-23 17:56:03 +0200

    added communicator file

---
+++ golem/network/transport/communicator.py
@@ -0,0 +1,11 @@
+# import twisted.persisted. TODO
+import twisted.internet.defer
+
+
+class Communicator:
+    def __init__(self):
+        self.
+
+    def send_message(self, node_id, msg, timeout, ): # TOOD : typy
+        deffered = twisted.internet.defer.execute()
+
