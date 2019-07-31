28961d5aa4f847776a9f26dd34bb2efdc9a5f655
Adam Wierzbicki <awierzbicki@golem.network>
2019-07-31 15:46:20 +0200

    Fixed TaskComputerSnapshotModel
    It had been referencing non-exisitng field of TaskComputerAdapter.

--- tests/golem/monitor/model/test_taskcomputersnapshotmodel.py
+++ tests/golem/monitor/model/test_taskcomputersnapshotmodel.py
@@ -4,13 +4,19 @@ import unittest.mock as mock
 from pydispatch import dispatcher
 
 from golem.monitor.test_helper import MonitorTestBaseClass
+from golem.task.taskcomputer import TaskComputerAdapter
 
 
 class TestTaskComputerSnapshotModel(MonitorTestBaseClass):
+
     def test_channel(self):
-        computer_mock = mock.MagicMock()
-        computer_mock.compute_tasks = compute_tasks = random.random() > 0.5
-        computer_mock.assigned_subtask = {'subtask_id': 'test_subtask_id'}
+        compute_tasks = random.random() > 0.5
+        computer_mock = mock.Mock(
+            spec=TaskComputerAdapter,
+            compute_tasks=compute_tasks
+        )
+        computer_mock.has_assigned_task.return_value = True
+        computer_mock.assigned_subtask_id = 'test_subtask_id'
 
         with mock.patch('golem.monitor.monitor.SenderThread.send') as mock_send:
             dispatcher.send(
