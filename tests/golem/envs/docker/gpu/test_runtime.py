55b510f95f91e9515d438d5d8c54f9f40386549f
Marek Franciszkiewicz <marek@golem.network>
2019-08-16 15:16:22 +0200

    New: DockerGPUEnvironment New: nvidia-container-runtime options support Bump: docker to v4.0.2

---
+++ tests/golem/envs/docker/gpu/test_runtime.py
@@ -0,0 +1,23 @@
+from unittest import TestCase, mock
+
+from golem.envs.docker.gpu import DockerGPURuntime
+
+
+class TestRuntime(TestCase):
+
+    def setUp(self) -> None:
+        self.runtime = DockerGPURuntime(
+            container_config=dict(
+                image='golemfactory/test',
+                command='/bin/false'
+            ),
+            port_mapper=mock.Mock()
+        )
+
+    def test_usage_counters(self):
+        with self.assertRaises(NotImplementedError):
+            self.runtime.usage_counters()
+
+    def test_call(self):
+        with self.assertRaises(NotImplementedError):
+            self.runtime.call('test')
