52f585dca1ada2902f9ef65c3a2dea93f5b8d8ee
Dariusz Rybi <dariusz@golem.network>
2019-08-22 10:53:14 +0200

    Extract event loop from twisted.reactor
    AsyncIO event loop is created in common.install_reactor()
    and can be extracted from twisted.internet.reactor when
    needed (in threads).

--- golem/decorators.py
+++ golem/decorators.py
@@ -6,20 +6,6 @@ from golem import model
 log = logging.getLogger('golem.decorators')
 
 
-def log_error(reraise=False):
-    def _curry(f):
-        @functools.wraps(f)
-        def _wrapper(*args, **kwargs):
-            try:
-                return f(*args, **kwargs)
-            except Exception:  # pylint: disable=broad-except
-                log.exception('Error in %r', f)
-                if reraise:
-                    raise
-        return _wrapper
-    return _curry
-
-
 def run_with_db():
     """Run only when DB is active"""
     def wrapped(f):
