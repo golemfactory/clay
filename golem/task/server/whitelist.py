7958d5ea8b347de71e578550cfbe93b159ac6329
Marek Franciszkiewicz <marek@golem.network>
2019-09-03 21:34:06 +0200

    New: Docker image whitelist RPC

---
+++ golem/task/server/whitelist.py
@@ -0,0 +1,76 @@
+from typing import Any, Dict, List
+
+from dataclasses import dataclass, field, asdict
+
+from golem.core.common import get_timestamp_utc
+from golem.envs.docker.whitelist import Whitelist, repository_from_image_name
+from golem.report import EventPublisher
+from golem.rpc import utils as rpc_utils
+from golem.rpc.mapping.rpceventnames import Environment
+
+MAX_DISCOVERED_IMAGES: int = 50
+
+
+@dataclass
+class DiscoveredDockerImage:
+    name: str
+    discovery_ts: float = field(default_factory=get_timestamp_utc)
+
+
+class DockerWhitelistRPC:
+    def __init__(self) -> None:
+        self._discovered: List[DiscoveredDockerImage] = list()
+
+    def _docker_image_discovered(
+            self,
+            name: str
+    ) -> None:
+        if Whitelist.is_whitelisted(name):
+            return
+        for discovered in self._discovered:
+            if discovered.name == name:
+                discovered.discovery_ts = get_timestamp_utc()
+                return
+
+        discovered_image = DiscoveredDockerImage(name)
+        self._discovered.append(discovered_image)
+        self._docker_refresh_discovered()
+
+        EventPublisher.publish(
+            Environment.evt_prereq_discovered,
+            asdict(discovered_image),
+            'docker')
+
+    def _docker_refresh_discovered(self) -> None:
+        """ Update the internal discovered Docker image collection based on
+            Whitelist status and the number of MAX_DISCOVERED_IMAGES stored """
+        self._discovered = [
+            discovered for discovered in self._discovered
+            if not Whitelist.is_whitelisted(
+                repository_from_image_name(discovered.name))
+        ]
+        self._discovered = self._discovered[-MAX_DISCOVERED_IMAGES:]
+
+    @rpc_utils.expose('env.docker.repository.discovered')
+    def _docker_discovered_get(self) -> Dict[str, Dict[str, Any]]:
+        return {
+            discovered.name: asdict(discovered)
+            for discovered in self._discovered
+        }
+
+    @staticmethod
+    @rpc_utils.expose('env.docker.repository.whitelist')
+    def _docker_whitelist_get() -> List[str]:
+        return Whitelist.get()
+
+    @rpc_utils.expose('env.docker.repository.whitelist.add')
+    def _docker_whitelist_add(self, image_name: str) -> None:
+        repository = repository_from_image_name(image_name)
+        Whitelist.add(repository)
+        self._docker_refresh_discovered()
+
+    @staticmethod
+    @rpc_utils.expose('env.docker.repository.whitelist.remove')
+    def _docker_whitelist_remove(image_name: str) -> None:
+        repository = repository_from_image_name(image_name)
+        Whitelist.remove(repository)
---
+++ tests/golem/task/server/test_whitelist.py
@@ -0,0 +1,120 @@
+import unittest
+from unittest import mock
+
+from freezegun import freeze_time
+
+from golem.core.common import get_timestamp_utc
+from golem.task.server.whitelist import DiscoveredDockerImage, \
+    DockerWhitelistRPC
+from golem.tools.testwithdatabase import TestWithDatabase
+
+ROOT_PATH = 'golem.task.server.whitelist'
+MAX_IMAGES = 5
+
+
+class TestDiscoveredDockerImage(unittest.TestCase):
+
+    @freeze_time("1000")
+    def test_new(self):
+        discovered_img = DiscoveredDockerImage(name='golemfactory/test')
+        self.assertEqual(discovered_img.name, 'golemfactory/test')
+        self.assertEqual(discovered_img.discovery_ts, get_timestamp_utc())
+
+
+@mock.patch(f'{ROOT_PATH}.MAX_DISCOVERED_IMAGES', MAX_IMAGES)
+@mock.patch(f'{ROOT_PATH}.EventPublisher')
+class TestDiscoveryStorage(TestWithDatabase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.whitelist_rpc = DockerWhitelistRPC()
+
+    def test_discovery_storage_limit(self, event_publisher):
+        for i in range(MAX_IMAGES * 2):
+            self.whitelist_rpc._docker_image_discovered(name=f'repo/{i}')
+
+        assert event_publisher.publish.call_count == MAX_IMAGES * 2
+        assert len(self.whitelist_rpc._discovered) == MAX_IMAGES
+
+        for i in range(MAX_IMAGES):
+            discovered_app = self.whitelist_rpc._discovered[i]
+            assert discovered_app.name == f'repo/{MAX_IMAGES + i}'
+
+    def test_discovered_twice(self, event_publisher):
+
+        with freeze_time("1000"):
+            initial_time = get_timestamp_utc()
+            self.whitelist_rpc._docker_image_discovered(name=f'repo/0')
+
+        assert len(self.whitelist_rpc._discovered) == 1
+        assert self.whitelist_rpc._discovered[0].discovery_ts == initial_time
+        assert event_publisher.publish.call_count == 1
+
+        with freeze_time("2000"):
+            update_time = get_timestamp_utc()
+            self.whitelist_rpc._docker_image_discovered(name=f'repo/0')
+
+        assert len(self.whitelist_rpc._discovered) == 1
+        assert self.whitelist_rpc._discovered[0].discovery_ts == update_time
+        assert event_publisher.publish.call_count == 1
+
+    @mock.patch(f'{ROOT_PATH}.Whitelist.is_whitelisted', return_value=True)
+    def test_discovered_when_whitelisted(self, _, event_publisher):
+        for i in range(MAX_IMAGES):
+            self.whitelist_rpc._docker_image_discovered(name=f'repo/{i}')
+
+        assert event_publisher.publish.call_count == 0
+        assert not self.whitelist_rpc._discovered
+
+    @mock.patch(f'{ROOT_PATH}.Whitelist.is_whitelisted', return_value=False)
+    def test_refresh(self, _, event_publisher):
+        for i in range(MAX_IMAGES):
+            self.whitelist_rpc._docker_image_discovered(name=f'repo/{i}')
+
+        assert event_publisher.publish.call_count == MAX_IMAGES
+        assert len(self.whitelist_rpc._discovered) == MAX_IMAGES
+
+        def is_whitelisted(name: str) -> bool:
+            return not name.startswith('test')
+
+        with mock.patch(
+            f'{ROOT_PATH}.Whitelist.is_whitelisted', side_effect=is_whitelisted
+        ):
+            self.whitelist_rpc._docker_image_discovered(name='test/0')
+
+        assert len(self.whitelist_rpc._discovered) == 1
+
+
+@mock.patch(f'{ROOT_PATH}.MAX_DISCOVERED_IMAGES', MAX_IMAGES)
+@mock.patch(f'{ROOT_PATH}.Whitelist')
+class TestAppManagerRPCMethods(TestWithDatabase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.whitelist_rpc = DockerWhitelistRPC()
+
+        with mock.patch(f'{ROOT_PATH}.EventPublisher'):
+            for i in range(MAX_IMAGES):
+                self.whitelist_rpc._docker_image_discovered(name=f'repo/{i}')
+
+    def test_docker_discovered_get(self, _):
+        discovered = self.whitelist_rpc._docker_discovered_get()
+        assert isinstance(discovered, dict)
+        assert len(discovered) == MAX_IMAGES
+        assert all(
+            isinstance(key, str) and isinstance(value, dict)
+            for key, value in discovered.items())
+
+    def test_docker_whitelist_get(self, whitelist):
+        self.whitelist_rpc._docker_whitelist_get()
+        assert whitelist.get.call_count == 1
+
+    def test_docker_whitelist_add(self, whitelist):
+        self.whitelist_rpc._docker_refresh_discovered = mock.Mock()
+        self.whitelist_rpc._docker_whitelist_add('repo/0')
+        assert whitelist.add.call_count == 1
+        assert self.whitelist_rpc._docker_refresh_discovered.call_count == 1
+
+    def test_docker_whitelist_remove(self, whitelist):
+        self.whitelist_rpc._docker_whitelist_remove('repo/0')
+        assert whitelist.remove.call_count == 1
