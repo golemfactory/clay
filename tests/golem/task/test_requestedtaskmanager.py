d244b18d7aa0454302634c120e8a6263880d6038
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2019-09-04 16:15:14 +0200

    Remove custom tempdir fixture and use pytest native one

--- tests/golem/task/test_requestedtaskmanager.py
+++ tests/golem/task/test_requestedtaskmanager.py
@@ -3,7 +3,7 @@ import asyncio
 from freezegun import freeze_time
 from golem_task_api.client import RequestorAppClient
 from golem_task_api.structs import Subtask
-from mock import Mock, patch, MagicMock
+from mock import Mock, MagicMock
 import pytest
 
 from golem.app_manager import AppManager
@@ -26,16 +26,21 @@ class AsyncMock(MagicMock):
 
 class TestRequestedTaskManager(AsyncDatabaseFixture):
 
-    def setup_method(self, method):
-        super().setup_method(method)
+    @pytest.fixture(autouse=True)
+    def setup_method(self, tmp_path):
+        super().setup_method(tmp_path)
+        self.tmp_path = tmp_path
+
         self.env_manager = Mock(spec=EnvironmentManager)
         self.app_manager = Mock(spec=AppManager)
         self.public_key = str.encode('0xdeadbeef')
+        self.rtm_path = self.tmp_path / 'rtm'
+        self.rtm_path.mkdir()
         self.rtm = RequestedTaskManager(
             env_manager=self.env_manager,
             app_manager=self.app_manager,
             public_key=self.public_key,
-            root_path=self.new_path
+            root_path=self.rtm_path
         )
 
     def test_create_task(self):
@@ -50,7 +55,7 @@ class TestRequestedTaskManager(AsyncDatabaseFixture):
             row = RequestedTask.get(RequestedTask.task_id == task_id)
             assert row.status == TaskStatus.creating
             assert row.start_time < default_now()
-            assert (self.new_path / golem_params.app_id / task_id).exists()
+            assert (self.rtm_path / golem_params.app_id / task_id).exists()
 
     @pytest.mark.asyncio
     async def test_init_task(self, monkeypatch):
@@ -237,7 +242,7 @@ class TestRequestedTaskManager(AsyncDatabaseFixture):
             name='a',
             task_timeout=1,
             subtask_timeout=1,
-            output_directory=self.new_path / 'output',
+            output_directory=self.tmp_path / 'output',
             resources=[],
             max_subtasks=1,
             max_price_per_hour=1,
