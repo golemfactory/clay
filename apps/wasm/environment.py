aa15755a1939039b0c96e5f88aa3f15b64d4c833
Jakub Konka <jakub.konka@golem.network>
2019-09-30 14:31:44 +0200

    Bump golemfactory/wasm to 0.5.1

--- apps/wasm/environment.py
+++ apps/wasm/environment.py
@@ -3,6 +3,6 @@ from golem.docker.environment import DockerEnvironment
 
 class WasmTaskEnvironment(DockerEnvironment):
     DOCKER_IMAGE = "golemfactory/wasm"
-    DOCKER_TAG = "0.5"
+    DOCKER_TAG = "0.5.1"
     ENV_ID = "WASM"
     SHORT_DESCRIPTION = "WASM Sandbox"
