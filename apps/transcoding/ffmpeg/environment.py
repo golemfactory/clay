592d07d87daa6ad2dea9899fd3d8e24215b0e892
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-10-28 11:07:35 +0100

    Update ffmpeg-tools to version 0.14.5

--- apps/transcoding/ffmpeg/environment.py
+++ apps/transcoding/ffmpeg/environment.py
@@ -4,7 +4,7 @@ from golem.docker.image import DockerImage
 
 class ffmpegEnvironment(DockerEnvironment):
     DOCKER_IMAGE = 'golemfactory/ffmpeg-experimental'
-    DOCKER_TAG = '0.98.0'
+    DOCKER_TAG = '0.98.1'
     ENV_ID = 'FFMPEG'
     SHORT_DESCRIPTION = ''
 
