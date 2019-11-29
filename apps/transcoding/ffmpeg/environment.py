da167ecc0789b1154d917643a5fa5bfd0998d0c7
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-11-29 16:49:01 +0100

    Bump docker image version to 0.100.0

--- apps/transcoding/ffmpeg/environment.py
+++ apps/transcoding/ffmpeg/environment.py
@@ -4,7 +4,7 @@ from golem.docker.image import DockerImage
 
 class ffmpegEnvironment(DockerEnvironment):
     DOCKER_IMAGE = 'golemfactory/ffmpeg-experimental'
-    DOCKER_TAG = '0.99.1'
+    DOCKER_TAG = '0.100.0'
     ENV_ID = 'FFMPEG'
     SHORT_DESCRIPTION = ''
 
