5dc6b51e739913b3d23d65d16758b6f9fce21de1
shadeofblue <blue@wave460.net>
2019-12-11 12:13:58 +0100

    fix force payment concent integration test (#4989)
    * fix force payment concent integration test
    
    * don't send `SystemExit` and `Exit` to sentry in any case
    * don't try to freeze raven's time calls
    
    * fix logging

--- scripts/node_integration_tests/nodes/requestor/day_backwards_dont_pay.py
+++ scripts/node_integration_tests/nodes/requestor/day_backwards_dont_pay.py
@@ -13,7 +13,7 @@ from scripts.node_integration_tests import helpers
 from golemapp import main  # noqa: E402 module level import not at top of file
 
 
-with freezegun.freeze_time(helpers.yesterday(), tick=True):
+with freezegun.freeze_time(helpers.yesterday(), tick=True, ignore=['raven']):
     with mock.patch(
             'golem.ethereum.paymentprocessor.PaymentProcessor.sendout',
             mock.Mock(return_value=True),
--- scripts/node_integration_tests/playbooks/concent/concent_config_base.py
+++ scripts/node_integration_tests/playbooks/concent/concent_config_base.py
@@ -4,5 +4,13 @@ from ..test_config_base import TestConfigBase, CONCENT_STAGING
 class ConcentTestConfigBase(TestConfigBase):
     def __init__(self):
         super().__init__()
-        for node_config in self.nodes.values():
+        self.enable_concent()
+
+    def enable_concent(self):
+        node_configs = []
+        for n in self.nodes.values():
+            node_configs.extend(
+                n if isinstance(n, list) else [n]
+            )
+        for node_config in node_configs:
             node_config.concent = CONCENT_STAGING
