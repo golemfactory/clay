392e59725014afb7b837724a5c36e17dd788c66c
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2019-12-13 12:09:01 +0100

    Bump ffmpeg-tools and docker image

--- apps/transcoding/ffmpeg/environment.py
+++ apps/transcoding/ffmpeg/environment.py
@@ -4,7 +4,7 @@ from golem.docker.image import DockerImage
 
 class ffmpegEnvironment(DockerEnvironment):
     DOCKER_IMAGE = 'golemfactory/ffmpeg-experimental'
-    DOCKER_TAG = '0.103.1'
+    DOCKER_TAG = '0.104.0'
     ENV_ID = 'FFMPEG'
     SHORT_DESCRIPTION = ''
 
