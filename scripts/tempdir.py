6c620e17e9b1e97ca2e7e883635032c4fb019199
shadeofblue <blue@wave460.net>
2020-03-04 13:06:58 +0100

    address @maaktweluit's remarks

--- scripts/tempdir.py
+++ scripts/tempdir.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from golem.core.common import is_osx
 
@@ -7,6 +8,13 @@ def fix_osx_tmpdir() -> bool:
     if not is_osx():
         return False
     tmpdir = os.environ.get('TMPDIR') or ''
-    if not (tmpdir.startswith('/private') or tmpdir.startswith('/tmp')):
+    if not re.match('^/(private|tmp|Users|Volumes).*', tmpdir):
         os.environ['TMPDIR'] = '/tmp'
-    return True
+        print(
+            f"\033[0;31m"
+            f"TMPDIR updated to something that Docker can mount: "
+            f"{os.environ.get('TMPDIR')}"
+            f"\033[0m"
+        )
+        return True
+    return False
