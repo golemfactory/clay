c76c67324757f408e7a4cb1f043ee4b428cbbefa
Kuba Mazurek <jakub.mazurek@golem.network>
2020-03-19 10:58:19 +0100

    Use subtask timeout status in taskmanager (#5136)

--- tests/golem/task/test_taskmanager.py
+++ tests/golem/task/test_taskmanager.py
@@ -839,7 +839,7 @@ class TestTaskManager(LogTestCase, TestDatabaseWithReactor,  # noqa # pylint: di
             )
             self.assertIs(
                 task_state.subtask_states["aabbcc"].status,
-                SubtaskStatus.failure,
+                SubtaskStatus.timeout,
             )
         # Task with task and subtask timeout
         with patch('golem.task.taskbase.Task.needs_computation',
@@ -869,7 +869,7 @@ class TestTaskManager(LogTestCase, TestDatabaseWithReactor,  # noqa # pylint: di
             )
             self.assertIs(
                 task_state.subtask_states["qwerty"].status,
-                SubtaskStatus.failure,
+                SubtaskStatus.timeout,
             )
             checker([("qwe", "qwerty", SubtaskOp.TIMEOUT),
                      ("qwe", None, TaskOp.TIMEOUT)])
