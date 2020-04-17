28603995439398ece1cafa22eff7b4eca7d91921
shadeofblue <blue@wave460.net>
2020-04-17 11:22:54 +0200

    alternate Rinkeby contract addresses for GNT, GNTB, GNTDeposit
    !!! DON'T MERGE TO PRODUCTION !!!

--- golem/config/environments/testnet.py
+++ golem/config/environments/testnet.py
@@ -36,12 +36,12 @@ class EthereumConfig:  # pylint:disable=too-many-instance-attributes
         ]
 
         self.CHAIN = RINKEBY
-        self.FAUCET_ENABLED = True
+        self.FAUCET_ENABLED = False
 
         self.CONTRACT_ADDRESSES = {
-            contracts.GNT: '0x924442A66cFd812308791872C4B242440c108E19',
-            contracts.GNTB: '0x123438d379BAbD07134d1d4d7dFa0BCbd56ca3F3',
-            contracts.Faucet: '0x77b6145E853dfA80E8755a4e824c4F510ac6692e',
+            contracts.GNT: '0xf29f58E2C0A718B1f7b925450585cA42ABa5cE36',
+            contracts.GNTB: '0x234E4151f828Cb5141ACA8436405EB8D5C04a06B',
+#            contracts.Faucet: '0x77b6145E853dfA80E8755a4e824c4F510ac6692e',
         }
 
         os.environ[CONCENT_ENVIRONMENT_VARIABLE] = os.environ.get(
